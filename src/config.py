63e222bf659f2d8b49ad792da4eab6c5fbafb157
Alan Nichol <amn41@cam.ac.uk>
2016-10-14 16:40:00 +0100

    bugfixes config

--- src/config.py
+++ src/config.py
@@ -1,13 +1,11 @@
 import os
 debug_mode=True
 
-#root_dir="/Users/alan/Developer/dialog/parsa"
-root_dir=os.environ["PARSA_DATA"] or  os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-#"/Users/alan/Developer/dialog/lastmile/framework/rasa/examples/recipes"
-data_dir = root_dir + "/data"
+
+data_dir = os.environ.get("PARSA_DATA")
 fe_file= data_dir + "/total_word_feature_extractor.dat"
 ner_file = data_dir + "/ner.dat"
-classifier_file = data_dir + "/intent_classifier.dat "
+classifier_file = data_dir + "/intent_classifier.dat"
 
 #RECIPES
 self_port=5002
