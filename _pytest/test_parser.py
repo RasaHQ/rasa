c7435c8ad7b85340316790d1c436fd0a1109b9ed
Alan Nichol <amn41@cam.ac.uk>
2016-10-14 16:40:54 +0100

    test wrapped in func

--- _pytest/test_parser.py
+++ _pytest/test_parser.py
@@ -1,23 +1,19 @@
 from parsa.mitie_interpreter import MITIEInterpreter
 from parsa import config
 
-# paths
-#test_data_file = config.data_dir + '/test_sentences.json'
 
 interpreter = MITIEInterpreter(config.classifier_file,config.ner_file,config.fe_file)
 
-samples = [
-  ("I would like some chinese food please",('inform', {'cuisine': 'chinese'})),
-  ("Is that vegetarian",('ask_constraint', {'diet': 'vegetarian'})),
-  ("How about a gluten and lactose free pudding",('inform', {'diet': 'glutenFree','diet' : 'lactoseFree','type':'dessert'})),
-  ("Suggest a dessert which is gluten and lactose free",('inform', {'diet': 'glutenFree','diet' : 'lactoseFree','type':'dessert'})),
-  ("Could you suggest another",('deny',{})),
-  ("Please give me a recipe for a pudding which does not include oats and is gluten free and lactose free",{})
-]
 
 
-for text, result in samples:
-    assert interpreter.parse(text) == result, "text : {0} \nresult : {1}".format(text,interpreter.parse(text))
+def test_samples():
+    samples = [
+      ("I would like some chinese food please",{'intent':'inform', 'entities' : {'cuisine': 'chinese'}}),
+      ("Is that vegetarian",{'intent':'ask_constraint', 'entities' :{'diet': 'vegetarian'}})
+    ]   
+
+    for text, result in samples:
+        assert interpreter.parse(text) == result, "text : {0} \nresult : {1}, expected {2}".format(text,interpreter.parse(text),result)
 
 
 
