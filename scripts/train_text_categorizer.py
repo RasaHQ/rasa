3d164a8760be4179a2845f4482354236a2080f95
Alan Nichol <amn41@cam.ac.uk>
2016-10-17 14:41:15 +0100

    simplified text categorizer

--- scripts/train_text_categorizer.py
+++ scripts/train_text_categorizer.py
@@ -1,16 +1,30 @@
 import json
 from mitie import *
+import os
+
 import config
 
+
+data_dir = "../data/"
+fe_file = data_dir + "total_word_feature_extractor.dat"
+training_data_file = data_dir + 'intents.json'
 output_file="text_categorizer.dat"
 
-# load data and add it to trainer
-intents_data = json.loads(open('intents.json').read())
+
+if (not os.path.exists(data_dir)):
+    raise ValueError("could not find data dir, please adjust path")
+
+# create trainer and load data
+trainer = text_categorizer_trainer(fe_file)
+intents_data = json.loads(open(training_data_file).read())
+trainer.num_threads = 4
+
+# add data to trainer
 for ut in intents_data["utterances"]:
     tokens = tokenize(ut["text"])
     trainer.add_labeled_text(tokens,ut["intent"])
 
-trainer.num_threads = 8
+# train and save model
 cat = trainer.train()
 cat.save_to_disk(output_file,pure_model=True)
 
