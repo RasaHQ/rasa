96231244b6d7736589c888ccca3909f5681eb22b
Alan Nichol <amn41@cam.ac.uk>
2016-10-26 11:33:29 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -5,12 +5,8 @@ setup(
   package_dir = {'parsa': 'src'},
   version = '0.0.0.1',
   install_requires=[
-        'requests',
-        'pytest',
-        'flask',
-        'mitie'
+        'pytest'
   ],
-  dependency_links = ['git+https://github.com/amn41/MITIE.git#egg=mitie'],
   description = "parsa: a natural language parser for bots",
   author = 'Alan Nichol',
   author_email = 'alan@golastmile.com',
