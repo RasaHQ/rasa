5f7b8351996ecd3f0aaabc2309844514a3a1502d
Alan Nichol <amn41@cam.ac.uk>
2016-10-26 11:26:28 +0100

    data router obj in server

--- src/server.py
+++ src/server.py
@@ -4,7 +4,6 @@ import json
 PORT_NUMBER = 5000
 
 
-
 def create_interpreter(backend):
     if (backend is None):
         return HelloGoodbyeInterpreter()
@@ -12,14 +11,36 @@ def create_interpreter(backend):
         from mitie_interpreter import MITIEInterpreter
         return MITIEInterpreter(config.classifier_file,config.ner_file,config.fe_file)
     else:
-        raise ValueError("Unknown backend type")
+        raise ValueError("unknown backend : {0}".format(backend))
 
-class ParsingRouter(object):
-    def __init__(self,backend=None):
+def create_emulator(service):
+    if (service is None):
+        from emulators import NoEmulator
+        return NoEmulator()
+    elif(service.lower() == 'wit'):
+        from emulators.wit import WitEmulator
+        return WitEmulator()
+    elif(service.lower() == 'luis'):
+        from emulators.luis import LUISEmulator
+        return LUISEmulator()
+    else:
+        raise ValueError("unknown service : {0}".format(service))
+
+
+
+class DataRouter(object):
+    def __init__(self,backend=None,service=None):
         self.interpreter = create_interpreter(backend)
+        self.emulator = create_emulator(service)
+
+    def extract(self,data):
+        return self.emulator.normalise_request_json(data)    
 
     def parse(self,text):
         return self.interpreter.parse(text)
+
+    def format(self,data):
+        return self.emulator.normalise_response_json(data)
 
 
 class ParsaRequestHandler(BaseHTTPRequestHandler):
@@ -37,16 +58,16 @@ class ParsaRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path=="/parse":
             self._set_headers()
-            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
-            data = json.loads(self.data_string)
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))            
+            data = router.extract(json.loads(self.data_string))            
             result = router.parse(data["text"])
-            s= json.dumps(result)
-            self.wfile.write(s)
+            response = router.format(result)
+            self.wfile.write(json.dumps(response))
         return
 
 
 try:
-    router = ParsingRouter()
+    router = DataRouter()
     server = HTTPServer(('', PORT_NUMBER), ParsaRequestHandler)
     print(vars(server))
     print 'Started httpserver on port ' , PORT_NUMBER
