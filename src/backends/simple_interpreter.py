d64880ad0cff184ede3d1b6c59418b905edfb14f
Alan Nichol <amn41@cam.ac.uk>
2016-10-27 15:41:10 +0100

    updated test, don't lowercase text

--- src/backends/simple_interpreter.py
+++ src/backends/simple_interpreter.py
@@ -1,13 +1,13 @@
-class HelloGoodbyeInterpreter():
+class HelloGoodbyeInterpreter(object):
     def __init__(self):
         self.name="hello_goodbye"
         self.his = ["hello","hi","hey"]
         self.byes = ["bye","goodbye"]
 
     def parse(self,text):
-        text, intent = text.lower(), "None"
+        text, intent = text, "None"
 
-        is_present = lambda x: x in text
+        is_present = lambda x: x in text.lower()
         if (True in map(is_present,self.his)):
             intent="hello"
         elif (True in map(is_present,self.byes)):
