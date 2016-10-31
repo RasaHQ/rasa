089613a61076c5ee8918085b6fc44fb3c9427af8
Alan Nichol <amn41@cam.ac.uk>
2016-10-31 18:03:50 +0000

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'parsa',
   packages = ['parsa','parsa.backends','parsa.emulators'],
   package_dir = {'parsa': 'src'},
-  version = '0.0.0.1',
+  version = '0.2.0',
   install_requires=[
         'pytest'
   ],
