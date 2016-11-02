11fecd684c1ec1d1867d1f008da3bc36890a8fab
Alan Nichol <amn41@cam.ac.uk>
2016-11-02 14:21:59 +0000

    capital P for port arg in server

--- src/server.py
+++ src/server.py
@@ -39,7 +39,7 @@ def create_argparser():
     parser = argparse.ArgumentParser(description='parse incoming text')
     parser.add_argument('-d','--server_model_dir', default=None, help='directory where model files are saved')
     parser.add_argument('-e','--emulate', default=None, choices=['wit','luis'], help='which service to emulate (default: None i.e. use simple built in format)')
-    parser.add_argument('-p','--port', default=5000, type=int, help='port on which to run server') 
+    parser.add_argument('-P','--port', default=5000, type=int, help='port on which to run server') 
     parser.add_argument('-c','--config', default=None, help="config file, all the command line options can also be passed via a (json-formatted) config file. NB command line args take precedence")  
     parser.add_argument('-l','--logfile', default='parsa_log.json', help='file where logs will be saved')
          
