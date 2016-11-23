d23237f2edb382f3b91f4d090aba7a7d2c6626df
Alan Nichol <alan@golastmile.com>
2016-11-23 19:52:30 +0000

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     'rasa_nlu.tokenizers'
   ],
   package_dir = {'rasa_nlu': 'src'},
-  version = '0.3.1',
+  version = '0.3.2',
   install_requires=[],
   description = "rasa_nlu: a natural language parser for bots",
   author = 'Alan Nichol',
