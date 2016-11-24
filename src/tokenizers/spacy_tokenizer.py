4f896c825d26d9d2b54c9e69742519883d187656
Alan Nichol <alan@golastmile.com>
2016-11-24 14:03:52 +0000

    remove erroneous flag in spacy.load

--- src/tokenizers/spacy_tokenizer.py
+++ src/tokenizers/spacy_tokenizer.py
@@ -4,7 +4,7 @@ import spacy
 class SpacyTokenizer(object):
     def __init__(self, language_name):
         self.nlp = spacy.load(language_name, tagger=False, parser=False,
-                              entity=False, matcher=False, add_vectors=False)
+                              entity=False, matcher=False)
 
     def tokenize(self, text):
         return [t.text.encode('utf-8') for t in self.nlp(text)]
