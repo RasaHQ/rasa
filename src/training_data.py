9763cf8d95d2c821f262fb624b66fec124223d05
Alan Nichol <alan@golastmile.com>
2016-11-24 12:40:58 +0000

    move file sweep to util module

--- src/training_data.py
+++ src/training_data.py
@@ -1,12 +1,12 @@
 import json, warnings, re, os
-
+from rasa_nlu import util
 
 class TrainingData(object):
     def __init__(self, resource_name, backend, language_name):
         self.intent_examples = []
         self.entity_examples = []
         self.resource_name = resource_name
-        self.files = self.get_resources(resource_name)
+        self.files = util.recursively_find_files(resource_name)
         self.fformat = self.guess_format(self.files)
         self.tokenizer = None
         self.language_name = language_name
@@ -33,27 +33,6 @@ class TrainingData(object):
         else:
             raise ValueError("unknown training file format : {0}".format(self.fformat))
 
-    def get_resources(self, resource_name):
-        """resource_name can be a folder or a file. In both cases we will return a list of files"""
-        resources = []
-        if os.path.isdir(resource_name):
-            # walk the fs tree and return a list of files
-            nodes_to_visit = [resource_name]
-            while(len(nodes_to_visit) > 0):
-                # skip hidden files
-                nodes_to_visit = filter( lambda f: not f.split("/")[-1].startswith('.'), nodes_to_visit)
-
-                current_node = nodes_to_visit[0]
-                # if current node is a folder, schedule its children for a visit. Else add them to the resources.
-                if (os.path.isdir(current_node)):
-                    nodes_to_visit += [os.path.join(current_node, f) for f in os.listdir(current_node)]
-                else:
-                    resources += [current_node]
-                nodes_to_visit = nodes_to_visit[1:]
-        else:
-            resources += [current_node]
-
-        return resources
 
     def as_json(self,**kwargs):
         return json.dumps( {
--- src/util.py
+++ src/util.py
@@ -12,3 +12,25 @@ def update_config(config,args,exclude=[],required=[]):
             raise ValueError("parameter {0} unspecified. Please provide a value via the command line or in the config file.".format(param))
 
     return config
+    
+def recursively_find_files(resource_name):
+    """resource_name can be a folder or a file. In both cases we will return a list of files"""
+    resources = []
+    if os.path.isdir(resource_name):
+        # walk the fs tree and return a list of files
+        nodes_to_visit = [resource_name]
+        while(len(nodes_to_visit) > 0):
+            # skip hidden files
+            nodes_to_visit = filter( lambda f: not f.split("/")[-1].startswith('.'), nodes_to_visit)
+
+            current_node = nodes_to_visit[0]
+            # if current node is a folder, schedule its children for a visit. Else add them to the resources.
+            if (os.path.isdir(current_node)):
+                nodes_to_visit += [os.path.join(current_node, f) for f in os.listdir(current_node)]
+            else:
+                resources += [current_node]
+            nodes_to_visit = nodes_to_visit[1:]
+    else:
+        resources += [current_node]
+
+    return resources    
