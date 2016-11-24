c5a78634291a2797a44152c1e5a959b8b6d49c1a
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-11-24 16:34:37 +0100

    collection of fixes necessary to use code for poc

--- src/classifiers/sklearn_intent_classifier.py
+++ src/classifiers/sklearn_intent_classifier.py
@@ -1,39 +1,32 @@
-import numpy as np
-from mitie import *
-
-from sklearn.svm import SVC
-from sklearn.decomposition import PCA
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import LabelEncoder
-from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
 
-import pickle
 
 class SklearnIntentClassifier(object):
-
     def __init__(self):
-        self.le=LabelEncoder()
+        self.le = LabelEncoder()
         self.tuned_parameters = [
-          {'C': [1,2,5, 10,20, 100], 'kernel': ['linear']}
-         ]
-        self.score = 'f1'#'precision'
-        self.clf = GridSearchCV(SVC(C=1), self.tuned_parameters, cv=2,scoring='%s_weighted' % self.score)                                         
+            {'C': [1, 2, 5, 10, 20, 100], 'kernel': ['linear']}
+        ]
+        self.score = 'f1'  # 'precision'
+        self.clf = GridSearchCV(SVC(C=1), self.tuned_parameters, cv=2, scoring='%s_weighted' % self.score)
 
-    def transform_labels(self,labels):
-        y=self.le.fit_transform(labels)
+    def transform_labels(self, labels):
+        y = self.le.fit_transform(labels)
         return y
-        
-    def train(self,X,y):    
+
+    def train(self, X, y):
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=0.1, random_state=0)
-        self.clf.fit(X_train, y_train)        
+        self.clf.fit(X_train, y_train)
 
-    def predict(self,X,to_labels=True):
+        # Test the trained model
+        print "Score of intent model on test data: ", self.clf.score(X_test, y_test)
+
+    def predict(self, X, to_labels=True):
         y_pred = self.clf.predict(X)
         if (to_labels):
             return self.le.inverse_transform(y_pred)
         return y_pred
-
-
-
-        
--- src/trainers/spacy_sklearn_trainer.py
+++ src/trainers/spacy_sklearn_trainer.py
@@ -1,5 +1,7 @@
 import spacy
-import os, datetime, json
+import os
+import datetime
+import json
 import cloudpickle
 from rasa_nlu.featurizers.spacy_featurizer import SpacyFeaturizer
 from rasa_nlu.classifiers.sklearn_intent_classifier import SklearnIntentClassifier
@@ -9,46 +11,49 @@ from training_utils import write_training_metadata
 
 class SpacySklearnTrainer(object):
     def __init__(self, config, language_name):
-        self.name="spacy_sklearn"
+        self.name = "spacy_sklearn"
         self.language_name = language_name
         self.training_data = None
         self.nlp = spacy.load(self.language_name)
 
         self.featurizer = SpacyFeaturizer(self.nlp)
         self.intent_classifier = SklearnIntentClassifier()
-        self.entity_extractor = SpacyEntityExtractor()    
-        
-    def train(self,data):
-        self.training_data = data
-        self.entity_extractor.train(self.nlp,data.entity_examples)        
-        self.train_intent_classifier(data.intent_examples)
-                        
-    def train_intent_classifier(self,intent_examples):
-        labels = [e["intent"] for e in intent_examples]
-        sents = [e["text"] for e in intent_examples]
-        y = self.intent_classifier.transform_labels(labels)
-        X = self.featurizer.create_bow_vecs(sents)
-        self.intent_classifier.train(X,y)
-        
-    def persist(self,path):
-        tstamp = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-        dirname = os.path.join(path,"model_"+tstamp)
-        os.mkdir(dirname)
-        data_file = os.path.join(dirname,"training_data.json")
-        classifier_file = os.path.join(dirname,"intent_classifier.pkl")
-        ner_dir = os.path.join(dirname,'ner')
-        os.mkdir(ner_dir)
-        entity_extractor_config_file = os.path.join(ner_dir,"config.json")
-        entity_extractor_file = os.path.join(ner_dir,"model")
+        self.entity_extractor = SpacyEntityExtractor()
 
-        write_training_metadata(dirname, tstamp, data_file, self.name, self.language_name,
+    def train(self, data):
+        self.training_data = data
+        self.train_entity_extractor(data.entity_examples)
+        self.train_intent_classifier(data.intent_examples)
+
+    def train_entity_extractor(self, entity_examples):
+        self.entity_extractor.train(self.nlp, entity_examples)
+
+    def train_intent_classifier(self, intent_examples):
+        labels = [e["intent"] for e in intent_examples]
+        sentences = [e["text"] for e in intent_examples]
+        y = self.intent_classifier.transform_labels(labels)
+        X = self.featurizer.create_bow_vecs(sentences)
+        self.intent_classifier.train(X, y)
+
+    def persist(self, path):
+        timestamp = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+        dir_name = os.path.join(path, "model_" + timestamp)
+        os.mkdir(dir_name)
+        data_file = os.path.join(dir_name, "training_data.json")
+        classifier_file = os.path.join(dir_name, "intent_classifier.pkl")
+        ner_dir = os.path.join(dir_name, 'ner')
+        os.mkdir(ner_dir)
+        entity_extractor_config_file = os.path.join(ner_dir, "config.json")
+        entity_extractor_file = os.path.join(ner_dir, "model")
+
+        write_training_metadata(dir_name, timestamp, data_file, self.name, self.language_name,
                                 classifier_file, ner_dir)
 
-        with open(data_file,'w') as f:
+        with open(data_file, 'w') as f:
             f.write(self.training_data.as_json(indent=2))
-        with open(classifier_file,'w') as f:
-            cloudpickle.dump(self.intent_classifier,f)
-        with open(entity_extractor_config_file,'w') as f:
+        with open(classifier_file, 'w') as f:
+            cloudpickle.dump(self.intent_classifier, f)
+        with open(entity_extractor_config_file, 'w') as f:
             json.dump(self.entity_extractor.ner.cfg, f)
-            
+
         self.entity_extractor.ner.model.dump(entity_extractor_file)
