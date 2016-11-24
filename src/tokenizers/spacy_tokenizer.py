b0870f41798a3e04dabc7ffcd94ddef560a3bfc8
Alan Nichol <alan@golastmile.com>
2016-11-24 10:12:27 +0000

    load vecs in spacy tokenizer

--- src/tokenizers/spacy_tokenizer.py
+++ src/tokenizers/spacy_tokenizer.py
@@ -2,6 +2,6 @@ import spacy
 
 class SpacyTokenizer(object):
     def __init__(self):
-        self.nlp = spacy.load('en',tagger=False, parser=False, entity=False, matcher=False,add_vectors=False)
+        self.nlp = spacy.load('en',tagger=False, parser=False, entity=False, matcher=False)
     def tokenize(self,text):
         return [t.text.encode('utf-8') for t in self.nlp(text)]
