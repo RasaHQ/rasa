0c8732fb66608dd9b4df070ab668d834ae0a9d4d
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2016-11-25 14:20:18 +0100

    fixed absolute imports to include complete package name

--- _pytest/trainers/test_mitie_trainer.py
+++ _pytest/trainers/test_mitie_trainer.py
@@ -1,7 +1,7 @@
 import pytest
 
 from rasa_nlu.trainers.mitie_trainer import MITIETrainer
-from train import load_configuration
+from rasa_nlu.train import load_configuration
 
 class TestMitieTrainer:
     def test_failure_on_invalid_lang(self):
