afb0c826ccf5f9d8251c639c8af644bd66ac56f7
Alan Nichol <alan@golastmile.com>
2016-11-29 16:29:26 +0000

    fix to trainer

--- _pytest/trainers/test_mitie_trainer.py
+++ _pytest/trainers/test_mitie_trainer.py
@@ -6,6 +6,6 @@ from rasa_nlu.train import load_configuration
 
 class TestMitieTrainer:
     def test_failure_on_invalid_lang(self):
-        config = load_configuration("config_example.json")
+        config = load_configuration("config_mitie.json")
         with pytest.raises(NotImplementedError):
-            MITIETrainer(config['backends']['mitie'], 'umpalumpa')
+            MITIETrainer(config['mitie_file'], 'umpalumpa')
