ad26a86b5a73aa9f7add898a424490d7ff4ca7c8
Alan Nichol <alan@golastmile.com>
2016-12-01 09:44:55 +0000

    easier-to-follow rewrite of config update tests

--- _pytest/test_util.py
+++ _pytest/test_util.py
@@ -2,42 +2,46 @@ from rasa_nlu import util
 from rasa_nlu import config_keys
 
 
-def test_update_config():
+def test_blank_config():
+    init_config = {}
+    cmdline_args = dict.fromkeys(config_keys, None)
+    env_vars = {}
+    result = {}
+    final_config = util.update_config(init_config, cmdline_args, env_vars)
+    assert final_config == result
 
-    test_cases = [
-      {
-        "init_config": {},
-        "cmdline_args": dict.fromkeys(config_keys, None),
-        "env_vars": {},
-        "result": {}
-      },
-      {
-        "init_config": {"path": "/path/to/dir"},
-        "cmdline_args": dict.fromkeys(config_keys, None),
-        "env_vars": {},
-        "result": {"path": "/path/to/dir"}
-      },
-      {
-        "init_config": {"path": "/path/to/dir"},
-        "cmdline_args": {"path": "/alternate/path"},
-        "env_vars": {},
-        "result": {"path": "/alternate/path"}
-      },
-      {
-        "init_config": {"path": "/path/to/dir"},
-        "cmdline_args": {},
-        "env_vars": {"RASA_PATH": "/alternate/path"},
-        "result": {"path": "/alternate/path"}
-      },
-      {
-        "init_config": {"path": "/path/to/dir"},
-        "cmdline_args": {"path": "/another/path"},
-        "env_vars": {"RASA_PATH": "/alternate/path"},
-        "result": {"path": "/another/path"}
-      }
-    ]
 
-    for case in test_cases:
-        final_config = util.update_config(case["init_config"], case["cmdline_args"], case["env_vars"])
-        print(case)
-        assert final_config == case["result"]
+def test_init_config_unchanged():
+    init_config = {"path": "/path/to/dir"}
+    cmdline_args = dict.fromkeys(config_keys, None)
+    env_vars = {}
+    result = {"path": "/path/to/dir"}
+    final_config = util.update_config(init_config, cmdline_args, env_vars)
+    assert final_config == result
+
+
+def test_cmdline_overrides_init():
+    init_config = {"path": "/path/to/dir"}
+    cmdline_args = {"path": "/alternate/path"}
+    env_vars = {}
+    result = {"path": "/alternate/path"}
+    final_config = util.update_config(init_config, cmdline_args, env_vars)
+    assert final_config == result
+
+
+def test_envvar_overrides_init():
+    init_config = {"path": "/path/to/dir"}
+    cmdline_args = {}
+    env_vars = {"RASA_PATH": "/alternate/path"}
+    result = {"path": "/alternate/path"}
+    final_config = util.update_config(init_config, cmdline_args, env_vars)
+    assert final_config == result
+
+
+def test_cmdline_overrides_envvar():
+    init_config = {"path": "/path/to/dir"}
+    cmdline_args = {"path": "/another/path"}
+    env_vars = {"RASA_PATH": "/alternate/path"}
+    result = {"path": "/another/path"}
+    final_config = util.update_config(init_config, cmdline_args, env_vars)
+    assert final_config == result
