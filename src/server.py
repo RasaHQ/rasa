1d1dd2a407f65adbfb1be4e20de9e61b6c9c214c
Alan Nichol <alan@golastmile.com>
2016-12-02 15:48:19 +0000

    fix import

--- src/server.py
+++ src/server.py
@@ -19,7 +19,7 @@ class RasaNLUServer(object):
         self.data_router = DataRouter(config, self.interpreter, self.emulator)
 
         if 'DYNO' in os.environ and config.backend == 'mitie':  # running on Heroku
-            from rasa_nlu.featurizers import MITIEFeaturizer
+            from rasa_nlu.featurizers.mitie_featurizer import MITIEFeaturizer
             self.featurizer = MITIEFeaturizer(config.mitie_file)
 
     def __create_interpreter(self):
