de1bba697462ee24e21b223b6a65db87ac8566d4
Alan Nichol <alan@golastmile.com>
2016-12-02 16:21:49 +0000

    remove defaults from argparser

--- src/server.py
+++ src/server.py
@@ -206,21 +206,21 @@ class RasaRequestHandler(BaseHTTPRequestHandler):
 
 def create_argparser():
     parser = argparse.ArgumentParser(description='parse incoming text')
-    parser.add_argument('-c', '--config', default=None,
+    parser.add_argument('-c', '--config',
                         help="config file, all the command line options can also be passed via a (json-formatted) " +
                              "config file. NB command line args take precedence")
-    parser.add_argument('-d', '--server_model_dir', default=None,
+    parser.add_argument('-d', '--server_model_dir',
                         help='directory containing model to for parser to use')
-    parser.add_argument('-e', '--emulate', default=None, choices=['wit', 'luis', 'api'],
+    parser.add_argument('-e', '--emulate', choices=['wit', 'luis', 'api'],
                         help='which service to emulate (default: None i.e. use simple built in format)')
-    parser.add_argument('-l', '--language', default='en', choices=['de', 'en'], help="model and data language")
-    parser.add_argument('-m', '--mitie_file', default='data/total_word_feature_extractor.dat',
+    parser.add_argument('-l', '--language', choices=['de', 'en'], help="model and data language")
+    parser.add_argument('-m', '--mitie_file',
                         help='file with mitie total_word_feature_extractor')
-    parser.add_argument('-p', '--path', default=None, help="path where model files will be saved")
-    parser.add_argument('-P', '--port', default=5000, type=int, help='port on which to run server')
-    parser.add_argument('-t', '--token', default=None,
+    parser.add_argument('-p', '--path', help="path where model files will be saved")
+    parser.add_argument('-P', '--port', type=int, help='port on which to run server')
+    parser.add_argument('-t', '--token',
                         help="auth token. If set, reject requests which don't provide this token as a query parameter")
-    parser.add_argument('-w', '--write', default='rasa_nlu_log.json', help='file where logs will be saved')
+    parser.add_argument('-w', '--write', help='file where logs will be saved')
 
     return parser
 
