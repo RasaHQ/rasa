06f2fd57a1e5a72428683bb4f0d68f0531f6963e
Alan Nichol <alan@golastmile.com>
2017-01-18 21:15:10 +0000

    add rasa NLU version to model metadata

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('rasa_nlu').version
+
 class Interpreter(object):
     def parse(self, text):
         raise NotImplementedError()
+
