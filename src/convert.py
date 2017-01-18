fd0a848be3ba0e3887c2930ce54c9420ab632fe0
Alan Nichol <alan@golastmile.com>
2017-01-18 16:21:53 +0000

    pep8 fixes

--- src/convert.py
+++ src/convert.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from rasa_nlu.training_data import TrainingData
 
+
 def create_argparser():
     parser = argparse.ArgumentParser(description='train a custom language parser')
     parser.add_argument('-d', '--data_file',
@@ -13,12 +14,13 @@ def create_argparser():
                         help='file where to save training data in rasa format')
     return parser
 
+
 def write_file(td, out_file):
-    with open(out_file,'wb') as f:
+    with open(out_file, 'wb') as f:
         f.write(td.as_json(indent=2))
 
 if __name__ == "__main__":
-    parser = create_argparser()    
+    parser = create_argparser()
     args = parser.parse_args()
     td = TrainingData(args.data_file, args.backend, args.language)
     write_file(td, args.out_file)
