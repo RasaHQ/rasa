03cbba529a127c78b167dd50768985a02e2ebb24
Alice <alice.coucke@gmail.com>
2017-02-14 12:06:27 +0100

    add test in test_tokenizers

--- _pytest/test_tokenizers.py
+++ _pytest/test_tokenizers.py
@@ -27,3 +27,4 @@ def test_mitie():
 
     tk.tokenize(u"Hi. My name is rasa") == [u'Hi', u'My', u'name', u'is', u'rasa']
     tk.tokenize(u"ὦ ἄνδρες ᾿Αθηναῖοι.") == [u'ὦ', u'ἄνδρες', u'᾿Αθηναῖοι']
+    tk.tokenize_with_offsets(u"Give me the forecast for lunch") == ([u'Give', u'me', u'the', u'forecast', u'for', u'lunch'], [0, 5, 8, 12, 21, 25])
