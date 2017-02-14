5ce099a23902541d53811c3811580ba9a9d517f5
Alan Nichol <alan@golastmile.com>
2017-02-14 15:26:58 +0100

    when saving model check intent/entity part exists before getting basename

--- src/trainers/training_utils.py
+++ src/trainers/training_utils.py
@@ -6,12 +6,19 @@ import rasa_nlu
 def write_training_metadata(output_folder, timestamp, data_file, backend_name,
                             language_name, intent_file, entity_file,
                             feature_file=None):
+
+    intent_filename, entity_filename = None, None
+    if intent_file:
+        intent_filename = os.path.basename(intent_file)
+    if entity_file:
+        entity_filename = os.path.basename(entity_file)
+
     metadata = {
         "trained_at": timestamp,
         "training_data": os.path.basename(data_file),
         "backend": backend_name,
-        "intent_classifier": os.path.basename(intent_file),
-        "entity_extractor": os.path.basename(entity_file),
+        "intent_classifier": intent_filename,
+        "entity_extractor": entity_filename,
         "feature_extractor": feature_file,
         "language_name": language_name,
         "rasa_nlu_version": rasa_nlu.__version__
