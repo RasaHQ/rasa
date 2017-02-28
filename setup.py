9b767957478a53fd169cb59c4bd168b86861f3b0
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-02-28 18:21:38 +0100

    fixed setup py

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from setuptools import setup
 __version__ = None   # Avoids IDE errors, but actual version is read from version.py
 exec(open('src/version.py').read())
 
+tests_requires = [
+    "pytest-pep8",
+    "pytest-xdist",
+    "pytest-services",
+    "pytest-flask",
+]
+
+install_requires = [
+    "requests",
+    "pathlib",
+    "cloudpickle",
+    "boto3",
+]
+
 setup(
     name='rasa_nlu',
     packages=[
@@ -14,11 +28,13 @@ setup(
         'rasa_nlu.featurizers',
         'rasa_nlu.interpreters',
         'rasa_nlu.trainers',
-        'rasa_nlu.tokenizers'
+        'rasa_nlu.tokenizers',
     ],
     package_dir={'rasa_nlu': 'src'},
     version=__version__,
-    install_requires=[],
+    install_requires=install_requires,
+    tests_require=tests_requires,
+    extras_require={'test': tests_requires},
     description="rasa NLU a natural language parser for bots",
     author='Alan Nichol',
     author_email='alan@golastmile.com',
