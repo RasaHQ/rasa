7aaa8a196b0810cd45cf48c40744b5b988c3b1ba
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-03-02 14:59:27 +0100

    Improved error message on missing intent examples using sklearn

--- src/trainers/sklearn_trainer_utils.py
+++ src/trainers/sklearn_trainer_utils.py
@@ -13,10 +13,12 @@ def train_intent_classifier(intent_examples, featurizer, max_num_threads, test_s
     :return: trained classifier
     :rtype: SklearnIntentClassifier
     """
-
     intent_classifier = SklearnIntentClassifier(max_num_threads=max_num_threads)
     labels = [e["intent"] for e in intent_examples]
     sentences = [e["text"] for e in intent_examples]
+
+    if len(set(labels)) < 2:
+        raise Exception("Can not train an intent classifier. Need at least 2 different classes.")
     y = intent_classifier.transform_labels_str2num(labels)
     X = featurizer.features_for_sentences(sentences)
     intent_classifier.train(X, y, test_split_size)
