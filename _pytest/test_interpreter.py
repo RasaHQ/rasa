60e2eadea6fb7de73b735231cf158ea38743b890
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-03-02 13:33:41 +0100

    added more interpreter tests

--- _pytest/test_interpreter.py
+++ _pytest/test_interpreter.py
@@ -1,31 +1,52 @@
-from rasa_nlu.interpreters.simple_interpreter import HelloGoodbyeInterpreter
+import pytest
 
-interpreter = HelloGoodbyeInterpreter()
+import utilities
+from rasa_nlu.utils.mitie import MITIE_BACKEND_NAME
+from rasa_nlu.utils.mitie import MITIE_SKLEARN_BACKEND_NAME
+from rasa_nlu.utils.spacy import SPACY_BACKEND_NAME
 
 
-def test_samples():
+@pytest.mark.parametrize("backend_name", [
+    MITIE_BACKEND_NAME,
+    MITIE_SKLEARN_BACKEND_NAME,
+    SPACY_BACKEND_NAME,
+])
+def test_samples(backend_name):
+    interpreter = utilities.interpreter_for(utilities.base_test_conf(backend_name))
     samples = [
         (
-            "Hey there",
+            u"hey",
             {
-                'text': "Hey there",
                 'intent': 'greet',
                 'entities': [],
-                'confidence': 1.0
+                'min_confidence': 0.3
             }
         ),
         (
-            "good bye for now",
+            u"good bye",
             {
-                'text': "good bye for now",
                 'intent': 'goodbye',
                 'entities': [],
-                'confidence': 1.0
+                'min_confidence': 0.3
+            }
+        ),
+        (
+            u"i am looking for an indian spot",
+            {
+                'intent': 'restaurant_search',
+                'entities': [{"start": 20, "end": 26, "value": "indian", "entity": "cuisine"}],
+                'min_confidence': 0.3
             }
         )
     ]
 
-    for text, result in samples:
-        assert interpreter.parse(text) == result, "text : {0} \nresult : {1}, expected {2}".format(text,
-                                                                                                   interpreter.parse(
-                                                                                                       text), result)
+    for text, gold in samples:
+        result = interpreter.parse(text)
+        assert result['text'] == text, \
+            "Wrong text for sample '{}'".format(text)
+        assert result['intent'] == gold['intent'], \
+            "Wrong intent for sample '{}'".format(text)
+        assert result['confidence'] >= gold['min_confidence'], \
+            "Low confidence for sample '{}'".format(text)
+        assert result['entities'] == gold['entities'], \
+            "Wrong entities for sample '{}'".format(text)
