e06eaa7ec16bd73f2920d43f5c833f7497cb0b3c
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-03-15 12:52:39 +0100

    fixed regression on mitie entity extraction

--- src/interpreters/mitie_interpreter_utils.py
+++ src/interpreters/mitie_interpreter_utils.py
@@ -8,7 +8,7 @@ def get_entities(text, tokens, extractor, featurizer):
         entities = extractor.extract_entities(tokens, featurizer.feature_extractor)
         for e in entities:
             _range = e[0]
-            _regex = u"\s*".join(tokens[i] for i in _range)
+            _regex = u"\s*".join(re.escape(tokens[i]) for i in _range)
             expr = re.compile(_regex)
             m = expr.search(text[offset:])
             start, end = m.start() + offset, m.end() + offset
