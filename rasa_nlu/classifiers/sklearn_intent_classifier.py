7f320cc9084af19f589f2d0cc8f862b28213e1b8
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2017-03-29 16:42:56 +0800

    back to normal string

--- rasa_nlu/classifiers/sklearn_intent_classifier.py
+++ rasa_nlu/classifiers/sklearn_intent_classifier.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from builtins import str
 from builtins import zip
 import os
 import io
