f4c4fbf9f326e987b84e8ef220b9f85918c1bdce
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2017-03-29 19:11:10 +0800

    line too long

--- rasa_nlu/classifiers/sklearn_intent_classifier.py
+++ rasa_nlu/classifiers/sklearn_intent_classifier.py
@@ -67,7 +67,8 @@ class SklearnIntentClassifier(Component):
         y = self.transform_labels_str2num(labels)
         X = intent_features
 
-        tuned_parameters = [{'C': [1, 2, 5, 10, 20, 100], 'kernel': [str('linear')]}] # dirty str fix because sklearn is expecting str not instance of basestr...
+        # dirty str fix because sklearn is expecting str not instance of basestr...
+        tuned_parameters = [{'C': [1, 2, 5, 10, 20, 100], 'kernel': [str('linear')]}]
         cv_splits = min(5, np.min(np.bincount(y)))
         self.clf = GridSearchCV(SVC(C=1, probability=True),
                                 param_grid=tuned_parameters, n_jobs=num_threads,
