fixed cross fold calculation. #239

rasa_nlu/classifiers/sklearn_intent_classifier.py
@@ -61,7 +61,7 @@ class SklearnIntentClassifier(Component):
         X = intent_features
 
         tuned_parameters = [{'C': [1, 2, 5, 10, 20, 100], 'kernel': ['linear']}]
-        cv_splits = min(5, np.min(np.bincount(y)))
+        cv_splits = max(2, min(5, np.min(np.bincount(y))))
         self.clf = GridSearchCV(SVC(C=1, probability=True),
                                 param_grid=tuned_parameters, n_jobs=num_threads,
                                 cv=cv_splits, scoring='f1_weighted')