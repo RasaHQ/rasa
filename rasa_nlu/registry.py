71a64188b70666b96725ea68770ac9f281ac341c
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-04-11 19:25:26 +0200

    Fixed regression, removed duckling from default pipeline

--- rasa_nlu/registry.py
+++ rasa_nlu/registry.py
@@ -48,7 +48,6 @@ registered_pipeline_templates = {
         "nlp_spacy",
         "ner_spacy",
         "ner_synonyms",
-        "ner_duckling",
         "intent_featurizer_spacy",
         "intent_classifier_sklearn",
     ],
