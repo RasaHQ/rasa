847ef39f3094a17f847a496e8a67df11b0fe530c
oziee <kiza.soza@gmail.com>
2017-05-12 23:10:30 +1000

    duckling fix..
    python indent was wrong

--- rasa_nlu/extractors/duckling_extractor.py
+++ rasa_nlu/extractors/duckling_extractor.py
@@ -101,7 +101,7 @@ class DucklingExtractor(EntityExtractor):
         # type: (Text) -> Dict[Text, Any]
         file_name = self.name+".json"
         full_name = os.path.join(model_dir, file_name)
-            with io.open(full_name, 'w', encoding="utf-8") as f:
+        with io.open(full_name, 'w', encoding="utf-8") as f:
             f.write(json.dumps({"dimensions": self.dimensions}, ensure_ascii=False))
         return {"ner_duckling_persisted": file_name}
 
