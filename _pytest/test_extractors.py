5c6e69ce3fc4a0d6acfa4cfc06dbc52e5b23bce6
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-05-24 13:55:40 +0200

    fixed pep8

--- _pytest/test_extractors.py
+++ _pytest/test_extractors.py
@@ -36,7 +36,7 @@ def test_crf_json_from_BILOU(spacy_nlp):
     ext.BILOU_flag = True
     sentence = u"I need a home cleaning close-by"
     r = ext._from_crf_to_json(spacy_nlp(sentence), ['O', 'O', 'O', 'B-what', 'L-what', 'B-where', 'I-where', 'L-where'])
-    assert len(r) == 2 , "There should be two entities"
+    assert len(r) == 2, "There should be two entities"
     assert r[0] == {u'start': 9, u'end': 22, u'value': u'home cleaning', u'entity': u'what'}
     assert r[1] == {u'start': 23, u'end': 31, u'value': u'close-by', u'entity': u'where'}
 
@@ -47,7 +47,7 @@ def test_crf_json_from_non_BILOU(spacy_nlp):
     ext.BILOU_flag = False
     sentence = u"I need a home cleaning close-by"
     r = ext._from_crf_to_json(spacy_nlp(sentence), ['O', 'O', 'O', 'what', 'what', 'where', 'where', 'where'])
-    assert len(r) == 5, "There should be five entities"    # non BILOU will split multi-word entities - hence 5
+    assert len(r) == 5, "There should be five entities"  # non BILOU will split multi-word entities - hence 5
     assert r[0] == {u'start': 9, u'end': 13, u'value': u'home', u'entity': u'what'}
     assert r[1] == {u'start': 14, u'end': 22, u'value': u'cleaning', u'entity': u'what'}
     assert r[2] == {u'start': 23, u'end': 28, u'value': u'close', u'entity': u'where'}
