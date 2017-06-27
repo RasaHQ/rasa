efeb3c2f902e27560d5e3486d8dd0b34cfc9bc0e
phlf <ph.lefur@gmail.com>
2017-06-27 09:24:25 +0200

    Isolated imports through multiprocessing in test_sanity

--- _pytest/test_sanity.py
+++ _pytest/test_sanity.py
@@ -8,6 +8,7 @@ import pkgutil
 from collections import defaultdict
 
 import pytest
+from multiprocessing import Queue, Process
 from six import PY2
 
 
@@ -22,10 +23,6 @@ def import_submodules(package_name, skip_list):
         full_name = package.__name__ + '.' + name
         if full_name not in skip_list:
             imported_module = importlib.import_module(full_name)
-            # if PY2:
-            #     reload(imported_module)
-            # else:
-            #     importlib.reload(imported_module)
             results.append(full_name)
             if is_pkg:
                 results += import_submodules(full_name, skip_list)
@@ -36,7 +33,23 @@ def import_submodules(package_name, skip_list):
 def test_no_global_imports_of_banned_package(banned_package):
     """This test ensures that neither of the banned packages are imported module wise in any of our code files.
 
-    If one of the dependencies is needed, they should be imported within a function."""
+     If one of the dependencies is needed, they should be imported within a function."""
+
+    q = Queue()
+    p = Process(target=get_tracked_imports, args=(q,))
+    p.start()
+    tracked_imports = q.get()
+    p.join()
+
+    def find_modules_importing(name):
+        return {v for k, vs in tracked_imports.items() if k.startswith(name) for v in vs}
+
+    assert not find_modules_importing(banned_package), \
+        "No module should import {} globally. Found in {}".format(
+            banned_package, ", ".join(find_modules_importing(banned_package)))
+
+
+def get_tracked_imports(q):
     import inspect
 
     # To track imports accross modules, we will replace the default import function
@@ -65,14 +78,9 @@ def test_no_global_imports_of_banned_package(banned_package):
     # import all available modules and track imports on the way
     import_submodules("rasa_nlu", skip_list={})
 
-    def find_modules_importing(name):
-        return {v for k, vs in tracked_imports.items() if k.startswith(name) for v in vs}
-
-    assert not find_modules_importing(banned_package), \
-        "No module should import {} globally. Found in {}".format(
-            banned_package, ", ".join(find_modules_importing(banned_package)))
-
     if PY2:
         __builtin__.__import__ = original_import_function
     else:
         builtins.__import__ = original_import_function
+
+    q.put(tracked_imports)
