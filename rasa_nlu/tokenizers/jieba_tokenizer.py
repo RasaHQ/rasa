f7a5b76bd2d6b4a63ebf4356c830256ed5d83a38
crownpku <crownpku@gmail.com>
2017-07-12 14:05:35 +0800

    bug fix

--- rasa_nlu/tokenizers/jieba_tokenizer.py
+++ rasa_nlu/tokenizers/jieba_tokenizer.py
@@ -2,18 +2,14 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from builtins import str
-import re
 
 from typing import Any
 from typing import Dict
 from typing import List
 from typing import Text
-from typing import Tuple
 
 from rasa_nlu.config import RasaNLUConfig
-from rasa_nlu.tokenizers import Token
-from rasa_nlu.tokenizers import Tokenizer
+from rasa_nlu.tokenizers import Tokenizer, Token
 from rasa_nlu.components import Component
 from rasa_nlu.training_data import Message
 from rasa_nlu.training_data import TrainingData
@@ -23,14 +19,6 @@ class JiebaTokenizer(Tokenizer, Component):
     name = "tokenizer_jieba"
 
     provides = ["tokens"]
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def required_packages(cls):
-        # type: () -> List[Text]
-        return ["jieba"]
 
     def train(self, training_data, config, **kwargs):
         # type: (TrainingData, RasaNLUConfig, **Any) -> None
@@ -42,31 +30,17 @@ class JiebaTokenizer(Tokenizer, Component):
         # type: (Message, **Any) -> None
 
         message.set("tokens", self.tokenize(message.text))
-    
-    def tokenize_with_offsets(self, text):
-        # type: (Text) -> Tuple[List[Text], List[int]]
-        import jieba
-
-        _text = text.encode('utf-8')
-        offsets = []
-        offset = 0
-        tokens = [w for w in jieba.lcut(_text)]
-        for tok in tokens:
-            m = re.search(re.escape(tok), text[offset:], re.UNICODE)
-            if m is None:
-                message = "Invalid Jieba offset. Token '{}' in message '{}'.".format(str(tok),str(text.encode('utf-8')))
-                raise ValueError(message)
-            offsets.append(offset + m.start())
-            offset += m.end()
-        return tokens, offsets
 
     def tokenize(self, text):
         # type: (Text) -> List[Token]
+        import jieba
 
-        _text = text.encode('utf-8')
-        tokenized = self.tokenize_with_offsets(_text)
-        tokens = [Token(token.decode('utf-8'), self._byte_to_char_offset(_text, offset)) for token, offset in tokenized]
+        words = jieba.lcut(text.encode('utf-8'))
+        running_offset = 0
+        tokens = []
+        for word in words:
+            word_offset = text.index(word, running_offset)
+            word_len = len(word)
+            running_offset = word_offset + word_len
+            tokens.append(Token(word, word_offset))
         return tokens
-
-    def _byte_to_char_offset(self, text, byte_offset):
-        return len(text[:byte_offset].decode('utf-8'))
