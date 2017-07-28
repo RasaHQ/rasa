0b072fdddfaa692b517b1b8a15696e4c6c11a6a6
Nathan Zylbersztejn <nzylber1@francoiss-mbp-2.ad.ypg.com>
2017-07-28 10:41:53 -0400

    Ensure a word is not a synonym to itself (e.g mexican is not a syn of mexican as it seems to be specified in api.ai file format)

--- rasa_nlu/training_data.py
+++ rasa_nlu/training_data.py
@@ -111,7 +111,7 @@ class TrainingData(object):
         # type: (**Any) -> str
         """Represent this set of training examples as json adding the passed meta information."""
 
-        syns_as_tuples = sorted(self.entity_synonyms.items(), key=lambda x: x[1])
+        syns_as_tuples = sorted([i for i in self.entity_synonyms.items() if i[0] != i[1]], key=lambda x: x[1])
         self.entity_synonyms = []
         for i, s in enumerate(syns_as_tuples):
             if i == 0 or s[1] != syns_as_tuples[i-1][1]:
