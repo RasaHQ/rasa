19f3abbf9bc52b5bfd7907db3c294dece00ee957
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-10-06 21:00:13 +0200

    improved action test coverage

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -3,9 +3,9 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from rasa_core.actions.action import ActionRestart, UtterAction, \
+    ActionListen, Action
 from rasa_core.events import Restarted
-
-from rasa_core.actions.action import ActionRestart
 from rasa_core.trackers import DialogueStateTracker
 
 
@@ -16,3 +16,14 @@ def test_restart(default_dispatcher, default_domain):
                                    default_domain.default_topic)
     events = ActionRestart().run(default_dispatcher, tracker, default_domain)
     assert events == [Restarted()]
+
+
+def test_text_format():
+    assert "{}".format(ActionListen()) == \
+           "Action('action_listen')"
+    assert "{}".format(UtterAction("my_action_name")) == \
+           "UtterAction('my_action_name')"
+
+
+def test_default_reset_topic():
+    assert not Action().resets_topic()
