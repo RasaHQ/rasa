c2bd8857cc5433757472f5c45d8be451e15714c4
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-10-08 19:24:15 +0200

    added action tests

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -3,6 +3,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import pytest
+from examples.concerts.actions import ActionSearchConcerts
+
+from rasa_core.actions.factories import action_factory_by_name, RemoteAction
+
 from rasa_core.actions.action import ActionRestart, UtterAction, \
     ActionListen, Action
 from rasa_core.events import Restarted
@@ -27,3 +32,45 @@ def test_text_format():
 
 def test_default_reset_topic():
     assert not Action().resets_topic()
+
+
+def test_action_factories():
+    assert action_factory_by_name("local") is not None
+    assert action_factory_by_name("remote") is not None
+    with pytest.raises(Exception):
+        action_factory_by_name("unknown_name")
+
+
+def test_local_action_factory_module_import():
+    instantiated_actions = action_factory_by_name("local")(
+            ["action_listen", "examples.concerts.actions.ActionSearchConcerts",
+             "utter_test"], ["utter_test"])
+    assert len(instantiated_actions) == 3
+    assert isinstance(instantiated_actions[0], ActionListen)
+    assert isinstance(instantiated_actions[1], ActionSearchConcerts)
+    assert isinstance(instantiated_actions[2], UtterAction)
+
+
+def test_remote_action_factory_module_import():
+    instantiated_actions = action_factory_by_name("remote")(
+            ["action_listen", "random_name", "utter_test"], ["utter_test"])
+    assert len(instantiated_actions) == 3
+    assert isinstance(instantiated_actions[0], ActionListen)
+    assert isinstance(instantiated_actions[1], RemoteAction)
+    assert isinstance(instantiated_actions[2], RemoteAction)
+
+
+def test_local_action_factory_module_import_fails_on_invalid():
+    with pytest.raises(ValueError):
+        action_factory_by_name("local")(["random_name"], ["utter_test"])
+
+    with pytest.raises(ValueError):
+        action_factory_by_name("local")(["utter_default"], ["utter_test"])
+
+    with pytest.raises(ValueError):
+        action_factory_by_name("local")(["examples.UnavailableClass"],
+                                        ["utter_test"])
+
+    with pytest.raises(ValueError):
+        action_factory_by_name("local")(["nonexistant.UnavailableClass"],
+                                        ["utter_test"])
