6fa527062059e9bfae02cecf738e9e7fbb936c09
Tom Bocklisch <tom.bocklisch@scalableminds.com>
2017-10-24 15:21:20 +0200

    fixed failing tests

--- tests/test_domain_spec_dm.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
-from rasa_core.featurizers import BinaryFeaturizer
-from rasa_core.policies.ensemble import PolicyEnsemble
-
