a77b69c0b99d2827a8c4376c2fc6a74e823f0499
Xiaoquan Kong <u1mail2me@gmail.com>
2017-10-25 18:26:34 +0800

    Fix issue that test will fail in python3 on patched warnings

--- tests/base/test_py2_patched_warnings.py
+++ tests/base/test_py2_patched_warnings.py
@@ -7,23 +7,25 @@ from __future__ import unicode_literals
 import warnings
 
 import pytest
+import six
 
 import rasa_nlu.py2_patched_warnings as patched_warnings
 
 
-def test_standard_warnings():
-    """
-    standard library `warnings` under python2 can't handle unicode correctly,
-    it will raise an exception
-    """
-    with pytest.raises(UnicodeEncodeError):
-        warnings.warn("中文")
+if six.PY2:
 
+    def test_standard_warnings():
+        """
+        standard library `warnings` under python2 can't handle unicode correctly,
+        it will raise an exception
+        """
+        with pytest.raises(UnicodeEncodeError):
+            warnings.warn("中文")
 
-def test_patched_warnings():
-    """
-    patched `warnings` can handle unicode correctly
-    """
+    def test_patched_warnings():
+        """
+        patched `warnings` can handle unicode correctly
+        """
 
-    patched_warnings.warn("中文")
-    patched_warnings.warn("english")
+        patched_warnings.warn("中文")
+        patched_warnings.warn("english")
