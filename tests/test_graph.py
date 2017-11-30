ecda2f6d74d1c47b3192719ca9d983edc15bb359
Tom Bocklisch <tom@rasa.ai>
2017-11-30 15:39:05 +0100

    fixed graph test

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -1,6 +1,19 @@
 from rasa_core.training import StoryGraph
 
 
+def check_graph_is_sorted(g, sorted_nodes, removed_edges):
+    incoming_edges = {k: [s for s, vs in g.items() if k in vs]
+                      for k in g.keys()}
+
+    visited = set()
+    for n in sorted_nodes:
+        deps = incoming_edges.get(n, [])
+        # checks that all incoming edges are from nodes we have already visited
+        assert all([d in visited or (d, n) in removed_edges for d in deps]), \
+            "Found an incoming edge from a node that wasn't visited yet!"
+        visited.add(n)
+
+
 def test_node_ordering():
     example_graph = {
         "a": ["b", "c", "d"],
@@ -10,8 +23,9 @@ def test_node_ordering():
         "e": ["f"],
         "f": []}
     sorted_nodes, removed_edges = StoryGraph.topological_sort(example_graph)
-    assert list(sorted_nodes) == ['e', 'f', 'a', 'c', 'd', 'b']
+
     assert removed_edges == set()
+    check_graph_is_sorted(example_graph, sorted_nodes, removed_edges)
 
 
 def test_node_ordering_with_cycle():
@@ -23,7 +37,6 @@ def test_node_ordering_with_cycle():
         "e": ["f"],
         "f": ["e"]}
     sorted_nodes, removed_edges = StoryGraph.topological_sort(example_graph)
-    assert list(sorted_nodes) == ['e', 'f', 'a', 'c', 'd', 'b']
-    assert len(removed_edges) == 2
-    assert ("f", "e") in removed_edges
-    assert ("d", "a") in removed_edges
+
+    check_graph_is_sorted(example_graph, sorted_nodes, removed_edges)
+
