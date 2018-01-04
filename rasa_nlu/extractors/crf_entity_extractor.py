d7ac685f0e24a35aa675f92aad8fe6615652a446
Thomas Werkmeister <thomas@twerkmeister.com>
2018-01-04 11:07:36 +0800

    fixed code complexity + test for spacy version number

--- rasa_nlu/extractors/crf_entity_extractor.py
+++ rasa_nlu/extractors/crf_entity_extractor.py
@@ -308,6 +308,13 @@ class CRFEntityExtractor(EntityExtractor):
         else:
             return None
 
+    def __tag_of_token(selfself, token):
+        import spacy
+        if spacy.__version__ > "2" and token._.has("tag"):
+            return token._.get("tag")
+        else:
+            return token.tag_
+
     def _from_text_to_crf(self, message, entities=None):
         # type: (Message, List[Text]) -> List[Tuple[Text, Text, Text, Text]]
         """Takes a sentence and switches it to crfsuite format."""
@@ -316,7 +323,7 @@ class CRFEntityExtractor(EntityExtractor):
         for i, token in enumerate(message.get("spacy_doc")):
             pattern = self.__pattern_of_token(message, i)
             entity = entities[i] if entities else "N/A"
-            tag = token._.get("tag") if token._.has("tag") else token.tag_
+            tag = self.__tag_of_token(token)
             crf_format.append((token.text, tag, entity, pattern))
         return crf_format
 
