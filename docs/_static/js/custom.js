49343c4313d31f102080ab3117c58cf6c045dea9
psharmz <s1466415@sms.ed.ac.uk>
2018-01-12 09:23:36 -0500

    remove unused disabled emoji thingy for now

--- docs/_static/js/custom.js
+++
@@ -1,6 +0,0 @@
-var checkboxes = $("input[type='checkbox']"),
-    submitButt = $("input[type='submit']");
-
-checkboxes.click(function() {
-    submitButt.attr("disabled", !checkboxes.is(":checked"));
-});
