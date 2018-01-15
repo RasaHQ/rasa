f0684b66e6476d8d72c82bf766639282063685e7
psharmz <s1466415@sms.ed.ac.uk>
2018-01-15 08:16:19 -0500

    fix path in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -151,7 +151,7 @@ html_theme_path = ["_themes", ]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static/css/custom.css']
+html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
