1ed33c4fc869817b4e55cf0a3d57a7a959fdc4af
Vova Vv <mr.voov@gmail.com>
2018-01-31 16:44:20 +0100

    style changes

--- examples/restaurantbot/bot.py
+++ examples/restaurantbot/bot.py
@@ -42,8 +42,8 @@ class ActionSuggest(Action):
     def run(self, dispatcher, tracker, domain):
         dispatcher.utter_message("here's what I found:")
         dispatcher.utter_message(tracker.get_slot("matches"))
-        dispatcher.utter_message("is it ok for you? " \
-                                 "hint: I'm not going to " \
+        dispatcher.utter_message("is it ok for you? "
+                                 "hint: I'm not going to "
                                  "find anything else :)")
         return []
 
