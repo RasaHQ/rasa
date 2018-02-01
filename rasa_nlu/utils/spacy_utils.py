c3e028717f40153dd07e966d93751012bbaedfc2
Felipe Martin <me@fmartingr.com>
2018-02-01 16:09:29 +0100

    Using cls argument on component loading to avoid inheritance issues

--- rasa_nlu/utils/spacy_utils.py
+++ rasa_nlu/utils/spacy_utils.py
@@ -103,7 +103,7 @@ class SpacyNLP(Component):
 
         nlp = spacy.load(model_metadata.get("spacy_model_name"), parser=False)
         cls.ensure_proper_language_model(nlp)
-        return SpacyNLP(nlp, model_metadata.get("language"), model_metadata.get("spacy_model_name"))
+        return cls(nlp, model_metadata.get("language"), model_metadata.get("spacy_model_name"))
 
     @staticmethod
     def ensure_proper_language_model(nlp):
