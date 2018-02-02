a367b3dbb60c22ff9b3c4e96a6e4966483b7ccc6
Thomas Werkmeister <thomas@twerkmeister.com>
2018-02-02 10:45:40 +0800

    changed interpreter test to test for entity types not for specific entity occurence

--- tests/base/test_interpreter.py
+++ tests/base/test_interpreter.py
@@ -5,54 +5,26 @@ from __future__ import absolute_import
 import pytest
 
 from tests import utilities
-from rasa_nlu import registry
+from rasa_nlu import registry, training_data
 
 
 @utilities.slowtest
 @pytest.mark.parametrize("pipeline_template", list(registry.registered_pipeline_templates.keys()))
-def test_samples(pipeline_template, component_builder):
+def test_interpreter(pipeline_template, component_builder):
+    test_data = "data/examples/rasa/demo-rasa.json"
     _conf = utilities.base_test_conf(pipeline_template)
-    _conf["data"] = "./data/examples/rasa/demo-rasa.json"
-
+    _conf["data"] = test_data
+    td = training_data.load_data(test_data)
     interpreter = utilities.interpreter_for(component_builder, _conf)
-    available_intents = ["greet", "restaurant_search", "affirm", "goodbye", "None"]
-    samples = [
-        (
-            "good bye",
-            {
-                'intent': 'goodbye',
-                'entities': []
-            }
-        ),
-        (
-            "i am looking for an indian spot",
-            {
-                'intent': 'restaurant_search',
-                'entities': [
-                    {"start": 20,
-                     "end": 26,
-                     "value": "indian",
-                     "entity": "cuisine"},
-                    {"end": 26,
-                     "entity": "NORP",
-                     "start": 20,
-                     "value": "indian"}]
-            }
-        )
-    ]
 
-    for text, gold in samples:
+    texts = ["good bye", "i am looking for an indian spot"]
+
+    for text in texts:
         result = interpreter.parse(text, time=None)
-        assert result['text'] == text, \
-            "Wrong text for sample '{}'".format(text)
-        assert result['intent']['name'] in available_intents, \
-            "Wrong intent for sample '{}'".format(text)
-        assert result['intent']['confidence'] >= 0, \
-            "Low confidence for sample '{}'".format(text)
-
-        # This ensures the model doesn't detect entities that are not present
-        # Models on our test data set are not stable enough to require the entities to be found
+        assert result['text'] == text
+        assert not result['intent']['name'] or result['intent']['name'] in td.intents
+        assert result['intent']['confidence'] >= 0
+        # Ensure the model doesn't detect entity types that are not present
+        # Models on our test data set are not stable enough to require the exact entities to be found
         for entity in result['entities']:
-            del entity["extractor"]
-            assert entity in gold['entities'], \
-                "Wrong entities for sample '{}'".format(text)
+            assert entity['entity'] in td.entities
