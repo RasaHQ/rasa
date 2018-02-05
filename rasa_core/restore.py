5e2956e5b969c9887c4d295c6c0f1b89191f32eb
Tom Bocklisch <tom@rasa.ai>
2018-02-05 11:43:01 +0100

    more commets

--- rasa_core/restore.py
+++ rasa_core/restore.py
@@ -8,6 +8,7 @@ import json
 import logging
 
 from builtins import str
+from typing import Text, Optional, List
 
 from rasa_core import utils, evaluate
 from rasa_core.actions.action import ActionListen
@@ -44,8 +45,11 @@ def create_argument_parser():
     return parser
 
 
-def check_prediction_aligns_with_story(last_prediction,
-                                       actions_between_utterances):
+def _check_prediction_aligns_with_story(last_prediction,
+                                        actions_between_utterances):
+    # type: (List[Text], List[Text]) -> None
+    """Emit a warning if predictions do not align with expected actions."""
+
     p, a = evaluate.align_lists(last_prediction, actions_between_utterances)
     if p != a:
         logger.warn("Model predicted different actions than the "
@@ -53,7 +57,53 @@ def check_prediction_aligns_with_story(last_prediction,
                     "{} but got {}.".format(p, a))
 
 
+def _replay_events(tracker, agent):
+    # type: (DialogueStateTracker, Agent) -> None
+    """Take a tracker and replay the logged user utterances against an agent.
+
+    During replaying of the user utterances, the executed actions and events
+    created by the agent are compared to the logged ones of the tracker that
+    is getting replayed. If they differ, a warning is logged.
+
+    At the end, the tracker stored in the agents tracker store for the
+    same sender id will have quite the same state as the one
+    that got replayed."""
+
+    actions_between_utterances = []
+    last_prediction = [ActionListen().name()]
+
+    for i, event in enumerate(tracker.events_after_latest_restart()):
+        if isinstance(event, UserUttered):
+            _check_prediction_aligns_with_story(last_prediction,
+                                                actions_between_utterances)
+
+            actions_between_utterances = []
+            print(utils.wrap_with_color(event.text, utils.bcolors.OKGREEN))
+            agent.handle_message(event.text, sender_id=tracker.sender_id,
+                                 output_channel=ConsoleOutputChannel())
+            tracker = agent.tracker_store.retrieve(tracker.sender_id)
+            last_prediction = evaluate.actions_since_last_utterance(tracker)
+
+        elif isinstance(event, ActionExecuted):
+            actions_between_utterances.append(event.action_name)
+
+    _check_prediction_aligns_with_story(last_prediction,
+                                        actions_between_utterances)
+
+
+def _load_tracker_from_json(tracker_dump, agent):
+    # type: (Text, Agent) -> DialogueStateTracker
+    """Read the json dump from the file and instantiate a tracker it."""
+
+    tracker_json = json.loads(utils.read_file(tracker_dump))
+    sender_id = tracker_json.get("sender_id", UserMessage.DEFAULT_SENDER_ID)
+    return DialogueStateTracker.from_dict(sender_id,
+                                          tracker_json.get("events", []),
+                                          agent.domain)
+
+
 def main(model_directory, nlu_model=None, tracker_dump=None):
+    # type: (Text, Optional[Text], Optional[Text]) -> Agent
     """Run the agent."""
 
     logger.info("Rasa process starting")
@@ -61,38 +111,14 @@ def main(model_directory, nlu_model=None, tracker_dump=None):
 
     logger.info("Finished loading agent. Loading stories now.")
 
-    tracker_json = json.loads(utils.read_file(tracker_dump))
-    sender_id = tracker_json.get("sender_id", UserMessage.DEFAULT_SENDER_ID)
-    tracker = DialogueStateTracker.from_dict(sender_id,
-                                             tracker_json.get("events", []),
-                                             agent.domain)
-
-    actions_between_utterances = []
-    last_prediction = [ActionListen().name()]
-
-    for i, event in enumerate(tracker.events_after_latest_restart()):
-        if isinstance(event, UserUttered):
-            check_prediction_aligns_with_story(last_prediction,
-                                               actions_between_utterances)
-
-            actions_between_utterances = []
-            print(utils.wrap_with_color(event.text, utils.bcolors.OKGREEN))
-            agent.handle_message(event.text, sender_id=sender_id,
-                                 output_channel=ConsoleOutputChannel())
-            tracker = agent.tracker_store.retrieve(sender_id)
-            last_prediction = evaluate.actions_since_last_utterance(tracker)
-
-        elif isinstance(event, ActionExecuted):
-            actions_between_utterances.append(event.action_name)
-
-    check_prediction_aligns_with_story(last_prediction,
-                                       actions_between_utterances)
+    tracker = _load_tracker_from_json(tracker_dump, agent)
+    _replay_events(tracker, agent)
 
     print(utils.wrap_with_color(
             "You can now continue the dialogue. "
             "Use '/stop' to exit the conversation.",
             utils.bcolors.OKGREEN + utils.bcolors.UNDERLINE))
-    agent.handle_channel(ConsoleInputChannel(sender_id))
+    agent.handle_channel(ConsoleInputChannel(tracker.sender_id))
 
     return agent
 
