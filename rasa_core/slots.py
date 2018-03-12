dbd4e3a41c4d4c9e056268ea7f9701014835e46c
Tom Bocklisch <tom@rasa.ai>
2018-03-12 16:57:42 +0100

    added warnings to cat slot and allow to pass val split to train

--- rasa_core/slots.py
+++ rasa_core/slots.py
@@ -188,6 +188,15 @@ class CategoricalSlot(Slot):
             for i, v in enumerate(self.values):
                 if v == str(self.value).lower():
                     r[i] = 1.0
+                    break
+            else:
+                logger.warn("Categorical slot '{}' is set to a value ('{}') "
+                            "that is not specified in the domain. "
+                            "Value will be ignored and the slot will "
+                            "behave as if it no value is set. "
+                            "Make sure to add all values a categorical "
+                            "slot should store to the domain."
+                            "".format(self.name, self.value))
         except (TypeError, ValueError):
             # probably hit something strange
             return r
