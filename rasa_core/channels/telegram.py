b931c79c1bdcdd3ff2696e9b5edbcc71e9cc766c
Aman Tiwari <tiwari.aman85@gmail.com>
2018-03-14 16:20:11 +0530

    test fix
    Blank space removed

--- rasa_core/channels/telegram.py
+++ rasa_core/channels/telegram.py
@@ -106,7 +106,6 @@ class TelegramInput(HttpInputComponent):
             else:
                 return "Invalid webhook"
         set_webhook()
-        
         @telegram_webhook.route("/webhook", methods=['GET', 'POST'])
         def message():
             if request.method == 'POST':
