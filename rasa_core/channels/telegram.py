0430042424a32154ced166945123168e63dd81a3
Aman Tiwari <tiwari.aman85@gmail.com>
2018-03-17 19:48:05 +0530

    Logger Update
    info & warn update for set_webhook

--- rasa_core/channels/telegram.py
+++ rasa_core/channels/telegram.py
@@ -103,8 +103,10 @@ class TelegramInput(HttpInputComponent):
             s = out_channel.setWebhook(self.webhook_url)
             if s:
                 return "Webhook setup successful"
+                logger.info("Webhook Setup Successful")
             else:
                 return "Invalid webhook"
+                logger.info("Webhook Setup Failed")
         set_webhook()
         
         @telegram_webhook.route("/webhook", methods=['GET', 'POST'])
