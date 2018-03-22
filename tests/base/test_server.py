86429d765266234539479e20b6403ad4a6142702
ricwo <ric.wkr@gmail.com>
2018-03-22 19:06:12 +0000

    rogue line

--- tests/base/test_server.py
+++ tests/base/test_server.py
@@ -235,7 +235,6 @@ def test_unload_model_error(app):
     model_err = "http://dummy-uri/models?model=my_model"
     response = yield app.delete(model_err)
     rjs = yield response.json()
-    print(rjs)
     assert response.code == 500, "Model not found"
     assert rjs['error'] == "Failed to unload model my_model for project default."
 
