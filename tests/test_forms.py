3135343e09a1742945184e582b46d8fcf7e03236
Alan Nichol <alan@golastmile.com>
2018-04-13 06:29:35 +0200

    add test for form with free text input

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -26,6 +26,20 @@ class ActionSearchRestaurants(FormAction):
         pass
 
 
+class ActionSearchPeople(FormAction):
+
+    REQUIRED_FIELDS = [
+	FreeTextFormField("person_name")
+    ]
+
+    def name(self):
+        return 'action_search_people'
+
+    def submit(self, dispatcher, tracker, domain):
+        pass
+
+
+
 def test_restaurant_form():
     domain = TemplateDomain.load("data/test_domains/restaurant_form.yml")
     tracker_store = InMemoryTrackerStore(domain)
@@ -59,3 +73,34 @@ def test_restaurant_form():
 
     assert events[1].key == "requested_slot"
     assert events[1].value == "people"
+
+
+def test_people_form():
+    domain = TemplateDomain.load("data/test_domains/people_form.yml")
+    tracker_store = InMemoryTrackerStore(domain)
+    out = CollectingOutputChannel()
+    sender_id = "test-people"
+    dispatcher = Dispatcher(sender_id, out, domain)
+    tracker = tracker_store.get_or_create_tracker(sender_id)
+
+    # first user utterance
+    tracker.update(UserUttered("", intent={"name": "inform"}))
+    events = ActionSearchRestaurants().run(dispatcher, tracker, domain)
+    assert len(events) == 1
+    assert isinstance(events[0], SlotSet)
+    assert events[0].key == "requested_slot"
+    assert events[0].value == "person_name"
+    tracker.update(events[0])
+
+    # second user utterance
+    name = "Rasa Due"
+    tracker.update(
+        UserUttered(name,
+                    intent={"name": "inform"}))
+
+    events = ActionSearchRestaurants().run(dispatcher, tracker, domain)
+    assert len(events) == 1
+    assert isinstance(events[0], SlotSet)
+
+    assert events[0].key == "person_name"
+    assert events[0].value == name
