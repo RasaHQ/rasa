b46ba78dc054933bcd31f50874f0442894c670af
Tom Bocklisch <tom@rasa.ai>
2018-04-17 16:30:46 +0200

    improved formatting

--- tests/base/test_tokenizers.py
+++ tests/base/test_tokenizers.py
@@ -9,40 +9,57 @@ def test_whitespace():
     from rasa_nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
     tk = WhitespaceTokenizer()
 
-    assert [t.text for t in tk.tokenize("Forecast for lunch")] == ['Forecast', 'for', 'lunch']
-    assert [t.offset for t in tk.tokenize("Forecast for lunch")] == [0, 9, 13]
+    assert [t.text for t in tk.tokenize("Forecast for lunch")] == \
+           ['Forecast', 'for', 'lunch']
 
-    assert [t.text for t in tk.tokenize("hey ńöñàśçií how're you?")] == ['hey', 'ńöñàśçií', 'how\'re', 'you?']
-    assert [t.offset for t in tk.tokenize("hey ńöñàśçií how're you?")] == [0, 4, 13, 20]
+    assert [t.offset for t in tk.tokenize("Forecast for lunch")] == \
+           [0, 9, 13]
+
+    assert [t.text for t in tk.tokenize("hey ńöñàśçií how're you?")] == \
+           ['hey', 'ńöñàśçií', 'how\'re', 'you?']
+
+    assert [t.offset for t in tk.tokenize("hey ńöñàśçií how're you?")] == \
+           [0, 4, 13, 20]
 
 
 def test_spacy(spacy_nlp):
     from rasa_nlu.tokenizers.spacy_tokenizer import SpacyTokenizer
     tk = SpacyTokenizer()
-    
-    assert [t.text for t in tk.tokenize(spacy_nlp("Forecast for lunch"))] == ['Forecast', 'for', 'lunch']
-    assert [t.offset for t in tk.tokenize(spacy_nlp("Forecast for lunch"))] == [0, 9, 13]
 
-    assert [t.text for t in tk.tokenize(spacy_nlp("hey ńöñàśçií how're you?"))] == \
+    text = "Forecast for lunch"
+    assert [t.text for t in tk.tokenize(spacy_nlp(text))] == \
+           ['Forecast', 'for', 'lunch']
+    assert [t.offset for t in tk.tokenize(spacy_nlp(text))] == \
+           [0, 9, 13]
+
+    text = "hey ńöñàśçií how're you?"
+    assert [t.text for t in tk.tokenize(spacy_nlp(text))] == \
            ['hey', 'ńöñàśçií', 'how', '\'re', 'you', '?']
-    assert [t.offset for t in tk.tokenize(spacy_nlp("hey ńöñàśçií how're you?"))] == [0, 4, 13, 16, 20, 23]
+    assert [t.offset for t in tk.tokenize(spacy_nlp(text))] == \
+           [0, 4, 13, 16, 20, 23]
 
 
 def test_mitie():
     from rasa_nlu.tokenizers.mitie_tokenizer import MitieTokenizer
     tk = MitieTokenizer()
 
-    assert [t.text for t in tk.tokenize("Forecast for lunch")] == ['Forecast', 'for', 'lunch']
-    assert [t.offset for t in tk.tokenize("Forecast for lunch")] == [0, 9, 13]
+    text = "Forecast for lunch"
+    assert [t.text for t in tk.tokenize(text)] == \
+           ['Forecast', 'for', 'lunch']
+    assert [t.offset for t in tk.tokenize(text)] == \
+           [0, 9, 13]
 
-    assert [t.text for t in tk.tokenize("hey ńöñàśçií how're you?")] == ['hey', 'ńöñàśçií', 'how', '\'re', 'you', '?']
-    assert [t.offset for t in tk.tokenize("hey ńöñàśçií how're you?")] == [0, 4, 13, 16, 20, 23]
+    text = "hey ńöñàśçií how're you?"
+    assert [t.text for t in tk.tokenize(text)] == \
+           ['hey', 'ńöñàśçií', 'how', '\'re', 'you', '?']
+    assert [t.offset for t in tk.tokenize(text)] == \
+           [0, 4, 13, 16, 20, 23]
 
-    
+
 def test_jieba():
     from rasa_nlu.tokenizers.jieba_tokenizer import JiebaTokenizer
     tk = JiebaTokenizer()
-    
+
     assert [t.text for t in tk.tokenize("我想去吃兰州拉面")] == \
            ['我', '想', '去', '吃', '兰州', '拉面']
 
