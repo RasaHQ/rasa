dcd584e6891a1d863320fe9c49e55ae988c13482
Tom Bocklisch <tom@rasa.ai>
2018-04-17 16:27:12 +0200

    merged master

--- docs/languages.rst
+++ docs/languages.rst
@@ -30,6 +30,24 @@ Jieba-MITIE    chinese (``zh``)
 
 These languages can be set as part of the :ref:`section_configuration`.
 
+Jieba-MITIE
+^^^^^^^^^^^
+
+Some notes about using the Jieba tokenizer together with MITIE on chinese
+language data: To use it, you need a proper MITIE feature extractor, e.g.
+``data/total_word_feature_extractor_zh.dat``. It should be trained
+from a Chinese corpus using the MITIE wordrep tools
+(takes 2-3 days for training).
+
+For training, please build the
+[MITIE Wordrep Tool](https://github.com/mit-nlp/MITIE/tree/master/tools/wordrep).
+Note that Chinese corpus should be tokenized first before feeding
+into the tool for training. Close-domain corpus that best matches
+user case works best.
+
+A trained model from Chinese Wikipedia Dump and Baidu Baike can be
+downloaded from [中文Blog](http://www.crownpku.com/2017/07/27/%E7%94%A8Rasa_NLU%E6%9E%84%E5%BB%BA%E8%87%AA%E5%B7%B1%E7%9A%84%E4%B8%AD%E6%96%87NLU%E7%B3%BB%E7%BB%9F.html).
+
 Adding a new language
 ---------------------
 We want to make the process of adding new languages as simple as possible to increase the number of
--- rasa_nlu/tokenizers/jieba_tokenizer.py
+++ rasa_nlu/tokenizers/jieba_tokenizer.py
@@ -3,25 +3,39 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import glob
+import logging
+
 from rasa_nlu.components import Component
 from rasa_nlu.config import RasaNLUModelConfig
 from rasa_nlu.tokenizers import Tokenizer, Token
 from rasa_nlu.training_data import Message, TrainingData
 from typing import Any, List, Text
 
+logger = logging.getLogger(__name__)
+
 
 class JiebaTokenizer(Tokenizer, Component):
-    
     name = "tokenizer_jieba"
 
     provides = ["tokens"]
 
-    languages = ["zh"]
-    
+    language_list = ["zh"]
+
     @classmethod
     def required_packages(cls):
         # type: () -> List[Text]
         return ["jieba"]
+
+    @staticmethod
+    def load_custom_dictionary(path):
+        import jieba
+
+        jieba_userdicts = glob.glob("{}/*".format(path))
+        for jieba_userdict in jieba_userdicts:
+            logger.info("Loading Jieba User Dictionary at "
+                        "{}".format(jieba_userdict))
+            jieba.load_userdict(jieba_userdict)
 
     def train(self, training_data, config, **kwargs):
         # type: (TrainingData, RasaNLUModelConfig, **Any) -> None
