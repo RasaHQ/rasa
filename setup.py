171469630120fc29c92d238382c3de21440df641
Tom Bocklisch <tom@rasa.ai>
2018-04-18 13:24:19 +0200

    prepared for new pypi

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 import io
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 # Avoids IDE errors, but actual version is read from version.py
 __version__ = None
 exec(open('rasa_core/version.py').read())
 
-try:
-    import pypandoc
-    readme = pypandoc.convert_file('README.md', 'rst')
-except (IOError, ImportError):
-    with io.open('README.md', encoding='utf-8') as f:
-        readme = f.read()
+# Get the long description from the README file
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 tests_requires = [
     "pytest",
@@ -60,15 +60,17 @@ extras_requires = {
 }
 
 setup(
-    name='rasa_core',
+    name='rasa-core',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         # supported python versions
+        "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
+        "Topic :: Software Development :: Libraries",
     ],
     packages=find_packages(exclude=["tests", "tools"]),
     version=__version__,
@@ -78,15 +80,22 @@ setup(
     include_package_data=True,
     description="Machine learning based dialogue engine "
                 "for conversational software.",
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Rasa Technologies GmbH',
     author_email='hi@rasa.com',
+    maintainer="Tom Bocklisch",
+    maintainer_email="tom@rasa.com",
     license='Apache 2.0',
     keywords="nlp machine-learning machine-learning-library bot bots "
              "botkit rasa conversational-agents conversational-ai chatbot"
              "chatbot-framework bot-framework",
     url="https://rasa.ai",
-    download_url="https://github.com/RasaHQ/rasa_core/archive/{}.tar.gz".format(__version__)
+    download_url="https://github.com/RasaHQ/rasa_core/archive/{}.tar.gz".format(__version__),
+    project_urls={
+        'Bug Reports': 'https://github.com/rasahq/rasa_core/issues',
+        'Source': 'https://github.com/rasahq/rasa_core',
+    },
 )
 
 print("\nWelcome to Rasa Core!")
