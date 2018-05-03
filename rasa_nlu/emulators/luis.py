5f42c78faf99a70cd51823200cd096f8ab7e53e5
Hari Krishna Akurathi <hkrishna608@hotmail.com>
2018-05-03 17:52:24 +0200

    Fix for the emulators test failure

--- rasa_nlu/emulators/luis.py
+++ rasa_nlu/emulators/luis.py
@@ -47,9 +47,9 @@ class LUISEmulator(NoEmulator):
                 {
                     "entity": e["value"],
                     "type": e["entity"],
-                    "startIndex": e["start"],
-                    "endIndex": e["end"]-1,
-                    "score": e["confidence"]
+                    "startIndex": e["start"] if "start" in e else None,
+                    "endIndex": (e["end"] - 1) if "end" in e else None,
+                    "score": e["confidence"] if "confidence" in e else None
                 } for e in data["entities"]
             ] if "entities" in data else []
         }
