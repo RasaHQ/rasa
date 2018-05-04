acfa56ffcefcd100235186dbdd563f94f53aeb83
Hari Krishna Akurathi <hkrishna608@hotmail.com>
2018-05-04 10:36:13 +0200

    fixed codeclimate issues

--- rasa_nlu/emulators/luis.py
+++ rasa_nlu/emulators/luis.py
@@ -33,6 +33,12 @@ class LUISEmulator(NoEmulator):
             top = self._top_intent(data)
             return [top] if top else []
 
+    def check_key(self, key, dict_):
+        if key == "end":
+            return (dict_[key] - 1) if key in dict_ else None
+        else:
+            return dict_[key] if key in dict_ else None
+
     def normalise_response_json(self, data):
         # type: (Dict[Text, Any]) -> Dict[Text, Any]
         """Transform data to luis.ai format."""
@@ -47,9 +53,9 @@ class LUISEmulator(NoEmulator):
                 {
                     "entity": e["value"],
                     "type": e["entity"],
-                    "startIndex": e["start"] if "start" in e else None,
-                    "endIndex": (e["end"] - 1) if "end" in e else None,
-                    "score": e["confidence"] if "confidence" in e else None
+                    "startIndex": self.check_key("start", e),
+                    "endIndex": self.check_key("end", e),
+                    "score": self.check_key("confidence", e)
                 } for e in data["entities"]
             ] if "entities" in data else []
         }
