2a897796914ab94794e0ec80c64d5598c77f8e83
Xiaoquan Kong <u1mail2me@gmail.com>
2018-05-08 21:32:10 +0800

    clean useless codes

--- tests/base/test_project_loader.py
+++ tests/base/test_project_loader.py
@@ -3,7 +3,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import mock
 from mock import patch
 
 from rasa_nlu.project_loader import ProjectLoader
@@ -19,15 +18,8 @@ def test_list_projects_in_cloud_method():
             def list_projects(self):
                 return [UniqueValue()]
 
-            def list_models(self, project):
-                return [UniqueValue()]
-
         return MockedClass()
 
-    def mocked_data_router_init(self, *args, **kwargs):
-        self.config = None
-
-    with mock.patch.object(persistor, 'get_persistor',
-                           mocked_get_persistor):
+    with patch.object(persistor, 'get_persistor', mocked_get_persistor):
         return_value = ProjectLoader('', '')._list_projects_in_cloud()  # noqa
     assert isinstance(return_value[0], UniqueValue)
