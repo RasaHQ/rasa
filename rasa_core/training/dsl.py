c9a4bb83f90dfd77116ddebd00299ec58ea03667
Vova Vv <mr.voov@gmail.com>
2018-05-28 15:17:18 +0200

    change warn to warning

--- rasa_core/training/dsl.py
+++ rasa_core/training/dsl.py
@@ -230,8 +230,10 @@ class StoryFileReader(object):
                                      line[1:].split(" OR ")]
                     self.add_user_messages(user_messages, line_num)
                 else:  # reached an unknown type of line
-                    logger.warning("Skipping line {}. No valid command found. "
-                                   "Line Content: '{}'".format(line_num, line))
+                    logger.warning("Skipping line {}. "
+                                   "No valid command found. "
+                                   "Line Content: '{}'"
+                                   "".format(line_num, line))
             except Exception as e:
                 msg = "Error in line {}: {}".format(line_num, e.message)
                 logger.error(msg, exc_info=1)
@@ -295,12 +297,14 @@ class StoryFileReader(object):
                 logger.warning("Stating user intents with a leading '_' is "
                                "deprecated. The new format is "
                                "'* {}'. Please update "
-                               "your example '{}' to the new format.".format(c, m))
+                               "your example '{}' to the new format."
+                               "".format(c, m))
             intent_name = utterance.intent.get("name")
             if intent_name not in self.domain.intents:
-                logger.warning("Found unknown intent '{}' on line {}. Please, "
-                               "make sure that all intents are listed in your "
-                               "domain yaml.".format(intent_name, line_num))
+                logger.warning("Found unknown intent '{}' on line {}. "
+                               "Please, make sure that all intents are "
+                               "listed in your domain yaml."
+                               "".format(intent_name, line_num))
             parsed_messages.append(utterance)
         self.current_step_builder.add_user_messages(parsed_messages)
 
