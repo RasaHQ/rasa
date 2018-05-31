21d00141c2631384f905d762b14f95140a207642
Vova Vv <mr.voov@gmail.com>
2018-05-31 13:22:13 +0200

    info->debug for num aug rounds and align next line

--- rasa_core/training/generator.py
+++ rasa_core/training/generator.py
@@ -93,7 +93,7 @@ class TrainingDataGenerator(object):
         phase = 0
         min_num_aug_phases = 3 if self.config.augmentation_factor > 0 else 0
         logger.debug("Number of augmentation rounds is {}"
-                    "".format(min_num_aug_phases))
+                     "".format(min_num_aug_phases))
 
         # placeholder to track gluing process of checkpoints
         used_checkpoints = set()  # type: Set[Text]
