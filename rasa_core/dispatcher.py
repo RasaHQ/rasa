0249f41c8fd5e272c899629bccde85fb2aecd9fc
Pritam <pritampathak96@gmail.com>
2018-06-07 11:12:47 +0530

    Adding more comments in dispatcher.py

--- rasa_core/dispatcher.py
+++ rasa_core/dispatcher.py
@@ -132,17 +132,22 @@ class Dispatcher(object):
         self.utter_response(message)
 
     @staticmethod
-    def _template_variables(filled_slots, kwargs):
-        """Combine slot values and key word arguments to fill templates."""
-
+    def _template_variables(filled_slots, **kwargs):
+        # type: (Text) -> List
+        """Fill in slots in the template variables."""
         if filled_slots is None:
             filled_slots = {}
+        # Copying the filled slots in the template variables.
         template_vars = filled_slots.copy()
         template_vars.update(kwargs.items())
         return template_vars
 
     def _fill_template_text(self, template, filled_slots=None, **kwargs):
+        #type: (Text, **Any) -> Dict[Text, Any]
+        """"Combine slot values and key word arguments to fill templates."""
+        # Getting the slot values in the template variables
         template_vars = self._template_variables(filled_slots, kwargs)
+        # Filling the template variables in the template
         if template_vars:
             try:
                 template["text"] = template["text"].format(**template_vars)
@@ -160,8 +165,9 @@ class Dispatcher(object):
     def retrieve_template(self, template_name, filled_slots=None, **kwargs):
         # type: (Text, **Any) -> Dict[Text, Any]
         """Retrieve a named template from the domain."""
-
+        # Fetching a random template for the passed template name
         r = copy.deepcopy(self.domain.random_template_for(template_name))
+        # Filling the slots in the template and returning the template
         if r is not None:
             return self._fill_template_text(r, filled_slots, **kwargs)
         else:
