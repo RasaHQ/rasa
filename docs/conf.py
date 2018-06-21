e4811f2cb9a3895be573b1377fc2ebd87be409f9
Alan Nichol <alan@golastmile.com>
2018-06-21 09:39:49 +0200

    add import

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 import re
 import sys
 import os
+import rasabaster
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
