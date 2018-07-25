9e3244c53896fc9eb940f8406d65557bf337d14d
Tupac <tupac@metriq.io>
2018-07-25 14:50:01 +0200

    Add token to local storage

--- rasa_core/channels/teams.py
+++ rasa_core/channels/teams.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -6,6 +8,7 @@ from __future__ import unicode_literals
 import json
 import logging
 from requests import post
+import datetime
 
 from flask import Blueprint, request, jsonify
 
@@ -18,6 +21,9 @@ logger = logging.getLogger(__name__)
 class Teams(OutputChannel):
     """A Microsoft Teams communication channel. Not using any library yet."""
 
+    token_expiration_date = datetime.datetime.now()
+    headers = None
+
     def __init__(self, teams_id, teams_secret, conversation, bot_id,
                  service_url):
         # type: (Text, Text, Dict[Text]) -> None
@@ -25,32 +31,35 @@ class Teams(OutputChannel):
         self.teams_id = teams_id
         self.teams_secret = teams_secret
         self.conversation = conversation
-        self.global_uri = "%s/v3/" % service_url
+        self.global_uri = "%sv3/" % service_url
         self.bot_id = bot_id
 
     def get_headers(self):
-        # TODO : Not ask a new token if token still valid. Expiration check
+        if Teams.token_expiration_date < datetime.datetime.now():
+            microsoft_oauth2_url = 'https://login.microsoftonline.com'
+            microsoft_oauth2_path = 'botframework.com/oauth2/v2.0/token'
+            uri = "%s/%s" % (microsoft_oauth2_url, microsoft_oauth2_path)
+            grant_type = 'client_credentials'
+            scope = 'https://api.botframework.com/.default'
+            payload = {'client_id': self.teams_id,
+                       'client_secret': self.teams_secret,
+                       'grant_type': grant_type,
+                       'scope': scope}
 
-        microsoft_oauth2_url = 'https://login.microsoftonline.com'
-        microsoft_oauth2_path = 'botframework.com/oauth2/v2.0/token'
-        uri = "%s/%s" % (microsoft_oauth2_url, microsoft_oauth2_path)
-        grant_type = 'client_credentials'
-        scope = 'https://api.botframework.com/.default'
-        payload = {'client_id': self.teams_id,
-                   'client_secret': self.teams_secret,
-                   'grant_type': grant_type,
-                   'scope': scope}
-
-        token_response = post(uri, data=payload)
-        if token_response.status_code == 200:
-            token_data = token_response.json()
-            access_token = token_data['access_token']
-            token_expiration = token_data['expires_in']
-            headers = {'content-type': 'application/json',
-                       'Authorization': 'Bearer ' + access_token}
-            return headers
+            token_response = post(uri, data=payload)
+            if token_response.ok:
+                token_data = token_response.json()
+                access_token = token_data['access_token']
+                token_expiration = token_data['expires_in']
+                Teams.token_expiration_date = datetime.datetime.now() + \
+                    datetime.timedelta(seconds=int(token_expiration))
+                Teams.headers = {"content-type": "application/json",
+                                "Authorization": "Bearer %s" % access_token}
+                return Teams.headers
+            else:
+                logger.error('Could not get Teams token')
         else:
-            logger.error('Could not get Teams token')
+            return Teams.headers
 
     def send(self, recipient_id, message_data):
         # type: (Text, Dict[Text, Any]) -> None
@@ -68,15 +77,15 @@ class Teams(OutputChannel):
                     }
                 },
                 "text": ""}
-        data.update(message_data)
 
+        data.update(message_data)
         headers = self.get_headers()
         send_response = post(post_message_uri,
                              headers=headers,
                              data=json.dumps(data))
-        
-        status_code = send_response.status_code
-        if status_code not in [200, 201, 202, 204]:
+
+        # status_code = send_response.status_code
+        if not send_response.ok:
             logger.error('Error in send: %s', send_response.text)
 
     def send_text_message(self, recipient_id, message):
@@ -109,8 +118,7 @@ class Teams(OutputChannel):
         self.send(recipient_id, buttons_message)
 
     def send_custom_message(self, recipient_id, elements):
-        custom_message = {"attachments": elements}
-        self.send(recipient_id, custom_message)
+        self.send(recipient_id, elements[0])
 
 
 class TeamsInput(HttpInputComponent):
@@ -141,15 +149,18 @@ class TeamsInput(HttpInputComponent):
         @teams_webhook.route("/api/messages", methods=['POST'])
         def webhook():
             postdata = request.get_json(force=True)
-            logger.info(postdata)
+            # logger.info(postdata)
             try:
-                out_channel = Teams(self.teams_id, self.teams_secret,
-                                    postdata["conversation"],
-                                    postdata["recipient"],
-                                    postdata["serviceUrl"])
-                user_msg = UserMessage(postdata["text"], out_channel,
-                                       postdata["from"]["id"])
-                on_new_message(user_msg)
+                if postdata["type"] == "message":
+                    out_channel = Teams(self.teams_id, self.teams_secret,
+                                        postdata["conversation"],
+                                        postdata["recipient"],
+                                        postdata["serviceUrl"])
+                    user_msg = UserMessage(postdata["text"], out_channel,
+                                           postdata["from"]["id"])
+                    on_new_message(user_msg)
+                else:
+                    logger.info("Not received message type")
             except Exception as e:
                 logger.error("Exception when trying to handle "
                              "message.{0}".format(e))
