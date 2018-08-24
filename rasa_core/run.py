cd082af2de59b226ef4c223fb47cbac2cecd615f
Tom Bocklisch <tom@rasa.com>
2018-08-24 14:37:30 +0200

    reduce model pull frequency

--- rasa_core/run.py
+++ rasa_core/run.py
@@ -283,7 +283,7 @@ def serve_application(initial_agent,
 
 def load_agent(core_model, interpreter, endpoints,
                tracker_store=None,
-               wait_time_between_pulls=10):
+               wait_time_between_pulls=100):
     if endpoints.model:
         return agent.load_from_server(
                 interpreter=interpreter,
