549db7982febccfdbab8e66f0d9476e8ea8e924a
Alan Nichol <alan@golastmile.com>
2018-09-11 11:01:20 +0200

    type annotation

--- rasa_core/training/online.py
+++ rasa_core/training/online.py
@@ -468,7 +468,8 @@ def _write_stories_to_file(export_file_path, sender_id, endpoint):
 
 def _predict_till_next_listen(endpoint,  # type: EndpointConfig
                               sender_id,  # type: Text
-                              finetune): # type: Boolean
+                              finetune  # type: Boolean
+                              ):
     # type: (...) -> None
     # given a state, predict next action via asking a human
 
