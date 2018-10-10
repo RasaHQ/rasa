7feabb7dc67336b92f0489ded54eb52946437294
Zhipeng-Chang <zchang0302@gmail.com>
2018-10-10 09:16:34 -0600

    failed on last build, copied the "test_get_tracker_with_jwt" function from Rasa master and re-test

--- tests/test_server.py
+++ tests/test_server.py
@@ -234,7 +234,6 @@ def test_list_conversations_with_jwt(secured_app):
                                headers=jwt_header)
     assert response.status_code == 403
 
-
 def test_get_tracker_with_jwt(secured_app):
     # token generated with secret "core" and algorithm HS256
     # on https://jwt.io/
@@ -266,7 +265,6 @@ def test_get_tracker_with_jwt(secured_app):
     response = secured_app.get("/conversations/testuser/tracker",
                                headers=jwt_header)
     assert response.status_code == 200
-
 
 def test_list_conversations_with_token(secured_app):
     response = secured_app.get("/conversations?token=rasa")
