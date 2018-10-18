20e542de29ece634e8a7ad4adca8d344f1277874
twollnik <tom.wollnik@student.hpi.de>
2018-10-18 10:27:56 +0200

    fix pep8 linting

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,23 +7,24 @@ from rasa_core.policies.memoization import MemoizationPolicy
 
 def test_handle_precedence_and_defaults_for_config():
 
-    config_data = {'policies':[
-        {'name':'FallbackPolicy', 'nlu_threshold':0.5},
-        {'name':'KerasPolicy'}
+    config_data = {'policies': [
+        {'name': 'FallbackPolicy', 'nlu_threshold' :0.5},
+        {'name': 'KerasPolicy'}
     ]}
     fallback_args = {
         'nlu_threshold': 1,
-        'core_threshold':1,
-        'fallback_action_name':'some_name'
+        'core_threshold': 1,
+        'fallback_action_name': 'some_name'
     }
     expected_config_data = {'policies': [
-        {'name': 'FallbackPolicy', 'nlu_threshold' :1,
-        'core_threshold': 1, 'fallback_action_name': 'some_name'},
+        {'name': 'FallbackPolicy', 'nlu_threshold': 1,
+            'core_threshold': 1, 'fallback_action_name': 'some_name'},
         {'name': 'KerasPolicy', 'max_history': 3}
     ]}
     new_config_data = handle_precedence_and_defaults(
                             config_data, fallback_args, None)
     assert new_config_data == expected_config_data
+
 
 def test_load_config():
 
