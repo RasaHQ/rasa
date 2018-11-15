d71b5758f4f9b95628e18075ee523dbc37c7dcd6
Simon Senkl <development@senkl.eu>
2018-11-15 11:16:39 +0100

    added more detailed policy checks to test

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,6 +13,7 @@ from rasa_core.policies.keras_policy import KerasPolicy
 from rasa_core.policies.fallback import FallbackPolicy
 from rasa_core.policies.form_policy import FormPolicy
 from rasa_core.policies.ensemble import PolicyEnsemble
+from rasa_core.featurizers import BinarySingleStateFeaturizer, MaxHistoryTrackerFeaturizer
 
 
 @pytest.mark.parametrize("filename", glob.glob(
@@ -25,6 +26,25 @@ def test_load_config(filename):
 
 
 def test_ensemble_from_dict():
+    def check_memoization(p):
+        assert p.max_history == 5
+
+    def check_keras(p):
+        featurizer = p.featurizer
+        state_featurizer = featurizer.state_featurizer
+        # Assert policy
+        assert p.epochs == 50
+        # Assert featurizer
+        assert isinstance(featurizer, MaxHistoryTrackerFeaturizer)
+        assert featurizer.max_history == 5
+        # Assert state_featurizer
+        assert isinstance(state_featurizer, BinarySingleStateFeaturizer)
+
+    def check_fallback(p):
+        assert p.fallback_action_name == 'action_default_fallback'
+        assert p.nlu_threshold == 0.7
+        assert p.core_threshold == 0.7
+
     ensemble_dict = {'policies': [
             {'epochs': 50, 'name': 'KerasPolicy', 'featurizer': [
                 {'max_history': 5, 'name': 'MaxHistoryTrackerFeaturizer',
@@ -35,10 +55,23 @@ def test_ensemble_from_dict():
              'nlu_threshold': 0.7,
              'fallback_action_name': 'action_default_fallback'},
             {'name': 'FormPolicy'}]}
-
     ensemble = PolicyEnsemble.from_dict(ensemble_dict)
+
+    # Check if all policies are present
     assert len(ensemble) == 4
-    assert any([isinstance(p, MemoizationPolicy) for p in ensemble])
+    # MemoizationPolicy is parent of FormPolicy
+    assert any([isinstance(p, MemoizationPolicy) and
+        not isinstance(p, FormPolicy) for p in ensemble])
     assert any([isinstance(p, KerasPolicy) for p in ensemble])
     assert any([isinstance(p, FallbackPolicy) for p in ensemble])
     assert any([isinstance(p, FormPolicy) for p in ensemble])
+
+    # Verify policy configurations
+    for policy in ensemble:
+        if isinstance(policy, MemoizationPolicy) \
+        and not isinstance(policy, FormPolicy):
+            check_memoization(policy)
+        elif isinstance(policy, KerasPolicy):
+            check_keras(policy)
+        elif isinstance(policy, FallbackPolicy):
+            check_fallback(policy)
