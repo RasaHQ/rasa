e1839dfe26ed89e18beb1029dfdbcfea9f5ad30d
Simon Senkl <development@senkl.eu>
2018-11-15 01:25:51 +0100

    added test case for Ensemble.from_dict

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,6 +9,10 @@ import pytest
 from tests.conftest import ExamplePolicy
 from rasa_core.config import load
 from rasa_core.policies.memoization import MemoizationPolicy
+from rasa_core.policies.keras_policy import KerasPolicy
+from rasa_core.policies.fallback import FallbackPolicy
+from rasa_core.policies.form_policy import FormPolicy
+from rasa_core.policies.ensemble import PolicyEnsemble
 
 
 @pytest.mark.parametrize("filename", glob.glob(
@@ -18,3 +22,26 @@ def test_load_config(filename):
     assert len(loaded) == 2
     assert isinstance(loaded[0], MemoizationPolicy)
     assert isinstance(loaded[1], ExamplePolicy)
+
+
+def test_ensemble_from_dict():
+    ensemble_dict = {'policies': [
+            {'epochs': 50, 'name': 'KerasPolicy', 'featurizer': [
+                {'max_history': 5, 'name': 'MaxHistoryTrackerFeaturizer',
+                 'state_featurizer': [
+                 	{'name': 'BinarySingleStateFeaturizer'}]}]},
+            {'max_history': 5, 'name': 'MemoizationPolicy'},
+            {'core_threshold': 0.7, 'name': 'FallbackPolicy',
+             'nlu_threshold': 0.7,
+             'fallback_action_name': 'action_default_fallback'},
+            {'name': 'FormPolicy'}]}
+
+    ensemble = PolicyEnsemble.from_dict(ensemble_dict)
+    assert len(ensemble) == 4
+    assert any([isinstance(p, MemoizationPolicy) for p in ensemble])
+    assert any([isinstance(p, KerasPolicy) for p in ensemble])
+    assert any([isinstance(p, FallbackPolicy) for p in ensemble])
+    assert any([isinstance(p, FormPolicy) for p in ensemble])
+
+
+
