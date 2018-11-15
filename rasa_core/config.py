82f462b8a94a680bd0fb3ebcc6f660e15a4caf47
Tom Bocklisch <tom@rasa.com>
2018-11-15 18:03:39 +0100

    removed future imports

--- rasa_core/config.py
+++ rasa_core/config.py
@@ -1,8 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from typing import Optional, Text, Dict, Any, List
 
 from rasa_core import utils
--- rasa_core/constants.py
+++ rasa_core/constants.py
@@ -1,8 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 DEFAULT_SERVER_PORT = 5005
 
 DEFAULT_SERVER_FORMAT = "http://localhost:{}"
