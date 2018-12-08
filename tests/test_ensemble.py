d82a3b2588797aa76afe6178d7d5fe904df07cff
Andrew <andrewluotechnologies@outlook.com>
2018-12-07 23:05:49 -0800

    Fixed code style in test_ensemble.py

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -3,7 +3,8 @@ import pytest
 from rasa_core.policies import Policy
 from rasa_core.policies.ensemble import PolicyEnsemble
 
-class WorkingPolicy(Policy):    
+
+class WorkingPolicy(Policy):
     @classmethod
     def load(cls, path):
         return WorkingPolicy()
@@ -20,6 +21,7 @@ class WorkingPolicy(Policy):
     def __eq__(self, other):
         return isinstance(other, WorkingPolicy)
 
+
 def test_policy_loading_simple(tmpdir):
 
     original_policy_ensemble = PolicyEnsemble([WorkingPolicy()])
@@ -28,8 +30,9 @@ def test_policy_loading_simple(tmpdir):
 
     loaded_policy_ensemble = PolicyEnsemble.load(tmpdir)
     assert original_policy_ensemble.policies == loaded_policy_ensemble.policies
-    
-class LoadReturnsNonePolicy(Policy):    
+
+
+class LoadReturnsNonePolicy(Policy):
     @classmethod
     def load(cls, path):
         return None
@@ -43,15 +46,17 @@ class LoadReturnsNonePolicy(Policy):
     def predict_action_probabilities(self, tracker, domain):
         pass
 
+
 def test_policy_loading_load_returns_none(tmpdir):
     original_policy_ensemble = PolicyEnsemble([LoadReturnsNonePolicy()])
     original_policy_ensemble.train([], None)
     original_policy_ensemble.persist(tmpdir)
-    
+
     with pytest.raises(Exception):
         PolicyEnsemble.load(tmpdir)
-        
-class LoadReturnsWrongTypePolicy(Policy):    
+
+
+class LoadReturnsWrongTypePolicy(Policy):
     @classmethod
     def load(cls, path):
         return ""
@@ -65,10 +70,11 @@ class LoadReturnsWrongTypePolicy(Policy):
     def predict_action_probabilities(self, tracker, domain):
         pass
 
+
 def test_policy_loading_load_returns_wrong_type(tmpdir):
     original_policy_ensemble = PolicyEnsemble([LoadReturnsWrongTypePolicy()])
     original_policy_ensemble.train([], None)
     original_policy_ensemble.persist(tmpdir)
-    
+
     with pytest.raises(Exception):
         PolicyEnsemble.load(tmpdir)
