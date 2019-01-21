890943957cbe8dd27122615845f71f615fc71d5c
Nikolai <kol@posteo.de>
2019-01-21 16:29:00 +0100

    test if message id is correctly read and logged

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -16,6 +16,13 @@ def test_message_processor(default_processor):
     assert {'recipient_id': 'default',
             'text': 'hey there Core!'} == out.latest_output()
 
+def test_message_id_logging(default_processor):
+    from rasa_core.trackers import DialogueStateTracker
+    message = UserMessage("If Meg was an egg would she still have a leg?")
+    tracker = DialogueStateTracker('1',[])
+    default_processor._handle_message_with_tracker(message, tracker)
+    logged_event = tracker.events[-1]
+    assert logged_event.message_id == message.message_id
 
 def test_parsing(default_processor):
     message = Message('/greet{"name": "boy"}')
