2bf1b27f8cc6297bb443c373dd5fed5798f7a3e2
Ella Rohm-Ensing <erohmensing@gmail.com>
2019-01-25 14:19:05 +0100

    fixed indentation error

--- rasa_core/agent.py
+++ rasa_core/agent.py
@@ -494,10 +494,10 @@ class Agent(object):
                             " to the policy configuration instead. More info "
                             "https://rasa.com/docs/core/migrations.html")
         if kwargs.get('epochs') or kwargs.get('max_history') or kwargs.get(
-            'batch_size'):
-                raise Exception("Passing policy configuration parameters "
-                                "to `agent.train(...)` is not supported anymore. "
-                                "Specify parameters directly in the"
+                'batch_size'):
+            raise Exception("Passing policy configuration parameters "
+                                "to `agent.train(...)` is not supported "
+                                "anymore. Specify parameters directly in the"
                                 " policy configuration instead. More info "
                                 "https://rasa.com/docs/core/migrations.html")
 
