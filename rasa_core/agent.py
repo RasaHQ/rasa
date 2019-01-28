da280716d5eaa79c51c662678409407d7ebfe0f8
Vladimir Vlasov <mr.voov@gmail.com>
2019-01-28 11:53:43 +0100

    Apply suggestions from code review
    formatting
    
    Co-Authored-By: erohmensing <erohmensing@gmail.com>

--- rasa_core/agent.py
+++ rasa_core/agent.py
@@ -493,15 +493,15 @@ class Agent(object):
         if kwargs.get('featurizer'):
             raise Exception("Passing `featurizer` "
                             "to `agent.train(...)` is not supported anymore. "
-                            "Pass appropriate featurizer directly"
-                            " to the policy configuration instead. More info "
+                            "Pass appropriate featurizer directly "
+                            "to the policy configuration instead. More info "
                             "https://rasa.com/docs/core/migrations.html")
         if kwargs.get('epochs') or kwargs.get('max_history') or kwargs.get(
                 'batch_size'):
             raise Exception("Passing policy configuration parameters "
                             "to `agent.train(...)` is not supported "
-                            "anymore. Specify parameters directly in the"
-                            " policy configuration instead. More info "
+                            "anymore. Specify parameters directly in the "
+                            "policy configuration instead. More info "
                             "https://rasa.com/docs/core/migrations.html")
 
         if isinstance(training_trackers, str):
