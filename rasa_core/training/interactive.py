8b905dd1b2203cf841dfb4e021d964c4b8665d88
paulaWesselmann <pw439@cam.ac.uk>
2019-02-19 18:00:38 +0100

    fork saving fix

--- rasa_core/training/interactive.py
+++ rasa_core/training/interactive.py
@@ -588,6 +588,17 @@ def _ask_if_quit(sender_id: Text, endpoint: EndpointConfig) -> bool:
     elif answer == "undo":
         raise UndoLastStep()
     elif answer == "fork":
+        # a fork is created and the first story line saved as if quit
+        story_path, nlu_path, domain_path = _request_export_info()
+
+        tracker = retrieve_tracker(endpoint, sender_id)
+        evts = tracker.get("events", [])
+
+        _write_stories_to_file(story_path, evts)
+        _write_nlu_to_file(nlu_path, evts)
+        _write_domain_to_file(domain_path, evts, endpoint)
+
+        logger.info("Successfully wrote stories and NLU data")
         raise ForkTracker()
     elif answer == "restart":
         raise RestartConversation()
