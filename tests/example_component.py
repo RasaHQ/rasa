0577b1565cfc1091132e2d3f9c6d61482ffbd547
Tom Bocklisch <tom@rasa.com>
2019-02-22 18:10:56 +0100

    Update tests/example_component.py
    Co-Authored-By: Ghostvv <mr.voov@gmail.com>

--- tests/example_component.py
+++ tests/example_component.py
@@ -72,7 +72,7 @@ class MyComponent(Component):
 
     @classmethod
     def load(cls,
-             meta: Dict,
+             meta: Dict[Text, Any],
              model_dir: Optional[Text] = None,
              model_metadata: Optional['Metadata'] = None,
              cached_component: Optional['Component'] = None,
