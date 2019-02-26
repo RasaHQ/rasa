2694b975d10f1e94b0f5a677bbab2688f89df08b
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-02-26 18:23:02 -0500

    keep all params in data

--- rasa_nlu/emulators/__init__.py
+++ rasa_nlu/emulators/__init__.py
@@ -7,28 +7,22 @@ class NoEmulator(object):
 
     def normalise_request_json(self, data: Dict[Text, Any]) -> Dict[Text, Any]:
 
-        _data = {}
-        _data["text"] = data["q"][0] if type(data["q"]) == list else data["q"]
-
+        _data = {"text": data["q"][0] if type(data["q"]) == list else data["q"]}
         if not data.get("project"):
             _data["project"] = "default"
         elif type(data["project"]) == list:
             _data["project"] = data["project"][0]
-            del data["project"]
         else:
             _data["project"] = data["project"]
-            del data["project"]
 
         if data.get("model"):
             if type(data["model"]) == list:
                 _data["model"] = data["model"][0]
             else:
                 _data["model"] = data["model"]
-            del data["model"]
 
         if "time" in data:
             _data['time'] = data["time"]
-            del data["time"]
 
         for key in data.keys():
             _data[key] = data[key]
