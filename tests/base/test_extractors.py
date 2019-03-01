4045d8722dbbfa0e56026e8ba0bf68add4cfb240
Maurice Doepke <mauricedoepke@outlook.de>
2019-03-01 16:17:31 +0100

    test in and exclusion of filtered dimensions within the same message

--- tests/base/test_extractors.py
+++ tests/base/test_extractors.py
@@ -268,7 +268,6 @@ def test_spacy_ner_extractor(component_builder, spacy_nlp):
         "intent": "restaurant_search",
         "entities": [],
         "spacy_doc": spacy_nlp("anywhere in the west")})
-
     ext.process(example, spacy_nlp=spacy_nlp)
 
     assert len(example.get("entities", [])) == 1
@@ -282,14 +281,21 @@ def test_spacy_ner_extractor(component_builder, spacy_nlp):
 
     # Test dimension filtering
 
-    example = Message("anywhere in the West", {
-        "intent": "restaurant_search",
+    example = Message("anywhere in the West with Sebastian Thrun", {
+        "intent": "example_intent",
         "entities": [],
-        "spacy_doc": spacy_nlp("anywhere in the west")})
+        "spacy_doc": spacy_nlp("anywhere in the West with Sebastian Thrun")})
     _config = RasaNLUModelConfig({"pipeline":
                                  [{"name": "SpacyEntityExtractor"}]})
-    _config.set_component_attr(0, dimensions=["EVENT"])
+    _config.set_component_attr(0, dimensions=["PERSON"])
     ext = component_builder.create_component(_config.for_component(0), _config)
     ext.process(example, spacy_nlp=spacy_nlp)
 
-    assert len(example.get("entities", [])) == 0
+    assert len(example.get("entities", [])) == 1
+    assert example.get("entities")[0] == {
+        'start': 26,
+        'extractor': 'SpacyEntityExtractor',
+        'end': 31,
+        'value': 'Sebastian Thrun',
+        'entity': 'PERSON',
+        'confidence': None}
