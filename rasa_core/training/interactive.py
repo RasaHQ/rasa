8e78687c64ad4ee24770cb355624324f16956fce
paulaWesselmann <p.wesselmann@rasa.com>
2019-03-04 11:55:57 +0100

    validate both intent and entities

--- rasa_core/training/interactive.py
+++ rasa_core/training/interactive.py
@@ -397,6 +397,8 @@ def _request_fork_from_user(
                                              endpoint)
 
     if fork_idx is not None:
+        # TODO: add saving of tracker up to now in stories here?
+
         return tracker.get("events", [])[:int(fork_idx)]
     else:
         return None
@@ -595,6 +597,18 @@ def _ask_if_quit(sender_id: Text, endpoint: EndpointConfig) -> bool:
     elif answer == "undo":
         raise UndoLastStep()
     elif answer == "fork":
+        # first the path taken is saved as when the user quits
+        story_path, nlu_path, domain_path = _request_export_info()
+
+        tracker = retrieve_tracker(endpoint, sender_id)
+        evts = tracker.get("events", [])
+
+        _write_stories_to_file(story_path, evts)
+        _write_nlu_to_file(nlu_path, evts)
+        _write_domain_to_file(domain_path, evts, endpoint)
+
+        logger.info("Successfully wrote stories and NLU data")
+
         raise ForkTracker()
     elif answer == "restart":
         raise RestartConversation()
@@ -997,6 +1011,13 @@ def _validate_user_text(latest_message: Dict[Text, Any],
     parse_data = latest_message.get("parse_data", {})
     text = _as_md_message(parse_data)
     intent = parse_data.get("intent", {}).get("name")
+    entities = parse_data.get("entities", [])
+    if entities:
+        entity = []
+        for e in entities:
+            entity.append('[' + e['value'] + '](' + e['entity'] + ')')
+    else:
+        entity = "'None'"
 
     if intent is None:
         print("The NLU classification for '{}' returned '{}'"
@@ -1004,9 +1025,9 @@ def _validate_user_text(latest_message: Dict[Text, Any],
         return False
     else:
         question = questionary.confirm(
-            "Is the NLU classification for '{}' with intent '{}' correct?"
-            "\n    AND are all occurring entities labeled correctly?"
-            "".format(text, intent))
+            "Is the NLU classification for '{}' with intent '{}' "
+            "and entities: {} correct?"
+            "".format(text, intent, entity))
 
         return _ask_or_abort(question, sender_id, endpoint)
 
