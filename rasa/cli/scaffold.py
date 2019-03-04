19e6858988f5411346b3081fff182669a75112e1
Tom Bocklisch <tom@rasa.com>
2019-03-04 17:40:24 +0100

    Update rasa/cli/scaffold.py
    Co-Authored-By: wochinge <mail@tobias-wochinger.de>

--- rasa/cli/scaffold.py
+++ rasa/cli/scaffold.py
@@ -26,7 +26,7 @@ def print_train_or_instructions(args: argparse.Namespace, path: Text) -> None:
         args.config = os.path.join(path, "config.yml")
         args.stories = os.path.join(path, "data", "core")
         args.domain = os.path.join(path, "domain.yml")
-        args.nlu = os.path.join(path, "data/nlu")
+        args.nlu = os.path.join(path, "data", "nlu")
         args.out = os.path.join(path, create_output_path())
 
         args.model = train.train(args)
