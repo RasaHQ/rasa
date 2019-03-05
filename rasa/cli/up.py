7cc34470ee947ffed54c006e2a4100d4a962e10e
ricwo <ric.wkr@gmail.com>
2019-03-05 23:49:35 +0100

    update models url

--- rasa/cli/up.py
+++ rasa/cli/up.py
@@ -1,10 +1,10 @@
 import argparse
 from multiprocessing import Process
-from threading import Thread
 from typing import List
 
-import rasa.cli.run
 from rasa_core.utils import EndpointConfig, print_success
+
+import rasa.cli.run
 
 
 def add_subparser(subparsers: argparse._SubParsersAction,
@@ -20,26 +20,25 @@ def add_subparser(subparsers: argparse._SubParsersAction,
 
 
 def start_core(platform_token):
-    import rasa_core
     from rasa_core.utils import AvailableEndpoints
     _endpoints = AvailableEndpoints(
         # TODO: LOCAL make endpoints more configurable, esp ports
         model=EndpointConfig("http://localhost:5002"
-                             "/api/projects/default/models/core/tags/production",
+                             "/api/projects/default/models/tags/production",
                              token=platform_token,
-                             wait_time_between_pulls=10),
+                             wait_time_between_pulls=1),
         event_broker=EndpointConfig(**{"type": "file"}),
         nlg=EndpointConfig("http://localhost:5002"
                            "/api/nlg",
                            token=platform_token))
 
-    from rasa_core.interpreter import NaturalLanguageInterpreter
     from rasa_core import broker
     _broker = broker.from_endpoint_config(_endpoints.event_broker)
 
     from rasa_core.tracker_store import TrackerStore
     _tracker_store = TrackerStore.find_tracker_store(
         None, _endpoints.tracker_store, _broker)
+
     from rasa_core.run import load_agent
     _agent = load_agent("models",
                         interpreter=None,
@@ -47,6 +46,7 @@ def start_core(platform_token):
                         endpoints=_endpoints)
     from rasa_core.run import serve_application
     print_success("About to start core")
+
     serve_application(_agent,
                       "rasa",
                       5005,
@@ -63,7 +63,6 @@ def start_event_service():
 
 def up(args: argparse.Namespace):
     from rasa_core.utils import print_success
-
     print_success("Starting Rasa Core")
     from rasa_platform import config
     p = Process(target=start_core, args=(config.platform_token,))
