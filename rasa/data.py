a9c3fbbbe60a45745edcb4451ca540d37e79d7a1
Tobias Wochinger <t.wochinger@rasa.com>
2019-03-05 17:34:10 +0100

    add missing module for joint directories and its tests

---
+++ rasa/data.py
@@ -0,0 +1,65 @@
+import io
+import json
+import os
+import shutil
+import tempfile
+from typing import Tuple, List, Text
+import re
+
+
+def get_core_directory(directory: Text) -> Text:
+    core_files, _ = _get_core_nlu_files(directory)
+    return _copy_files_to_new_dir(core_files)
+
+
+def get_nlu_directory(directory: Text) -> Text:
+    _, nlu_files = _get_core_nlu_files(directory)
+    return _copy_files_to_new_dir(nlu_files)
+
+
+def get_core_nlu_directories(directory: Text) -> Tuple[Text, Text]:
+    story_files, nlu_data_files = _get_core_nlu_files(directory)
+
+    story_directory = _copy_files_to_new_dir(story_files)
+    nlu_directory = _copy_files_to_new_dir(nlu_data_files)
+
+    return story_directory, nlu_directory
+
+
+def _get_core_nlu_files(directory: Text) -> Tuple[List[Text], List[Text]]:
+    story_files = []
+    nlu_data_files = []
+
+    for root, _, files in os.walk(directory):
+        for f in [f for f in files if f.endswith(".json") or f.endswith(".md")]:
+            full_path = os.path.join(root, f)
+            if _is_nlu_file(full_path):
+                nlu_data_files.append(full_path)
+            else:
+                story_files.append(full_path)
+
+    return story_files, nlu_data_files
+
+
+def _is_nlu_file(file_path: Text) -> bool:
+    with io.open(file_path, encoding="utf-8") as f:
+        if file_path.endswith(".json"):
+            content = f.read()
+            is_nlu_file = json.loads(content).get("rasa_nlu_data") is not None
+        else:
+            is_nlu_file = any(_contains_nlu_pattern(l) for l in f)
+    return is_nlu_file
+
+
+def _contains_nlu_pattern(text: Text) -> bool:
+    nlu_pattern = r"\s*##\s*(intent|regex||synonym|lookup):"
+
+    return re.match(nlu_pattern, text) is not None
+
+
+def _copy_files_to_new_dir(files: List[Text]) -> Text:
+    directory = tempfile.mkdtemp()
+    for f in files:
+        shutil.copy2(f, directory)
+
+    return directory
---
+++ tests/test_data.py
@@ -0,0 +1,69 @@
+import io
+import json
+import os
+import tempfile
+
+import pytest
+
+import rasa.data as data
+
+
+def test_get_core_directory(project):
+    data_dir = os.path.join(project, "data")
+    core_directory = data.get_core_directory(data_dir)
+
+    assert os.listdir(core_directory) == ["stories.md"]
+
+
+def test_get_nlu_directory(project):
+    data_dir = os.path.join(project, "data")
+    nlu_directory = data.get_nlu_directory(data_dir)
+
+    assert os.listdir(nlu_directory) == ["nlu.md"]
+
+
+def test_get_core_nlu_directories(project):
+    data_dir = os.path.join(project, "data")
+    core_directory, nlu_directory = data.get_core_nlu_directories(data_dir)
+
+    assert os.listdir(core_directory) == ["stories.md"]
+    assert os.listdir(nlu_directory) == ["nlu.md"]
+
+
+@pytest.mark.parametrize("line", [
+    "##intent:aintent",
+    "##synonym: synonym",
+    "##regex:a_regex",
+    " ##lookup:additional"])
+def test_contains_nlu_pattern(line):
+    assert data._contains_nlu_pattern(line)
+
+
+def test_is_nlu_file_with_json():
+    test = {"rasa_nlu_data": {"lookup_tables": [
+        {"name": "plates", "elements": ["beans", "rice", "tacos", "cheese"]}]}}
+
+    directory = tempfile.mkdtemp()
+    file = os.path.join(directory, "test.json")
+    with io.open(file, "w") as f:
+        f.write(json.dumps(test))
+
+    assert data._is_nlu_file(file)
+
+
+def test_is_not_nlu_file_with_json():
+    directory = tempfile.mkdtemp()
+    file = os.path.join(directory, "test.json")
+    with io.open(file, "w") as f:
+        f.write('{"test": "a"}')
+
+    assert not data._is_nlu_file(file)
+
+
+@pytest.mark.parametrize("line", [
+    "- example",
+    "## story intent 1 + two"
+    "##slots"
+    "* entry"])
+def test_not_contains_nlu_pattern(line):
+    assert not data._contains_nlu_pattern(line)
