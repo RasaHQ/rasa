1693119d50e452023b86c473891dd18b692ed319
Tom Metcalfe <t.metcalfe@rasa.com>
2019-03-13 15:08:44 +0100

    Update test_server

--- tests/base/test_server.py
+++ tests/base/test_server.py
@@ -213,7 +213,7 @@ def test_evaluate_invalid_project_error(app, rasa_default_train_data):
     rjs = yield response.json()
     assert response.code == 500, "The project cannot be found"
     assert "error" in rjs
-    assert rjs["error"] == "Project project123 could not be found"
+    assert rjs["error"] == "Project 'project123' could not be found"
 
 
 @pytest.inlineCallbacks
