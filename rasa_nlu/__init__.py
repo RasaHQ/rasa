837e26187bfa92936c264d1f7e2ef231f5fd22b8
Tom Bocklisch <tom@rasa.com>
2019-03-21 07:29:24 +0100

    added backwards compatible package

---
+++ rasa_nlu/__init__.py
@@ -0,0 +1,10 @@
+import rasa.nlu
+import sys
+import warnings
+
+# this makes sure old code can still import from `rasa_nlu`
+# although the package has been moved to `rasa.nlu`
+sys.modules['rasa_nlu'] = rasa.nlu
+
+warnings.warn("The 'rasa_nlu' package hase been renamed. You should change "
+              "your imports to use 'rasa.nlu' instead.", UserWarning)
