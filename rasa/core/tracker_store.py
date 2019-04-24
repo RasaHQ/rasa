f97fee545ec1167aae93fb89907df976a68f3bea
Tobias Wochinger <t.wochinger@rasa.com>
2019-04-24 13:01:43 +0200

    black formatting

--- rasa/core/tracker_store.py
+++ rasa/core/tracker_store.py
@@ -310,8 +310,7 @@ class SQLTrackerStore(TrackerStore):
         super(SQLTrackerStore, self).__init__(domain, event_broker)
 
     def keys(self) -> List[Text]:
-        sender_ids = self.session.query(
-            self.SQLEvent.sender_id).distinct().all()
+        sender_ids = self.session.query(self.SQLEvent.sender_id).distinct().all()
         return [sender_id for (sender_id,) in sender_ids]
 
     def retrieve(self, sender_id: Text) -> DialogueStateTracker:
