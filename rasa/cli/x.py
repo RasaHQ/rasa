5b9523cbaab22cb64ab6c57b12c6ac8ea4e4b666
Tom Bocklisch <tom@rasa.com>
2019-04-25 08:05:54 +0200

    added metrics consent and global config

--- rasa/cli/x.py
+++ rasa/cli/x.py
@@ -1,13 +1,25 @@
 import argparse
+import datetime
 import importlib.util
 import logging
+import os
 import signal
 import sys
 from multiprocessing import get_context
-from typing import List, Text
+from typing import List, Text, Dict, Any
+
+import rasa.utils.io
+import questionary
 
 import rasa.cli.run
+import rasa.core.utils
+from rasa.cli.utils import print_success, get_validated_path
 
+from rasa.constants import (
+    GLOBAL_USER_CONFIG_PATH, DEFAULT_ENDPOINTS_PATH,
+    DEFAULT_CREDENTIALS_PATH)
+
+logger = logging.getLogger(__name__)
 
 # noinspection PyProtectedMember
 def add_subparser(
@@ -15,7 +27,7 @@ def add_subparser(
 ):
     from rasa.core import cli
 
-    up_parser_args = {
+    x_parser_args = {
         "parents": parents,
         "conflict_handler": "resolve",
         "formatter_class": argparse.ArgumentDefaultsHelpFormatter,
@@ -23,9 +35,15 @@ def add_subparser(
 
     if is_rasa_x_installed():
         # we'll only show the help msg for the command if Rasa X is actually installed
-        up_parser_args["help"] = "Start Rasa X and the Interface"
+        x_parser_args["help"] = "Start Rasa X and the Interface"
 
-    shell_parser = subparsers.add_parser("up", **up_parser_args)
+    shell_parser = subparsers.add_parser("x", **x_parser_args)
+
+    shell_parser.add_argument(
+        "--no_prompt", "--no-prompt",
+        action="store_true",
+        help="Automatic yes or default options to prompts and oppressed warnings",
+    )
 
     shell_parser.add_argument(
         "--production",
@@ -67,7 +85,7 @@ def add_subparser(
 
     rasa.cli.run.add_run_arguments(shell_parser)
 
-    shell_parser.set_defaults(func=up)
+    shell_parser.set_defaults(func=rasa_x)
 
     cli.arguments.add_logging_option_arguments(shell_parser)
 
@@ -88,16 +106,70 @@ def start_event_service():
     p.start()
 
 
+def _write_config(global_config: Dict[Text, Any]) -> None:
+    """Read global Rasa configuration."""
+
+    os.makedirs(os.path.dirname(GLOBAL_USER_CONFIG_PATH), exist_ok=True)
+
+    rasa.core.utils.dump_obj_as_yaml_to_file(GLOBAL_USER_CONFIG_PATH, global_config)
+
+
+def _read_config() -> Dict[Text, Any]:
+    """Read global Rasa configuration."""
+
+    if not os.path.exists(GLOBAL_USER_CONFIG_PATH):
+        return {}
+
+    # noinspection PyBroadException
+    try:
+        return rasa.utils.io.read_yaml_file(GLOBAL_USER_CONFIG_PATH)
+    except Exception:
+        # if things go south we pretend there is no config
+        return {}
+
+
+def is_metrics_collection_enabled(args: argparse.Namespace) -> bool:
+    """Make sure the user consents to any metrics collection."""
+
+    global_config = _read_config()
+
+    if "metrics" in global_config:
+        return global_config["metrics"].get("enabled", False)
+
+    allow_metrics = questionary.confirm(
+        "Rasa will track a minimal amount of anonymized usage information "
+        "(like how often you use the 'train' button) to help us improve Rasa X. "
+        "None of your training data or conversations will ever be sent to Rasa. "
+        "Are you OK with Rasa collecting anonymized usage data?"
+    ).skip_if(args.no_prompt, default=True).ask()
+
+    print_success("Your decision has been stored into {}. "
+                  "".format(GLOBAL_USER_CONFIG_PATH))
+
+    if not args.no_prompt:
+        date = datetime.datetime.now()
+        global_config["metrics"] = {"enabled": allow_metrics, "date": date}
+        _write_config(global_config)
+
+    return allow_metrics
+
+
 def _core_service(args: argparse.Namespace, endpoints: "AvailableEndpoints" = None):
     """Starts the Rasa Core application."""
     from rasa.core.run import serve_application
     from rasa.nlu.utils import configure_colored_logging
 
     configure_colored_logging(args.loglevel)
-    logging.getLogger("rasa.core.agent").setLevel(logging.ERROR)
     logging.getLogger("apscheduler.executors.default").setLevel(logging.WARNING)
 
+    args.credentials = get_validated_path(
+        args.credentials, "credentials", DEFAULT_CREDENTIALS_PATH, True
+    )
+
     if endpoints is None:
+        args.endpoints = get_validated_path(
+            args.endpoints, "endpoints", DEFAULT_ENDPOINTS_PATH, True
+        )
         from rasa.core.utils import AvailableEndpoints
 
         endpoints = AvailableEndpoints.read_endpoints(args.endpoints)
@@ -157,7 +229,7 @@ def is_rasa_x_installed():
 def generate_rasa_x_token(length=16):
     """Generate a hexadecimal secret token used to access the Rasa X API.
 
-    A new token is generated on every `rasa up` command.
+    A new token is generated on every `rasa x` command.
     """
 
     from secrets import token_hex
@@ -165,7 +237,7 @@ def generate_rasa_x_token(length=16):
     return token_hex(length)
 
 
-def up(args: argparse.Namespace):
+def rasa_x(args: argparse.Namespace):
     from rasa.cli.utils import print_success, print_error, signal_handler
     from rasa.core.utils import configure_file_logging
     from rasa.utils.io import configure_colored_logging
@@ -177,16 +249,16 @@ def up(args: argparse.Namespace):
     logging.getLogger("socketio").setLevel(logging.ERROR)
 
     if not args.loglevel == logging.DEBUG:
-        logging.getLogger().setLevel(logging.ERROR)
+        logging.getLogger().setLevel(logging.WARNING)
         logging.getLogger("py.warnings").setLevel(logging.ERROR)
         logging.getLogger("apscheduler").setLevel(logging.ERROR)
-        logging.getLogger("rasa.cli").setLevel(logging.ERROR)
-        logging.getLogger("rasa.nlu").setLevel(logging.ERROR)
-        logging.getLogger("rasa.core").setLevel(logging.ERROR)
+        logging.getLogger("rasa").setLevel(logging.WARNING)
         logging.getLogger("sanic.root").setLevel(logging.ERROR)
 
     configure_colored_logging(args.loglevel)
     configure_file_logging(args.loglevel, args.log_file)
+
+    metrics = is_metrics_collection_enabled(args)
 
     if args.production:
         print_success("Starting Rasa X in production mode... 🚀")
@@ -195,7 +267,7 @@ def up(args: argparse.Namespace):
         print_success("Starting Rasa X in local mode... 🚀")
         if not is_rasa_x_installed():
             print_error(
-                "Rasa X is not installed. The `rasa up` "
+                "Rasa X is not installed. The `rasa x` "
                 "command requires an installation of Rasa X."
             )
             sys.exit(1)
@@ -209,4 +281,6 @@ def up(args: argparse.Namespace):
 
         start_core_for_local_platform(args, rasa_x_token=rasa_x_token)
 
-        main_local(args.project_path, args.data_path, token=rasa_x_token)
+        main_local(args.project_path, args.data_path,
+                   token=rasa_x_token,
+                   metrics=metrics)
