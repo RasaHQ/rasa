7f45ae346fd7dd94870f375793512c62e9e93b06
paulaWesselmann <p.wesselmann@rasa.com>
2019-05-02 10:15:55 +0200

    New RasaChat token check URL

--- rasa/core/channels/rasa_chat.py
+++ rasa/core/channels/rasa_chat.py
@@ -26,7 +26,7 @@ class RasaChatInput(RestInput):
         self.base_url = url
 
     async def _check_token(self, token):
-        url = "{}/user".format(self.base_url)
+        url = "{}/validate".format(self.base_url)
         headers = {"Authorization": token}
         logger.debug("Requesting user information from auth server {}.".format(url))
 
