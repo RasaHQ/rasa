30cdfe20d20a8dc216a8ca6de533cb178336e7ef
Tanja Bergmann <tabergma@gmail.com>
2019-05-09 16:09:46 +0200

    Add rasa shell nlu subparser.

--- rasa/cli/shell.py
+++ rasa/cli/shell.py
@@ -4,7 +4,9 @@ import os
 
 from typing import List
 
-from rasa.cli.utils import print_error, print_success
+from rasa.cli.default_arguments import add_model_param
+
+from rasa.cli.utils import print_error
 
 import rasa.cli.run
 import rasa.cli.arguments.arguments
@@ -28,9 +30,51 @@ def add_subparser(
     )
     rasa.cli.run.add_run_arguments(shell_parser)
 
+    run_subparsers = shell_parser.add_subparsers()
+
+    shell_nlu_subparser = run_subparsers.add_parser(
+        "nlu",
+        parents=parents,
+        conflict_handler="resolve",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        help="Interpret messages on the command line using your NLU model.",
+    )
+    shell_nlu_subparser.set_defaults(func=shell_nlu)
+    add_model_param(shell_nlu_subparser)
+
     rasa.cli.arguments.arguments.add_logging_option_arguments(shell_parser)
+    rasa.cli.arguments.arguments.add_logging_option_arguments(shell_nlu_subparser)
 
     shell_parser.set_defaults(func=shell)
+
+
+def shell_nlu(args: argparse.Namespace):
+    from rasa.cli.utils import get_validated_path
+    from rasa.constants import DEFAULT_MODELS_PATH
+    from rasa.model import get_model, get_model_subdirectories
+    import rasa.nlu.run
+
+    args.connector = "cmdline"
+
+    model = get_validated_path(args.model, "model", DEFAULT_MODELS_PATH)
+    model_path = get_model(model)
+    if not model_path:
+        print_error(
+            "No model found. Train a model before running the "
+            "server using `rasa train nlu`."
+        )
+        return
+
+    _, nlu_model = get_model_subdirectories(model_path)
+
+    if not os.path.exists(nlu_model):
+        print_error(
+            "No NLU model found. Train a model before running the "
+            "server using `rasa train nlu`."
+        )
+        return
+
+    rasa.nlu.run.run_cmdline(nlu_model)
 
 
 def shell(args: argparse.Namespace):
@@ -53,10 +97,6 @@ def shell(args: argparse.Namespace):
 
     if not os.path.exists(core_model):
         import rasa.nlu.run
-
-        print_success(
-            "NLU interpreter loaded. Type a message to obtain its intent and entities."
-        )
 
         rasa.nlu.run.run_cmdline(nlu_model)
     else:
