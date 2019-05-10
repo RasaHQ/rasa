340181960d23b2bcabe5d88e05ee3a733a7fc08f
Tanja Bergmann <tabergma@gmail.com>
2019-05-10 12:36:02 +0200

    Refactor data arguments.

---
+++ rasa/cli/arguments/data.py
@@ -0,0 +1,46 @@
+import argparse
+
+from rasa.cli.arguments.default_arguments import add_nlu_data_param
+
+
+def add_convert_arguments(parser):
+    parser.add_argument(
+        "-d", "--data_file", required=True, help="file or dir containing training data"
+    )
+
+    parser.add_argument(
+        "-o",
+        "--out_file",
+        required=True,
+        help="file where to save training data in rasa format",
+    )
+
+    parser.add_argument("-l", "--language", default="en", help="language of the data")
+
+    parser.add_argument(
+        "-f",
+        "--format",
+        required=True,
+        choices=["json", "md"],
+        help="Output format the training data should be converted into.",
+    )
+    return parser
+
+
+def add_split_arguments(parser: argparse.ArgumentParser) -> None:
+    add_nlu_data_param(parser)
+
+    parser.add_argument(
+        "--training-fraction",
+        type=float,
+        default=0.8,
+        help="Percentage of the data which should be the training data",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--out",
+        type=str,
+        default="train_test_split",
+        help="Directory where the split files should be stored",
+    )
--- rasa/cli/data.py
+++ rasa/cli/data.py
@@ -2,8 +2,7 @@ import argparse
 from typing import List
 
 from rasa import data
-from rasa.cli.arguments import convert as arguments
-from rasa.cli.arguments.default_arguments import add_nlu_data_param
+from rasa.cli.arguments import data as arguments
 from rasa.cli.utils import get_validated_path
 from rasa.constants import DEFAULT_DATA_PATH
 
@@ -22,8 +21,8 @@ def add_subparser(
         help="Utils for the Rasa training files",
     )
     data_parser.set_defaults(func=lambda _: data_parser.print_help(None))
-    data_subparsers = data_parser.add_subparsers()
 
+    data_subparsers = data_parser.add_subparsers()
     convert_parser = data_subparsers.add_parser(
         "convert",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -31,17 +30,17 @@ def add_subparser(
         help="Convert Rasa data between different formats",
     )
     convert_parser.set_defaults(func=lambda _: convert_parser.print_help(None))
-    convert_subparsers = convert_parser.add_subparsers()
 
+    convert_subparsers = convert_parser.add_subparsers()
     convert_nlu_parser = convert_subparsers.add_parser(
         "nlu",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         parents=parents,
         help="Convert NLU training data between markdown and json",
     )
-
-    arguments.add_arguments(convert_nlu_parser)
     convert_nlu_parser.set_defaults(func=convert.main)
+
+    arguments.add_convert_arguments(convert_nlu_parser)
 
     split_parser = data_subparsers.add_parser(
         "split",
@@ -50,8 +49,8 @@ def add_subparser(
         help="Split Rasa data in training and test data",
     )
     split_parser.set_defaults(func=lambda _: split_parser.print_help(None))
-    split_subparsers = split_parser.add_subparsers()
 
+    split_subparsers = split_parser.add_subparsers()
     nlu_split_parser = split_subparsers.add_parser(
         "nlu",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -59,24 +58,7 @@ def add_subparser(
         "percentages",
     )
     nlu_split_parser.set_defaults(func=split_nlu_data)
-    _add_split_args(nlu_split_parser)
-
-
-def _add_split_args(parser: argparse.ArgumentParser) -> None:
-    add_nlu_data_param(parser)
-    parser.add_argument(
-        "--training-fraction",
-        type=float,
-        default=0.8,
-        help="Percentage of the data which should be the training data",
-    )
-    parser.add_argument(
-        "-o",
-        "--out",
-        type=str,
-        default="train_test_split",
-        help="Directory where the split files should be stored",
-    )
+    arguments.add_split_arguments(nlu_split_parser)
 
 
 def split_nlu_data(args):
