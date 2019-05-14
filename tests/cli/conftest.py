4917de7459d40b185d8041f5dd15b4f268d6944d
Tanja Bergmann <tabergma@gmail.com>
2019-05-14 14:32:49 +0200

    Add test for rasa data.

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 # we reuse a bit of pytest's own testing machinery, this should eventually come
 # from a separatedly installable pytest-cli plugin.
@@ -7,6 +8,18 @@ pytest_plugins = ["pytester"]
 
 @pytest.fixture
 def run(testdir):
+    def do_run(*args):
+        args = ["rasa"] + list(args)
+        return testdir.run(*args), testdir.tmpdir
+
+    return do_run
+
+
+@pytest.fixture
+def run_in_default_project(testdir):
+    os.environ["LOG_LEVEL"] = "ERROR"
+    testdir.run("rasa", "init", "--no-prompt")
+
     def do_run(*args):
         args = ["rasa"] + list(args)
         return testdir.run(*args), testdir.tmpdir
--- tests/cli/test_rasa_data.py
+++ tests/cli/test_rasa_data.py
@@ -1,3 +1,36 @@
+import os
+
+
+def test_data_split_nlu(run_in_default_project):
+
+    result, temp_dir = run_in_default_project(
+        "data", "split", "nlu", "--nlu", "data/nlu.md", "--training-fraction", "0.75"
+    )
+
+    assert os.path.exists(os.path.join(temp_dir, "train_test_split"))
+    assert os.path.exists(os.path.join(temp_dir, "train_test_split", "test_data.md"))
+    assert os.path.exists(
+        os.path.join(temp_dir, "train_test_split", "training_data.md")
+    )
+
+
+def test_data_convert_nlu(run_in_default_project):
+
+    result, temp_dir = run_in_default_project(
+        "data",
+        "convert",
+        "nlu",
+        "--data-file",
+        "data/nlu.md",
+        "--out-file",
+        "out_nlu_data.json",
+        "-f",
+        "json",
+    )
+
+    assert os.path.exists(os.path.join(temp_dir, "out_nlu_data.json"))
+
+
 def test_data_split_help(run):
     help, _ = run("data", "split", "nlu", "--help")
 
