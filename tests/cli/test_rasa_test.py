36d7c385485f6e47bdeed32c19ddaed89a54543e
Tanja Bergmann <tabergma@gmail.com>
2019-05-15 13:33:58 +0200

    Update tests.

--- tests/cli/test_rasa_test.py
+++ tests/cli/test_rasa_test.py
@@ -16,12 +16,13 @@ def test_test_core(run_in_default_project):
 def test_test_help(run):
     output = run("test", "--help")
 
-    help_text = """usage: rasa test [-h] [-v] [-vv] [--quiet] [-m MODEL]
-                 [--max-stories MAX_STORIES] [--core CORE] [--output OUTPUT]
-                 [--e2e] [--endpoints ENDPOINTS] [--fail-on-prediction-errors]
-                 [-s STORIES] [--url URL] [-u NLU] [-c CONFIG] [-f FOLDS]
-                 [--report [REPORT]] [--successes [SUCCESSES]]
-                 [--errors ERRORS] [--histogram HISTOGRAM] [--confmat CONFMAT]
+    help_text = """usage: rasa test [-h] [-v] [-vv] [--quiet] [-m MODEL] [-s STORIES]
+                 [--max-stories MAX_STORIES] [--output OUTPUT] [--e2e]
+                 [--endpoints ENDPOINTS] [--fail-on-prediction-errors]
+                 [--url URL] [-u NLU] [--report [REPORT]]
+                 [--successes [SUCCESSES]] [--errors ERRORS]
+                 [--histogram HISTOGRAM] [--confmat CONFMAT] [-c CONFIG]
+                 [-f FOLDS]
                  {core,nlu} ..."""
 
     lines = help_text.split("\n")
@@ -33,10 +34,10 @@ def test_test_help(run):
 def test_test_nlu_help(run):
     output = run("test", "nlu", "--help")
 
-    help_text = """usage: rasa test nlu [-h] [-v] [-vv] [--quiet] [-m MODEL] [-u NLU] [-c CONFIG]
-                     [-f FOLDS] [--report [REPORT]] [--successes [SUCCESSES]]
+    help_text = """usage: rasa test nlu [-h] [-v] [-vv] [--quiet] [-m MODEL] [-u NLU]
+                     [--report [REPORT]] [--successes [SUCCESSES]]
                      [--errors ERRORS] [--histogram HISTOGRAM]
-                     [--confmat CONFMAT]"""
+                     [--confmat CONFMAT] [-c CONFIG] [-f FOLDS]"""
 
     lines = help_text.split("\n")
 
@@ -47,10 +48,10 @@ def test_test_nlu_help(run):
 def test_test_core_help(run):
     output = run("test", "core", "--help")
 
-    help_text = """usage: rasa test core [-h] [-v] [-vv] [--quiet] [--max-stories MAX_STORIES]
-                      [--core CORE] [-u NLU] [--output OUTPUT] [--e2e]
-                      [--endpoints ENDPOINTS] [--fail-on-prediction-errors]
-                      [-s STORIES] [--url URL] [-m MODEL [MODEL ...]]"""
+    help_text = """usage: rasa test core [-h] [-v] [-vv] [--quiet] [-m MODEL [MODEL ...]]
+                      [-s STORIES] [--max-stories MAX_STORIES]
+                      [--output OUTPUT] [--e2e] [--endpoints ENDPOINTS]
+                      [--fail-on-prediction-errors] [--url URL]"""
 
     lines = help_text.split("\n")
 
