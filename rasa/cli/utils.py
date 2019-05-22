e5096dec3a0240d25c1d1af8478a29f4318ae47f
Tom Metcalfe <t.metcalfe@rasa.com>
2019-05-22 17:30:31 +0200

    update FAIL

--- rasa/cli/utils.py
+++ rasa/cli/utils.py
@@ -157,8 +157,7 @@ class bcolors(object):
     OKBLUE = "\033[94m"
     OKGREEN = "\033[92m"
     WARNING = "\033[93m"
-    FAIL = "\033[" \
-           ""
+    FAIL = "\033[91m"
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
