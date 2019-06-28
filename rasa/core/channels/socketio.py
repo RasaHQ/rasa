0736924bf0c558daad924c81b63291fb85c80612
mechko <mechko@mk0.eu>
2019-06-28 12:58:16 +0200

    #3863 added attachment handling to socketio channel

--- rasa/core/channels/socketio.py
+++ rasa/core/channels/socketio.py
@@ -98,6 +98,12 @@ class SocketIOOutput(OutputChannel):
 
         await self.sio.emit(self.bot_message_evt, **json_message)
 
+    async def send_attachment(
+        self, recipient_id: Text, attachment: Iterable[Dict[Text, Any]], **kwargs: Any
+    ) -> None:
+        """Sends attachment to the output."""
+        await self._send_message(self.sid, {"attachment": attachment})
+
 
 class SocketIOInput(InputChannel):
     """A socket.io input channel."""
