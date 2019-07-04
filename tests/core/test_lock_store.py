76c8fe0465e99155796e55f26bbc51ab14503e94
ricwo <ric.wkr@gmail.com>
2019-07-04 23:29:44 +0200

    improve comments in test

--- tests/core/test_lock_store.py
+++ tests/core/test_lock_store.py
@@ -136,14 +136,17 @@ async def test_message_order(tmpdir_factory: TempdirFactory, default_agent: Agen
     start_time = time.time()
     n_messages = 10
     lock_wait = 0.1
-    # let's write the incoming order and the order of results temp files
+
+    # let's write the incoming order of messages and the order of results to temp files
     temp_path = tmpdir_factory.mktemp("message_order")
     results_file = temp_path / "results_file.txt"
     incoming_order_file = temp_path / "incoming_order_file.txt"
 
-    # we need to mock `Agent.handle_message()` so we can introduce an
-    # artificial holdup (`wait`)
-    # We'll also return the message text straight away instead
+    # We need to mock `Agent.handle_message()` so we can introduce an
+    # artificial holdup (`wait`). In the mocked method, we'll
+    # record messages as they come and and as they're processed in files so we
+    # can check the order later on. We don't need the return value of this method so
+    # we'll just return None.
     async def mocked_handle_message(
         self, message: UserMessage, wait: Union[int, float]
     ) -> None:
@@ -196,5 +199,4 @@ async def test_message_order(tmpdir_factory: TempdirFactory, default_agent: Agen
         # the sum of all wait times and (n_messages - 1) * `lock_wait`
         time_limit = np.sum(wait_times[1:])
         time_limit += (n_messages - 1) * lock_wait
-
         assert time.time() - start_time < time_limit
