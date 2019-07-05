f46763a87c3d63904be5b0b9166ed99d63bfc5f2
ricwo <ric.wkr@gmail.com>
2019-07-05 19:41:34 +0200

    increase sleep in test

--- tests/core/test_lock_store.py
+++ tests/core/test_lock_store.py
@@ -248,7 +248,7 @@ async def test_lock_error(default_agent: Agent):
             await asyncio.gather(*(asyncio.ensure_future(t) for t in tasks))
 
         # wait for lock to expire and cancel pending tasks
-        await asyncio.sleep(0.03)
+        await asyncio.sleep(0.1)
 
         for task in asyncio.Task.all_tasks():
             task.cancel()
