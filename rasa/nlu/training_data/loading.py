b6122f7947440ba1632db4e3be163f88dbe58eb6
Tobias Wochinger <t.wochinger@rasa.com>
2019-07-09 18:13:53 +0200

    re-add _guess_format(...) to not brake compatibility with Rasa X

--- rasa/nlu/training_data/loading.py
+++ rasa/nlu/training_data/loading.py
@@ -153,3 +153,11 @@ def guess_format(filename: Text) -> Text:
                 break
 
     return guess
+
+
+def _guess_format(filename: Text) -> Text:
+    logging.warning(
+        "Using '_guess_format' is deprecated since Rasa 1.1.5. "
+        "Please us 'guess_format' instead."
+    )
+    return guess_format(filename)
