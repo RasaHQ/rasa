a970d7dc3523c6224c77466990447ff844b6bd89
sibish <sibbsnb@gmail.com>
2019-07-15 20:36:20 -0700

    Add Tests for WhitespaceTokenizer:
    case_sensitive option to WhitespaceTokenizer #3827
    https://github.com/RasaHQ/rasa/issues/3827

--- tests/nlu/base/test_tokenizers.py
+++ tests/nlu/base/test_tokenizers.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from unittest.mock import patch
-
+from rasa.nlu.training_data import TrainingData, Message
+from tests.nlu import utilities
+import copy
 
 def test_whitespace():
     from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
@@ -43,35 +45,119 @@ def test_whitespace():
     ]
 
     assert [
-        t.offset for t in tk.tokenize("привет! 10.000, ńöñàśçií. (how're you?)")
-    ] == [0, 8, 16, 27, 31, 34]
+               t.offset for t in tk.tokenize("привет! 10.000, ńöñàśçií. (how're you?)")
+           ] == [0, 8, 16, 27, 31, 34]
 
     # urls are single token
     assert [
-        t.text
-        for t in tk.tokenize(
+               t.text
+               for t in tk.tokenize(
             "https://www.google.com/search?client="
             "safari&rls=en&q="
             "i+like+rasa&ie=UTF-8&oe=UTF-8 "
             "https://rasa.com/docs/nlu/"
             "components/#tokenizer-whitespace"
         )
-    ] == [
-        "https://www.google.com/search?"
-        "client=safari&rls=en&q=i+like+rasa&ie=UTF-8&oe=UTF-8",
-        "https://rasa.com/docs/nlu/components/#tokenizer-whitespace",
-    ]
+           ] == [
+               "https://www.google.com/search?"
+               "client=safari&rls=en&q=i+like+rasa&ie=UTF-8&oe=UTF-8",
+               "https://rasa.com/docs/nlu/components/#tokenizer-whitespace",
+           ]
 
     assert [
-        t.offset
-        for t in tk.tokenize(
+               t.offset
+               for t in tk.tokenize(
             "https://www.google.com/search?client="
             "safari&rls=en&q="
             "i+like+rasa&ie=UTF-8&oe=UTF-8 "
             "https://rasa.com/docs/nlu/"
             "components/#tokenizer-whitespace"
         )
-    ] == [0, 83]
+           ] == [0, 83]
+
+
+def test_whitespace_with_case():
+    print("hello")
+    from rasa.nlu.tokenizers.whitespace_tokenizer import WhitespaceTokenizer
+
+    component_config = {"case_sensitive": False}
+    tk = WhitespaceTokenizer(component_config)
+    message = Message("Forecast for LUNCH")
+    tk.process(message)
+    assert message.text == "forecast for lunch"
+
+    component_config = {"case_sensitive": True}
+    tk = WhitespaceTokenizer(component_config)
+    message = Message("Forecast for LUNCH")
+    tk.process(message)
+    assert message.text == "Forecast for LUNCH"
+
+    component_config = {}
+    tk = WhitespaceTokenizer(component_config)
+    message = Message("Forecast for LUNCH")
+    tk.process(message)
+    assert message.text == "Forecast for LUNCH"
+
+    _config = utilities.base_test_conf("supervised_embeddings")
+    examples = [
+        Message(
+            "Any Mexican restaurant will do",
+            {
+                "intent": "restaurant_search",
+                "entities": [
+                    {"start": 4, "end": 11, "value": "Mexican", "entity": "cuisine"}
+                ],
+            },
+        ),
+        Message(
+            "I want Tacos!",
+            {
+                "intent": "restaurant_search",
+                "entities": [
+                    {"start": 7, "end": 12, "value": "Mexican", "entity": "cuisine"}
+                ],
+            },
+        ),
+    ]
+
+    examples_1 = copy.deepcopy(examples)
+    examples_2 = copy.deepcopy(examples)
+
+    component_config = {"case_sensitive": False}
+    tk = WhitespaceTokenizer(component_config)
+    tk.train(TrainingData(training_examples=examples), _config)
+    assert examples[0].data.get("tokens")[0].text == "any"
+    assert examples[0].data.get("tokens")[1].text == "mexican"
+    assert examples[0].data.get("tokens")[2].text == "restaurant"
+    assert examples[0].data.get("tokens")[3].text == "will"
+    assert examples[0].data.get("tokens")[4].text == "do"
+    assert examples[1].data.get("tokens")[0].text == "i"
+    assert examples[1].data.get("tokens")[1].text == "want"
+    assert examples[1].data.get("tokens")[2].text == "tacos"
+
+    component_config = {"case_sensitive": True}
+    tk = WhitespaceTokenizer(component_config)
+    tk.train(TrainingData(training_examples=examples_1), _config)
+    assert examples_1[0].data.get("tokens")[0].text == "Any"
+    assert examples_1[0].data.get("tokens")[1].text == "Mexican"
+    assert examples_1[0].data.get("tokens")[2].text == "restaurant"
+    assert examples_1[0].data.get("tokens")[3].text == "will"
+    assert examples_1[0].data.get("tokens")[4].text == "do"
+    assert examples_1[1].data.get("tokens")[0].text == "I"
+    assert examples_1[1].data.get("tokens")[1].text == "want"
+    assert examples_1[1].data.get("tokens")[2].text == "Tacos"
+
+    component_config = {}
+    tk = WhitespaceTokenizer(component_config)
+    tk.train(TrainingData(training_examples=examples_2), _config)
+    assert examples_2[0].data.get("tokens")[0].text == "Any"
+    assert examples_2[0].data.get("tokens")[1].text == "Mexican"
+    assert examples_2[0].data.get("tokens")[2].text == "restaurant"
+    assert examples_2[0].data.get("tokens")[3].text == "will"
+    assert examples_2[0].data.get("tokens")[4].text == "do"
+    assert examples_2[1].data.get("tokens")[0].text == "I"
+    assert examples_2[1].data.get("tokens")[1].text == "want"
+    assert examples_2[1].data.get("tokens")[2].text == "Tacos"
 
 
 def test_spacy(spacy_nlp):
@@ -142,9 +228,10 @@ def test_jieba_load_dictionary(tmpdir_factory):
     component_config = {"dictionary_path": dictionary_path}
 
     with patch.object(
-        JiebaTokenizer, "load_custom_dictionary", return_value=None
+            JiebaTokenizer, "load_custom_dictionary", return_value=None
     ) as mock_method:
         tk = JiebaTokenizer(component_config)
         tk.tokenize("")
 
     mock_method.assert_called_once_with(dictionary_path)
+
