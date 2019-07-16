a22237993499ba4a62469d4c9e4fbf12156b4c42
Federico Tedin <federicotedin@gmail.com>
2019-07-16 10:05:39 +0200

    Always return after starting server in cli/run.py.

--- rasa/cli/run.py
+++ rasa/cli/run.py
@@ -98,12 +98,14 @@ def run(args: argparse.Namespace):
     # start server if remote storage is configured
     if args.remote_storage is not None:
         rasa.run(**vars(args))
+        return
 
     # start server if model server is configured
     endpoints = AvailableEndpoints.read_endpoints(args.endpoints)
     model_server = endpoints.model if endpoints and endpoints.model else None
     if model_server is not None:
         rasa.run(**vars(args))
+        return
 
     # start server if local model found
     args.model = _validate_model_path(args.model, "model", DEFAULT_MODELS_PATH)
@@ -115,6 +117,7 @@ def run(args: argparse.Namespace):
 
     if local_model_set:
         rasa.run(**vars(args))
+        return
 
     print_error(
         "No model found. You have three options to provide a model:\n"
