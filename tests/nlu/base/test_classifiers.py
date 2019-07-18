d72dbf71b675ffea8972888d7e8056ce4007321a
Nikolai <n.zaki@rasa.com>
2019-07-18 11:21:49 +0200

    created files that will hold tests and data for keyword classifier

---
+++ tests/nlu/base/test_classifiers.py
@@ -0,0 +1,13 @@
+import pytest
+
+from rasa.nlu.classifiers import KeywordIntentClassifier
+
+class ClassifierTestCollection():
+    """Tests every classifier needs to filfill.
+
+    Each classifier can have additional tests in its own class."""
+
+    def
+
+def test_keyword_classifier():
+    NotImplemented
