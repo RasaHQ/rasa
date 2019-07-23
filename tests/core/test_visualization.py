d31c926b669ee4a8144ae7ee960ebda1790bd439
Samuel Gaus <sam@gaus.co.uk>
2019-07-23 09:23:12 +0100

    Assert correct label in graph

--- tests/core/test_visualization.py
+++ tests/core/test_visualization.py
@@ -97,6 +97,6 @@ async def test_graph_persistence(default_domain, tmpdir):
     with open(out_file, "r") as graph_file:
         content = graph_file.read()
 
-    assert r'label="/change_bank_details\{\"address\":\ \"123 Fake Street\"}"' in generated_graph
+    assert r'label="/greet\{\"cuisine\":\ \"italian\"\}"' in generated_graph
     assert "isClient = true" in content
     assert "graph = `{}`".format(generated_graph) in content
