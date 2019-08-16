cd8d03c4752183a18c7f44133bac7bdcf51916ef
Tanja Bergmann <tabergma@gmail.com>
2019-08-16 13:43:26 +0200

    update knowledge base bot example

--- examples/knowledgebasebot/actions.py
+++ examples/knowledgebasebot/actions.py
@@ -5,7 +5,7 @@ from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
 class ActionMyKB(ActionQueryKnowledgeBase):
     def __init__(self):
         # load knowledge base with data from the given file
-        knowledge_base = InMemoryKnowledgeBase.load("knowledge_base_data.json")
+        knowledge_base = InMemoryKnowledgeBase("knowledge_base_data.json")
 
         # overwrite the representation function of the hotel object
         # by default the representation function is just the name of the object
