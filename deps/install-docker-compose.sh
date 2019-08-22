0997ab5dcc5b021f17bad5a77f31d56a31f74516
Bartek Minch <bartosz.minch@techmo.pl>
2019-08-22 12:44:38 +0200

    {rasa} Remove gitlab files

--- deps/install-docker-compose.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
