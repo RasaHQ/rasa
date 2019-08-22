848dfa96f465843646bca57d0f434ce428778706
Vova Vv <mr.voov@gmail.com>
2019-08-22 13:37:10 +0200

    add test for printing form stories

--- tests/core/test_policies.py
+++ tests/core/test_policies.py
@@ -17,7 +17,7 @@ from rasa.core.actions.action import (
 )
 from rasa.core.constants import USER_INTENT_RESTART, USER_INTENT_BACK
 from rasa.core.channels.channel import UserMessage
-from rasa.core.domain import Domain, InvalidDomain
+from rasa.core.domain import Domain
 from rasa.core.events import ActionExecuted
 from rasa.core.featurizers import (
     BinarySingleStateFeaturizer,
