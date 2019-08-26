1e603d72c002276188fbfd3b30c07e71d8c15fc1
Tom Bocklisch <tom@rasa.com>
2019-08-26 11:58:15 +0200

    implemented SSL server support #4322

--- rasa/core/run.py
+++ rasa/core/run.py
@@ -74,6 +74,22 @@ def _create_app_without_api(cors: Optional[Union[Text, List[Text]]] = None):
     return app
 
 
+def _create_ssl_context(ssl_certificate: Optional[Text],
+                        ssl_keyfile: Optional[Text],
+                        ssl_password: Text) -> Optional["SSLContext"]:
+    """Create a SSL context (for the sanic server) if a proper certificate is passed."""
+
+    if ssl_certificate:
+        import ssl
+        ssl_context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        ssl_context.load_cert_chain(ssl_certificate,
+                                    keyfile=ssl_keyfile,
+                                    password=ssl_password)
+        return ssl_context
+    else:
+        return None
+
+
 def configure_app(
     input_channels: Optional[List["InputChannel"]] = None,
     cors: Optional[Union[Text, List[Text]]] = None,
@@ -123,7 +139,7 @@ def configure_app(
             """Small wrapper to shut down the server once cmd io is done."""
             await asyncio.sleep(1)  # allow server to start
             await console.record_messages(
-                server_url=constants.DEFAULT_SERVER_FORMAT.format(port)
+                server_url=constants.DEFAULT_SERVER_FORMAT.format("http", port)
             )
 
             logger.info("Killing Sanic server now.")
@@ -147,6 +163,9 @@ def serve_application(
     endpoints: Optional[AvailableEndpoints] = None,
     remote_storage: Optional[Text] = None,
     log_file: Optional[Text] = None,
+    ssl_certificate: Optional[Text] = None,
+    ssl_keyfile: Optional[Text] = None,
+    ssl_password: Text = "",
 ):
     if not channel and not credentials:
         channel = "cmdline"
@@ -165,9 +184,12 @@ def serve_application(
         log_file=log_file,
     )
 
+    ssl_context = _create_ssl_context(ssl_certificate, ssl_keyfile, ssl_password)
+    protocol = "https" if ssl_context else "http"
+
     logger.info(
         "Starting Rasa server on "
-        "{}".format(constants.DEFAULT_SERVER_FORMAT.format(port))
+        "{}".format(constants.DEFAULT_SERVER_FORMAT.format(protocol, port))
     )
 
     app.register_listener(
@@ -183,7 +205,7 @@ def serve_application(
 
     update_sanic_log_level(log_file)
 
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, ssl=ssl_context)
 
 
 # noinspection PyUnusedLocal
