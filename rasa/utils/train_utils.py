881583b62c64884c4dea19ebdf151c309d525924
Vova Vv <mr.voov@gmail.com>
2019-08-28 16:43:35 +0200

    add ability to extract attention weights from other attention types

--- rasa/utils/train_utils.py
+++ rasa/utils/train_utils.py
@@ -967,7 +967,7 @@ def extract_attention(attention_weights) -> Optional["tf.Tensor"]:
     attention = [
         tf.expand_dims(t, 0)
         for name, t in attention_weights.items()
-        if name.endswith("multihead_attention/dot_product_attention")
+        if "multihead_attention/dot_product" in name and not name.endswith("/logits")
     ]
 
     if attention:
