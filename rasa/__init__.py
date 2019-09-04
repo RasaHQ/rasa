351dc9334acf9fdf2baec27bba6788d9b353f9d4
Daksh <d.varshneya@rasa.com>
2019-09-04 20:14:14 +0200

    moved abseil logging disabling to rasa/__init__.py

--- rasa/__init__.py
+++ rasa/__init__.py
@@ -10,3 +10,16 @@ from rasa.train import train
 from rasa.test import test
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# remove handler for abseil,
+# a fix for tf 1.14 double logging suggested in
+# https://github.com/tensorflow/tensorflow/issues/26691#issuecomment-500369493
+# from
+# https://github.com/dhalperi/pybatfish/blob/f8ddd3938148f9a5d9c14c371a099802c564fac3/pybatfish/client/capirca.py#L33-L50
+try:
+    import absl.logging
+
+    logging.root.removeHandler(absl.logging._absl_handler)
+    absl.logging._warn_preinit_stderr = False
+except Exception:
+    pass
