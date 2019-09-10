db60477b351cb4573175af3fcf852a5c50b471be
Federico Tedin <federicotdn@users.noreply.github.com>
2019-09-10 16:46:20 +0200

    Update scripts/ping_slack_about_package_release.sh
    Co-Authored-By: Tom Bocklisch <tom@rasa.com>

--- scripts/ping_slack_about_package_release.sh
+++ scripts/ping_slack_about_package_release.sh
@@ -2,7 +2,7 @@
 
 if [[ ${TRAVIS_TAG} =~ ^[0-9]+\\.[0-9]+\\.[0-9]+$ ]]; then
     curl -X POST -H "Content-type: application/json" \
-	 --data "{\"text\":\"New *Rasa* version ${TRAVIS_TAG} has been released! Changelog: https://rasa.com/docs/rasa/${TRAVIS_TAG}/changelog/#id1\"}" \
+	 --data "{\"text\":\"💥 New *Rasa* version ${TRAVIS_TAG} has been released! Changelog: https://rasa.com/docs/rasa/${TRAVIS_TAG}/changelog/#id1\"}" \
 	 "https://hooks.slack.com/services/T0GHWFTS8/BMTQQL47K/${SLACK_WEBHOOK_TOKEN}"
 fi
 
