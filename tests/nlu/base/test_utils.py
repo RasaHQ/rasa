cbfe64116456989f8dbf261f991b4613a85979f1
Tom Bocklisch <tom@rasa.com>
2019-09-11 12:57:25 +0200

    import module instead of functions

--- tests/nlu/base/test_utils.py
+++ tests/nlu/base/test_utils.py
@@ -6,15 +6,7 @@ import pytest
 import tempfile
 import shutil
 import rasa.utils.io as io_utils
-from rasa.nlu.utils import (
-    is_model_dir,
-    is_url,
-    ordered,
-    relative_normpath,
-    remove_model,
-    write_json_to_file,
-    write_to_file,
-)
+from rasa.nlu import utils
 
 
 @pytest.fixture(scope="function")
@@ -30,7 +22,7 @@ def fake_model_dir(empty_model_dir):
     metadata_file = "metadata.json"
     metadata_content = {"pipeline": "pretrained_embeddings_spacy", "language": "en"}
     metadata_path = os.path.join(empty_model_dir, metadata_file)
-    write_json_to_file(metadata_path, metadata_content)
+    utils.write_json_to_file(metadata_path, metadata_content)
 
     fake_obj = {"Fake", "model"}
     fake_obj_path = os.path.join(empty_model_dir, "component.pkl")
@@ -41,8 +33,8 @@ def fake_model_dir(empty_model_dir):
 
 def test_relative_normpath():
     test_file = "/my/test/path/file.txt"
-    assert relative_normpath(test_file, "/my/test") == "path/file.txt"
-    assert relative_normpath(None, "/my/test") is None
+    assert utils.relative_normpath(test_file, "/my/test") == "path/file.txt"
+    assert utils.relative_normpath(None, "/my/test") is None
 
 
 def test_list_files_invalid_resource():
@@ -73,45 +65,45 @@ def test_creation_of_existing_dir(tmpdir):
 
 def test_ordered():
     target = {"a": [1, 3, 2], "c": "a", "b": 1}
-    assert ordered(target) == [("a", [1, 2, 3]), ("b", 1), ("c", "a")]
+    assert utils.ordered(target) == [("a", [1, 2, 3]), ("b", 1), ("c", "a")]
 
 
 def test_empty_is_model_dir(empty_model_dir):
-    assert is_model_dir(empty_model_dir)
+    assert utils.is_model_dir(empty_model_dir)
 
 
 def test_non_existent_folder_is_no_model_dir():
-    assert not is_model_dir("nonexistent_for_sure_123/")
+    assert not utils.is_model_dir("nonexistent_for_sure_123/")
 
 
 def test_data_folder_is_no_model_dir():
-    assert not is_model_dir("data/")
+    assert not utils.is_model_dir("data/")
 
 
 def test_model_folder_is_model_dir(fake_model_dir):
-    assert is_model_dir(fake_model_dir)
+    assert utils.is_model_dir(fake_model_dir)
 
 
 def test_remove_model_empty(empty_model_dir):
-    assert remove_model(empty_model_dir)
+    assert utils.remove_model(empty_model_dir)
 
 
 def test_remove_model_with_files(fake_model_dir):
-    assert remove_model(fake_model_dir)
+    assert utils.remove_model(fake_model_dir)
 
 
 def test_remove_model_invalid(empty_model_dir):
     test_file = "something.else"
     test_content = "Some other stuff"
     test_file_path = os.path.join(empty_model_dir, test_file)
-    write_to_file(test_file_path, test_content)
+    utils.write_to_file(test_file_path, test_content)
 
     with pytest.raises(ValueError):
-        remove_model(empty_model_dir)
+        utils.remove_model(empty_model_dir)
 
     os.remove(test_file_path)
 
 
 def test_is_url():
-    assert not is_url("./some/file/path")
-    assert is_url("https://rasa.com/")
+    assert not utils.is_url("./some/file/path")
+    assert utils.is_url("https://rasa.com/")
