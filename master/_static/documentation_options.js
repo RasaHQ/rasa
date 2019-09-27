8008b082d7911dc78fa7a36b79d061c486a641b8
Travis CI <builds@travis-ci.com>
2019-09-27 18:13:18 +0000

    AUTO sphinxcontrib-versioning 20190927 95d2f35f733
    CI: true
    LANG: en_US.UTF-8
    LC_ALL: en_US.UTF-8
    TRAVIS: true
    TRAVIS_BRANCH: master
    TRAVIS_BUILD_ID: 129483501
    TRAVIS_BUILD_NUMBER: 9638
    TRAVIS_COMMIT: 95d2f35f733ceaafd9204657e29b3a950d168c43
    TRAVIS_COMMIT_RANGE: 7a2b7c2eef63...95d2f35f733c
    TRAVIS_EVENT_TYPE: push
    TRAVIS_JOB_ID: 239891007
    TRAVIS_JOB_NUMBER: 9638.7
    TRAVIS_OS_NAME: linux
    TRAVIS_PULL_REQUEST: false
    TRAVIS_PYTHON_VERSION: 3.6
    TRAVIS_REPO_SLUG: RasaHQ/rasa
    TRAVIS_SECURE_ENV_VARS: true
    TRAVIS_TAG:
    TRAVIS_TEST_RESULT: 0
    USER: travis

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.4.0a1',
+    VERSION: '1.3.6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '',
