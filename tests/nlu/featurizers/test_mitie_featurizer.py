7e1cd8df600a679fab75ce0ebbc02faa9ed357c7
Tanja Bergmann <tabergma@gmail.com>
2019-10-18 17:21:58 +0200

    restructure tests

---
+++ tests/nlu/featurizers/test_mitie_featurizer.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from rasa.nlu.tokenizers.mitie_tokenizer import MitieTokenizer
+from rasa.nlu.config import RasaNLUModelConfig
+
+
+def test_mitie_featurizer(mitie_feature_extractor, default_config):
+    from nlu.featurizers.dense_featurizer.mitie_featurizer import MitieFeaturizer
+
+    mitie_component_config = {"name": "MitieFeaturizer"}
+    ftr = MitieFeaturizer.create(mitie_component_config, RasaNLUModelConfig())
+    sentence = "Hey how are you today"
+    tokens = MitieTokenizer().tokenize(sentence)
+    vecs = ftr.features_for_tokens(tokens, mitie_feature_extractor)
+    expected = np.array([0.0, -4.4551446, 0.26073121, -1.46632245, -1.84205751])
+    assert np.allclose(vecs[:5], expected, atol=1e-5)
---
+++ tests/nlu/featurizers/test_regex_featurizer.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pytest
+
+from rasa.nlu import training_data
+from rasa.nlu.tokenizers.tokenizer import Token
+from rasa.nlu.tokenizers.mitie_tokenizer import MitieTokenizer
+from rasa.nlu.tokenizers.spacy_tokenizer import SpacyTokenizer
+from rasa.nlu.training_data import Message
+from rasa.nlu.training_data import TrainingData
+from rasa.nlu.config import RasaNLUModelConfig
+
+
+@pytest.mark.parametrize(
+    "sentence, expected, labeled_tokens",
+    [
+        ("hey how are you today", [0.0, 1.0, 0.0], [0]),
+        ("hey 456 how are you", [1.0, 1.0, 0.0], [1, 0]),
+        ("blah balh random eh", [0.0, 0.0, 0.0], []),
+        ("a 1 digit number", [1.0, 0.0, 1.0], [1, 1]),
+    ],
+)
+def test_regex_featurizer(sentence, expected, labeled_tokens, spacy_nlp):
+    from nlu.featurizers.sparse_featurizer.regex_featurizer import RegexFeaturizer
+
+    patterns = [
+        {"pattern": "[0-9]+", "name": "number", "usage": "intent"},
+        {"pattern": "\\bhey*", "name": "hello", "usage": "intent"},
+        {"pattern": "[0-1]+", "name": "binary", "usage": "intent"},
+    ]
+    ftr = RegexFeaturizer(known_patterns=patterns)
+
+    # adds tokens to the message
+    tokenizer = SpacyTokenizer()
+    message = Message(sentence)
+    message.set("spacy_doc", spacy_nlp(sentence))
+    tokenizer.process(message)
+
+    result = ftr.features_for_patterns(message)
+    assert np.allclose(result, expected, atol=1e-10)
+
+    # the tokenizer should have added tokens
+    assert len(message.get("tokens", [])) > 0
+    # the number of regex matches on each token should match
+    for i, token in enumerate(message.get("tokens")):
+        token_matches = token.get("pattern").values()
+        num_matches = sum(token_matches)
+        assert num_matches == labeled_tokens.count(i)
+
+
+@pytest.mark.parametrize(
+    "sentence, expected, labeled_tokens",
+    [
+        ("lemonade and mapo tofu", [1, 1], [0.0, 2.0, 3.0]),
+        ("a cup of tea", [1, 0], [3.0]),
+        ("Is burrito my favorite food?", [0, 1], [1.0]),
+        ("I want club?mate", [1, 0], [2.0, 3.0]),
+    ],
+)
+def test_lookup_tables(sentence, expected, labeled_tokens, spacy_nlp):
+    from nlu.featurizers.sparse_featurizer.regex_featurizer import RegexFeaturizer
+
+    lookups = [
+        {
+            "name": "drinks",
+            "elements": ["mojito", "lemonade", "sweet berry wine", "tea", "club?mate"],
+        },
+        {"name": "plates", "elements": "data/test/lookup_tables/plates.txt"},
+    ]
+    ftr = RegexFeaturizer(lookup_tables=lookups)
+
+    # adds tokens to the message
+    tokenizer = SpacyTokenizer()
+    message = Message(sentence)
+    message.set("spacy_doc", spacy_nlp(sentence))
+    tokenizer.process(message)
+
+    result = ftr.features_for_patterns(message)
+    assert np.allclose(result, expected, atol=1e-10)
+
+    # the tokenizer should have added tokens
+    assert len(message.get("tokens", [])) > 0
+    # the number of regex matches on each token should match
+    for i, token in enumerate(message.get("tokens")):
+        token_matches = token.get("pattern").values()
+        num_matches = sum(token_matches)
+        assert num_matches == labeled_tokens.count(i)
