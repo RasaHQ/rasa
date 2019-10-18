fb24e35f87a8081d06772f88ff874f87a4d254ca
Tanja Bergmann <tabergma@gmail.com>
2019-10-18 14:01:51 +0200

    Add cls token to spacy tokenizer.

--- rasa/nlu/tokenizers/spacy_tokenizer.py
+++ rasa/nlu/tokenizers/spacy_tokenizer.py
@@ -1,5 +1,5 @@
 import typing
-from typing import Any
+from typing import Any, Dict, Text, List, Optional
 
 from rasa.nlu.components import Component
 from rasa.nlu.config import RasaNLUModelConfig
@@ -7,14 +7,11 @@ from rasa.nlu.tokenizers import Token, Tokenizer
 from rasa.nlu.training_data import Message, TrainingData
 
 from rasa.nlu.constants import (
-    MESSAGE_RESPONSE_ATTRIBUTE,
-    MESSAGE_INTENT_ATTRIBUTE,
     MESSAGE_TEXT_ATTRIBUTE,
     MESSAGE_TOKENS_NAMES,
-    MESSAGE_ATTRIBUTES,
     MESSAGE_SPACY_FEATURES_NAMES,
-    MESSAGE_VECTOR_FEATURE_NAMES,
     SPACY_FEATURIZABLE_ATTRIBUTES,
+    CLS_TOKEN,
 )
 
 if typing.TYPE_CHECKING:
@@ -32,6 +29,16 @@ class SpacyTokenizer(Tokenizer, Component):
         for attribute in SPACY_FEATURIZABLE_ATTRIBUTES
     ]
 
+    defaults = {
+        # Add a __cls__ token to the end of the list of tokens
+        "add_cls_token": False
+    }
+
+    def __init__(self, component_config: Optional[Dict[Text, Any]] = None) -> None:
+        """Construct a new tokenizer using the SpacyTokenizer framework."""
+        super(SpacyTokenizer, self).__init__(component_config)
+        self.add_cls_token = self.component_config["add_cls_token"]
+
     def train(
         self, training_data: TrainingData, config: RasaNLUModelConfig, **kwargs: Any
     ) -> None:
@@ -47,17 +54,18 @@ class SpacyTokenizer(Tokenizer, Component):
                         MESSAGE_TOKENS_NAMES[attribute], self.tokenize(attribute_doc)
                     )
 
-    def get_doc(self, message, attribute):
-
+    def get_doc(self, message: Message, attribute: Text) -> "Doc":
         return message.get(MESSAGE_SPACY_FEATURES_NAMES[attribute])
 
     def process(self, message: Message, **kwargs: Any) -> None:
-
         message.set(
             MESSAGE_TOKENS_NAMES[MESSAGE_TEXT_ATTRIBUTE],
             self.tokenize(self.get_doc(message, MESSAGE_TEXT_ATTRIBUTE)),
         )
 
-    def tokenize(self, doc: "Doc") -> typing.List[Token]:
-
-        return [Token(t.text, t.idx) for t in doc]
+    def tokenize(self, doc: "Doc") -> List[Token]:
+        tokens = [Token(t.text, t.idx) for t in doc]
+        if self.add_cls_token:
+            idx = doc[-1].idx + len(doc[-1].text) + 1
+            tokens = tokens + [Token(CLS_TOKEN, idx)]
+        return tokens
