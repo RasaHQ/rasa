ab91d4786134060343302cf43681ded573876c7e
Amogh Mannekote <msamogh@gmail.com>
2019-10-22 14:31:42 +0200

    Black formatting

--- rasa/model.py
+++ rasa/model.py
@@ -334,7 +334,9 @@ def merge_model(source: Text, target: Text) -> bool:
         return False
 
 
-def should_retrain(new_fingerprint: Fingerprint, old_model: Text, train_path: Text) -> Tuple[bool, bool, bool]: 
+def should_retrain(
+    new_fingerprint: Fingerprint, old_model: Text, train_path: Text
+) -> Tuple[bool, bool, bool]:
     """Check which components of a model should be retrained.
 
     Args:
