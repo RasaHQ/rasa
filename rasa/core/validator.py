dc20b1cf83a4f6cb2a1af3a64b93f23b887d6387
joaorobson <joaorobsonmartins@gmail.com>
2019-10-30 10:38:42 -0300

    Add method to validate if domain is valid

--- rasa/core/validator.py
+++ rasa/core/validator.py
@@ -199,3 +199,14 @@ class Validator:
         logger.info("Validating utterances...")
         stories_are_valid = self.verify_utterances_in_stories(ignore_warnings)
         return intents_are_valid and stories_are_valid and there_is_no_duplication
+
+    def verify_domain_is_empty(self) -> bool:
+        """Checks if the domain is empty."""
+
+        return self.domain.is_empty()
+
+    def verify_domain(self) -> bool:
+        """ Runs the validations on domain."""
+
+        is_valid = not self.verify_domain_is_empty()
+        return is_valid
