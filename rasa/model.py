48725df608920ca27ab2b23c4b45bd33037de0ed
Tobias Wochinger <t.wochinger@rasa.com>
2019-10-30 11:12:38 +0100

    Apply suggestions from code review
    Co-Authored-By: Tanja <tabergma@gmail.com>

--- rasa/model.py
+++ rasa/model.py
@@ -67,7 +67,7 @@ SECTION_NLU = Section(
         FINGERPRINT_RASA_VERSION_KEY,
     ],
 )
-SECTION_NLG = Section(name="NLG Templates", relevant_keys=[FINGERPRINT_NLG_KEY])
+SECTION_NLG = Section(name="NLG templates", relevant_keys=[FINGERPRINT_NLG_KEY])
 
 
 class FingerprintComparisonResult:
@@ -76,7 +76,7 @@ class FingerprintComparisonResult:
         nlu: bool = True,
         core: bool = True,
         nlg: bool = True,
-        force_trainining: bool = False,
+        force_training: bool = False,
     ):
         """Creates a `FingerprintComparisonResult` instance.
 
@@ -390,7 +390,7 @@ def should_retrain(
         train_path: Path to the directory in which the new model will be trained.
 
     Returns:
-        A ShouldRetrain object indicating whether Rasa Core and/or Rasa NLU needs
+        A FingerprintComparisonResult object indicating whether Rasa Core and/or Rasa NLU needs
         to be retrained or not.
 
     """
