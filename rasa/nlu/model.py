32e65f44048a0cca4e88339a15ce2587233318db
kohn1001 <nattykohn@gmail.com>
2019-11-05 18:37:49 +0200

    formatter fix

--- rasa/nlu/model.py
+++ rasa/nlu/model.py
@@ -381,6 +381,10 @@ class Interpreter(object):
 
         output = self.default_output_attributes()
         output.update(message.as_dict(only_output_properties=only_output_properties))
+
         if self.model_metadata:
-            output.update({"metadata": {"model": self.model_metadata.get("trained_at")}})
+            output.update(
+                {"metadata": {"model": self.model_metadata.get("trained_at")}}
+            )
+
         return output
