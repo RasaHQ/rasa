37fc8f1936cb2013eb9f58f04e7484c4b76374d7
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2019-11-12 15:33:58 -0300

    Changing exception to ValueError

--- rasa/nlu/classifiers/embedding_intent_classifier.py
+++ rasa/nlu/classifiers/embedding_intent_classifier.py
@@ -525,7 +525,7 @@ class EmbeddingIntentClassifier(Component):
             #     "Need at least 2 different classes. "
             #     "Skipping training of classifier."
             # )
-            raise Exception("Can not train a classifier. "
+            raise ValueError("Can not train a classifier. "
                             "Need at least 2 different classes. "
                             "Skipping training of classifier.")
 
