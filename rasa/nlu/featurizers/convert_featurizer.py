2fcf924210d197a1fbfe93f87c16cb95d6bddbcd
Daksh <d.varshneya@rasa.com>
2019-11-14 10:17:18 +0100

    formatter, removed prints

--- rasa/nlu/featurizers/convert_featurizer.py
+++ rasa/nlu/featurizers/convert_featurizer.py
@@ -44,7 +44,7 @@ class ConvertFeaturizer(Featurizer):
             self.encoding_tensor = self.module(self.text_placeholder)
             self.session.run(tf.tables_initializer())
             self.session.run(tf.global_variables_initializer())
-            
+
     def __init__(self, component_config: Dict[Text, Any] = None) -> None:
 
         super(ConvertFeaturizer, self).__init__(component_config)
@@ -60,20 +60,13 @@ class ConvertFeaturizer(Featurizer):
 
         bs = 64
 
-        print(len(training_data.intent_examples))
-
         for attribute in [MESSAGE_TEXT_ATTRIBUTE]:
 
             start_index = 0
 
             while start_index < len(training_data.intent_examples):
 
-                if start_index % 100 == 0:
-                    print('done', start_index)
-
-                end_index = min(start_index+bs,len(training_data.intent_examples))
-
-                print(start_index,end_index)
+                end_index = min(start_index + bs, len(training_data.intent_examples))
                 batch_examples = training_data.intent_examples[start_index:end_index]
                 batch_text = [ex.get(attribute) for ex in batch_examples]
 
@@ -81,27 +74,22 @@ class ConvertFeaturizer(Featurizer):
 
                 for index, ex in enumerate(batch_examples):
 
-                    # print(type(batch_feats[index]),batch_feats[index].shape)
-
-                    # if batch_feats[index] is None:
-                    # print(batch_text[index])
-
-                    ex.set(MESSAGE_VECTOR_FEATURE_NAMES[attribute], batch_feats[index])
-                    # ex.set(MESSAGE_VECTOR_FEATURE_NAMES[attribute],self._combine_with_existing_features(ex,batch_feats[index], MESSAGE_VECTOR_FEATURE_NAMES[attribute]))
+                    ex.set(
+                        MESSAGE_VECTOR_FEATURE_NAMES[attribute],
+                        self._combine_with_existing_features(
+                            ex,
+                            batch_feats[index],
+                            MESSAGE_VECTOR_FEATURE_NAMES[attribute],
+                        ),
+                    )
 
                 start_index += bs
 
-            '''for index,example in enumerate(training_data.intent_examples):
-
-                if index % 100 == 0:
-                    print('done',index)
-           
-                self._set_lm_features(example, attribute)
-            '''
-
     def compute_features(self, batch_examples):
 
-        return self.session.run(self.encoding_tensor, feed_dict={self.text_placeholder: batch_examples})
+        return self.session.run(
+            self.encoding_tensor, feed_dict={self.text_placeholder: batch_examples}
+        )
 
     def _set_lm_features(self, example, attribute=MESSAGE_TEXT_ATTRIBUTE):
 
@@ -118,5 +106,5 @@ class ConvertFeaturizer(Featurizer):
     def process(self, message: Message, **kwargs: Any) -> None:
 
         feats = self.compute_features([message.get(MESSAGE_TEXT_ATTRIBUTE)])[0]
-        message.set(MESSAGE_VECTOR_FEATURE_NAMES[MESSAGE_TEXT_ATTRIBUTE],feats)
+        message.set(MESSAGE_VECTOR_FEATURE_NAMES[MESSAGE_TEXT_ATTRIBUTE], feats)
         # self._set_lm_features(message)
