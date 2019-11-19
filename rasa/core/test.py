fb4d8ce5a8761a86dcde7f6ab3f1f2647464f05a
Tobias Wochinger <t.wochinger@rasa.com>
2019-11-19 13:36:02 +0100

    remove unused imports

--- rasa/core/test.py
+++ rasa/core/test.py
@@ -594,7 +594,6 @@ async def compare_models_in_dir(
     model_dir: Text, stories_file: Text, output: Text
 ) -> None:
     """Evaluates multiple trained models in a directory on a test set."""
-    from rasa.core import utils
     import rasa.utils.io as io_utils
 
     number_correct = defaultdict(list)
@@ -622,7 +621,6 @@ async def compare_models_in_dir(
 
 async def compare_models(models: List[Text], stories_file: Text, output: Text) -> None:
     """Evaluates provided trained models on a test set."""
-    from rasa.core import utils
 
     number_correct = defaultdict(list)
 
