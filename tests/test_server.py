796f6a26f76d76d8c8b5ac3f6915c77f18375bb4
Brian Hopkins <b.hopkins@rasa.com>
2019-11-19 09:10:18 -0500

    removing old regex stuff

--- tests/test_server.py
+++ tests/test_server.py
@@ -106,7 +106,6 @@ def test_version(rasa_app: SanicTestClient):
 def test_status(rasa_app: SanicTestClient):
     _, response = rasa_app.get("/status")
     model_file = response.json["model_file"]
-    test_folder_pattern = re.search(r"models\d/\d+-\d+.tar.gz", model_file)
     assert response.status == 200
     assert "fingerprint" in response.json
     assert "model_file" in response.json
