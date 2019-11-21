a88fa535495229bec89e90bf086343cc17735ab4
Tobias Wochinger <t.wochinger@rasa.com>
2019-11-21 17:19:21 +0100

    add types

--- rasa/core/training/structures.py
+++ rasa/core/training/structures.py
@@ -169,7 +169,7 @@ class StoryStep:
         self.story_string_helper.form_prefix_string = ""
         self.story_string_helper.no_form_prefix_string = ""
 
-    def as_story_string(self, flat=False, e2e=False):
+    def as_story_string(self, flat: bool = False, e2e: bool = False):
         # if the result should be flattened, we
         # will exclude the caption and any checkpoints.
 
@@ -378,7 +378,7 @@ class Story:
         events.append(ActionExecuted(ACTION_LISTEN_NAME))
         return Dialogue(sender_id, events)
 
-    def as_story_string(self, flat=False, e2e=False):
+    def as_story_string(self, flat: bool = False, e2e: bool = False) -> Text:
         story_content = ""
 
         # initialize helper for first story step
