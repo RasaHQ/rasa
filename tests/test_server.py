0e3c5fb124c82383f979d23f0cc56c9d3f5499ef
Brian Hopkins <b.hopkins@rasa.com>
2019-11-21 08:32:53 -0500

    adding from review

--- tests/test_server.py
+++ tests/test_server.py
@@ -103,12 +103,14 @@ def test_version(rasa_app: SanicTestClient):
     )
 
 
-def test_status(rasa_app: SanicTestClient):
+def test_status(rasa_app: SanicTestClient, trained_rasa_model: Text):
     _, response = rasa_app.get("/status")
+    model_file = response.json["model_file"]
     assert response.status == 200
     assert "fingerprint" in response.json
-    assert "model_file" in response.json
-    assert os.path.isfile(response.json["model_file"])
+    assert os.path.isfile(model_file)
+    assert model_file == trained_rasa_model
+
 
 
 def test_status_nlu_only(rasa_app_nlu: SanicTestClient):
