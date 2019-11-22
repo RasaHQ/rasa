bcdf4fcccb0185ad27bd1d52b29b3f9c2c7c3e1f
Brian Hopkins <b.hopkins@rasa.com>
2019-11-22 11:55:32 -0500

    fixed broken tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -111,16 +111,13 @@ def test_status(rasa_app: SanicTestClient, trained_rasa_model: Text):
     assert model_file == trained_rasa_model
 
 
-def test_status_nlu_only(rasa_app_nlu: SanicTestClient, trained_rasa_model: Text):
+def test_status_nlu_only(rasa_app_nlu: SanicTestClient, trained_nlu_model: Text):
     _, response = rasa_app_nlu.get("/status")
     model_file = response.json["model_file"]
     assert response.status == 200
     assert "fingerprint" in response.json
     assert "model_file" in response.json
-    # TODO: Eventually update with assert model_file == trained_rasa_model
-    # believe bug is causing NLU to be retrained twice breaking above
-    # code
-    assert os.path.isfile(model_file)
+    assert model_file == trained_nlu_model
 
 
 def test_status_secured(rasa_secured_app: SanicTestClient):
