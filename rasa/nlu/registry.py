31c88e5ceadae40aa89d11f44aeb6ab441a392c9
Ella <erohmensing@gmail.com>
2019-11-27 15:07:13 +0100

    use futurewarnings for end user deprecation warnings

--- rasa/nlu/registry.py
+++ rasa/nlu/registry.py
@@ -158,8 +158,7 @@ def get_component_class(component_name: Text) -> Type["Component"]:
                 # that class
                 module_name, _, class_name = component_name.rpartition(".")
                 raise Exception(
-                    "Failed to find class '{}' in module '{}'.\n"
-                    "".format(component_name, class_name, module_name)
+                    f"Failed to find class '{class_name}' in module '{module_name}'.\n"
                 )
             except ImportError as e:
                 # when component_name is a path to a class but that path is invalid or
@@ -169,15 +168,15 @@ def get_component_class(component_name: Text) -> Type["Component"]:
 
                 if is_path:
                     module_name, _, _ = component_name.rpartition(".")
-                    exception_message = "Failed to find module '{}'. \n{}".format(
-                        module_name, e
+                    exception_message = (
+                        f"Failed to find module '{module_name}'. \n{e}"
                     )
                 else:
                     exception_message = (
-                        "Cannot find class '{}' from global namespace. "
-                        "Please check that there is no typo in the class "
-                        "name and that you have imported the class into the global "
-                        "namespace.".format(component_name)
+                        f"Cannot find class '{component_name}' from global namespace. "
+                        f"Please check that there is no typo in the class "
+                        f"name and that you have imported the class into the global "
+                        f"namespace."
                     )
 
                 raise ModuleNotFoundError(exception_message)
@@ -188,7 +187,7 @@ def get_component_class(component_name: Text) -> Type["Component"]:
                 f"contains old style component name `{component_name}`, "
                 f"you should change it to its class name: "
                 f"`{old_style_names[component_name]}`.",
-                DeprecationWarning,
+                FutureWarning,
             )
             component_name = old_style_names[component_name]
 
