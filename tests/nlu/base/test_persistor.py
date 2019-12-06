533ff376397f8fb0dc7ae7e26dc667410a6d0bb4
cianclarke <cian@cianclarke.com>
2019-12-06 14:39:02 -0500

    More lint & bump coverage

--- tests/nlu/base/test_persistor.py
+++ tests/nlu/base/test_persistor.py
@@ -35,6 +35,7 @@ async def test_list_method_method_in_AWSPersistor(component_builder, tmpdir):
 
     assert len(result) == 1
 
+
 # noinspection PyPep8Naming
 @mock_s3
 def test_list_models_method_raise_exeception_in_AWSPersistor():
@@ -142,13 +143,26 @@ def test_retrieve_tar_archive(model, archive):
             persistor.Persistor().retrieve(model, "dst")
         f.assert_called_once_with(archive)
 
+
 # noinspection PyPep8Naming
 @mock_s3
 def test_retrieve_tar_archive_with_s3_namespace():
-    model='/my/s3/project/model.tar.gz'
-    destination='dst'
+    model = "/my/s3/project/model.tar.gz"
+    destination = "dst"
     with patch.object(persistor.AWSPersistor, "_decompress") as decompress:
         with patch.object(persistor.AWSPersistor, "_retrieve_tar") as retrieve:
             persistor.AWSPersistor("rasa-test").retrieve(model, destination)
-        decompress.assert_called_once_with('model.tar.gz', destination)
+        decompress.assert_called_once_with("model.tar.gz", destination)
         retrieve.assert_called_once_with(model)
+
+
+# noinspection PyPep8Naming
+@mock_s3
+def test_s3_private_retrieve_tar():
+    model = "/my/s3/project/model.tar.gz"
+    awsPersistor = persistor.AWSPersistor("rasa-test")
+    with patch.object(awsPersistor.bucket, "download_fileobj") as download_fileobj:
+        awsPersistor._retrieve_tar(model)
+    retrieveArgs = download_fileobj.call_args[0]
+    assert retrieveArgs[0] == model
+    assert retrieveArgs[1].name == "model.tar.gz"
