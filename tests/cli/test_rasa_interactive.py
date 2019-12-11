bbdc969214e698a62020603dafac9165d7bf6a84
Ella <erohmensing@gmail.com>
2019-12-11 23:49:12 +0100

    add tests that train is called if no model passed

--- tests/cli/test_rasa_interactive.py
+++ tests/cli/test_rasa_interactive.py
@@ -7,6 +7,7 @@ from _pytest.pytester import RunResult
 
 import rasa
 from rasa.cli import interactive, train
+from rasa import train as rasa_train
 
 
 def test_interactive_help(run: Callable[..., RunResult]):
@@ -61,5 +62,62 @@ def test_pass_arguments_to_rasa_train(
     # If the `Namespace` object does not have all required fields this will throw
     train.train(args)
 
-    # Assert `train` was actually code
+    # Assert `train` was actually called
     mock.method.assert_called_once()
+
+
+def test_train_called_when_no_model_passed(
+    default_stack_config: Text, monkeypatch: MonkeyPatch,
+) -> None:
+    parser = argparse.ArgumentParser()
+    sub_parser = parser.add_subparsers()
+    interactive.add_subparser(sub_parser, [])
+
+    args = parser.parse_args(
+        [
+            "interactive",
+            "--config",
+            default_stack_config,
+            "--data",
+            "examples/moodbot/data",
+        ]
+    )
+    interactive._set_not_required_args(args)
+
+    # Mock actual training and interactive learning methods
+    mock = Mock()
+    monkeypatch.setattr(train, "train", mock.train_model)
+    monkeypatch.setattr(interactive, "perform_interactive_learning", mock.method)
+
+    interactive.interactive(args)
+    mock.train_model.assert_called_once()
+
+
+def test_train_core_called_when_no_model_passed_and_core(
+    default_stack_config: Text, monkeypatch: MonkeyPatch,
+) -> None:
+    parser = argparse.ArgumentParser()
+    sub_parser = parser.add_subparsers()
+    interactive.add_subparser(sub_parser, [])
+
+    args = parser.parse_args(
+        [
+            "interactive",
+            "core",
+            "--config",
+            default_stack_config,
+            "--stories",
+            "examples/moodbot/data/stories.md",
+            "--domain",
+            "examples/moodbot/domain.yml",
+        ]
+    )
+    interactive._set_not_required_args(args)
+
+    # Mock actual training and interactive learning methods
+    mock = Mock()
+    monkeypatch.setattr(train, "train_core", mock.train_core)
+    monkeypatch.setattr(interactive, "perform_interactive_learning", mock.method)
+
+    interactive.interactive(args)
+    mock.train_core.assert_called_once()
