f9b4f82c4d28778d9e79ff5b608688a6b2179d1e
Tanja Bergmann <tabergma@gmail.com>
2019-12-12 09:42:47 +0100

    update changelog entry

--- rasa/nlu/featurizers/sparse_featurizer/ngram_featurizer.py
+++ rasa/nlu/featurizers/sparse_featurizer/ngram_featurizer.py
@@ -12,16 +12,15 @@ class NGramFeaturizer(Featurizer):
     def __init__(self, component_config: Optional[Dict[Text, Any]] = None):
         super(NGramFeaturizer, self).__init__(component_config)
 
-        warnings.warn(
-            "DEPRECATION warning: Using `NGramFeaturizer` is deprecated. "
+        raise NotImplementedError(
+            "REMOVAL warning: You cannot use `NGramFeaturizer` anymore. "
             "Please use `CountVectorsFeaturizer` instead. The following settings"
-            "should match the previous `NGramFeaturizer`:"
+            "match the previous `NGramFeaturizer`:"
             ""
             "- name: 'CountVectorsFeaturizer'"
             "  analyzer: 'char_wb'"
             "  min_ngram: 3"
             "  max_ngram: 17"
             "  max_features: 10"
-            "  min_df: 5",
-            FutureWarning,
+            "  min_df: 5"
         )
