dc824af0eb476ebecf7732fc064c6e522026838f
Cian Clarke <cian@cianclarke.com>
2019-12-16 21:01:10 +0000

    Update rasa/nlu/persistor.py
    Co-Authored-By: ricwo <ric.wkr@gmail.com>

--- rasa/nlu/persistor.py
+++ rasa/nlu/persistor.py
@@ -155,7 +155,7 @@ class AWSPersistor(Persistor):
         """Downloads a model that has previously been persisted to s3."""
         tar_name = os.path.basename(model_full_path)
         with open(tar_name, "wb") as f:
-            self.bucket.download_fileobj(model_full_path, f)
+            self.bucket.download_fileobj(model_path, f)
 
 
 class GCSPersistor(Persistor):
