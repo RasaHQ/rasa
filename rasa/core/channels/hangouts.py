11fca7798621b6705d86764f2cfd918f91e73c8f
muelerma <hermingk@aol.com>
2020-01-10 20:03:17 +0100

    Added changelog, black formatting

--- rasa/core/channels/hangouts.py
+++ rasa/core/channels/hangouts.py
@@ -25,7 +25,6 @@ logger = logging.getLogger(__name__)
 
 
 class HangoutsOutput(OutputChannel):
-
     @classmethod
     def name(cls) -> Text:
         return "hangouts"
@@ -34,73 +33,51 @@ class HangoutsOutput(OutputChannel):
         self.messages = {}
 
     def _text_card(self, message: Dict[Text, Any]) -> Dict:
-        
+
         card = {
-            "cards":[
+            "cards": [
                 {
-                    "sections":[
-                        {
-                            "widgets":[
-                                {
-                                "textParagraph":{"text": message["text"]}
-                                }
-                            ]
-                        }
+                    "sections": [
+                        {"widgets": [{"textParagraph": {"text": message["text"]}}]}
                     ]
                 }
             ]
         }
-        return card 
+        return card
 
     def _image_card(self, image: Text) -> Dict:
         card = {
-            "cards":[
-                {
-                    "sections":[
-                        {
-                            "widgets":[
-                                {
-                                "image":{"imageUrl":image}
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
+            "cards": [{"sections": [{"widgets": [{"image": {"imageUrl": image}}]}]}]
         }
         return card
 
     def _text_button_card(self, text: Text, buttons: List) -> Dict:
         hangouts_buttons = []
         for b in buttons:
-            try: 
+            try:
                 b_txt, b_pl = b["title"], b["payload"]
             except KeyError:
-                logger.error("Buttons must be a list of dicts with 'title' and 'payload' as keys")
+                logger.error(
+                    "Buttons must be a list of dicts with 'title' and 'payload' as keys"
+                )
 
             hangouts_buttons.append(
-                {"textButton":{
-                    "text": b_txt,
-                    "onClick":{
-                        "action":{
-                            "actionMethodName": b_pl
-                        }
+                {
+                    "textButton": {
+                        "text": b_txt,
+                        "onClick": {"action": {"actionMethodName": b_pl}},
                     }
-                }}
+                }
             )
 
         card = {
-            "cards":[
+            "cards": [
                 {
-                    "sections":[
+                    "sections": [
                         {
-                            "widgets":[
-                                {
-                                    "textParagraph":{"text": text}
-                                },
-                                {
-                                    "buttons": hangouts_buttons
-                                }
+                            "widgets": [
+                                {"textParagraph": {"text": text}},
+                                {"buttons": hangouts_buttons},
                             ]
                         }
                     ]
@@ -109,9 +86,8 @@ class HangoutsOutput(OutputChannel):
         }
         return card
 
-
     def _combine_cards(self, c1: Dict, c2: Dict) -> Dict:
-        return {"cards":[*c1["cards"], *c2["cards"]]}       
+        return {"cards": [*c1["cards"], *c2["cards"]]}
 
     async def _persist_message(self, message: Dict) -> None:
         """Google Hangouts only accepts single dict with single key 'text'
@@ -120,14 +96,22 @@ class HangoutsOutput(OutputChannel):
         In case the bot sends multiple messages, all are transformed to either
         cards or text output"""
 
-        # check whether current and previous message will send 'text' or 'card' 
-        if self.messages.get("text"): msg_state = "text"
-        elif self.messages.get("cards"): msg_state = "cards"
-        else: msg_state = None
+        # check whether current and previous message will send 'text' or 'card'
+        if self.messages.get("text"):
+            msg_state = "text"
+        elif self.messages.get("cards"):
+            msg_state = "cards"
+        else:
+            msg_state = None
 
-        if message.get("text"): msg_new = "text"
-        elif message.get("cards"): msg_new = "cards"
-        else: raise Exception("Your message to Hangouts channel must either contain 'text' or 'cards'!")
+        if message.get("text"):
+            msg_new = "text"
+        elif message.get("cards"):
+            msg_new = "cards"
+        else:
+            raise Exception(
+                "Your message to Hangouts channel must either contain 'text' or 'cards'!"
+            )
 
         # depending on above outcome, convert messages into same type and combine
         if msg_new == msg_state == "text":
@@ -148,21 +132,26 @@ class HangoutsOutput(OutputChannel):
             text_card = self._text_card(self.messages)
             new_messages = self._combine_cards(text_card, message)
 
-        elif msg_new == "text": new_messages = {"text": message.get("text")}
-        else: new_messages = message
+        elif msg_new == "text":
+            new_messages = {"text": message.get("text")}
+        else:
+            new_messages = message
 
         self.messages = new_messages
 
+    async def send_text_message(
+        self, recipient_id: Text, text: Text, **kwargs: Any
+    ) -> None:
 
-    async def send_text_message(self, recipient_id: Text, text: Text, **kwargs: Any) -> None:
-        
         await self._persist_message({"text": text})
 
     async def send_image_url(self, recipient_id: Text, image: Text, **kwargs) -> None:
 
         await self._persist_message(self._image_card(image))
 
-    async def send_text_with_buttons(self, recipient_id: Text, text: Text, buttons: List, **kwargs) -> None:
+    async def send_text_with_buttons(
+        self, recipient_id: Text, text: Text, buttons: List, **kwargs
+    ) -> None:
 
         await self._persist_message(self._text_button_card(text, buttons))
 
@@ -175,7 +164,9 @@ class HangoutsOutput(OutputChannel):
     ) -> None:
         raise NotImplementedError
 
-    async def send_custom_json(self, recipient_id: Text, json_message: Dict, **kwargs) -> None:
+    async def send_custom_json(
+        self, recipient_id: Text, json_message: Dict, **kwargs
+    ) -> None:
         """Custom json payload is simply forwarded to Google Hangouts without
         any modifications. Use this for more complex cards, which can be created
         in actions.py."""
@@ -187,20 +178,21 @@ class HangoutsInput(InputChannel):
     """
     Channel that uses Google Hangouts Chat API to communicate.
     """
+
     @classmethod
     def from_credentials(cls, credentials: Optional[Dict[Text, Any]]) -> InputChannel:
         credentials = credentials or {}
         return cls(
             credentials.get("user_added_intent"),
             credentials.get("room_added_intent"),
-            credentials.get("removed_intent")
+            credentials.get("removed_intent"),
         )
 
     def __init__(
-        self, 
-        user_added_intent: Optional[Text] = None, 
-        room_added_intent: Optional[Text] = None, 
-        removed_intent: Optional[Text] = None
+        self,
+        user_added_intent: Optional[Text] = None,
+        room_added_intent: Optional[Text] = None,
+        removed_intent: Optional[Text] = None,
     ) -> None:
 
         self.user_added_intent = user_added_intent
@@ -250,20 +242,19 @@ class HangoutsInput(InputChannel):
         self, on_new_message: Callable[[UserMessage], Awaitable[None]]
     ) -> Blueprint:
 
-        custom_webhook = Blueprint('hangouts_webhook', __name__)
+        custom_webhook = Blueprint("hangouts_webhook", __name__)
 
-
-        @custom_webhook.route("/", methods=['GET'])
+        @custom_webhook.route("/", methods=["GET"])
         async def health(request: Request) -> HTTPResponse:
             return response.json({"status": "ok"})
 
-        @custom_webhook.route("/webhook", methods=['POST'])
+        @custom_webhook.route("/webhook", methods=["POST"])
         async def receive(request: Request) -> HTTPResponse:
 
             sender_id = self._extract_sender(request)
             room_name = self._extract_room(request)
             text = self._extract_message(request)
-            if text is None: 
+            if text is None:
                 return response.text("OK")
             input_channel = self._extract_input_channel(request)
 
@@ -272,13 +263,16 @@ class HangoutsInput(InputChannel):
             try:
                 await on_new_message(
                     UserMessage(
-                        text, collector, sender_id, input_channel=input_channel, metadata={"room": room_name}
+                        text,
+                        collector,
+                        sender_id,
+                        input_channel=input_channel,
+                        metadata={"room": room_name},
                     )
                 )
             except CancelledError:
                 logger.error(
-                    "Message handling timed out for "
-                    "user message '{}'.".format(text)
+                    "Message handling timed out for " "user message '{}'.".format(text)
                 )
             except Exception:
                 logger.exception(
@@ -289,4 +283,3 @@ class HangoutsInput(InputChannel):
             return response.json(collector.messages)
 
         return custom_webhook
-
