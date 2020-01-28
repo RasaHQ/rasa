e046c761d3b62a0effa145765febf32a34ab1586
skerdudou <skerdudou@users.noreply.github.com>
2020-01-28 17:27:46 +0100

    Update pika.py
    Make rabbitMq messages persistent uppon rabbitMq server restart.
    
    The queue si already created "durable" and persist over server restart but messages in it doesn't by default.
    We need to flag messages as persistent with `delivery_mode`
    source : https://www.rabbitmq.com/tutorials/tutorial-two-python.html

--- rasa/core/brokers/pika.py
+++ rasa/core/brokers/pika.py
@@ -337,7 +337,10 @@ class PikaEventBroker(EventBroker):
 
         kwargs = {"app_id": self.rasa_environment} if self.rasa_environment else {}
 
-        return BasicProperties(**kwargs)
+        return BasicProperties(
+            delivery_mode = 2, # make message persistent
+            **kwargs
+        )
 
     def _publish(self, body: Text) -> None:
         if self._pika_connection.is_closed:
