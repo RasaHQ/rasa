4417a78f7bb16004166c4175b5f59af320e9efef
Alan Nichol <alan@rasa.com>
2020-02-14 08:48:06 -0500

    super janky but kinda working

--- rasa/core/nlg/modified.py
+++ rasa/core/nlg/modified.py
@@ -12,6 +12,9 @@ from rasa.core.nlg.template import TemplatedNaturalLanguageGenerator
 import tensorflow_hub as tfhub
 import tensorflow as tf
 import tensorflow_text
+import numpy as np
+import random
+
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +26,7 @@ class ModifiedTemplateGenerator(TemplatedNaturalLanguageGenerator):
 
     def __init__(self, templates: Dict[Text, List[Dict[Text, Any]]]) -> None:
         self.templates = templates
+        self.modifiers = ["Nevermind.", "Ok.", "Yes, that's right.", "Actually no.", "Whatever."]
 
     async def generate(
         self,
@@ -33,10 +37,76 @@ class ModifiedTemplateGenerator(TemplatedNaturalLanguageGenerator):
     ) -> Optional[Dict[Text, Any]]:
         """Generate a response for the requested template."""
         filled_slots = tracker.current_slot_values()
-        return self.generate_from_slots(
+        template = self.generate_from_slots(
             template_name, filled_slots, output_channel, **kwargs
         )
+        enhanced_text = self.prepend_and_rerank(template['text'])
+        template['text'] = enhanced_text
+        return template
 
+    def generate_modified_texts(self, text):
+        return [text] + [
+            f"{m} {text}" for m in self.modifiers
+        ]
+
+    def rank(self, candidates):
+        sess = None
+
+        if sess is not None:
+            sess.close()
+
+        sess = tf.InteractiveSession(graph=tf.Graph())
+
+        module = tfhub.Module("http://models.poly-ai.com/convert/v1/model.tar.gz")
+
+        text_placeholder = tf.placeholder(dtype=tf.string, shape=[None])
+        context_encoding_tensor = module(text_placeholder, signature="encode_context")
+        response_encoding_tensor = module(text_placeholder, signature="encode_response")
+
+        encoding_dim = int(context_encoding_tensor.shape[1])
+
+        sess.run(tf.tables_initializer())
+        sess.run(tf.global_variables_initializer())
+
+        def encode_contexts(texts):
+            return sess.run(context_encoding_tensor, feed_dict={text_placeholder: texts})
+
+        def encode_responses(texts):
+            return sess.run(response_encoding_tensor, feed_dict={text_placeholder: texts})
+
+        # Encode the responses in batches of 64.
+        batch_size = 64
+        response_encodings = []
+        for i in range(0, len(candidates), batch_size):
+            batch = candidates[i:i + batch_size]
+            response_encodings.append(encode_responses(batch))
+        #response_encodings = encode_responses([candidates])
+
+        response_encodings = np.concatenate(response_encodings)
+
+        context = "should I thank you?" # @param {type:"string"}
+        if context:
+            context_encoding = encode_contexts([context])
+            scores = np.dot(response_encodings, context_encoding.T).flatten()
+            top_idx = np.argsort(scores)[::-1]
+            logger.error(top_idx)
+            logger.error(top_idx.shape)
+            logger.error(top_idx.dtype)
+            for i in range(len(candidates)):
+                idx = top_idx[i]
+                logger.error(f"[{float(scores[idx])}] {candidates[idx]}")
+            #top_index = np.argmax(scores)
+            #top_score = float(scores[top_index])
+
+        scores = None
+        sorted_candidates = [candidates[i] for i in top_idx]
+        return scores, sorted_candidates
+
+    
+    def prepend_and_rerank(self, text):
+        candidates = self.generate_modified_texts(text)
+        scores, candidates = self.rank(candidates)
+        return candidates[0]
 
 if __name__ == "__main__":
     sess = None
