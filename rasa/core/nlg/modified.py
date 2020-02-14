caaeb314bb274a13edbb93fffc2a517e5cbd8d9e
Alan Nichol <alan@rasa.com>
2020-02-14 09:28:09 -0500

    groundwork for multi context encoder

--- rasa/core/nlg/modified.py
+++ rasa/core/nlg/modified.py
@@ -26,7 +26,7 @@ class ModifiedTemplateGenerator(TemplatedNaturalLanguageGenerator):
 
     def __init__(self, templates: Dict[Text, List[Dict[Text, Any]]]) -> None:
         self.templates = templates
-        self.modifiers = ["Nevermind.", "Ok.", "Yes, that's right.", "Actually no.", "Whatever."]
+        self.modifiers = ["Nevermind.", "Ok.", "Yes, that's right.", "Actually no.", "Whatever.", "I'll repeat.", "Again. "]
 
     async def generate(
         self,
@@ -57,19 +57,47 @@ class ModifiedTemplateGenerator(TemplatedNaturalLanguageGenerator):
 
         sess = tf.InteractiveSession(graph=tf.Graph())
 
-        module = tfhub.Module("http://models.poly-ai.com/convert/v1/model.tar.gz")
+        module = tfhub.Module("http://models.poly-ai.com/multi_context_convert/v1/model.tar.gz")
+
 
         text_placeholder = tf.placeholder(dtype=tf.string, shape=[None])
-        context_encoding_tensor = module(text_placeholder, signature="encode_context")
+        extra_text_placeholder = tf.placeholder(dtype=tf.string, shape=[None])
+        context_encoding_tensor = module(
+            {
+                'context': text_placeholder,
+                'extra_context': extra_text_placeholder,
+            },
+            signature="encode_context"
+        )
         response_encoding_tensor = module(text_placeholder, signature="encode_response")
 
         encoding_dim = int(context_encoding_tensor.shape[1])
 
         sess.run(tf.tables_initializer())
         sess.run(tf.global_variables_initializer())
+        
+        def encode_context(dialogue_history):
+            """Encode the dialogue context to the response ranking vector space.
 
-        def encode_contexts(texts):
-            return sess.run(context_encoding_tensor, feed_dict={text_placeholder: texts})
+            Args:
+                dialogue_history: a list of strings, the dialogue history, in
+                    chronological order.
+            """
+
+            # The context is the most recent message in the history.
+            context = dialogue_history[-1]
+
+            extra_context = list(dialogue_history[:-1])
+            extra_context.reverse()
+            extra_context_feature = " ".join(extra_context)
+
+            return sess.run(
+                context_encoding_tensor,
+                feed_dict={
+                    text_placeholder: [context],
+                    extra_text_placeholder: [extra_context_feature],
+                }
+            )[0]
 
         def encode_responses(texts):
             return sess.run(response_encoding_tensor, feed_dict={text_placeholder: texts})
@@ -84,19 +112,26 @@ class ModifiedTemplateGenerator(TemplatedNaturalLanguageGenerator):
 
         response_encodings = np.concatenate(response_encodings)
 
-        context = tracker.latest_message.text # @param {type:"string"}
-        if context:
-            context_encoding = encode_contexts([context])
-            scores = np.dot(response_encodings, context_encoding.T).flatten()
-            top_idx = np.argsort(scores)[::-1]
-            for i in range(len(candidates)):
-                idx = top_idx[i]
-                logger.error(f"[{float(scores[idx])}] {candidates[idx]}")
+        dialogue_history = self.extract_history(tracker)
+
+        context_encoding = encode_contexts(dialogue_history)
+        scores = context_encoding.dot(response_encodings.T)
+        top_idx = np.argsort(scores)[::-1]
+        for i in range(len(candidates)):
+            idx = top_idx[i]
+            logger.error(f"[{float(scores[idx])}] {candidates[idx]}")
 
         scores = None
         sorted_candidates = [candidates[i] for i in top_idx]
         return scores, sorted_candidates
 
+    def extract_history(self, tracker):
+        from rasa.core.events import UserUttered, BotUttered
+        turns = []
+        for event in tracker.events:
+            if isinstance(event, UserUttered) or isinstance(event, BotUttered):
+                turns.append(event.text)
+        return turns
     
     def prepend_and_rerank(self, text, tracker):
         candidates = self.generate_modified_texts(text)
