144011eea0e74802a8d0f36fa9991d001ef5db2c
ricwo <ric.wkr@gmail.com>
2020-03-23 15:42:04 +0100

    add missing backtick

--- rasa/core/utils.py
+++ rasa/core/utils.py
@@ -301,7 +301,7 @@ def is_limit_reached(num_messages: int, limit: int) -> bool:
         limit: Limit on the number of messages.
 
     Returns:
-        `True` if the limit has been reached, otherwise `False.
+        `True` if the limit has been reached, otherwise `False`.
     """
     return limit is not None and num_messages >= limit
 
