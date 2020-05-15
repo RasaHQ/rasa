1f8b9f38d4db695ab24e566156fb60044eac0976
Tom Bocklisch <tom@rasa.com>
2020-05-15 19:50:34 +0200

    fix moodbot model race condition in tests

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -51,8 +51,6 @@ E2E_STORY_FILE_TRIPS_CIRCUIT_BREAKER = (
     "data/test_evaluations/end_to_end_trips_circuit_breaker.md"
 )
 
-MOODBOT_MODEL_PATH = "examples/moodbot/models/"
-
 DEFAULT_ENDPOINTS_FILE = "data/test_endpoints/example_endpoints.yml"
 
 TEST_DIALOGUES = [
