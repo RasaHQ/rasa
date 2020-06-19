c51bc6780eda4b49f511ae02b1b5647f80edf73c
Christina Koss <c.koss@rasa.com>
2020-06-19 17:19:22 +0200

    add tests for dumping

--- tests/importers/test_autoconfig.py
+++ tests/importers/test_autoconfig.py
@@ -1,26 +1,28 @@
+import shutil
 from pathlib import Path
-from typing import List, Text, Set, Any, Dict
+from typing import List, Text, Set
 from unittest.mock import Mock
 
 import pytest
+from _pytest.capture import CaptureFixture
 from _pytest.monkeypatch import MonkeyPatch
 
+from rasa.constants import CONFIG_AUTOCONFIGURABLE_KEYS
 from rasa.importers import autoconfig
 from rasa.utils import io as io_utils
 
-EMPTY_CONFIG = "rasa/cli/initial_project/config.yml"
-CONFIG_PIPELINE_ONLY = "data/test_config/config_pipeline_only.yml"
-CONFIG_POLICIES_ONLY = "data/test_config/config_policies_only.yml"
-SOME_CONFIG = "data/test_config/arbitrary_example_config.yml"
+CONFIG_FOLDER = "data/test_config"
+
+SOME_CONFIG = CONFIG_FOLDER + "/arbitrary_example_config.yml"
 DEFAULT_CONFIG = "rasa/importers/default_config.yml"
 
 
 @pytest.mark.parametrize(
     "config_path, autoconfig_keys",
     [
-        (EMPTY_CONFIG, ["pipeline", "policies"]),
-        (CONFIG_PIPELINE_ONLY, ["policies"]),
-        (CONFIG_POLICIES_ONLY, ["pipeline"]),
+        ("rasa/cli/initial_project/config.yml", ["pipeline", "policies"]),
+        (CONFIG_FOLDER + "/config_pipeline_only.yml", ["policies"]),
+        (CONFIG_FOLDER + "/config_policies_only.yml", ["pipeline"]),
         (SOME_CONFIG, []),
     ],
 )
@@ -51,7 +53,7 @@ def test_create_config_for_keys(keys: List[Text]):
     assert len(config) == len(keys)  # no other keys are configured
 
 
-def test_dump_config_missing_file(tmp_path: Path):
+def test_dump_config_missing_file(tmp_path: Path, capsys: CaptureFixture):
 
     config_path = tmp_path / "non_existent_config.yml"
 
@@ -61,18 +63,56 @@ def test_dump_config_missing_file(tmp_path: Path):
 
     actual = io_utils.read_file(config_path)
     expected = io_utils.read_file(
-        "data/test_config/arbitrary_example_config_after_dumping.yml"
+        CONFIG_FOLDER + "/arbitrary_example_config_after_dumping.yml"
     )
 
     assert actual == expected
-    # TODO: add assertion for the printed warning
+
+    captured = capsys.readouterr()
+    assert "does not exist or is empty" in captured.out
+
+    for k in CONFIG_AUTOCONFIGURABLE_KEYS:
+        assert k in captured.out
 
 
-def test_dump_config(tmp_path: Path):
-    # things that are done while dumping:
-    # - auto configured keys are dumped with commented out configuration
-    # - existing comments are kept
-    # - the other sections are kept
-    # - it works with and without empty lines between the keys
-    # - info printed if keys were configured automatically
-    pass
+# Test a few cases that are known to be potentially tricky (have failed in the past)
+@pytest.mark.parametrize(
+    "input_file, expected_file, autoconfig_keys",
+    [
+        (
+            "config_with_comments.yml",
+            "config_with_comments_after_dumping.yml",
+            ["policies"],
+        ),
+        (
+            "config_empty.yml",
+            "config_empty_after_dumping.yml",
+            ["policies", "pipeline"],
+        ),  # no empty lines!
+    ],
+)
+def test_dump_config(
+    tmp_path: Path,
+    input_file: Text,
+    expected_file: Text,
+    capsys: CaptureFixture,
+    autoconfig_keys: List[Text],
+):
+    config_path = str(tmp_path / "config.yml")
+    shutil.copyfile(CONFIG_FOLDER + "/" + input_file, config_path)
+
+    autoconfig.get_auto_configuration(config_path)
+
+    actual = io_utils.read_file(config_path)
+    expected = io_utils.read_file(CONFIG_FOLDER + "/" + expected_file)
+
+    assert actual == expected
+
+    captured = capsys.readouterr()
+    assert "does not exist or is empty" not in captured.out
+
+    for k in CONFIG_AUTOCONFIGURABLE_KEYS:
+        if k in autoconfig_keys:
+            assert k in captured.out
+        else:
+            assert k not in captured.out
