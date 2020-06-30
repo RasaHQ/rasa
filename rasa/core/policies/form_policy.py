002072b27e4b6bcf7f6fd5b0e3c51e76532f00c4
Tobias Wochinger <t.wochinger@rasa.com>
2020-06-30 14:45:59 +0200

    both RulePolicy and FormPolicy fulfill the requirement for forms in the domain

--- rasa/core/policies/form_policy.py
+++ rasa/core/policies/form_policy.py
@@ -45,16 +45,22 @@ class FormPolicy(MemoizationPolicy):
         if not domain:
             return
 
-        # has_form_policy = ensemble is not None and any(
-        #     isinstance(p, cls) for p in ensemble.policies
-        # )
-        # if domain.form_names and not has_form_policy:
-        #     raise InvalidDomain(
-        #         "You have defined a form action, but haven't added the "
-        #         "FormPolicy to your policy ensemble. Either remove all "
-        #         "forms from your domain or exclude the FormPolicy from your "
-        #         "policy configuration."
-        #     )
+        from rasa.core.policies.rule_policy import RulePolicy
+
+        suited_policies_for_forms = (FormPolicy, RulePolicy)
+
+        has_policy_for_forms = ensemble is not None and any(
+            isinstance(policy, suited_policies_for_forms)
+            for policy in ensemble.policies
+        )
+
+        if domain.form_names and not has_policy_for_forms:
+            raise InvalidDomain(
+                "You have defined a form action, but haven't added the "
+                "FormPolicy to your policy ensemble. Either remove all "
+                "forms from your domain or exclude the FormPolicy from your "
+                "policy configuration."
+            )
 
     @staticmethod
     def _get_active_form_name(state: Dict[Text, float]) -> Optional[Text]:
