aa1d35ac48028f99c244901e75f4f2c9dd4a1391
Alexander Khizov <degiz@users.noreply.github.com>
2020-07-08 17:35:56 +0200

    Update rasa/cli/arguments/default_arguments.py
    Co-authored-by: Federico Tedin <federicotdn@users.noreply.github.com>

--- rasa/cli/arguments/default_arguments.py
+++ rasa/cli/arguments/default_arguments.py
@@ -56,8 +56,8 @@ def add_domain_param(
         "--domain",
         type=str,
         default=DEFAULT_DOMAIN_PATH,
-        help="Domain specification. It can be a single 'yaml' file, or a directory "
-        "that contains several files with domain specification in it. The content "
+        help="Domain specification. This can be a single YAML file, or a directory "
+        "that contains several files with domain specifications in it. The content "
         "of these files will be read and merged together.",
     )
 
