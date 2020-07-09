0b6ea51bf9dfbdd4f161f7463ae2678f41700d41
Tobias Wochinger <t.wochinger@rasa.com>
2020-07-09 13:56:29 +0200

    add clarification comment

--- rasa/core/actions/forms.py
+++ rasa/core/actions/forms.py
@@ -325,6 +325,9 @@ class FormAction(LoopAction):
             event.key for event in validate_events if isinstance(event, SlotSet)
         ]
 
+        # If the custom action doesn't return a SlotSet event for an extracted slot
+        # candidate we assume that it was valid. The custom action has to return a
+        # SlotSet(slot_name, None) event to mark a Slot as invalid.
         return validate_events + [
             event for event in events if event.key not in validated_slot_names
         ]
