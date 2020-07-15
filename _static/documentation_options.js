aaadf3f7de213ff27c8e2854f7483374889ea261
GitHub CI <builds@github-ci.com>
2020-07-15 09:20:40 +0000

    AUTO sphinxcontrib-versioning 20200714 b47625ca36f
    CI: true
    LANG: C.UTF-8
    USER: runner

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0a1',
+    VERSION: '1.10.7',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
