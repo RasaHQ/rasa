e7715a3b1f9ee9ec8f07e4e50db6dc4007933824
m-vdb <m.verger@rasa.com>
2020-07-15 17:04:00 +0200

    fix command + remove unnecessary print

--- scripts/push_docs_to_branch.sh
+++ scripts/push_docs_to_branch.sh
@@ -5,13 +5,11 @@ set -Eeuo pipefail
 TMP_DOCS_FOLDER=tmp-documentation
 TODAY=`date "+%Y%m%d"`
 
-# use a temp directory, clone the $DOCS_BRANCH in it
-echo "Cloning $GITHUB_REPOSITORY#$DOCS_BRANCH into $TMP_DOCS_FOLDER..."
-mkdir $TMP_DOCS_FOLDER
-git clone --depth=1 --branch $DOCS_BRANCH git@github.com:$GITHUB_REPOSITORY.git $TMP_DOCS_FOLDER
+# clone the $DOCS_BRANCH in a temp directory
+git clone --depth=1 --branch=$DOCS_BRANCH git@github.com:$GITHUB_REPOSITORY.git $TMP_DOCS_FOLDER
 
 echo "Updating the docs..."
-cp $DOCS_FOLDER/* $TMP_DOCS_FOLDER/
+cp -R $DOCS_FOLDER/* $TMP_DOCS_FOLDER/
 cd $TMP_DOCS_FOLDER
 
 if [ -z "$NEW_VERSION" ]
