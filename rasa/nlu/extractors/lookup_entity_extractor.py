e359e70ef796142c29aaadfe95a20b7320f56560
Tanja Bergmann <tabergma@gmail.com>
2020-07-15 10:47:15 +0200

    update lookup_entity_extractor.py

--- rasa/nlu/extractors/lookup_entity_extractor.py
+++ rasa/nlu/extractors/lookup_entity_extractor.py
@@ -3,7 +3,13 @@ import logging
 from typing import Any, Dict, List, Optional, Text
 
 import rasa.utils.common as common_utils
-from rasa.nlu.constants import ENTITIES
+from rasa.nlu.constants import (
+    ENTITIES,
+    ENTITY_ATTRIBUTE_TYPE,
+    ENTITY_ATTRIBUTE_START,
+    ENTITY_ATTRIBUTE_VALUE,
+    ENTITY_ATTRIBUTE_END,
+)
 from rasa.nlu.training_data import Message
 from rasa.nlu.extractors.extractor import EntityExtractor
 
@@ -19,7 +25,7 @@ class LookupEntityExtractor(EntityExtractor):
 
     defaults = {
         # lookup key for extracting lookup entities,
-        # it contains the dictonary of lookup entity names
+        # it contains the dictionary of lookup entity names
         # and their respective data files
         # example:
         # - name: LookupEntityExtractor
@@ -28,101 +34,101 @@ class LookupEntityExtractor(EntityExtractor):
         #      person: /some/other/path/person.txt
         "lookup": None,
         # lower case the entity value from the lookup file and
-        # user query while comparing them
-        "lowercase": True
+        # user message while comparing them
+        "lowercase": True,
     }
 
     def __init__(self, component_config: Optional[Dict[Text, Any]] = None):
         super(LookupEntityExtractor, self).__init__(component_config)
 
-        if component_config is not None and "lookup" in component_config:
-            if component_config["lookup"] is not None:
-                # check if the entities and respective file path exists
-                for key, value in list(component_config["lookup"].items()):
-                    self._validate_lookup_entry(key, value)
-            else:
-                message = (
-                    "Can't extract Lookup Entities, "
-                    "Please provide a valid entries for "
-                    "entities and their respective file paths."
-                )
-                raise ValueError(message)
-        else:
+        if self.component_config["lookup"] is None:
             message = (
-                "Can't extract Lookup Entities, "
-                "Please configure the lookup entities in the config.yml."
+                "Can't extract any entities as the option 'lookup' is not set."
+                "Please provide valid lookup entries for entities and their respective "
+                "file paths."
+            )
+            raise ValueError(message)
+
+        self.lookup = self.component_config["lookup"]
+        self.lowercase = self.component_config["lowercase"]
+
+        # check if the entities and respective file path exists
+        for entity_type, file_path in self.lookup.items():
+            self._validate_lookup_entry(entity_type, file_path)
+
+        if not self.lookup:
+            message = (
+                "Can't extract any entities as the option 'lookup' just contains "
+                "invalid entries. Please provide valid lookup entries for entities and "
+                "their respective file paths."
             )
             raise ValueError(message)
 
     def _validate_lookup_entry(self, entity: Text, file_path: Text) -> None:
-        if file_path is not None:
-            if not os.path.isfile(file_path):
-                # remove the entity from the lookup dictionary,
-                # if the file path doesn't exist
-                self.component_config["lookup"].pop(entity)
-                message = (
-                    f"The file path '{file_path}' for entity '{entity}' "
-                    "does not exist. "
-                    "Please provide a valid file path."
-                )
-                common_utils.raise_warning(message)
-        else:
+        if file_path is None:
             # remove the entity from the lookup dictionary,
             # if the file path is not provided
-            self.component_config["lookup"].pop(entity)
+            self.lookup.pop(entity)
             message = (
                 f"No file path for entity '{entity}' was given. "
-                "Please provide a valid file path."
+                f"Please provide a valid file path."
             )
             common_utils.raise_warning(message)
 
-    def _parse_entities(self, user_input: Text) -> List[Dict[Text, Any]]:
-        """Extract entities from the user input."""
-        for entity, file_path in self.component_config["lookup"].items():
-            results = self._extract_entities(user_input, entity, file_path)
-            return results
+        elif not os.path.isfile(file_path):
+            # remove the entity from the lookup dictionary,
+            # if the file path doesn't exist
+            self.lookup.pop(entity)
+            message = (
+                f"The file path '{file_path}' for entity '{entity}' does not exist. "
+                f"Please provide a valid file path."
+            )
+            common_utils.raise_warning(message)
+
+    def _parse_message(self, user_message: Text) -> List[Dict[Text, Any]]:
+        """Parse the given user message and extract the entities."""
+        entities = []
+
+        for entity, file_path in self.lookup.items():
+            entities += self._extract_entities(user_message, entity, file_path)
+
+        return entities
 
     def _extract_entities(
-        self, user_input: Text,
-        entity: Text, file_path: Text
+        self, user_message: Text, entity: Text, file_path: Text
     ) -> List[Dict[Text, Any]]:
-        """
-        This method does the actual entity extraction work.
-        So here we are running the loop over the list of data in the text file
-        and check whether it exists in the user's message
-        """
-        results = []
-        user_input_temp = user_input
-        if self.component_config["lowercase"]:
-            # check for lower case
-            user_input_temp = user_input.lower()
+        """Extract entities of the given type from the given user message."""
+        entities = []
+        _user_message = user_message
+        if self.lowercase:
+            _user_message = _user_message.lower()
 
         with open(file_path, "r") as file:
             for example in file:
-                # check for lower case
-                if self.component_config["lowercase"]:
-                    example = example.lower().strip()
-                else:
+                example = example.strip()
+                if self.lowercase:
                     example = example.strip()
-                if example in user_input_temp:
-                    start_index = user_input_temp.index(example)
+
+                if example in _user_message:
+                    start_index = _user_message.index(example)
                     end_index = start_index + len(example)
-                    results.append({
-                        "entity": entity,
-                        "start": start_index,
-                        "end": end_index,
-                        "value": user_input[start_index:end_index]
-                    })
-        return results
+                    entities.append(
+                        {
+                            ENTITY_ATTRIBUTE_TYPE: entity,
+                            ENTITY_ATTRIBUTE_START: start_index,
+                            ENTITY_ATTRIBUTE_END: end_index,
+                            ENTITY_ATTRIBUTE_VALUE: user_message[start_index:end_index],
+                        }
+                    )
+
+        return entities
 
     def process(self, message: Message, **kwargs: Any) -> None:
         """Retrieve the text message, parse the entities."""
 
-        extracted_entities = self._parse_entities(message.text)
+        extracted_entities = self._parse_message(message.text)
         extracted_entities = self.add_extractor_name(extracted_entities)
 
         message.set(
-            ENTITIES,
-            message.get(ENTITIES, []) + extracted_entities,
-            add_to_output=True,
+            ENTITIES, message.get(ENTITIES, []) + extracted_entities, add_to_output=True
         )
