67db047ea9291f47fad519f9aedf2a9e57f9e574
m-vdb <m.verger@rasa.com>
2020-07-15 17:44:15 +0200

    also push source files

--- scripts/push_docs_to_branch.sh
+++ scripts/push_docs_to_branch.sh
@@ -2,6 +2,9 @@
 
 set -Eeuo pipefail
 
+RASA_FOLDER=rasa
+TESTS_FOLDER=tests
+DATA_FOLDER=data
 TMP_DOCS_FOLDER=tmp-documentation
 TODAY=`date "+%Y%m%d"`
 
@@ -9,7 +12,12 @@ TODAY=`date "+%Y%m%d"`
 git clone --depth=1 --branch=$DOCS_BRANCH git@github.com:$GITHUB_REPOSITORY.git $TMP_DOCS_FOLDER
 
 echo "Updating the docs..."
-cp -R $DOCS_FOLDER/* $TMP_DOCS_FOLDER/
+cp -R $DOCS_FOLDER $TMP_DOCS_FOLDER/docs
+# we need these because they are imported by docs files
+cp -R $RASA_FOLDER $TMP_DOCS_FOLDER/$RASA_FOLDER
+cp -R $DATA_FOLDER $TMP_DOCS_FOLDER/$DATA_FOLDER
+cp -R $TESTS_FOLDER $TMP_DOCS_FOLDER/$TESTS_FOLDER
+
 cd $TMP_DOCS_FOLDER
 
 if [ -z "$NEW_VERSION" ]
