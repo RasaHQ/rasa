c6aa1ccaff336ae7a775d47e3c989c64fa95c5dc
Tobias Wochinger <t.wochinger@rasa.com>
2020-07-17 15:09:24 +0200

    Add story data to nlu and e2e actions to domain (#6083) (#6220)
    * Add story data to nlu and e2e actions to domain (#6083)
    
    * make `intent_name` a property
    
    * add missing constructor return type `None`
    
    * add additional field `e2e_text` to `ActionExecuted`
    
    * add `E2EImporter`
    
    * add e2e actions from stories to domain
    
    * set `e2e_text` when reading data
    
    * wrap importer with `E2EImporter`
    
    * implement caching for stories
    
    * Apply suggestions from code review
    
    Co-authored-by: ricwo <20581300+ricwo@users.noreply.github.com>
    
    * more type annotations
    
    * move `project` fixture to root `conftest`
    
    * extract `additional_messages_from_stories`
    
    * pass correct language to TrainingDataImporter
    
    * fix 'e2e_text' type
    
    Co-authored-by: ricwo <20581300+ricwo@users.noreply.github.com>
    
    * don't use `E2EImporter` when training NLU only
    
    * don't use additional training examples for IntentClassifiers and EntityExtractors
    
    * relax NLU example min text length due to additional e2e examples
    
    * remove old markdown e2e hack
    
    * fix spacy index out range error
    
    * fix pytype errors
    
    Co-authored-by: ricwo <20581300+ricwo@users.noreply.github.com>

--- rasa/importers/importer.py
+++ rasa/importers/importer.py
@@ -382,7 +382,6 @@ def _messages_from_action(event: ActionExecuted) -> Message:
         action_name=event.action_name, action_text=event.e2e_text or "",
     )
 
-
 def _additional_training_data_from_default_actions() -> TrainingData:
     from rasa.nlu.training_data import Message
     from rasa.core.actions import action
