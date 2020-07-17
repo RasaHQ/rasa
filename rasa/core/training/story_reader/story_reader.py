2a8e2979a1d06ca061022a6e792788bd742646fc
Tobias Wochinger <t.wochinger@rasa.com>
2020-07-17 11:02:35 +0200

    remove old markdown e2e hack

--- rasa/core/training/story_reader/story_reader.py
+++ rasa/core/training/story_reader/story_reader.py
@@ -42,19 +42,12 @@ class StoryReader:
         self._add_current_stories_to_result()
         self.current_step_builder = StoryStepBuilder(name, source_name)
 
-    def _add_event(
-        self, event_name: Text, parameters: Dict[Text, Any], is_e2e: bool = False
-    ) -> None:
+    def _add_event(self, event_name: Text, parameters: Dict[Text, Any]) -> None:
 
         # add 'name' only if event is not a SlotSet,
         # because there might be a slot with slot_key='name'
         if "name" not in parameters and event_name != SlotSet.type_name:
             parameters["name"] = event_name
-
-            if is_e2e:
-                # TODO: This is somewhat hacky and needs be cleaned up in YAML
-                #  implementation
-                parameters["e2e_text"] = action_as_message.text
 
         parsed_events = Event.from_story_string(
             event_name, parameters, default=ActionExecuted
