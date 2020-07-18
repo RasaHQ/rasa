7d50813897f2a709384226db3b83914423aedf4f
Zhenya Razumovskaia <evgeniar@yahoo.com>
2020-07-18 12:06:03 -0400

    removing unnecessary imports

--- rasa/nlu/model.py
+++ rasa/nlu/model.py
@@ -17,8 +17,6 @@ from rasa.nlu.extractors.extractor import EntityExtractor  # pytype: disable=pyi
 from rasa.nlu.persistor import Persistor
 from rasa.nlu.training_data import Message, TrainingData
 from rasa.nlu.utils import write_json_to_file
-from rasa.nlu.classifiers.classifier import IntentClassifier
-from rasa.nlu.extractors.extractor import EntityExtractor
 from rasa.nlu.constants import ACTION_NAME, ACTION_TEXT, MESSAGE_INTENT_NAME
 
 MODEL_NAME_PREFIX = "nlu_"
