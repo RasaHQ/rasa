a417d56801a29aee20d97c223649567cb0b8c113
alwx <alwxndr@gmail.com>
2020-07-19 21:23:34 +0200

    Small code style updates

--- rasa/core/__init__.py
+++ rasa/core/__init__.py
@@ -3,6 +3,7 @@ import logging
 import rasa
 
 from rasa.core.train import train
+from rasa.core.test import evaluate_stories
 from rasa.core.visualize import visualize
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
