5faa4bb157f0c4e69ba7844425fdc5d9c36d6661
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-07-23 14:21:56 -0400

    allow rasa to run without interruption when model is outdated

--- rasa/core/interpreter.py
+++ rasa/core/interpreter.py
@@ -37,7 +37,12 @@ class NaturalLanguageInterpreter:
         if isinstance(obj, NaturalLanguageInterpreter):
             return obj
         elif isinstance(obj, str) and os.path.exists(obj):
-            return RasaNLUInterpreter(model_directory=obj)
+            from rasa.nlu.model import UnsupportedModelError
+            try:
+                return RasaNLUInterpreter(model_directory=obj)
+            except UnsupportedModelError as e:
+                logger.warning(e.message)
+                return RegexInterpreter()
         elif isinstance(obj, str) and not os.path.exists(obj):
             # user passed in a string, but file does not exist
             logger.warning(
--- rasa/core/policies/ensemble.py
+++ rasa/core/policies/ensemble.py
@@ -318,11 +318,15 @@ class PolicyEnsemble:
             )
 
     @classmethod
-    def load(cls, path: Text) -> "PolicyEnsemble":
+    def load(cls, path: Text) -> Optional["PolicyEnsemble"]:
         """Loads policy and domain specification from storage"""
 
         metadata = cls.load_metadata(path)
-        cls.ensure_model_compatibility(metadata)
+        try:
+            cls.ensure_model_compatibility(metadata)
+        except UnsupportedDialogueModelError as e:
+            logger.warning(e.message)
+            return None
         policies = []
         for i, policy_name in enumerate(metadata["policy_names"]):
             policy_cls = registry.policy_from_module_path(policy_name)
