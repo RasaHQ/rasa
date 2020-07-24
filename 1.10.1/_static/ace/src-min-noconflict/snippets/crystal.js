6aefb9bca0059256c13bb7975cb91f2a2eb25fb2
GitHub CI <builds@github-ci.com>
2020-07-24 16:43:52 +0000

    AUTO sphinxcontrib-versioning 20200724 ffeb6eaeb7c
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.1/_static/ace/src-min-noconflict/snippets/crystal.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/crystal"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
---
+++ 2.0.0a1/_static/ace/src-min-noconflict/snippets/lisp.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/lisp"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
