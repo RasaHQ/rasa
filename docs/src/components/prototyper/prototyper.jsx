81e1dc16d8dc8897a9bea24bd02f9055a519f5f7
m-vdb <m.verger@rasa.com>
2020-07-29 16:10:43 +0200

    <Prototyoper /> high-level component

---
+++ docs/src/components/prototyper/prototyper.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import ThemeContext from '@theme/theme-context';
+
+const Prototyper = ({children, startPrototyperApi}) => {
+    const [hasStarted, setHasStarted] = useState(false);
+    const [trainingData, setTrainingData] = useState({});
+
+    const onLiveCodeStart = (name, value) => {
+        setTrainingData({...trainingData, name: value});
+    };
+
+    const onLiveCodeChange = (name, value) => {
+        setTrainingData({...trainingData, name: value});
+        if (!hasStarted) {
+            // track the start here
+            setHasStarted(true);
+            fetch(startPrototyperApi, {
+                method: 'POST'
+            });
+        }
+    };
+
+    return (
+      <ThemeContext.Provider value={{onLiveCodeChange, onLiveCodeStart}}>
+        {children}
+      </ThemeContext.Provider>
+    );
+};
+
+export default Prototyper;
