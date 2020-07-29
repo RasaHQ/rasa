23873620dae01135c984056764f818623739de81
GitHub CI <builds@github-ci.com>
2020-07-29 12:35:03 +0000

    AUTO sphinxcontrib-versioning 20200729 c3c31bf972e
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.4.6/_static/ace/src-min-noconflict/snippets/plain_text.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/plain_text"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
