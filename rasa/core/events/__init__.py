ebc782b87e5beaffb609ebc4915e8d77fc279f34
Zhenya Razumovskaia <evgeniar@yahoo.com>
2020-08-02 08:35:07 -0400

    black formatting

--- rasa/core/events/__init__.py
+++ rasa/core/events/__init__.py
@@ -1016,10 +1016,13 @@ class ActionExecuted(Event):
             return False
         else:
             # if both have e2e_text compare both action name and e2e text
-            if hasattr(self, 'e2e_text') and hasattr(other, 'e2e_text'):
-                return self.action_name == other.action_name and self.e2e_text == other.e2e_text
+            if hasattr(self, "e2e_text") and hasattr(other, "e2e_text"):
+                return (
+                    self.action_name == other.action_name
+                    and self.e2e_text == other.e2e_text
+                )
             # if both of them don't have the attribute, only compare action name
-            if hasattr(self, 'e2e_text') == hasattr(other, 'e2e_text'):
+            if hasattr(self, "e2e_text") == hasattr(other, "e2e_text"):
                 return self.action_name == other.action_name
             else:
                 return False
