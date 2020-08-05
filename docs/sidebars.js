0da1886118ab3f6e9e9024df130d4bdb85959cd9
Ella <erohmensing@gmail.com>
2020-08-05 16:46:43 +0200

    move some sidebar stuff around for now

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,177 +2,191 @@ module.exports = {
   someSidebar: [
     {
       type: 'category',
-      label: 'Getting Started',
-      collapsed: false,
-      items: [
-        'index',
-        'prototype-an-assistant',
-        'installation',
-        'cheatsheet',
-        'migrate-from',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Best Practices',
-      collapsed: false,
-      items: [
-        'cdd',
-        'generating-nlu-data',
-        'writing-stories',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Conversation Patterns',
-      collapsed: false,
-      items: [
-        'chitchat-faqs',
-        'business-logic',
-        'fallback-handoff',
-        'unexpected-input',
-        'contextual-conversations',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Preparing For Production',
-      collapsed: false,
-      items: [
-        'tuning-your-model',
-        'testing-your-assistant',
-        'setting-up-ci-cd',
-        'how-to-deploy',
-        'messaging-and-voice-channels',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Reference',
-      collapsed: false,
-      items: [
-        'glossary',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Rasa Open Source',
+      label: 'Building Assistants',
       collapsed: false,
       items: [
         {
           type: 'category',
-          label: 'Training Data',
+          label: 'Getting Started',
+          collapsed: true,
           items: [
-            'training-data-format',
-            'stories',
+            'index',
+            'prototype-an-assistant',
+            'installation',
+            'cheatsheet',
+            'migrate-from',
           ],
         },
-        'domain',
         {
           type: 'category',
-          label: 'Config',
+          label: 'Best Practices',
+          collapsed: true,
+          items: [
+            'cdd',
+            'generating-nlu-data',
+            'writing-stories',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Conversation Patterns',
+          collapsed: true,
+          items: [
+            'chitchat-faqs',
+            'business-logic',
+            'fallback-handoff',
+            'unexpected-input',
+            'contextual-conversations',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Preparing For Production',
+          collapsed: true,
+          items: [
+            'tuning-your-model',
+            'testing-your-assistant',
+            'setting-up-ci-cd',
+            'how-to-deploy',
+            'messaging-and-voice-channels',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Reference',
+          collapsed: true,
+          items: [
+            'glossary',
+          ],
+        },
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Rasa Concepts',
+      collapsed: false,
+      items: [
+        {
+          type: 'category',
+          label: 'Rasa Open Source',
+          collapsed: true,
           items: [
             {
               type: 'category',
-              label: 'Pipeline Components',
+              label: 'Training Data',
               items: [
-                'components/language-models',
-                'components/tokenizers',
-                'components/featurizers',
-                'components/intent-classifiers',
-                'components/entity-extractors',
-                'components/selectors',
-                'components/custom-nlu-components',
+                'training-data-format',
+                'stories',
+              ],
+            },
+            'domain',
+            {
+              type: 'category',
+              label: 'Config',
+              items: [
+                {
+                  type: 'category',
+                  label: 'Pipeline Components',
+                  items: [
+                    'components/language-models',
+                    'components/tokenizers',
+                    'components/featurizers',
+                    'components/intent-classifiers',
+                    'components/entity-extractors',
+                    'components/selectors',
+                    'components/custom-nlu-components',
+                  ],
+                },
+                {
+                  type: 'category',
+                  label: 'Policies',
+                  items: [
+                    'policies',
+                  ],
+                },
+                'training-data-importers',
               ],
             },
             {
               type: 'category',
-              label: 'Policies',
+              label: 'Actions',
               items: [
-                'policies',
+                'responses',
+                'retrieval-actions',
+                'forms',
+                'reminders-and-external-events',
+                'default-actions',
               ],
             },
-            'training-data-importers',
+            {
+              type: 'category',
+              label: 'Channel Connectors',
+              items: [
+                'connectors/your-own-website',
+                'connectors/facebook-messenger',
+                'connectors/slack',
+                'connectors/telegram',
+                'connectors/twilio',
+                'connectors/hangouts',
+                'connectors/microsoft-bot-framework',
+                'connectors/cisco-webex-teams',
+                'connectors/rocketchat',
+                'connectors/mattermost',
+              ],
+            },
+            {
+              type: 'category',
+              label: 'Architecture', // name still confusing with architecture page elsewhere
+              items: [
+                'tracker-stores',
+                'event-brokers',
+                'model-storage',
+                'lock-stores',
+                'nlg',
+              ],
+            },
           ],
         },
         {
           type: 'category',
-          label: 'Actions',
+          label: 'Rasa SDK',
+          collapsed: true,
           items: [
-            'responses',
-            'retrieval-actions',
-            'forms',
-            'reminders-and-external-events',
-            'default-actions',
+            'running-action-server',
+            {
+              type: 'category',
+              label: 'Custom Actions',
+              items: [
+                'actions',
+                'knowledge-bases',
+              ],
+            },
+            'tracker-dispatcher',
+            'events',
+            'rasa-sdk-changelog'
           ],
         },
         {
           type: 'category',
-          label: 'Channel Connectors',
+          label: 'APIs',
+          collapsed: true,
           items: [
-            'connectors/your-own-website',
-            'connectors/facebook-messenger',
-            'connectors/slack',
-            'connectors/telegram',
-            'connectors/twilio',
-            'connectors/hangouts',
-            'connectors/microsoft-bot-framework',
-            'connectors/cisco-webex-teams',
-            'connectors/rocketchat',
-            'connectors/mattermost',
+            'architecture',
+            'command-line-interface',
+            'http-api',
+            'jupyter-notebooks',
           ],
         },
         {
           type: 'category',
-          label: 'Architecture', // name still confusing with architecture page elsewhere
+          label: 'Versioning',
+          collapsed: true,
           items: [
-            'tracker-stores',
-            'event-brokers',
-            'model-storage',
-            'lock-stores',
-            'nlg',
+            'changelog',
+            'migration-guide',
           ],
         },
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Rasa SDK',
-      collapsed: false,
-      items: [
-        'running-action-server',
-        {
-          type: 'category',
-          label: 'Custom Actions',
-          items: [
-            'actions',
-            'knowledge-bases',
-          ],
-        },
-        'tracker-dispatcher',
-        'events',
-        'rasa-sdk-changelog'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Reference',
-      collapsed: false,
-      items: [
-        'architecture',
-        'command-line-interface',
-        'http-api',
-        'jupyter-notebooks',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Versioning',
-      collapsed: false,
-      items: [
-        'changelog',
-        'migration-guide',
-      ],
-    },
+      ]
+    }
   ],
 };
