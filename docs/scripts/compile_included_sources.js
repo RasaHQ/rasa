862a145b76340addbfc66a246621d136bf319c3b
GitHub CI <builds@github-ci.com>
2020-08-05 13:59:35 +0000

    AUTO docusaurus 20200805

---
+++ docs/scripts/compile_included_sources.js
@@ -0,0 +1,9 @@
+const getIncludedSources = require('../plugins/included_source.js');
+
+
+
+console.info('Computing included sources');
+getIncludedSources({
+    docsDir: './docs',
+    include: ['**.mdx', '**.md'],
+});
