f1cce59d792cb286057cea5ea88520fa7532c8fb
Zhenya Razumovskaia <evgeniar@yahoo.com>
2020-08-08 20:34:29 -0400

    type check

--- rasa/nlu/model.py
+++ rasa/nlu/model.py
@@ -398,12 +398,8 @@ class Interpreter:
         Returns:
             message: it contains the tokens and features which are the output of the NLU pipeline;
         """
-        from rasa.nlu.extractors import extractor
-        from rasa.nlu.classifiers import classifier
 
         for component in self.pipeline:
-            if not isinstance(
-                component, (extractor.EntityExtractor, classifier.IntentClassifier)
-            ):
+            if not isinstance(component, (EntityExtractor, IntentClassifier)):
                 component.process(message, attribute, **self.context)
         return message
