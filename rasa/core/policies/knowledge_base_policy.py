7f06420b7f60e49a5c4bfa7a380837515ea4cb10
Tanja Bergmann <tabergma@gmail.com>
2020-08-12 12:06:26 +0200

    knowledge base policy

--- rasa/core/policies/knowledge_base_policy.py
+++ rasa/core/policies/knowledge_base_policy.py
@@ -1,10 +1,424 @@
-from typing import Any
+import copy
+import logging
+import os
+from pathlib import Path
 
-from core.policies.ted_policy import TEDPolicy
+import numpy as np
+import tensorflow as tf
+import tensorflow_addons as tfa
+from scipy import sparse
+
+from typing import Any, List, Optional, Text, Dict, Tuple, Union
+
+import rasa.utils.io as io_utils
+from rasa.core.policies.ted_policy import TEDPolicy
+from rasa.core.domain import Domain
+from rasa.core.featurizers import (
+    TrackerFeaturizer,
+    FullDialogueTrackerFeaturizer,
+    LabelTokenizerSingleStateFeaturizer,
+    MaxHistoryTrackerFeaturizer,
+    E2ESingleStateFeaturizer,
+)
+from rasa.nlu.constants import ACTION_NAME, INTENT, ACTION_TEXT
+from rasa.core.interpreter import NaturalLanguageInterpreter, RegexInterpreter
+from rasa.core.policies.policy import Policy
+from rasa.core.constants import DEFAULT_POLICY_PRIORITY, DIALOGUE
+from rasa.core.trackers import DialogueStateTracker
+from rasa.utils import train_utils
+from rasa.utils.tensorflow import layers
+from rasa.utils.tensorflow.transformer import TransformerEncoder
+from rasa.utils.tensorflow.models import RasaModel
+from rasa.utils.tensorflow.model_data import RasaModelData, FeatureSignature
+from rasa.utils.tensorflow.constants import (
+    LABEL,
+    HIDDEN_LAYERS_SIZES,
+    TRANSFORMER_SIZE,
+    NUM_TRANSFORMER_LAYERS,
+    NUM_HEADS,
+    BATCH_SIZES,
+    BATCH_STRATEGY,
+    EPOCHS,
+    RANDOM_SEED,
+    RANKING_LENGTH,
+    LOSS_TYPE,
+    SIMILARITY_TYPE,
+    NUM_NEG,
+    EVAL_NUM_EXAMPLES,
+    EVAL_NUM_EPOCHS,
+    NEGATIVE_MARGIN_SCALE,
+    REGULARIZATION_CONSTANT,
+    SCALE_LOSS,
+    USE_MAX_NEG_SIM,
+    MAX_NEG_SIM,
+    MAX_POS_SIM,
+    EMBEDDING_DIMENSION,
+    DROP_RATE_DIALOGUE,
+    DROP_RATE_LABEL,
+    DROP_RATE_ATTENTION,
+    WEIGHT_SPARSITY,
+    KEY_RELATIVE_ATTENTION,
+    VALUE_RELATIVE_ATTENTION,
+    MAX_RELATIVE_POSITION,
+    SOFTMAX,
+    AUTO,
+    BALANCED,
+    TENSORBOARD_LOG_DIR,
+    TENSORBOARD_LOG_LEVEL,
+)
+
+
+logger = logging.getLogger(__name__)
+
+DIALOGUE_FEATURES = f"{DIALOGUE}_features"
+LABEL_FEATURES = f"{LABEL}_features"
+LABEL_IDS = f"{LABEL}_ids"
+
+SAVE_MODEL_FILE_NAME = "knowledge_base_policy"
 
 
 class KnowledgeBasePolicy(TEDPolicy):
-    def __init__(self, **kwargs: Any):
-        super().__init__(**kwargs)
 
-    # TODO
+    SUPPORTS_ONLINE_TRAINING = True
+
+    # please make sure to update the docs when changing a default parameter
+    defaults = {
+        # ## Architecture of the used neural network
+        # Hidden layer sizes for layers before the dialogue and label embedding layers.
+        # The number of hidden layers is equal to the length of the corresponding
+        # list.
+        HIDDEN_LAYERS_SIZES: {DIALOGUE: [], LABEL: [], f"{DIALOGUE}_name_text": [100]},
+        # Number of units in transformer
+        TRANSFORMER_SIZE: 128,
+        # Number of transformer layers
+        NUM_TRANSFORMER_LAYERS: 1,
+        # Number of attention heads in transformer
+        NUM_HEADS: 4,
+        # If 'True' use key relative embeddings in attention
+        KEY_RELATIVE_ATTENTION: False,
+        # If 'True' use value relative embeddings in attention
+        VALUE_RELATIVE_ATTENTION: False,
+        # Max position for relative embeddings
+        MAX_RELATIVE_POSITION: None,
+        # ## Training parameters
+        # Initial and final batch sizes:
+        # Batch size will be linearly increased for each epoch.
+        BATCH_SIZES: [8, 32],
+        # Strategy used whenc creating batches.
+        # Can be either 'sequence' or 'balanced'.
+        BATCH_STRATEGY: BALANCED,
+        # Number of epochs to train
+        EPOCHS: 1,
+        # Set random seed to any 'int' to get reproducible results
+        RANDOM_SEED: None,
+        # ## Parameters for embeddings
+        # Dimension size of embedding vectors
+        EMBEDDING_DIMENSION: 20,
+        # The number of incorrect labels. The algorithm will minimize
+        # their similarity to the user input during training.
+        NUM_NEG: 20,
+        # Type of similarity measure to use, either 'auto' or 'cosine' or 'inner'.
+        SIMILARITY_TYPE: AUTO,
+        # The type of the loss function, either 'softmax' or 'margin'.
+        LOSS_TYPE: SOFTMAX,
+        # Number of top actions to normalize scores for loss type 'softmax'.
+        # Set to 0 to turn off normalization.
+        RANKING_LENGTH: 10,
+        # Indicates how similar the algorithm should try to make embedding vectors
+        # for correct labels.
+        # Should be 0.0 < ... < 1.0 for 'cosine' similarity type.
+        MAX_POS_SIM: 0.8,
+        # Maximum negative similarity for incorrect labels.
+        # Should be -1.0 < ... < 1.0 for 'cosine' similarity type.
+        MAX_NEG_SIM: -0.2,
+        # If 'True' the algorithm only minimizes maximum similarity over
+        # incorrect intent labels, used only if 'loss_type' is set to 'margin'.
+        USE_MAX_NEG_SIM: True,
+        # If 'True' scale loss inverse proportionally to the confidence
+        # of the correct prediction
+        SCALE_LOSS: True,
+        # ## Regularization parameters
+        # The scale of regularization
+        REGULARIZATION_CONSTANT: 0.001,
+        # The scale of how important is to minimize the maximum similarity
+        # between embeddings of different labels,
+        # used only if 'loss_type' is set to 'margin'.
+        NEGATIVE_MARGIN_SCALE: 0.8,
+        # Dropout rate for embedding layers of dialogue features.
+        DROP_RATE_DIALOGUE: 0.1,
+        # Dropout rate for embedding layers of label, e.g. action, features.
+        DROP_RATE_LABEL: 0.0,
+        # Dropout rate for attention.
+        DROP_RATE_ATTENTION: 0,
+        # Sparsity of the weights in dense layers
+        WEIGHT_SPARSITY: 0.8,
+        # ## Evaluation parameters
+        # How often calculate validation accuracy.
+        # Small values may hurt performance, e.g. model accuracy.
+        EVAL_NUM_EPOCHS: 20,
+        # How many examples to use for hold out validation set
+        # Large values may hurt performance, e.g. model accuracy.
+        EVAL_NUM_EXAMPLES: 0,
+        # If you want to use tensorboard to visualize training and validation metrics,
+        # set this option to a valid output directory.
+        TENSORBOARD_LOG_DIR: None,
+        # Define when training metrics for tensorboard should be logged.
+        # Either after every epoch or for every training step.
+        # Valid values: 'epoch' and 'minibatch'
+        TENSORBOARD_LOG_LEVEL: "epoch",
+    }
+
+    @staticmethod
+    def _standard_featurizer(max_history: Optional[int] = None) -> TrackerFeaturizer:
+        if max_history is None:
+            return FullDialogueTrackerFeaturizer(E2ESingleStateFeaturizer())
+        else:
+            return MaxHistoryTrackerFeaturizer(
+                E2ESingleStateFeaturizer(), max_history=max_history
+            )
+
+    def __init__(
+        self,
+        featurizer: Optional[TrackerFeaturizer] = None,
+        priority: int = DEFAULT_POLICY_PRIORITY,
+        max_history: Optional[int] = None,
+        model: Optional[RasaModel] = None,
+        **kwargs: Any,
+    ) -> None:
+        """Declare instance variables with default values."""
+
+        if not featurizer:
+            featurizer = self._standard_featurizer(max_history)
+
+        super().__init__(featurizer, priority)
+        if isinstance(featurizer, FullDialogueTrackerFeaturizer):
+            self.is_full_dialogue_featurizer_used = True
+        else:
+            self.is_full_dialogue_featurizer_used = False
+
+        self._load_params(**kwargs)
+
+        self.model = model
+
+        self._label_data: Optional[RasaModelData] = None
+        self.data_example: Optional[Dict[Text, List[np.ndarray]]] = None
+
+    def _load_params(self, **kwargs: Dict[Text, Any]) -> None:
+        self.config = copy.deepcopy(self.defaults)
+        self.config.update(kwargs)
+
+        self.config = train_utils.check_deprecated_options(self.config)
+
+        self.config = train_utils.update_similarity_type(self.config)
+        self.config = train_utils.update_evaluation_parameters(self.config)
+
+    def train(
+        self,
+        training_trackers: List[DialogueStateTracker],
+        domain: Domain,
+        interpreter: NaturalLanguageInterpreter,
+        **kwargs: Any,
+    ) -> None:
+        """Train the policy on given training trackers."""
+
+        # dealing with training data
+        # TODO we also need to featurize the database schema
+        # TODO labels are the grammar rules
+        training_data = self.featurize_for_training(
+            training_trackers, domain, interpreter, **kwargs
+        )
+
+        self._label_data = self._create_label_data(domain, interpreter)
+
+        training_data_X = self._fill_in_features(training_data.X)
+
+        # extract actual training data to feed to model
+        model_data = self._create_model_data(
+            training_data_X, np.array(training_data.true_length), training_data.y
+        )
+        if model_data.is_empty():
+            logger.error(
+                f"Can not train '{self.__class__.__name__}'. No data was provided. "
+                f"Skipping training of the policy."
+            )
+            return
+
+        # keep one example for persisting and loading
+        self.data_example = model_data.first_data_example()
+
+        self.model = KnowledgeBaseModel(
+            model_data.get_signature(),
+            self.config,
+            isinstance(self.featurizer, MaxHistoryTrackerFeaturizer),
+            self._label_data,
+        )
+
+        self.model.fit(
+            model_data,
+            self.config[EPOCHS],
+            self.config[BATCH_SIZES],
+            self.config[EVAL_NUM_EXAMPLES],
+            self.config[EVAL_NUM_EPOCHS],
+            batch_strategy=self.config[BATCH_STRATEGY],
+        )
+
+    def predict_action_probabilities(
+        self,
+        tracker: DialogueStateTracker,
+        domain: Domain,
+        interpreter: NaturalLanguageInterpreter = RegexInterpreter(),
+        **kwargs: Any,
+    ) -> List[float]:
+        """Predict the next action the bot should take.
+        Return the list of probabilities for the next actions.
+        """
+
+        # TODO always return utter_result as action
+        # TODO update the tracker state and set a QueryEvent
+
+        return [0.0]
+
+    def persist(self, path: Text) -> None:
+        """Persists the policy to a storage."""
+
+        if self.model is None:
+            logger.debug(
+                "Method `persist(...)` was called "
+                "without a trained model present. "
+                "Nothing to persist then!"
+            )
+            return
+
+        model_path = Path(path)
+        tf_model_file = model_path / f"{SAVE_MODEL_FILE_NAME}.tf_model"
+
+        io_utils.create_directory_for_file(tf_model_file)
+
+        self.featurizer.persist(path)
+
+        self.model.save(str(tf_model_file))
+
+        io_utils.json_pickle(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.priority.pkl", self.priority
+        )
+        io_utils.pickle_dump(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.meta.pkl", self.config
+        )
+        io_utils.pickle_dump(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.data_example.pkl", self.data_example
+        )
+        io_utils.pickle_dump(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.label_data.pkl", self._label_data
+        )
+
+    @classmethod
+    def load(cls, path: Text) -> "KnowledgeBasePolicy":
+        """Loads a policy from the storage.
+        **Needs to load its featurizer**
+        """
+
+        if not os.path.exists(path):
+            raise Exception(
+                f"Failed to load KnowledgeBase policy model. Path "
+                f"'{os.path.abspath(path)}' doesn't exist."
+            )
+
+        model_path = Path(path)
+        tf_model_file = model_path / f"{SAVE_MODEL_FILE_NAME}.tf_model"
+
+        featurizer = TrackerFeaturizer.load(path)
+
+        if not (model_path / f"{SAVE_MODEL_FILE_NAME}.data_example.pkl").is_file():
+            return cls(featurizer=featurizer)
+
+        loaded_data = io_utils.pickle_load(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.data_example.pkl"
+        )
+        label_data = io_utils.pickle_load(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.label_data.pkl"
+        )
+        meta = io_utils.pickle_load(model_path / f"{SAVE_MODEL_FILE_NAME}.meta.pkl")
+        priority = io_utils.json_unpickle(
+            model_path / f"{SAVE_MODEL_FILE_NAME}.priority.pkl"
+        )
+
+        model_data_example = RasaModelData(label_key=LABEL_IDS, data=loaded_data)
+        meta = train_utils.update_similarity_type(meta)
+
+        model = KnowledgeBaseModel.load(
+            str(tf_model_file),
+            model_data_example,
+            data_signature=model_data_example.get_signature(),
+            config=meta,
+            max_history_tracker_featurizer_used=isinstance(
+                featurizer, MaxHistoryTrackerFeaturizer
+            ),
+            label_data=label_data,
+        )
+
+        # build the graph for prediction
+        predict_data_example = RasaModelData(
+            label_key=LABEL_IDS,
+            data={
+                feature_name: features
+                for feature_name, features in model_data_example.items()
+                if DIALOGUE in feature_name
+            },
+        )
+        model.build_for_predict(predict_data_example)
+
+        return cls(featurizer=featurizer, priority=priority, model=model, **meta)
+
+
+# accessing _tf_layers with any key results in key-error, disable it
+# pytype: disable=key-error
+
+
+class KnowledgeBaseModel(RasaModel):
+    def __init__(
+        self,
+        data_signature: Dict[Text, List[FeatureSignature]],
+        config: Dict[Text, Any],
+        max_history_tracker_featurizer_used: bool,
+        label_data: RasaModelData,
+    ) -> None:
+        super().__init__(
+            name="KnowledgeBaseModel",
+            random_seed=config[RANDOM_SEED],
+            tensorboard_log_dir=config[TENSORBOARD_LOG_DIR],
+            tensorboard_log_level=config[TENSORBOARD_LOG_LEVEL],
+        )
+
+        self.config = config
+        self.max_history_tracker_featurizer_used = max_history_tracker_featurizer_used
+
+        # data
+        self.data_signature = data_signature
+        self._check_data()
+
+        self.predict_data_signature = {
+            feature_name: features
+            for feature_name, features in data_signature.items()
+            if DIALOGUE in feature_name
+        }
+
+        # optimizer
+        self.optimizer = tf.keras.optimizers.Adam()
+
+        self.all_labels_embed = None
+
+        label_batch = label_data.prepare_batch()
+        self.tf_label_data = self.batch_to_model_data_format(
+            label_batch, label_data.get_signature()
+        )
+
+        # metrics
+        self.action_loss = tf.keras.metrics.Mean(name="loss")
+        self.action_acc = tf.keras.metrics.Mean(name="acc")
+        self.metrics_to_log += ["loss", "acc"]
+
+        # set up tf layers
+        self._tf_layers: Dict[Text : tf.keras.layers.Layer] = {}
+        self._prepare_layers()
+
+        # TODO
