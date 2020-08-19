da290a552e8ef6a32ff45467104a6e0cbd75a0f2
Lu Nelson <lunelson@gmail.com>
2020-08-19 16:05:20 +0200

    move unused theme components

---
+++ docs/themes/theme-classic/theme-unused/MDXPage/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import { MDXProvider } from '@mdx-js/react';
+import MDXComponents from '@theme/MDXComponents';
+
+function MDXPage(props) {
+	const { content: MDXPageContent } = props;
+	const { frontMatter, metadata } = MDXPageContent;
+	const { title, description } = frontMatter;
+	const { permalink } = metadata;
+
+	return (
+		<Layout title={title} description={description} permalink={permalink}>
+			<main>
+				<div className="container margin-vert--lg padding-vert--lg">
+					<MDXProvider components={MDXComponents}>
+						<MDXPageContent />
+					</MDXProvider>
+				</div>
+			</main>
+		</Layout>
+	);
+}
+
+export default MDXPage;
