9521fc935ad38860865dcb39064aafdf320e7fc7
Lu Nelson <lunelson@gmail.com>
2020-08-20 16:03:01 +0200

    convert tabs

--- docs/src/components/redoc.jsx
+++ docs/src/components/redoc.jsx
@@ -3,22 +3,22 @@ import usePromise from 'react-promise';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const Redoc = (props) => {
-	const getRedocStandalone = React.useCallback(async () => {
-		// using import() instead of require() keeps the package tree-shakeable.
-		const redoc = await import('redoc');
-		return redoc.RedocStandalone;
-	}, []);
+  const getRedocStandalone = React.useCallback(async () => {
+    // using import() instead of require() keeps the package tree-shakeable.
+    const redoc = await import('redoc');
+    return redoc.RedocStandalone;
+  }, []);
 
-	return (
-		<BrowserOnly fallback={<div>Loading...</div>}>
-			{() => {
-				// we need to import this here instead of at the top-level
-				// because it causes issues in production builds
-				const { value: RedocStandalone, loading } = usePromise(getRedocStandalone);
-				return loading ? <div>Loading...</div> : <RedocStandalone {...props} />;
-			}}
-		</BrowserOnly>
-	);
+  return (
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        // we need to import this here instead of at the top-level
+        // because it causes issues in production builds
+        const { value: RedocStandalone, loading } = usePromise(getRedocStandalone);
+        return loading ? <div>Loading...</div> : <RedocStandalone {...props} />;
+      }}
+    </BrowserOnly>
+  );
 };
 
 export default Redoc;
