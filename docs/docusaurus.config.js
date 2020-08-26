2d692ad0b594aae6675924a29b431a0cf9ab71f4
Lu Nelson <lunelson@gmail.com>
2020-08-26 12:15:01 +0200

    refactor config to separate usage from API via dropdown

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
-
 const remarkSources = require('remark-sources');
 const remarkCollapse = require('remark-collapse');
-
 const { remarkProgramOutput } = require('./plugins/program_output');
+
+const isDev = process.env.NODE_ENV === 'development';
 
 let versions = [];
 try {
@@ -21,13 +21,14 @@ const legacyVersion = {
 // FIXME: when deploying this for real, change to '/docs/rasa/'
 const BASE_URL = '/docs/rasa/next/';
 const SITE_URL = 'https://rasa.com';
+const RASA_X_URL = isDev ? 'http://localhost:3001' : SITE_URL;
 
 module.exports = {
   title: 'Rasa Open Source Documentation',
   tagline: 'Rasa Open Source Documentation',
   url: SITE_URL,
   baseUrl: BASE_URL,
-  favicon: 'img/favicon.ico',
+  favicon: '/img/favicon.ico',
   organizationName: 'RasaHQ',
   projectName: 'rasa',
   themeConfig: {
@@ -35,78 +36,96 @@ module.exports = {
       defaultMode: 'light',
       disableSwitch: true,
     },
-    versions: {
-      label: 'Versions',
-      to: '/', // "fake" link
-      position: 'left',
-      items:
-        versions.length > 0
-          ? [
-              {
-                label: versions[0],
-                to: '/',
-                activeBaseRegex: versions[0],
-              },
-              ...versions.slice(1).map((version) => ({
-                label: version,
-                to: `${version}/`,
-                activeBaseRegex: version,
-              })),
-              {
-                label: 'Master/Unreleased',
-                to: 'next/',
-                activeBaseRegex: `next`,
-              },
-              legacyVersion,
-            ]
-          : [
-              {
-                label: 'Master/Unreleased',
-                to: '/',
-                activeBaseRegex: `/`,
-              },
-              legacyVersion,
-            ],
-    },
     navbar: {
       hideOnScroll: false,
-      title: 'Rasa Docs',
+      title: 'Rasa Open Source',
       logo: {
-        alt: 'Rasa',
-        src: `${SITE_URL}/static/60e441f8eadef13bea0cc790c8cf188b/rasa-logo.svg`,
+        alt: 'Rasa Logo',
+        src: `/img/rasa-logo.svg`,
       },
       items: [
         {
-          href: path.join(SITE_URL, '/docs/'),
-          label: 'Overview',
+          href: `${SITE_URL}/docs/rasa-overview/`,
+          label: 'Overview of Rasa',
           position: 'left',
+          target: '_self',
         },
         {
-          to: path.join('/', BASE_URL),
           label: 'Rasa Open Source',
+          to: path.join('/', BASE_URL), // for purpose of route match styling
           position: 'left',
+          items: [
+            {
+              to: path.join('/', BASE_URL),
+              label: 'Usage',
+            },
+            {
+              to: path.join('/', BASE_URL, 'api'),
+              label: 'API',
+            },
+          ],
         },
         {
-          href: path.join(SITE_URL, '/docs/rasa-x/next/'),
           label: 'Rasa X',
           position: 'left',
+          items: [
+            {
+              href: `${RASA_X_URL}/docs/rasa-x/next/`,
+              label: 'Usage',
+              target: '_self',
+            },
+            {
+              href: `${RASA_X_URL}/docs/rasa-x/next/api`,
+              label: 'API',
+              target: '_self',
+            },
+          ],
         },
         {
-          to: path.join('/', BASE_URL, 'api'),
-          label: 'API',
-          position: 'left',
-        },
-        {
-          href: 'https://blog.rasa.com/',
+          target: '_self',
+          href: 'http://blog.rasa.com/',
           label: 'Blog',
           position: 'right',
         },
         {
-          href: path.join(SITE_URL, '/community/join/'),
+          target: '_self',
+          href: `${SITE_URL}/community/join/`,
           label: 'Community',
           position: 'right',
         },
-      ],
+        {
+          label: 'Versions',
+          to: '/', // "fake" link
+          position: 'left',
+          items:
+            versions.length > 0
+              ? [
+                  {
+                    label: versions[0],
+                    to: '/',
+                    activeBaseRegex: versions[0],
+                  },
+                  ...versions.slice(1).map((version) => ({
+                    label: version,
+                    to: `${version}/`,
+                    activeBaseRegex: version,
+                  })),
+                  {
+                    label: 'Master/Unreleased',
+                    to: 'next/',
+                    activeBaseRegex: `next`,
+                  },
+                  legacyVersion,
+                ]
+              : [
+                  {
+                    label: 'Master/Unreleased',
+                    to: '/',
+                    activeBaseRegex: `/`,
+                  },
+                  legacyVersion,
+                ],
+        }      ],
     },
     footer: {
       style: 'dark',
@@ -131,14 +150,12 @@ module.exports = {
       },
     ],
     ['@docusaurus/plugin-content-pages', {}],
-    [
-      '@docusaurus/plugin-sitemap',
+    ['@docusaurus/plugin-sitemap',
       {
         cacheTime: 600 * 1000, // 600 sec - cache purge period
         changefreq: 'weekly',
         priority: 0.5,
-      },
-    ],
+      }],
     [path.resolve(__dirname, './plugins/google-tagmanager'), {}],
   ],
 };
