d70365bab5402007d1e5a2ec0d53317700142256
Tobias Wochinger <t.wochinger@rasa.com>
2020-09-04 11:14:08 +0200

    remove randomness

--- tests/shared/utils/test_common.py
+++ tests/shared/utils/test_common.py
@@ -4,11 +4,9 @@ import rasa.shared.utils.common
 
 
 def test_all_subclasses():
-    num = random.randint(1, 10)
-
     class TestClass:
         pass
 
-    classes = [type(f"TestClass{i}", (TestClass,), {}) for i in range(num)]
+    classes = [type(f"TestClass{i}", (TestClass,), {}) for i in range(10)]
 
     assert rasa.shared.utils.common.all_subclasses(TestClass) == classes
