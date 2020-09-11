b4a11c234492d2cddcc7234ece699aa811189b86
alwx <alwxndr@gmail.com>
2020-09-11 21:08:05 +0200

    Black reformatting

--- rasa/core/policies/ensemble.py
+++ rasa/core/policies/ensemble.py
@@ -607,7 +607,9 @@ class SimplePolicyEnsemble(PolicyEnsemble):
         interpreter: NaturalLanguageInterpreter,
     ) -> Prediction:
         number_of_arguments_in_rasa_1_0 = 2
-        arguments = rasa.shared.utils.common.arguments_of(policy.predict_action_probabilities)
+        arguments = rasa.shared.utils.common.arguments_of(
+            policy.predict_action_probabilities
+        )
         if (
             len(arguments) > number_of_arguments_in_rasa_1_0
             and "interpreter" in arguments
