36e8f6806f0217731de7faf33294ff329b24c3bf
Melinda Loubser <32034278+melindaloubser1@users.noreply.github.com>
2020-09-16 22:28:52 +0200

    Revert "Conversation Patterns Docs"

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -72,7 +72,6 @@ module.exports = {
             'fallback-handoff',
             'unexpected-input',
             'contextual-conversations',
-            'reaching-out-to-user',
           ],
         },
         {
@@ -120,6 +119,7 @@ module.exports = {
             'custom-actions',
             'retrieval-actions',
             'forms',
+            'reminders-and-external-events',
             'default-actions',
           ],
         },
