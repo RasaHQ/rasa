5eea2ef79ea7e3103f1e8ab71d7bbd798c19f2fd
GitHub CI <builds@github-ci.com>
2020-09-22 11:41:40 +0000

    AUTO sphinxcontrib-versioning 20200922 031b5e2adc0
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.13/_static/ace/src-min-noconflict/snippets/csound_score.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/csound_score"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
---
+++ 1.10.13/_static/ace/src-min-noconflict/snippets/php_laravel_blade.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/php_laravel_blade"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
