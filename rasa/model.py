8d25ccd6f0dfcf23a2f4790c777ad245b7295b65
Lucas Dutra <ldutra98@gmail.com>
2020-09-28 22:26:03 -0300

    Change string dynamic value method to f string
    Co-authored-by: Leticia Meneses <lmbs.geo@gmail.com>

--- rasa/model.py
+++ rasa/model.py
@@ -152,7 +152,7 @@ def get_model(model_path: Text = DEFAULT_MODELS_PATH) -> TempDirectoryPath:
 
     model_relative_path = create_output_path(model_path)
     model_relative_path = os.path.relpath(model_relative_path)
-    logger.info("Loading model {}...".format(model_relative_path))
+    logger.info(f"Loading model {model_relative_path}...")
 
     return unpack_model(model_path)
 
