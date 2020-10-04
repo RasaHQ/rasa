a4f73e17249d667b489366b08912935385b41bf6
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2020-10-04 17:31:55 +0200

    Reformat slack connector code with black

--- rasa/core/channels/slack.py
+++ rasa/core/channels/slack.py
@@ -25,7 +25,9 @@ class SlackBot(OutputChannel):
     def __init__(self, token: Text, slack_channel: Optional[Text] = None) -> None:
 
         self.slack_channel = slack_channel
-        self.client = WebClient(token, run_async=True, session=ClientSession(trust_env=True))
+        self.client = WebClient(
+            token, run_async=True, session=ClientSession(trust_env=True)
+        )
         super().__init__()
 
     @staticmethod
