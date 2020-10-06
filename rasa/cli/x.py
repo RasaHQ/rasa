281262dd6d66254d2f6423cbd5bb4d80645bcce4
Tom Bocklisch <tom@rasa.com>
2020-10-06 14:27:24 +0200

    moved exception to exceptions

--- rasa/cli/x.py
+++ rasa/cli/x.py
@@ -10,6 +10,7 @@ import traceback
 from typing import Iterable, List, Optional, Text, Tuple
 
 import aiohttp
+from rasa.exceptions import MissingDependencyException
 import ruamel.yaml as yaml
 
 from rasa import telemetry
@@ -426,8 +427,6 @@ def run_locally(args: argparse.Namespace):
         # noinspection PyUnresolvedReferences
         from rasax.community import local  # pytype: disable=import-error
     except ModuleNotFoundError:
-        from rasa.nlu.components import MissingDependencyException
-
         raise MissingDependencyException(
             f"Rasa X does not seem to be installed, but is needed for this CLI command."
             f"You can find more information on how to install Rasa X in local mode"
