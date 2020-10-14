b16375b6c41728b51ede366b7a56f9684e8881ce
alwx <alwxndr@gmail.com>
2020-10-14 16:29:31 +0200

    Test fix

--- tests/core/test_broker.py
+++ tests/core/test_broker.py
@@ -38,13 +38,13 @@ TEST_CONNECTION_PARAMETERS = pika.connection.ConnectionParameters(
 
 
 def test_pika_broker_from_config(monkeypatch: MonkeyPatch):
+    # patch PikaEventBroker so it doesn't try to connect to RabbitMQ on init
+    monkeypatch.setattr(PikaEventBroker, "_connect", lambda _: None)
+
     cfg = read_endpoint_config(
         "data/test_endpoints/event_brokers/pika_endpoint.yml", "event_broker"
     )
     actual = EventBroker.create(cfg)
-
-    # patch PikaEventBroker so it doesn't try to connect to RabbitMQ on init
-    monkeypatch.setattr(PikaEventBroker, "_connect", lambda _: None)
 
     assert isinstance(actual, PikaEventBroker)
     assert actual.host == "localhost"
