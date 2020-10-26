61f1743e96e2713ebb98d9f011ca81bc46dd15bf
Lu Nelson <lunelson@gmail.com>
2020-10-26 12:44:30 +0100

    re-parametrize the container ID, remove isProd boolean

--- docs/plugins/google-tagmanager/index.js
+++ docs/plugins/google-tagmanager/index.js
@@ -20,8 +20,6 @@ module.exports = function (context, options) {
     );
   }
 
-  const isProd = process.env.NODE_ENV === 'production';
-
   return {
     name: 'google-tagmanager',
 
@@ -69,7 +67,7 @@ window.dataLayer = window.dataLayer || [{
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MMHSZCS');
+})(window,document,'script','dataLayer','${containerID}');
             `,
           },
           {
