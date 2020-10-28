980e0c565a2372e38248297e86b347f4f66b61cb
alwx <alwxndr@gmail.com>
2020-10-28 17:09:13 +0100

    Luis v7

--- rasa/shared/nlu/training_data/formats/luis.py
+++ rasa/shared/nlu/training_data/formats/luis.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Any, Dict, Text
+from typing import Any, Dict, List, Text
 
 from rasa.shared.nlu.constants import TEXT, INTENT, ENTITIES
 from rasa.shared.nlu.training_data.formats.readerwriter import JsonTrainingDataReader
@@ -12,19 +12,9 @@ logger = logging.getLogger(__name__)
 
 
 class LuisReader(JsonTrainingDataReader):
-    def read_from_json(self, js: Dict[Text, Any], **kwargs: Any) -> "TrainingData":
-        """Loads training data stored in the LUIS.ai data format."""
-        training_examples = []
-        regex_features = []
-
-        max_tested_luis_schema_version = 5
-        major_version = int(js["luis_schema_version"].split(".")[0])
-        if major_version > max_tested_luis_schema_version:
-            rasa.shared.utils.io.raise_warning(
-                f"Your luis data schema version {js['luis_schema_version']} "
-                f"is higher than 5.x.x. "
-                f"Training may not be performed correctly. "
-            )
+    @staticmethod
+    def _extract_regex_features(js: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        regex_features: List[Dict[Text, Any]] = []
 
         for r in js.get("regex_features", []):
             if r.get("activated", False):
@@ -32,11 +22,33 @@ class LuisReader(JsonTrainingDataReader):
                     {"name": r.get("name"), "pattern": r.get("pattern")}
                 )
 
+        for r in js.get("regex_entities", []):
+            regex_features.append(
+                {"name": r.get("name"), "pattern": r.get("regexPattern")}
+            )
+
+        return regex_features
+
+    def read_from_json(self, js: Dict[Text, Any], **kwargs: Any) -> "TrainingData":
+        """Loads training data stored in the LUIS.ai data format."""
+        training_examples = []
+
+        max_tested_luis_schema_version = 7
+        major_version = int(js["luis_schema_version"].split(".")[0])
+        if major_version > max_tested_luis_schema_version:
+            rasa.shared.utils.io.raise_warning(
+                f"Your luis data schema version {js['luis_schema_version']} "
+                f"is higher than 7.x.x. "
+                f"Training may not be performed correctly. "
+            )
+
+        regex_features = self._extract_regex_features(js)
+
         for s in js["utterances"]:
-            text = s.get(TEXT)
-            intent = s.get(INTENT)
+            text = s.get("text")
+            intent = s.get("intent")
             entities = []
-            for e in s.get(ENTITIES) or []:
+            for e in s.get("entities") or []:
                 start, end = e["startPos"], e["endPos"] + 1
                 val = text[start:end]
                 entities.append(
