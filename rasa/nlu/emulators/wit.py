9cf28ac9daaa46ef66d6efffac5da1ec84d502dc
Tobias Wochinger <t.wochinger@rasa.com>
2020-10-28 11:55:04 +0100

    adapt emulator to new format

--- rasa/nlu/emulators/wit.py
+++ rasa/nlu/emulators/wit.py
@@ -1,32 +1,52 @@
+from collections import defaultdict
 from typing import Any, Dict, List, Text
 
 from rasa.nlu.emulators.no_emulator import NoEmulator
+from rasa.shared.nlu.constants import (
+    ENTITIES,
+    ENTITY_ATTRIBUTE_TYPE,
+    ENTITY_ATTRIBUTE_ROLE,
+    ENTITY_ATTRIBUTE_VALUE,
+    ENTITY_ATTRIBUTE_END,
+    ENTITY_ATTRIBUTE_START,
+    TEXT,
+    INTENT,
+)
 
 
 class WitEmulator(NoEmulator):
-    def __init__(self) -> None:
+    """Emulates the response format of this wit.ai endpoint:
+    https://wit.ai/docs/http/20200513/#get__message_link
+    """
 
+    def __init__(self) -> None:
         super().__init__()
         self.name = "wit"
 
-    def normalise_response_json(self, data: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def normalise_response_json(self, data: Dict[Text, Any]) -> Dict[Text, Any]:
         """Transform data to wit.ai format."""
 
-        entities = {}
-        for entity in data["entities"]:
-            entities[entity["entity"]] = {
-                "confidence": None,
-                "type": "value",
-                "value": entity["value"],
-                "start": entity["start"],
-                "end": entity["end"],
+        entities = defaultdict(list)
+        for entity in data[ENTITIES]:
+            entity_name = entity[ENTITY_ATTRIBUTE_TYPE]
+            role = entity.get(ENTITY_ATTRIBUTE_ROLE, entity_name)
+            entity_name_including_role = f"{entity[ENTITY_ATTRIBUTE_TYPE]}:{role}"
+            normalized_entity = {
+                "confidence": entity.get("confidence_entity") or 1,
+                "name": entity_name,
+                "value": entity[ENTITY_ATTRIBUTE_VALUE],
+                # Entity value before value was transformed (e.g. by synonym mapper)
+                "body": data["text"][
+                    entity.get(ENTITY_ATTRIBUTE_START, 0) : entity.get(
+                        ENTITY_ATTRIBUTE_END, 0
+                    )
+                ],
+                "start": entity[ENTITY_ATTRIBUTE_START],
+                "end": entity[ENTITY_ATTRIBUTE_END],
+                "role": role,
+                "entities": [],
             }
 
-        return [
-            {
-                "_text": data["text"],
-                "confidence": data["intent"]["confidence"],
-                "intent": data["intent"]["name"],
-                "entities": entities,
-            }
-        ]
+            entities[entity_name_including_role].append(normalized_entity)
+
+        return {"text": data[TEXT], "intents": [data[INTENT]], "entities": entities}
