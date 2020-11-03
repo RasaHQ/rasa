46d3fde037cced69a0c2adb3d33f97a2cfd5bb3a
b-quachtran <b.quachtran@rasa.com>
2020-11-03 11:24:10 -0800

    Slack Events API updated

--- rasa/core/channels/slack.py
+++ rasa/core/channels/slack.py
@@ -211,7 +211,7 @@ class SlackInput(InputChannel):
         )
 
     @staticmethod
-    def _sanitize_user_message(text, uids_to_remove) -> Text:
+    def _sanitize_user_message(text: Text, uids_to_remove: List[Text]) -> Text:
         """Remove superfluous/wrong/problematic tokens from a message.
 
         Probably a good starting point for pre-formatting of user-provided text
@@ -367,10 +367,19 @@ class SlackInput(InputChannel):
             event = slack_event.get("event", {})
             thread_id = event.get("thread_ts", event.get("ts"))
 
+            users = []
+            if "authed_users" in slack_event:
+                users = slack_event.get("authed_users")
+            elif (
+                "authorizations" in slack_event
+                and len(slack_event.get("authorizations")) > 0
+            ):
+                users.append(slack_event.get("authorizations")[0].get("user_id"))
+
             return {
                 "out_channel": event.get("channel"),
                 "thread_id": thread_id,
-                "users": slack_event.get("authed_users"),
+                "users": users,
             }
 
         if content_type == "application/x-www-form-urlencoded":
@@ -379,10 +388,15 @@ class SlackInput(InputChannel):
             payload = json.loads(output["payload"][0])
             message = payload.get("message", {})
             thread_id = message.get("thread_ts", message.get("ts"))
+
+            users = []
+            if payload.get("user", {}).get("id"):
+                users.append(payload.get("user", {}).get("id"))
+
             return {
                 "out_channel": payload.get("channel", {}).get("id"),
                 "thread_id": thread_id,
-                "users": payload.get("user", {}).get("id"),
+                "users": users,
             }
 
         return {}
