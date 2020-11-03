0329fb8da559a60b52f49697f487dfbf69254eee
bjbredis <bbarnes@gmail.com>
2020-11-03 08:42:18 -0500

    more merge fixes

--- tests/core/test_tracker_stores.py
+++ tests/core/test_tracker_stores.py
@@ -40,12 +40,7 @@ from rasa.core.tracker_store import (
     DynamoTrackerStore,
     FailSafeTrackerStore,
 )
-<<<<<<< HEAD
-
-from rasa.core.trackers import DialogueStateTracker
-=======
 from rasa.shared.core.trackers import DialogueStateTracker
->>>>>>> origin
 from rasa.utils.endpoints import EndpointConfig, read_endpoint_config
 from tests.core.conftest import DEFAULT_ENDPOINTS_FILE, MockedMongoTrackerStore
 
