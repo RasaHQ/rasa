c145635f6ee9c0c1b2a2e9088dbc8d1fd20e77a2
Raj Nath Patel WX917587 <raj.nath.patel@huawei.com>
2020-11-05 15:03:45 +0000

    Register CustomSpacyNER module.

--- rasa/nlu/registry.py
+++ rasa/nlu/registry.py
@@ -15,6 +15,7 @@ from rasa.nlu.classifiers.keyword_intent_classifier import KeywordIntentClassifi
 from rasa.nlu.classifiers.mitie_intent_classifier import MitieIntentClassifier
 from rasa.nlu.classifiers.sklearn_intent_classifier import SklearnIntentClassifier
 from rasa.nlu.extractors.crf_entity_extractor import CRFEntityExtractor
+from rasa.nlu.extractors.custom_spacy_entity_extractor import SpacyCustomNER
 from rasa.nlu.extractors.duckling_entity_extractor import DucklingEntityExtractor
 from rasa.nlu.extractors.entity_synonyms import EntitySynonymMapper
 from rasa.nlu.extractors.mitie_entity_extractor import MitieEntityExtractor
@@ -71,6 +72,7 @@ component_classes = [
     LanguageModelTokenizer,
     # extractors
     SpacyEntityExtractor,
+    SpacyCustomNER,
     MitieEntityExtractor,
     CRFEntityExtractor,
     DucklingEntityExtractor,
