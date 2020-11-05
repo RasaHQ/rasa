4fbfd16fed9de16ee38ba750dfadb53a42b4aad1
Lydia Kalkbrenner <lydia.kalkbrenner@hotmail.de>
2020-11-05 16:33:14 +0100

    add test for validation entity value type number

--- tests/shared/utils/test_validation.py
+++ tests/shared/utils/test_validation.py
@@ -140,6 +140,7 @@ def test_validate_entity_dict_is_throwing_exceptions(invalid_data):
         {"entity": "e", "role": "c", "value": "text"},
         {"entity": "e", "group": "a", "value": "text"},
         {"entity": "e", "group": "a", "role": "c"},
+        {"entity": "e", "value": 3},
     ],
 )
 def test_entity_dict_is_valid(data):
