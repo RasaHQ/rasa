f447059b40a5f703aeee31e9a3ec9cdc10d84cda
m-vdb <m.verger@rasa.com>
2020-11-05 09:53:34 +0100

    data CLI --out argument is not required (it has a default)

--- rasa/cli/arguments/data.py
+++ rasa/cli/arguments/data.py
@@ -25,7 +25,6 @@ def set_convert_arguments(parser: argparse.ArgumentParser, data_type: Text):
 
     add_out_param(
         parser,
-        required=True,
         default=DEFAULT_CONVERTED_DATA_PATH,
         help_text="File (for `json` and `md`) or existing path (for `yaml`) "
         "where to save training data in Rasa format.",
