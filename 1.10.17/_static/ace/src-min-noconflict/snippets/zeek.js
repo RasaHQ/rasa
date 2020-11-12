920105c5e6382e107cb99274d4403bffedd4cc21
GitHub CI <builds@github-ci.com>
2020-11-12 16:43:51 +0000

    AUTO sphinxcontrib-versioning 20201112 08dd9611faa
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.17/_static/ace/src-min-noconflict/snippets/zeek.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/zeek"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
