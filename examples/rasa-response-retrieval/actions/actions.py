51e650ac73ee7409884e323bd078f2c246fefe68
Raj Nath Patel <raj.nath.patel@huawei.com>
2020-11-22 17:52:16 +0000

    Add more responses for random selection.

--- examples/rasa-response-retrieval/actions/actions.py
+++ examples/rasa-response-retrieval/actions/actions.py
@@ -6,52 +6,52 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-import json
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
-import fasttext
-
-
-class LanguageIdentification:
-    def __init__(self):
-        pretrained_lang_model = "lid.176.ftz"
-        self.model = fasttext.load_model(pretrained_lang_model)
-
-    def predict_lang(self, text):
-        predictions = self.model.predict(text)  # returns top 2 matching languages
-        return predictions[0][0].split('__')[-1]
-
-
-class MultilingualResponse:
-    def __init__(self):
-        with open("multilingual_response.json") as fp:
-            self.multilingual_response = json.load(fp)
-
-    def predict_response(self, intent, lang):
-        response = self.multilingual_response[intent][lang]  # returns top 2 matching languages
-        return response[0]
-
-
-multilingual_response = MultilingualResponse()
-language_detection = LanguageIdentification()
-
-
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_utter_language_select"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        print(tracker.latest_message)
-        intent = tracker.latest_message['intent'].get('name')
-        input_text = tracker.latest_message['text']
-        lang = language_detection.predict_lang(input_text)
-        response = multilingual_response.predict_response(intent=intent, lang=lang)
-        print(lang, intent, response)
-        dispatcher.utter_message(text=response)
-        return []
+# import json
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+# import fasttext
+#
+#
+# class LanguageIdentification:
+#     def __init__(self):
+#         pretrained_lang_model = "lid.176.ftz"
+#         self.model = fasttext.load_model(pretrained_lang_model)
+#
+#     def predict_lang(self, text):
+#         predictions = self.model.predict(text)  # returns top 2 matching languages
+#         return predictions[0][0].split('__')[-1]
+#
+#
+# class MultilingualResponse:
+#     def __init__(self):
+#         with open("multilingual_response.json") as fp:
+#             self.multilingual_response = json.load(fp)
+#
+#     def predict_response(self, intent, lang):
+#         response = self.multilingual_response[intent][lang]  # returns top 2 matching languages
+#         return response[0]
+#
+#
+# multilingual_response = MultilingualResponse()
+# language_detection = LanguageIdentification()
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_utter_language_select"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         print(tracker.latest_message)
+#         intent = tracker.latest_message['intent'].get('name')
+#         input_text = tracker.latest_message['text']
+#         lang = language_detection.predict_lang(input_text)
+#         response = multilingual_response.predict_response(intent=intent, lang=lang)
+#         print(lang, intent, response)
+#         dispatcher.utter_message(text=response)
+#         return []
