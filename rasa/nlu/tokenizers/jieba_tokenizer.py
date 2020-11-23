844bebc7595ed02afb445594986a541fbebbacd4
Xiaoquan Kong <u1mail2me@gmail.com>
2020-11-23 11:17:13 +0800

    Add docstring for JiebaTokenizer

--- rasa/nlu/tokenizers/jieba_tokenizer.py
+++ rasa/nlu/tokenizers/jieba_tokenizer.py
@@ -17,7 +17,8 @@ if typing.TYPE_CHECKING:
 
 
 class JiebaTokenizer(Tokenizer):
-
+    """This tokenizer is a wrapper for Jieba (https://github.com/fxsjy/jieba).
+    """
     supported_language_list = ["zh"]
 
     defaults = {
