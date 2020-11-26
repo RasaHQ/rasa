e2736c6b5fd3ed86a9615ff668050a4e27a5e41c
GitHub CI <builds@github-ci.com>
2020-11-26 15:29:13 +0000

    AUTO sphinxcontrib-versioning 20201126 1785b81bdb4
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.18/_static/ace/src-min-noconflict/snippets/c9search.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/c9search"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
