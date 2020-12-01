77ad137a3c1b2026e9462766060824f14ac8c5cc
Johannes E. M. Mosig <jem-mosig@protonmail.com>
2020-12-01 13:31:48 +0100

    Fix tests

--- tests/nlu/classifiers/test_diet_classifier.py
+++ tests/nlu/classifiers/test_diet_classifier.py
@@ -483,7 +483,7 @@ async def test_process_gives_diagnostic_data(
     diagnostic_data = message.get(DIAGNOSTIC_DATA)
 
     # The last component is DIETClassifier, which should add attention weights
-    name = "2_DIETClassifier"
+    name = "component_2_DIETClassifier"
     assert isinstance(diagnostic_data, dict)
     assert name in diagnostic_data
     assert "attention_weights" in diagnostic_data[name]
