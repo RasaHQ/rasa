41ecce1b4f8d4aa0a98334c579bed301a106e2c7
Greg Stephens <greg@udon.org>
2020-12-01 18:13:29 -0800

    server list

--- rasa/core/brokers/kafka.py
+++ rasa/core/brokers/kafka.py
@@ -23,7 +23,6 @@ class KafkaEventBroker(EventBroker):
         ssl_check_hostname=False,
         topic="rasa_core_events",
         client_id=None,
-        group_id=None,
         security_protocol="SASL_PLAINTEXT",
         loglevel=logging.ERROR,
     ) -> None:
@@ -32,7 +31,6 @@ class KafkaEventBroker(EventBroker):
         self.host = host
         self.topic = topic
         self.client_id = client_id
-        self.group_id = group_id
         self.security_protocol = security_protocol
         self.sasl_username = sasl_username
         self.sasl_password = sasl_password
@@ -85,28 +83,30 @@ class KafkaEventBroker(EventBroker):
     def _create_producer(self) -> None:
         import kafka
 
+        hosts = [self.host]
+        if type(self.host) == list:
+            hosts = self.host
+
         if self.security_protocol == "SASL_PLAINTEXT":
             self.producer = kafka.KafkaProducer(
-                bootstrap_servers=[self.host],
+                bootstrap_servers=hosts,
                 value_serializer=lambda v: json.dumps(v).encode(DEFAULT_ENCODING),
                 sasl_plain_username=self.sasl_username,
                 sasl_plain_password=self.sasl_password,
                 sasl_mechanism="PLAIN",
                 security_protocol=self.security_protocol,
                 client_id=self.client_id,
-                group_id=self.group_id,
             )
         elif self.security_protocol == "PLAINTEXT":
             self.producer = kafka.KafkaProducer(
-                bootstrap_servers=[self.host],
+                bootstrap_servers=hosts,
                 value_serializer=lambda v: json.dumps(v).encode(DEFAULT_ENCODING),
                 security_protocol=self.security_protocol,
                 client_id=self.client_id,
-                group_id=self.group_id,
             )
         elif self.security_protocol == "SSL":
             self.producer = kafka.KafkaProducer(
-                bootstrap_servers=[self.host],
+                bootstrap_servers=hosts,
                 value_serializer=lambda v: json.dumps(v).encode(DEFAULT_ENCODING),
                 ssl_cafile=self.ssl_cafile,
                 ssl_certfile=self.ssl_certfile,
@@ -114,11 +114,10 @@ class KafkaEventBroker(EventBroker):
                 ssl_check_hostname=False,
                 security_protocol=self.security_protocol,
                 client_id=self.client_id,
-                group_id=self.group_id,
             )
         elif self.security_protocol == "SASL_SSL":
             self.producer = kafka.KafkaProducer(
-                bootstrap_servers=[self.host],
+                bootstrap_servers=hosts,
                 value_serializer=lambda v: json.dumps(v).encode(DEFAULT_ENCODING),
                 ssl_cafile=self.ssl_cafile,
                 ssl_certfile=self.ssl_certfile,
@@ -127,7 +126,6 @@ class KafkaEventBroker(EventBroker):
                 sasl_mechanism="PLAIN",
                 security_protocol=self.security_protocol,
                 client_id=self.client_id,
-                group_id=self.group_id,
             )
         else:
             logger.error("Kafka security_protocol invalid or not set")
