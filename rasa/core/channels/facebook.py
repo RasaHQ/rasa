831e2dcc1c798f29251d70eb587fa1e8de028733
Sara Silva <saracsas2@gmail.com>
2020-12-07 00:24:36 -0300

    Fix lint problem

--- rasa/core/channels/facebook.py
+++ rasa/core/channels/facebook.py
@@ -279,7 +279,6 @@ class MessengerBot(OutputChannel):
         self, recipient_id: Text, json_message: Dict[Text, Any], **kwargs: Any
     ) -> None:
         """Sends custom json data to the output."""
-
         if isinstance(json_message, list):
             if isinstance(json_message.pop(), list):
                 recipient_id = json_message.pop().pop() or recipient_id
