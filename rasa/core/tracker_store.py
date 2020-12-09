2608a3f6923952bbda03a87f4ab4f2b0a4d1f3c2
m-vdb <m.verger@rasa.com>
2020-12-09 10:16:52 +0100

    remove unused nosec

--- rasa/core/tracker_store.py
+++ rasa/core/tracker_store.py
@@ -935,9 +935,7 @@ class SQLTrackerStore(TrackerStore):
 
         cursor = conn.connection.cursor()
         cursor.execute("COMMIT")
-        cursor.execute(
-            "SELECT 1 FROM pg_catalog.pg_database WHERE datname = ?", (db,)  # nosec
-        )
+        cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = ?", (db,))
         exists = cursor.fetchone()
         if not exists:
             try:
