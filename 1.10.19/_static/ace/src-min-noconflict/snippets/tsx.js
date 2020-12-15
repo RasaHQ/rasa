8ceff68834ed4a610a893e560d7ff1c878736fd7
GitHub CI <builds@github-ci.com>
2020-12-15 20:49:01 +0000

    AUTO sphinxcontrib-versioning 20201215 a9fd74f5ba3
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.19/_static/ace/src-min-noconflict/snippets/tsx.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/tsx"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
