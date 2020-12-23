d12b30d22b085bfc89cc247360ddc4cfc63c33bf
Vova Vv <mr.voov@gmail.com>
2020-12-24 00:53:03 +0100

    temporary run eagerly

--- rasa/nlu/classifiers/diet_classifier.py
+++ rasa/nlu/classifiers/diet_classifier.py
@@ -830,6 +830,7 @@ class DIETClassifier(IntentClassifier, EntityExtractor):
         )
 
         self.model.compile(
+            run_eagerly = True,
             optimizer=tf.keras.optimizers.Adam(self.component_config[LEARNING_RATE])
         )
         self.model.fit(
