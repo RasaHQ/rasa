f468e2887cb6caac797f575f5eae436de690c57c
m-vdb <m.verger@rasa.com>
2021-01-27 10:46:05 +0100

    add test case for race condition when creating tracker store db

--- tests/integration_tests/core/test_tracker_store.py
+++ tests/integration_tests/core/test_tracker_store.py
@@ -1,4 +1,8 @@
+import logging
+from unittest.mock import Mock, patch
+
 import pytest
+from _pytest.logging import LogCaptureFixture
 import sqlalchemy as sa
 
 from rasa.core.tracker_store import SQLTrackerStore
@@ -72,4 +76,49 @@ def test_sql_tracker_store_with_login_db_db_already_exists(
     tracker_store.engine.dispose()
 
 
-# TODO: IntegrityError is raised?
+@pytest.mark.timeout(10)
+def test_sql_tracker_store_with_login_db_race_condition(
+    postgres_login_db_connection: sa.engine.Connection, caplog: LogCaptureFixture
+):
+    original_execute = sa.engine.Connection.execute
+
+    def mock_execute(self, *args, **kwargs):
+        # this simulates a race condition
+        if kwargs == {"database_name": POSTGRES_TRACKER_STORE_DB}:
+            original_execute(
+                self.execution_options(isolation_level="AUTOCOMMIT"),
+                f"CREATE DATABASE {POSTGRES_TRACKER_STORE_DB}",
+            )
+            return Mock(rowcount=0)
+        else:
+            return original_execute(self, *args, **kwargs)
+
+    with patch.object(sa.engine.Connection, "execute", mock_execute):
+        with caplog.at_level(logging.ERROR):
+            tracker_store = SQLTrackerStore(
+                dialect="postgresql",
+                host=POSTGRES_HOST,
+                port=POSTGRES_PORT,
+                db=POSTGRES_TRACKER_STORE_DB,
+                login_db=POSTGRES_LOGIN_DB,
+            )
+
+    # IntegrityError has been caught and we log the error
+    assert any(
+        [
+            f"Could not create database '{POSTGRES_TRACKER_STORE_DB}'" in record.message
+            for record in caplog.records
+        ]
+    )
+    matching_rows = (
+        postgres_login_db_connection.execution_options(isolation_level="AUTOCOMMIT")
+        .execute(
+            sa.text(
+                "SELECT 1 FROM pg_catalog.pg_database WHERE datname = :database_name"
+            ),
+            database_name=POSTGRES_TRACKER_STORE_DB,
+        )
+        .rowcount
+    )
+    assert matching_rows == 1
+    tracker_store.engine.dispose()
