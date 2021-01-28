79b8308aa967bf7765463910fd9e9cb89a64b6d1
koaning <vincentwarmerdam@gmail.com>
2021-01-28 16:50:20 +0100

    its-probably-better-if-i-ask

--- rasa/nlu/tokenizers/tokenizer.py
+++ rasa/nlu/tokenizers/tokenizer.py
@@ -66,9 +66,9 @@ class Token:
 
 
 class Tokenizer(Component):
-    """Construct a new tokenizer using the WhitespaceTokenizer framework."""
-
     def __init__(self, component_config: Dict[Text, Any] = None) -> None:
+        """Construct a new tokenizer using the WhitespaceTokenizer framework."""
+
         super().__init__(component_config)
 
         # flag to check whether to split intents
