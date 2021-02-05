847503b4d9769fd83b822be4359f8e1fcd91fe10
GitHub CI <builds@github-ci.com>
2021-02-05 12:59:28 +0000

    AUTO sphinxcontrib-versioning 20210205 8291705fc47
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.22/_static/ace/src-min-noconflict/snippets/groovy.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/groovy"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
---
+++ 1.10.22/_static/ace/src-min-noconflict/snippets/kotlin.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/kotlin"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
