172d130cf50fd2b716465fb08757b4b10eb82190
m-vdb <m.verger@rasa.com>
2021-02-15 18:34:32 +0100

    script to update path of sources loaded with remark-source to a versioned path

---
+++ docs/scripts/update_versioned_sources.js
@@ -0,0 +1,12 @@
+const includedSources = require('../plugins/included_source.js');
+
+
+const version = process.argv[2];
+if (!version) {;
+    throw new Error("Missing version argument.");
+}
+
+console.info(`Updating sources in ${version} documentation`);
+includedSources.updateVersionedSources({
+  docsDir: `./versioned_docs/version-${version}/`,
+});
