8e71abe681621c7516cc08ea20c02fc3156a9c1b
GitHub CI <builds@github-ci.com>
2021-02-22 19:09:17 +0000

    AUTO sphinxcontrib-versioning 20210222 fc794f32cda
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.23/_static/ace/src-min-noconflict/snippets/turtle.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/turtle"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
