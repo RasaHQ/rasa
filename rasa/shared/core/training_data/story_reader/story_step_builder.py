d00cf53416b62c7e1b65e2cc998d0df0abe1bca5
Thomas Werkmeister <thomas@twerkmeister.com>
2021-02-24 14:25:19 +0100

    Fixed code style issues

--- rasa/shared/core/training_data/story_reader/story_step_builder.py
+++ rasa/shared/core/training_data/story_reader/story_step_builder.py
@@ -146,9 +146,12 @@ class StoryStepBuilder:
     def _generate_checkpoint_name_for_or_statement(
         self, messages: List[UserUttered]
     ) -> str:
-        """Generates a checkpoint name for an or statement based on the
-         current story/rule name, the current place in the story,
-         and the involved intents/e2e messages"""
+        """Generates a unique checkpoint name for an or statement.
+
+         The name is based on the current story/rule name,
+         the current place in the story,
+         and the involved intents/e2e messages."""
+
         messages_texts_or_intents = sorted([m.text or m.intent_name for m in messages])
 
         current_steps_event_counts = [str(len(s.events)) for s in self.current_steps]
