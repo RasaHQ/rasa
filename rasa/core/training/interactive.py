0c047c1a7b654c2664058925af5765c951b4135d
joao vitor silva <joaovitordmrosa@hotmail.com>
2021-03-07 04:19:39 -0300

    Changed 'i' variable name and add the 'interactive_story_prefix' variable
    Co-authored-by: Rafael Teodosio <rafzteodosio@gmail.com>

--- rasa/core/training/interactive.py
+++ rasa/core/training/interactive.py
@@ -816,10 +816,13 @@ def _write_stories_to_file(
     else:
         append_write = "w"  # make a new file if not
 
+    interactive_story_prefix = "interactive_story_"
     with open(
         export_story_path, "r", encoding=rasa.shared.utils.io.DEFAULT_ENCODING
     ) as f:
-        i = sum(1 for s in f if 'interactive_story_' in s) + 1  # count the times string 'interactive story' occur in file
+        interactive_story_counter = (
+            sum(1 for s in f if interactive_story_prefix in s) + 1
+        )  # count the times string 'interactive story' occur in file
 
     with open(
         export_story_path, append_write, encoding=rasa.shared.utils.io.DEFAULT_ENCODING
@@ -828,7 +831,9 @@ def _write_stories_to_file(
         for conversation in sub_conversations:
             parsed_events = rasa.shared.core.events.deserialise_events(conversation)
             tracker = DialogueStateTracker.from_events(
-                f"interactive_story_{i}", evts=parsed_events, slots=domain.slots
+                f"{interactive_story_prefix}{interactive_story_counter}",
+                evts=parsed_events,
+                slots=domain.slots,
             )
 
             if any(
