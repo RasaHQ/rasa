4a3d53abcdc2251112100abdba8898ca4ee8feb3
Vladimir Vlasov <vladimir@rasa.com>
2021-03-17 11:11:23 +0100

    Update rasa/nlu/featurizers/dense_featurizer/lm_featurizer.py
    Co-authored-by: Daksh Varshneya <d.varshneya@rasa.com>

--- rasa/nlu/featurizers/dense_featurizer/lm_featurizer.py
+++ rasa/nlu/featurizers/dense_featurizer/lm_featurizer.py
@@ -11,7 +11,6 @@ import rasa.shared.utils.io
 from rasa.shared.nlu.training_data.features import Features
 from rasa.nlu.tokenizers.tokenizer import Tokenizer
 from rasa.shared.nlu.training_data.tokens import Token
-from rasa.shared.nlu.training_data.training_data import TrainingDataChunk
 from rasa.shared.nlu.training_data.message import Message
 from rasa.nlu.constants import (
     DENSE_FEATURIZABLE_ATTRIBUTES,
