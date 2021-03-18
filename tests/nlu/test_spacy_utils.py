af6c037caa840f0ab15316783baf67126a96524c
koaning <vincentwarmerdam@gmail.com>
2021-03-18 17:27:53 +0100

    oh flake, oh you

--- tests/nlu/test_spacy_utils.py
+++ tests/nlu/test_spacy_utils.py
@@ -28,12 +28,12 @@ supported_langauges = [
 
 @pytest.mark.parametrize("lang", supported_langauges)
 def test_model_fallback_raises_warning(lang: str):
-    """In this test we want to make sure we raise a warning but we will perform a fallback."""
+    """Make sure we raise a warning but we will perform a fallback."""
     with pytest.warns(UserWarning):
         SpacyNLP._check_model_fallback(None, lang, warn=True)
 
 
 def test_model_raises_error_not_exist():
-    """In this test we pass a model name that does not exist. It should throw a direct error."""
+    """It should throw a direct error when a model doesn't exist."""
     with pytest.raises(InvalidModelError):
         SpacyNLP.create({"model": "dinosaurhead"}, RasaNLUModelConfig())
