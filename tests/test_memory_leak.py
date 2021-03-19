0a69c885a6ca93d11f1a8cb68cdfa2b64acb23c5
Tobias Wochinger <t.wochinger@rasa.com>
2021-03-19 09:29:49 +0100

    add new test for testing multiple training runs

--- tests/test_memory_leak.py
+++ tests/test_memory_leak.py
@@ -25,6 +25,8 @@ def _config_for_epochs(tmp_path: Path, epochs: int) -> Text:
 
 
 class MemoryLeakTest(abc.ABC):
+    """Generic template for memory leak tests."""
+
     @pytest.fixture
     @abc.abstractmethod
     def function_to_profile(self) -> Callable[[], Any]:
@@ -44,6 +46,9 @@ class MemoryLeakTest(abc.ABC):
     def interval(self) -> float:
         return 0.1
 
+    def trend_threshold(self) -> float:
+        return 0.3
+
     def test_for_memory_leak(
         self, function_to_profile: Callable[[], Any], name_for_dumped_files
     ) -> None:
@@ -60,12 +65,15 @@ class MemoryLeakTest(abc.ABC):
 
         coefficient = self._get_coefficient_for_results(results)
         print(coefficient)
-        # if this fails it's likely due to a memory leak
+        # if this fails this indicates a memory leak!
         # Suggested Next steps:
         #   1. Run this test locally
         #   2. Plot memory usage graph which was dumped in project, e.g.:
-        #      `mprof plot memory_usage_rasa_nlu_2.4.10_epochs1000_training_runs1_plot.txt`
-        assert coefficient < 0.3
+        #      ```
+        #      mprof plot \
+        #        memory_usage_rasa_nlu_2.4.10_epochs1000_training_runs1_plot.txt
+        #      ```
+        assert coefficient < self.trend_threshold()
 
     def _get_coefficient_for_results(self, results: List[Tuple[float]]) -> float:
         import numpy as np
@@ -95,11 +103,14 @@ class MemoryLeakTest(abc.ABC):
 
         import json
 
+        # dump result as json to be able analyze them without re-running the test
         with open(mprof_results, "w") as f:
             f.write(json.dumps(results))
 
 
 class TestNLULeakManyEpochs(MemoryLeakTest):
+    """Tests for memory leaks in NLU components when training with many epochs."""
+
     # [-0.00159855] for 2.4.0
     # [0.21319729] [0.18872215] for 2.2.0
     def epochs(self) -> int:
@@ -128,10 +139,56 @@ class TestNLULeakManyEpochs(MemoryLeakTest):
 
     @pytest.fixture()
     def name_for_dumped_files(self) -> Text:
-        return f"memory_usage_rasa_nlu_{rasa.__version__}_epochs{self.epochs()}_training_runs1"
+        return (
+            f"memory_usage_rasa_nlu_{rasa.__version__}_"
+            f"epochs{self.epochs()}_training_runs1"
+        )
+
+
+class TestNLULeakManyRuns(MemoryLeakTest):
+    """Tests for memory leaks in NLU components when training with many epochs.
+
+    If this fails but `TestNLULeakManyEpochs` does not, it is an indicator that
+    there is a leak in the data loading pipeline.
+    """
+
+    # [-0.00159855] for 2.4.0
+    # [0.21319729] [0.18872215] for 2.2.0
+    def training_runs(self) -> int:
+        return 30
+
+    @pytest.fixture()
+    def function_to_profile(
+        self,
+        # default_nlu_data: Text,  # 2.2.0
+        nlu_data_path: Text,
+        tmp_path: Path,
+    ) -> Callable[[], Any]:
+        # from rasa.train import train_nlu  # 2.2.0
+        from rasa.model_training import train_nlu
+
+        def profiled_train() -> None:
+            for _ in range(self.training_runs()):
+                train_nlu(
+                    _config_for_epochs(tmp_path, epochs=1),
+                    # default_nlu_data,  # 2.2.0
+                    nlu_data_path,
+                    output=str(tmp_path),
+                )
+
+        return profiled_train
+
+    @pytest.fixture()
+    def name_for_dumped_files(self) -> Text:
+        return (
+            f"memory_usage_rasa_nlu_{rasa.__version__}_"
+            f"epochs1_training_runs{self.training_runs()}"
+        )
 
 
 class TestCoreLeakManyEpochs(MemoryLeakTest):
+    """Tests for memory leaks in Core policies when training with many epochs."""
+
     # 2.4.0: [0.06253618]
     # 2.2.0: [0.35051641]
     def epochs(self) -> int:
@@ -163,4 +220,53 @@ class TestCoreLeakManyEpochs(MemoryLeakTest):
 
     @pytest.fixture()
     def name_for_dumped_files(self) -> Text:
-        return f"memory_usage_rasa_core_{rasa.__version__}_epochs{self.epochs()}_training_runs1"
+        return (
+            f"memory_usage_rasa_core_{rasa.__version__}_"
+            f"epochs{self.epochs()}_training_runs1"
+        )
+
+
+class TestCoreLeakManyRuns(MemoryLeakTest):
+    """Tests for memory leaks in Core policies when training with multiple runs.
+
+    If this fails but `TestCoreLeakManyEpochs` does not, it is an indicator that
+    there is a leak in the data loading pipeline.
+    """
+
+    def training_runs(self) -> int:
+        return 30
+
+    def trend_threshold(self) -> float:
+        return 0.35
+
+    @pytest.fixture()
+    def function_to_profile(
+        self,
+        domain_path: Text,
+        stories_path: Text,
+        # default_domain_path: Text,  # 2.2.0
+        # default_stories_file: Text,  # 2.2.0
+        tmp_path: Path,
+    ) -> Callable[[], Any]:
+        # from rasa.train import train_core  # 2.2.0
+        from rasa.model_training import train_core
+
+        def profiled_train() -> None:
+            for _ in range(self.training_runs()):
+                train_core(
+                    domain_path,
+                    # default_domain_path,  # 2.2.0
+                    _config_for_epochs(tmp_path, epochs=1),
+                    stories_path,
+                    # default_stories_file,  # 2.2.0
+                    output=str(tmp_path),
+                )
+
+        return profiled_train
+
+    @pytest.fixture()
+    def name_for_dumped_files(self) -> Text:
+        return (
+            f"memory_usage_rasa_core_{rasa.__version__}_"
+            f"epochs1_training_runs{self.training_runs()}"
+        )
