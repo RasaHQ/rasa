f5cc5eb7424991fa5816c11c3d59066ed4f007d3
thomas <t.kober@rasa.com>
2021-03-19 14:43:50 +0100

    properly revises the data augmentation tests

--- tests/nlu/data_augmentation/test_augmenter.py
+++ tests/nlu/data_augmentation/test_augmenter.py
@@ -1,10 +1,11 @@
-import os
-from typing import Callable
+import operator
+from typing import Callable, Text
 
-from _pytest.pytester import RunResult, Testdir
+import pytest
 from rasa.nlu.components import Component
 from rasa.nlu.constants import TOKENS_NAMES
 from rasa.shared.constants import LATEST_TRAINING_DATA_FORMAT_VERSION
+from rasa.shared.exceptions import InvalidConfigException
 from rasa.shared.nlu.training_data.message import Message
 import rasa.shared.nlu.training_data.loading
 import rasa.shared.utils.io
@@ -15,13 +16,10 @@ from rasa.shared.nlu.constants import (
 )
 
 
-def test_augmenter_create_tokenizer(run: Callable[..., RunResult],):
+def test_augmenter_create_tokenizer():
     from rasa.nlu.data_augmentation.augmenter import _create_tokenizer_from_config
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-    config_path = os.path.join(data_root, "data/test_nlu_paraphrasing/config.yml")
+    config_path = "data/test_nlu_paraphrasing/config.yml"
 
     tokenizer = _create_tokenizer_from_config(config_path=config_path)
 
@@ -37,45 +35,36 @@ def test_augmenter_create_tokenizer(run: Callable[..., RunResult],):
     assert tokens == expected_tokens
 
 
-def test_augmenter_create_tokenizer_empty_config(run: Callable[..., RunResult],):
+@pytest.mark.xfail(raises=InvalidConfigException)
+def test_augmenter_create_tokenizer_empty_config():
     from rasa.nlu.data_augmentation.augmenter import _create_tokenizer_from_config
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-    config_path = os.path.join(data_root, "data/test_nlu_paraphrasing/empty_config.yml")
+    config_path = "data/test_nlu_paraphrasing/empty_config.yml"
 
-    # Should fallback to a WhitespaceTokenizer
-    tokenizer = _create_tokenizer_from_config(config_path=config_path)
-
-    assert isinstance(tokenizer, Component)
-
-    # Test Config has a simple WhitespaceTokenizer
-    expected_tokens = ["xxx", "yyy", "zzz"]
-    message = Message(data={TEXT: "xxx yyy zzz", INTENT: "abc"})
-
-    tokenizer.process(message)
-    tokens = [token.text for token in message.get(TOKENS_NAMES[TEXT])]
-
-    assert tokens == expected_tokens
+    # Should raise InvalidConfigException
+    _ = _create_tokenizer_from_config(config_path=config_path)
 
 
-def test_augmenter_intent_collection(run: Callable[..., RunResult],):
+@pytest.mark.parametrize(
+    "intent_proportion, comparator_fn_1, comparator_fn_2",
+    [
+        (0.0, operator.le, operator.lt),
+        (0.5, operator.ge, operator.le),
+        (1.0, operator.ge, operator.le),
+    ],
+)
+def test_augmenter_intent_collection(
+    intent_proportion: float, comparator_fn_1: Callable, comparator_fn_2: Callable
+):
     from rasa.nlu.data_augmentation.augmenter import (
         _collect_intents_for_data_augmentation,
     )
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-    report_file = os.path.join(
-        data_root, "data/test_nlu_paraphrasing/nlu_classification_report.json"
-    )
+    report_file = "data/test_nlu_paraphrasing/nlu_classification_report.json"
     classification_report = rasa.shared.utils.io.read_json_file(report_file)
     nlu_training_data = rasa.shared.nlu.training_data.loading.load_data(
-        os.path.join(data_root, "data/test_nlu_paraphrasing/nlu_train.yml")
+        "data/test_nlu_paraphrasing/nlu_train.yml"
     )
-    intent_proportion = 0.5
 
     intents_to_augment = _collect_intents_for_data_augmentation(
         nlu_training_data=nlu_training_data,
@@ -85,119 +74,121 @@ def test_augmenter_intent_collection(run: Callable[..., RunResult],):
 
     num_intents = len(nlu_training_data.number_of_examples_per_intent)
     num_intents_to_augment_per_criterion = round(num_intents * intent_proportion)
-    assert len(intents_to_augment) >= num_intents_to_augment_per_criterion
-    assert len(intents_to_augment) <= num_intents
+
+    assert comparator_fn_1(
+        len(intents_to_augment), num_intents_to_augment_per_criterion
+    )
+    assert comparator_fn_2(len(intents_to_augment), num_intents)
 
 
-def test_augmenter_paraphrase_pool_creation(run: Callable[..., RunResult],):
+@pytest.mark.parametrize(
+    "intent_to_augment, min_paraphrase_sim_score, max_paraphrase_sim_score, expected_len",
+    [
+        ("ask_transfer_charge", 0.98, 0.99, 0),
+        ("check_earnings", 0.98, 0.99, 0),
+        ("check_recipients", 0.98, 0.99, 0),
+        ("ask_transfer_charge", 0.0, 0.99, 6),
+        ("check_earnings", 0.0, 0.99, 18),
+        ("check_recipients", 0.0, 0.99, 18),
+        ("ask_transfer_charge", 0.0, 0.45, 2),
+        ("check_earnings", 0.0, 0.45, 3),
+        ("check_recipients", 0.0, 0.45, 8),
+        ("ask_transfer_charge", 0.4, 0.5, 1),
+        ("check_earnings", 0.4, 0.5, 5),
+        ("check_recipients", 0.4, 0.5, 8),
+    ],
+)
+def test_augmenter_paraphrase_pool_creation(
+    intent_to_augment: Text,
+    min_paraphrase_sim_score: float,
+    max_paraphrase_sim_score: float,
+    expected_len: int,
+):
     from rasa.nlu.data_augmentation.augmenter import _create_paraphrase_pool
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-
     paraphrases = rasa.shared.nlu.training_data.loading.load_data(
-        os.path.join(data_root, "data/test_nlu_paraphrasing/paraphrases.yml")
+        "data/test_nlu_paraphrasing/paraphrases.yml"
     )
-
-    # See the file `rasa/data/test_nlu_paraphrasing/nlu_train.yml` for available intents
-    intents_to_augment = {"ask_transfer_charge", "check_earnings", "check_recipients"}
-
-    # Max sim score in `rasa/data/test_nlu_paraphrasing/paraphrases.yml` is 0.975
-    # Min sim score in `rasa/data/test_nlu_paraphrasing/paraphrases.yml` is 0.389
 
     pool = _create_paraphrase_pool(
         paraphrases=paraphrases,
-        intents_to_augment=intents_to_augment,
-        min_paraphrase_sim_score=0.98,
-        max_paraphrase_sim_score=0.99,
+        intents_to_augment={intent_to_augment},
+        min_paraphrase_sim_score=min_paraphrase_sim_score,
+        max_paraphrase_sim_score=max_paraphrase_sim_score,
     )
-    assert len(pool) == 0
-
-    pool = _create_paraphrase_pool(
-        paraphrases=paraphrases,
-        intents_to_augment=intents_to_augment,
-        min_paraphrase_sim_score=0.0,
-        max_paraphrase_sim_score=0.99,
-    )
-    assert len(pool["check_earnings"]) == 18
-    assert len(pool["check_recipients"]) == 18
-    assert len(pool["ask_transfer_charge"]) == 6
-
-    pool = _create_paraphrase_pool(
-        paraphrases=paraphrases,
-        intents_to_augment=intents_to_augment,
-        min_paraphrase_sim_score=0.0,
-        max_paraphrase_sim_score=0.45,
-    )
-
-    assert len(pool["check_earnings"]) == 3
-    assert len(pool["check_recipients"]) == 8
-    assert len(pool["ask_transfer_charge"]) == 2
-
-    pool = _create_paraphrase_pool(
-        paraphrases=paraphrases,
-        intents_to_augment=intents_to_augment,
-        min_paraphrase_sim_score=0.4,
-        max_paraphrase_sim_score=0.5,
-    )
-    assert len(pool["check_earnings"]) == 5
-    assert len(pool["check_recipients"]) == 8
-    assert len(pool["ask_transfer_charge"]) == 1
+    assert len(pool.get(intent_to_augment, [])) == expected_len
 
 
-def test_augmenter_augmentation_factor_resolution(run: Callable[..., RunResult],):
+@pytest.mark.parametrize(
+    "augmentation_factor, intent, size_after_augmentation",
+    [
+        (0.0, "check_recipients", None),
+        (0.0, "inform", None),
+        (0.0, "search_transactions", None),
+        (0.0, "check_balance", None),
+        (0.0, "transfer_money", None),
+        (0.1, "check_recipients", None),
+        (0.1, "inform", None),
+        (0.1, "search_transactions", None),
+        (0.1, "check_balance", None),
+        (0.1, "transfer_money", None),
+        (0.5, "check_recipients", None),
+        (0.5, "inform", 2),
+        (0.5, "search_transactions", None),
+        (0.5, "check_balance", None),
+        (0.5, "transfer_money", None),
+        (1.0, "check_recipients", 1),
+        (1.0, "inform", 4),
+        (1.0, "search_transactions", 1),
+        (1.0, "check_balance", 1),
+        (1.0, "transfer_money", 1),
+        (2.0, "check_recipients", 2),
+        (2.0, "inform", 8),
+        (2.0, "search_transactions", 2),
+        (2.0, "check_balance", 2),
+        (2.0, "transfer_money", 2),
+        (5.0, "check_recipients", 5),
+        (5.0, "inform", 20),
+        (5.0, "search_transactions", 5),
+        (5.0, "check_balance", 5),
+        (5.0, "transfer_money", 5),
+        (-1.0, "check_recipients", None),
+        (-1.0, "inform", None),
+        (-1.0, "search_transactions", None),
+        (-1.0, "check_balance", None),
+        (-1.0, "transfer_money", None),
+        (-666, "check_recipients", None),
+        (-666, "inform", None),
+        (-666, "search_transactions", None),
+        (-666, "check_balance", None),
+        (-666, "transfer_money", None),
+    ],
+)
+def test_augmenter_augmentation_factor_resolution(
+    augmentation_factor: float, intent: Text, size_after_augmentation: int
+):
     from rasa.nlu.data_augmentation.augmenter import _resolve_augmentation_factor
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-
     nlu_training_data = rasa.shared.nlu.training_data.loading.load_data(
-        os.path.join(data_root, "data/test_nlu_paraphrasing/nlu_train.yml")
+        "data/test_nlu_paraphrasing/nlu_train.yml"
     )
-    augmentation_factor = 2.0
-
-    assert_dict = {
-        "goodbye": 2,
-        "greet": 2,
-        "deny": 2,
-        "human_handoff": 2,
-        "affirm": 2,
-        "help": 2,
-        "ask_transfer_charge": 2,
-        "check_earnings": 2,
-        "thankyou": 2,
-        "check_recipients": 2,
-        "search_transactions": 2,
-        "transfer_money": 2,
-        "pay_cc": 2,
-        "check_balance": 2,
-        "inform": 8,
-    }
 
     resolved_dict = _resolve_augmentation_factor(
         nlu_training_data=nlu_training_data, augmentation_factor=augmentation_factor
     )
 
-    assert all([assert_dict[key] == resolved_dict[key] for key in assert_dict.keys()])
+    assert resolved_dict[intent] == size_after_augmentation
 
 
-def test_augmenter_build_max_vocab_expansion_training_set(
-    run: Callable[..., RunResult],
-):
+def test_augmenter_build_max_vocab_expansion_training_set():
     from rasa.nlu.data_augmentation.augmenter import (
         _create_augmented_training_data_max_vocab_expansion,
     )
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-
     nlu_training_data = rasa.shared.nlu.training_data.loading.load_data(
-        os.path.join(data_root, "data/test_nlu_paraphrasing/nlu_train.yml")
+        "data/test_nlu_paraphrasing/nlu_train.yml"
     )
-    config_path = os.path.join(data_root, "data/test_nlu_paraphrasing/config.yml")
+    config_path = "data/test_nlu_paraphrasing/config.yml"
 
     intent_to_check = "check_earnings"
     three_new_words = "xxx yyy zzz"
@@ -261,17 +252,13 @@ def test_augmenter_build_max_vocab_expansion_training_set(
     assert not all(augmented_data_should_not_contain.values())
 
 
-def test_augmenter_build_random_sampling_training_set(run: Callable[..., RunResult],):
+def test_augmenter_build_random_sampling_training_set():
     from rasa.nlu.data_augmentation.augmenter import (
         _create_augmented_training_data_random_sampling,
     )
 
-    data_root = os.path.dirname(
-        os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    )
-
     nlu_training_data = rasa.shared.nlu.training_data.loading.load_data(
-        os.path.join(data_root, "data/test_nlu_paraphrasing/nlu_train.yml")
+        "data/test_nlu_paraphrasing/nlu_train.yml"
     )
 
     intent_to_check = "check_earnings"
