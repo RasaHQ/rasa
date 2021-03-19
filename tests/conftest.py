ae2a586dac8adbe777f152854894eed926e13585
Tobias Wochinger <t.wochinger@rasa.com>
2021-03-19 17:28:03 +0100

    setup up CI for memory checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,6 +69,7 @@ PATH_PYTEST_MARKER_MAPPINGS = {
         Path("tests", "core", "test_training.py").absolute(),
         Path("tests", "core", "test_examples.py").absolute(),
     ],
+    "category_performance": [Path("tests", "test_memory_leak.py").absolute()],
 }
 
 
