77b9a39d8e849e3861f4b90086aa692d3d62640a
Matthew Vielkind <m.vielkind@rasa.com>
2021-03-23 09:48:25 -0400

    Add support for different languages and assistant voices

--- rasa/core/channels/twilio_voice.py
+++ rasa/core/channels/twilio_voice.py
@@ -3,9 +3,10 @@ from sanic import Blueprint, response
 from sanic.request import Request
 from sanic.response import HTTPResponse
 from twilio.twiml.voice_response import VoiceResponse, Gather
-from typing import Text, Callable, Awaitable, List, Any, Dict
+from typing import Text, Callable, Awaitable, List, Any, Dict, Optional, NoReturn
 
 from rasa.shared.core.events import BotUttered
+from rasa.shared.exceptions import RasaException
 from rasa.core.channels.channel import (
     InputChannel,
     CollectingOutputChannel,
@@ -14,10 +15,65 @@ from rasa.core.channels.channel import (
 
 
 class TwilioVoiceInput(InputChannel):
+
+    SUPPORTED_LANGUAGES = ["af", "ar", "ca", "zh", "zh-CN", "zh-HK", "hr", "cs", "da", "nl", "en", "en-gb", "fi", "fr",
+                           "de", "el", "he", "hi", "hu", "id", "it", "ja", "ko", "ms", "nb", "pl", "pt-BR", "pt", "ro",
+                           "ru", "es", "sv", "tl", "th", "tr", "vi"]
+
+    SUPPORTED_VOICES = ["man", "woman", "alice", "Polly.Mads", "Polly.Naja", "Polly.Lotte", "Polly.Reuben",
+                        "Polly.Nicole", "Polly.Russell", "Polly.Amy", "Polly.Brian", "Polly.Emma", "Polly.Amy-Neural",
+                        "Polly.Emma-Neural", "Polly.Brian-Neural", "Polly.Raveena", "Polly.Ivy", "Polly.Joanna",
+                        "Polly.Joey", "Polly.Justin", "Polly.Kendra", "Polly.Kimberly", "Polly.Matthew", "Polly.Salli",
+                        "Polly.Ivy-Neural", "Polly.Joanna-Neural", "Polly.Kendra-Neural", "Polly.Kimberly-Neural",
+                        "Polly.Sally-Neural", "Polly.Joey-Neural", "Polly.Justin-Neural", "Polly.Matthew-Neural",
+                        "Polly.Geraint", "Polly.Celine", "Polly.Mathieu", "Polly.Chantal", "Polly.Hans",
+                        "Polly.Marlene", "Polly.Vicki", "Polly.Dora", "Polly.Karl", "Polly.Carla", "Polly.Giorgio",
+                        "Polly.Mizuki", "Polly.Takumi", "Polly.Liv", "Polly.Jacek", "Polly.Jan", "Polly.Ewa",
+                        "Polly.Maja", "Polly.Ricardo", "Polly.Vitoria", "Polly.Camila-Neural", "Polly.Cristiano",
+                        "Polly.Ines", "Polly.Carmen", "Polly.Maxim", "Polly.Tatyana", "Polly.Conchita", "Polly.Enrique",
+                        "Polly.Miguel", "Polly.Penelope", "Polly.Lupe-Neural", "Polly.Astrid", "Polly.Filiz",
+                        "Polly.Gwyneth"]
+
     @classmethod
     def name(cls) -> Text:
         """Name of your custom channel."""
         return "twilio_voice"
+
+    @classmethod
+    def from_credentials(cls, credentials: Optional[Dict[Text, Any]]) -> InputChannel:
+        if not credentials:
+            cls.raise_missing_credentials_exception()
+
+        return cls(
+            credentials.get("assistant_voice", "woman"),
+            credentials.get("language", "en"),
+        )
+
+    def __init__(
+        self,
+        assistant_voice: Optional[Text],
+        language: Optional[Text],
+    ) -> None:
+        self.assistant_voice = assistant_voice
+        self.language = language
+
+        if language not in self.SUPPORTED_LANGUAGES:
+            self.raise_invalid_language_exception()
+
+        if assistant_voice not in self.SUPPORTED_VOICES:
+            self.raise_invalid_voice_exception()
+
+    def raise_invalid_language_exception(self) -> NoReturn:
+        raise RasaException(
+            f"{self.language} is an invalid language. Please refer to the documentation for a list "
+            f"of valid languages you can use for your voice assistant."
+        )
+
+    def raise_invalid_voice_exception(self) -> NoReturn:
+        raise RasaException(
+            f"{self.assistant_voice} is an invalid as an assistant voice. Please refer to the documentation for a list "
+            f"of valid voices you can use for your voice assistant."
+        )
 
     def blueprint(
         self, on_new_message: Callable[[UserMessage], Awaitable[None]]
@@ -25,17 +81,16 @@ class TwilioVoiceInput(InputChannel):
 
         twilio_voice_webhook = Blueprint("Twilio_voice_webhook", __name__)
 
-
         @twilio_voice_webhook.route("/", methods=["GET"])
         async def health(request: Request) -> HTTPResponse:
             return response.json({"status": "ok"})
 
         @twilio_voice_webhook.route("/webhook", methods=["POST"])
         async def receive(request: Request):
-            sender_id = request.form.get("From")  # method to get sender_id
-            text = request.form.get("SpeechResult")  # method to fetch text
-            input_channel = self.name()  # method to fetch input channel
-            call_status = request.form.get("CallStatus")  # method to fetch call status
+            sender_id = request.form.get("From")
+            text = request.form.get("SpeechResult")
+            input_channel = self.name()
+            call_status = request.form.get("CallStatus")
             call_sid = request.form.get("CallSid")  # Get the call identifier
 
             collector = TwilioVoiceCollectingOutputChannel()
@@ -53,10 +108,9 @@ class TwilioVoiceInput(InputChannel):
                 )
 
                 # Build the Twilio VoiceResponse.
-                twilio_response = build_twilio_voice_response(collector.messages)
-                return response.text(str(twilio_response), content_type="text/xml")
+                twilio_response = self.build_twilio_voice_response(collector.messages)
             # If the user doesn't respond to the previous message resend the last message.
-            elif text is None:
+            else:
                 # Get last user utterance from tracker.
                 tracker = request.app.agent.tracker_store.retrieve(sender_id)
                 last_response = next(
@@ -70,10 +124,33 @@ class TwilioVoiceInput(InputChannel):
                 else:
                     last_response = last_response.text
 
-                twilio_response = build_twilio_voice_response([{"text": last_response}])
-                return response.text(str(twilio_response), content_type="text/xml")
+                twilio_response = self.build_twilio_voice_response([{"text": last_response}])
+            return response.text(str(twilio_response), content_type="text/xml")
 
         return twilio_voice_webhook
+
+    def build_twilio_voice_response(
+            self, messages: List[Dict[Text, Any]]) -> VoiceResponse:
+        """Builds the Twilio Voice Response object."""
+        vr = VoiceResponse()
+        gather = Gather(
+            input="speech",
+            action="/webhooks/twilio_voice/webhook",
+            actionOnEmptyResult=True,
+            speechTimeout="auto",
+        )
+
+        # Add pauses between messages.
+        # Add a listener to the last message to listen for user response.
+        for i, message in enumerate(messages):
+            if i + 1 == len(messages):
+                gather.say(message["text"], voice=self.assistant_voice, language=self.language)
+                vr.append(gather)
+            else:
+                vr.say(message["text"], voice=self.assistant_voice, language=self.language)
+                vr.pause(length=1)
+
+        return vr
 
 
 class TwilioVoiceCollectingOutputChannel(CollectingOutputChannel):
@@ -104,26 +181,3 @@ class TwilioVoiceCollectingOutputChannel(CollectingOutputChannel):
         """For voice channel do not send images."""
 
         pass
-
-
-def build_twilio_voice_response(messages: List[Dict[Text, Any]]) -> VoiceResponse:
-    """Builds the Twilio Voice Response object."""
-    vr = VoiceResponse()
-    gather = Gather(
-        input="speech",
-        action="/webhooks/twilio_voice/webhook",
-        actionOnEmptyResult=True,
-        speechTimeout="auto",
-    )
-
-    # Add pauses between messages.
-    # Add a listener to the last message to listen for user response.
-    for i, message in enumerate(messages):
-        if i + 1 == len(messages):
-            gather.say(message["text"])
-            vr.append(gather)
-        else:
-            vr.say(message["text"])
-            vr.pause(length=1)
-
-    return vr
