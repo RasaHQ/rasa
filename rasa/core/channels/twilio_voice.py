a7c422a5be5bd36ce0257969e23c2e7c9760e073
Matthew Vielkind <m.vielkind@rasa.com>
2021-03-24 08:22:49 -0400

    Remove language support for now. Add unit tests for TwiML generation.

--- rasa/core/channels/twilio_voice.py
+++ rasa/core/channels/twilio_voice.py
@@ -16,23 +16,74 @@ from rasa.core.channels.channel import (
 
 class TwilioVoiceInput(InputChannel):
 
-    SUPPORTED_LANGUAGES = ["af", "ar", "ca", "zh", "zh-CN", "zh-HK", "hr", "cs", "da", "nl", "en", "en-gb", "fi", "fr",
-                           "de", "el", "he", "hi", "hu", "id", "it", "ja", "ko", "ms", "nb", "pl", "pt-BR", "pt", "ro",
-                           "ru", "es", "sv", "tl", "th", "tr", "vi"]
-
-    SUPPORTED_VOICES = ["man", "woman", "alice", "Polly.Mads", "Polly.Naja", "Polly.Lotte", "Polly.Reuben",
-                        "Polly.Nicole", "Polly.Russell", "Polly.Amy", "Polly.Brian", "Polly.Emma", "Polly.Amy-Neural",
-                        "Polly.Emma-Neural", "Polly.Brian-Neural", "Polly.Raveena", "Polly.Ivy", "Polly.Joanna",
-                        "Polly.Joey", "Polly.Justin", "Polly.Kendra", "Polly.Kimberly", "Polly.Matthew", "Polly.Salli",
-                        "Polly.Ivy-Neural", "Polly.Joanna-Neural", "Polly.Kendra-Neural", "Polly.Kimberly-Neural",
-                        "Polly.Sally-Neural", "Polly.Joey-Neural", "Polly.Justin-Neural", "Polly.Matthew-Neural",
-                        "Polly.Geraint", "Polly.Celine", "Polly.Mathieu", "Polly.Chantal", "Polly.Hans",
-                        "Polly.Marlene", "Polly.Vicki", "Polly.Dora", "Polly.Karl", "Polly.Carla", "Polly.Giorgio",
-                        "Polly.Mizuki", "Polly.Takumi", "Polly.Liv", "Polly.Jacek", "Polly.Jan", "Polly.Ewa",
-                        "Polly.Maja", "Polly.Ricardo", "Polly.Vitoria", "Polly.Camila-Neural", "Polly.Cristiano",
-                        "Polly.Ines", "Polly.Carmen", "Polly.Maxim", "Polly.Tatyana", "Polly.Conchita", "Polly.Enrique",
-                        "Polly.Miguel", "Polly.Penelope", "Polly.Lupe-Neural", "Polly.Astrid", "Polly.Filiz",
-                        "Polly.Gwyneth"]
+    SUPPORTED_VOICES = [
+        "man",
+        "woman",
+        "alice",
+        "Polly.Mads",
+        "Polly.Naja",
+        "Polly.Lotte",
+        "Polly.Reuben",
+        "Polly.Nicole",
+        "Polly.Russell",
+        "Polly.Amy",
+        "Polly.Brian",
+        "Polly.Emma",
+        "Polly.Amy-Neural",
+        "Polly.Emma-Neural",
+        "Polly.Brian-Neural",
+        "Polly.Raveena",
+        "Polly.Ivy",
+        "Polly.Joanna",
+        "Polly.Joey",
+        "Polly.Justin",
+        "Polly.Kendra",
+        "Polly.Kimberly",
+        "Polly.Matthew",
+        "Polly.Salli",
+        "Polly.Ivy-Neural",
+        "Polly.Joanna-Neural",
+        "Polly.Kendra-Neural",
+        "Polly.Kimberly-Neural",
+        "Polly.Sally-Neural",
+        "Polly.Joey-Neural",
+        "Polly.Justin-Neural",
+        "Polly.Matthew-Neural",
+        "Polly.Geraint",
+        "Polly.Celine",
+        "Polly.Mathieu",
+        "Polly.Chantal",
+        "Polly.Hans",
+        "Polly.Marlene",
+        "Polly.Vicki",
+        "Polly.Dora",
+        "Polly.Karl",
+        "Polly.Carla",
+        "Polly.Giorgio",
+        "Polly.Mizuki",
+        "Polly.Takumi",
+        "Polly.Liv",
+        "Polly.Jacek",
+        "Polly.Jan",
+        "Polly.Ewa",
+        "Polly.Maja",
+        "Polly.Ricardo",
+        "Polly.Vitoria",
+        "Polly.Camila-Neural",
+        "Polly.Cristiano",
+        "Polly.Ines",
+        "Polly.Carmen",
+        "Polly.Maxim",
+        "Polly.Tatyana",
+        "Polly.Conchita",
+        "Polly.Enrique",
+        "Polly.Miguel",
+        "Polly.Penelope",
+        "Polly.Lupe-Neural",
+        "Polly.Astrid",
+        "Polly.Filiz",
+        "Polly.Gwyneth",
+    ]
 
     @classmethod
     def name(cls) -> Text:
@@ -44,30 +95,13 @@ class TwilioVoiceInput(InputChannel):
         if not credentials:
             cls.raise_missing_credentials_exception()
 
-        return cls(
-            credentials.get("assistant_voice", "woman"),
-            credentials.get("language", "en"),
-        )
+        return cls(credentials.get("assistant_voice", "woman"),)
 
-    def __init__(
-        self,
-        assistant_voice: Optional[Text],
-        language: Optional[Text],
-    ) -> None:
+    def __init__(self, assistant_voice: Optional[Text],) -> None:
         self.assistant_voice = assistant_voice
-        self.language = language
-
-        if language not in self.SUPPORTED_LANGUAGES:
-            self.raise_invalid_language_exception()
 
         if assistant_voice not in self.SUPPORTED_VOICES:
             self.raise_invalid_voice_exception()
-
-    def raise_invalid_language_exception(self) -> NoReturn:
-        raise RasaException(
-            f"{self.language} is an invalid language. Please refer to the documentation for a list "
-            f"of valid languages you can use for your voice assistant."
-        )
 
     def raise_invalid_voice_exception(self) -> NoReturn:
         raise RasaException(
@@ -124,13 +158,16 @@ class TwilioVoiceInput(InputChannel):
                 else:
                     last_response = last_response.text
 
-                twilio_response = self.build_twilio_voice_response([{"text": last_response}])
+                twilio_response = self.build_twilio_voice_response(
+                    [{"text": last_response}]
+                )
             return response.text(str(twilio_response), content_type="text/xml")
 
         return twilio_voice_webhook
 
     def build_twilio_voice_response(
-            self, messages: List[Dict[Text, Any]]) -> VoiceResponse:
+        self, messages: List[Dict[Text, Any]]
+    ) -> VoiceResponse:
         """Builds the Twilio Voice Response object."""
         vr = VoiceResponse()
         gather = Gather(
@@ -144,10 +181,10 @@ class TwilioVoiceInput(InputChannel):
         # Add a listener to the last message to listen for user response.
         for i, message in enumerate(messages):
             if i + 1 == len(messages):
-                gather.say(message["text"], voice=self.assistant_voice, language=self.language)
+                gather.say(message["text"], voice=self.assistant_voice)
                 vr.append(gather)
             else:
-                vr.say(message["text"], voice=self.assistant_voice, language=self.language)
+                vr.say(message["text"], voice=self.assistant_voice)
                 vr.pause(length=1)
 
         return vr
@@ -171,9 +208,7 @@ class TwilioVoiceCollectingOutputChannel(CollectingOutputChannel):
         await self._persist_message(self._message(recipient_id, text=text))
 
         for b in buttons:
-            await self._persist_message(
-                self._message(recipient_id, text=b["title"])
-            )
+            await self._persist_message(self._message(recipient_id, text=b["title"]))
 
     async def send_image_url(
         self, recipient_id: Text, image: Text, **kwargs: Any
---
+++ tests/core/channels/test_twilio_voice.py
@@ -0,0 +1,55 @@
+import json
+import logging
+
+import pytest
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.asyncio
+async def test_twilio_voice_twiml_response_text():
+
+    from rasa.core.channels.twilio_voice import TwilioVoiceInput
+    from rasa.core.channels.twilio_voice import TwilioVoiceCollectingOutputChannel
+
+    tv = TwilioVoiceInput("woman", "en")
+
+    output_channel = TwilioVoiceCollectingOutputChannel()
+
+    await output_channel.send_text_message(recipient_id="Chuck Norris", text="Test:")
+    assert len(output_channel.messages) == 1
+    assert output_channel.messages[0]["text"] == "Test:"
+
+    twiml = tv.build_twilio_voice_response(output_channel.messages)
+    assert (
+        str(twiml)
+        == '<?xml version="1.0" encoding="UTF-8"?><Response><Gather action="/webhooks/twilio_voice/webhook" actionOnEmptyResult="true" input="speech" speechTimeout="auto"><Say language="en" voice="woman">Test:</Say></Gather></Response>'
+    )
+
+
+@pytest.mark.asyncio
+async def test_twilio_voice_twiml_response_buttons():
+
+    from rasa.core.channels.twilio_voice import TwilioVoiceInput
+    from rasa.core.channels.twilio_voice import TwilioVoiceCollectingOutputChannel
+
+    tv = TwilioVoiceInput("woman", "en")
+
+    output_channel = TwilioVoiceCollectingOutputChannel()
+    await output_channel.send_text_with_buttons(
+        recipient_id="Chuck Norris",
+        text="Buttons:",
+        buttons=[
+            {"title": "Yes", "payload": "/affirm"},
+            {"title": "No", "payload": "/deny"},
+        ],
+    )
+    assert len(output_channel.messages) == 3
+    message_str = " ".join([m["text"] for m in output_channel.messages])
+    assert message_str == "Buttons: Yes No"
+
+    twiml = tv.build_twilio_voice_response(output_channel.messages)
+    assert (
+        str(twiml)
+        == '<?xml version="1.0" encoding="UTF-8"?><Response><Say language="en" voice="woman">Buttons:</Say><Pause length="1" /><Say language="en" voice="woman">Yes</Say><Pause length="1" /><Gather action="/webhooks/twilio_voice/webhook" actionOnEmptyResult="true" input="speech" speechTimeout="auto"><Say language="en" voice="woman">No</Say></Gather></Response>'
+    )
