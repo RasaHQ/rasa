7e0c6ae226071e7a0e3a8b878d963ff1c4d984db
GitHub CI <builds@github-ci.com>
2021-03-29 20:46:56 +0000

    AUTO sphinxcontrib-versioning 20210329 9a1c0e3f910
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.24/_static/ace/src-min-noconflict/snippets/dot.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/dot"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
---
+++ 1.10.24/_static/ace/src-min-noconflict/snippets/svg.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/svg"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
