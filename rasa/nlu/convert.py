2a9b90d4d6bffa30f7577e5120500c8424b8d37c
martasls <martasls95@gmail.com>
2021-04-03 19:38:50 +0100

    Fix data path in convert data method

--- rasa/nlu/convert.py
+++ rasa/nlu/convert.py
@@ -8,14 +8,15 @@ from rasa.nlu.utils import write_to_file
 
 
 def convert_training_data(
-    data_file: Text, out_file: Text, output_format: Text, language: Text
+    data_files: list, out_file: Text, output_format: Text, language: Text
 ):
-    if not os.path.exists(data_file):
-        print_error(
-            "Data file '{}' does not exist. Provide a valid NLU data file using "
-            "the '--data' argument.".format(data_file)
-        )
-        return
+    for data_file in data_files:
+        if not os.path.exists(data_file):
+            print_error(
+                "Data file '{}' does not exist. Provide a valid NLU data file using "
+                "the '--data' argument.".format(data_file)
+            )
+            return
 
     if output_format == "json":
         td = rasa.shared.nlu.training_data.loading.load_data(data_file, language)
