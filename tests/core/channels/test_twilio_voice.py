fb5181a3e85a37b3d411fc2c51dcb7503d9903e8
Matthew Vielkind <m.vielkind@rasa.com>
2021-04-08 15:06:19 -0400

    Add check to make sure invalid assistant voices are raising an exception.

--- tests/core/channels/test_twilio_voice.py
+++ tests/core/channels/test_twilio_voice.py
@@ -3,16 +3,16 @@ import logging
 
 import pytest
 
+from rasa.shared.exceptions import RasaException
+from rasa.core.channels.twilio_voice import TwilioVoiceInput
+from rasa.core.channels.twilio_voice import TwilioVoiceCollectingOutputChannel
+
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.asyncio
 async def test_twilio_voice_twiml_response_text():
 
-    from rasa.core.channels.twilio_voice import TwilioVoiceInput
-    from rasa.core.channels.twilio_voice import TwilioVoiceCollectingOutputChannel
-
-    tv = TwilioVoiceInput("woman")
+    tv = TwilioVoiceInput(initial_prompt="hello", assistant_voice="woman")
 
     output_channel = TwilioVoiceCollectingOutputChannel()
 
@@ -27,13 +27,9 @@ async def test_twilio_voice_twiml_response_text():
     )
 
 
-@pytest.mark.asyncio
 async def test_twilio_voice_twiml_response_buttons():
 
-    from rasa.core.channels.twilio_voice import TwilioVoiceInput
-    from rasa.core.channels.twilio_voice import TwilioVoiceCollectingOutputChannel
-
-    tv = TwilioVoiceInput("woman")
+    tv = TwilioVoiceInput(initial_prompt="hello", assistant_voice="woman")
 
     output_channel = TwilioVoiceCollectingOutputChannel()
     await output_channel.send_text_with_buttons(
@@ -53,3 +49,9 @@ async def test_twilio_voice_twiml_response_buttons():
         str(twiml)
         == '<?xml version="1.0" encoding="UTF-8"?><Response><Say voice="woman">Buttons:</Say><Pause length="1" /><Say voice="woman">Yes</Say><Pause length="1" /><Gather action="/webhooks/twilio_voice/webhook" actionOnEmptyResult="true" input="speech" speechTimeout="auto"><Say voice="woman">No</Say></Gather></Response>'
     )
+
+
+async def test_twilio_invalid_assistant_voice():
+
+    with pytest.raises(RasaException):
+        tv = TwilioVoiceInput(initial_prompt="hello", assistant_voice="alien")
