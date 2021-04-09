bec12fdc710e54b96afa5a594f73fb7dcb69d3b2
GitHub CI <builds@github-ci.com>
2021-04-09 08:59:13 +0000

    AUTO docusaurus 20210409

--- tests/test_memory_leak.py
+++ tests/test_memory_leak.py
@@ -54,7 +54,7 @@ class MemoryLeakTest(abc.ABC):
     def function_to_profile(self) -> None:
         raise NotImplementedError
 
-    @pytest.mark.timeout(600)
+    @pytest.mark.timeout(600, func_only=True)
     def test_for_memory_leak(
         self, name_for_dumped_files: Text, tmp_path: Path,
     ) -> None:
