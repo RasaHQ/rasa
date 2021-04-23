8ccb5214e4815c4c0c076cbe6361b56e996f5756
Souvik Ghosh <souvik_g10@yahoo.com>
2021-04-23 10:17:02 +0200

    ref: handling of dictionaries in watson improved

--- rasa/shared/nlu/training_data/formats/watson.py
+++ rasa/shared/nlu/training_data/formats/watson.py
@@ -26,25 +26,28 @@ class WatsonReader(JsonTrainingDataReader):
         entity_synonyms = self._entity_synonyms(js)
         entity_synonyms = transform_entity_synonyms(entity_synonyms)
         all_entities = self._list_all_entities(js)
-
-        for intent in js["intents"]:
-            examples = intent.get("examples")
-            if examples == []:
+        print(js)
+        for intent in js.get("intents"):
+            examples = intent.get("examples", None)
+            intent = intent.get("intent", None)
+            if examples is None:
                 continue
-            for text in examples:
-                example_with_entities = self._unpack_entity_examples(
-                    js=js,
-                    text=text["text"],
-                    intent=intent["intent"],
-                    training_examples=training_examples,
-                    all_entities=all_entities,
-                )
-                if text["text"] in example_with_entities:
-                    continue
-                else:
-                    self._add_training_examples_without_entities(
-                        training_examples, intent["intent"], text["text"]
+            else:
+                for text in examples:
+                    utterance = text.get("text", None)
+                    example_with_entities = self._unpack_entity_examples(
+                        js=js,
+                        text=utterance,
+                        intent=intent,
+                        training_examples=training_examples,
+                        all_entities=all_entities,
                     )
+                    if utterance in example_with_entities:
+                        continue
+                    else:
+                        self._add_training_examples_without_entities(
+                            training_examples, intent, utterance
+                        )
 
         return TrainingData(training_examples, entity_synonyms)
 
@@ -77,17 +80,17 @@ class WatsonReader(JsonTrainingDataReader):
                 ]
                 for val in values_of_entity[0]:
                     entities = []
-                    unpack_text = text.replace(entity_name, val["value"]).replace(
-                        "@", ""
-                    )
-                    start_index = unpack_text.index(val["value"])
-                    end_index = start_index + len(val["value"])
+                    unpack_text = text.replace(
+                        entity_name, val.get("value", None)
+                    ).replace("@", "")
+                    start_index = unpack_text.index(val.get("value", None))
+                    end_index = start_index + len(val.get("value", None))
                     entities.append(
                         {
                             "entity": entity_name,
                             "start": start_index,
                             "end": end_index,
-                            "value": val["value"],
+                            "value": val.get("value", None),
                         }
                     )
                     data = {}
@@ -100,17 +103,23 @@ class WatsonReader(JsonTrainingDataReader):
 
     @staticmethod
     def _list_all_entities(js: Dict[Text, Any]) -> List:
-        entities = []
-        for entity in js["entities"]:
-            entities.append({entity["entity"]: entity["values"]})
-        return entities
+        all_entities = []
+        entities = js.get("entities", None)
+        if entities is not None:
+            for entity in entities:
+                all_entities.append({entity["entity"]: entity["values"]})
+        return all_entities
 
     def _entity_synonyms(self, js: Dict[Text, Any]) -> List:
         entity_synonyms = []
-
-        for entity in js["entities"]:
-            for val in entity["values"]:
-                entity_synonyms.append(
-                    {"value": val["value"], "synonyms": val["synonyms"]}
-                )
+        entities = js.get("entities", None)
+        if entities is not None:
+            for entity in entities:
+                for val in entity["values"]:
+                    entity_synonyms.append(
+                        {
+                            "value": val.get("value", None),
+                            "synonyms": val.get("synonyms", None),
+                        }
+                    )
         return entity_synonyms
