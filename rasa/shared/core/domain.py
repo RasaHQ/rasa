72a9ade49c12acaeec6e640a3a0d736f065774e4
Ella Rohm-Ensing <erohmensing@gmail.com>
2021-04-23 15:58:56 +0200

    continue to except ValueErrors

--- rasa/shared/core/domain.py
+++ rasa/shared/core/domain.py
@@ -1788,7 +1788,7 @@ class Domain:
 
         try:
             content = rasa.shared.utils.io.read_yaml_file(filename)
-        except (RasaException, YamlSyntaxException):
+        except (RasaException, ValueError, YamlSyntaxException):
             return False
 
         return any(key in content for key in ALL_DOMAIN_KEYS)
