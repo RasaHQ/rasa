c120ff4b1cbf8af04cc7b6bfe116c0f3de6e7e04
Johannes E. M. Mosig <j.mosig@rasa.com>
2021-04-28 18:13:22 +0200

    Add doc strings

--- rasa/utils/tensorflow/layers_utils.py
+++ rasa/utils/tensorflow/layers_utils.py
@@ -3,9 +3,26 @@ from tensorflow import Tensor
 
 
 def random_indices(batch_size: Tensor, n: Tensor, n_max: Tensor) -> Tensor:
+    """Creates batch_size * n random indices that run from 0 to n_max.
+    
+    Args:
+        batch_size: Number of items in each batch
+        n: Number of random indices in each example
+        n_max: Maximum index
+
+    Returns:
+        A uniformly distributed integer tensor of indices
+    """
     return tf.random.uniform(shape=(batch_size, n), maxval=n_max, dtype=tf.int32)
 
 
 def batch_flatten(x: Tensor) -> Tensor:
-    """Make tensor 2D."""
+    """Makes a tensor 2D.
+    
+    Args:
+        x: Any tensor with at least 2 dimensions
+        
+    Returns:
+        The reshaped tensor
+    """
     return tf.reshape(x, (-1, x.shape[-1]))
