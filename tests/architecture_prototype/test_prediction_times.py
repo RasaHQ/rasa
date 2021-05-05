a35376bff6f312c7ffd0102b25cfce717e2660b8
Tobias Wochinger <t.wochinger@rasa.com>
2021-05-05 09:28:25 +0200

    add profiling

--- tests/architecture_prototype/test_prediction_times.py
+++ tests/architecture_prototype/test_prediction_times.py
@@ -1,4 +1,5 @@
 import asyncio
+import cProfile
 import os
 import time
 
@@ -17,12 +18,18 @@ async def test_predictions():
 
     start = time.time()
     print(f"Started model predictions at {start}")
+    profile = cProfile.Profile()
+    profile.enable()
+
     for i in range(1000):
         await agent.handle_message(
             UserMessage(text="can you help me to build a bot", sender_id=f"sender_{i}")
         )
 
     end = time.time()
+    profile.disable()
+
+    profile.dump_stats("./test_inference.prof")
     print(f"Finished model predictions at {end}. Total time: {end - start}")
 
 
