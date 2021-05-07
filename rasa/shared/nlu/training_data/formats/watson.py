4a0381ee6d7948e7565d3838bd6920662f8d5c78
Souvik Ghosh <souvik_g10@yahoo.com>
2021-05-07 15:32:15 +0200

    fix: small improvements on coding style

--- rasa/shared/nlu/training_data/formats/watson.py
+++ rasa/shared/nlu/training_data/formats/watson.py
@@ -5,6 +5,7 @@ from rasa.shared.nlu.constants import (
     INTENT,
     ENTITIES,
     TEXT,
+    VALID_FEATURE_TYPES,
 )
 from rasa.shared.nlu.training_data.formats.readerwriter import JsonTrainingDataReader
 from rasa.shared.nlu.training_data.util import transform_entity_synonyms
@@ -23,28 +24,26 @@ class WatsonReader(JsonTrainingDataReader):
         entity_synonyms = self._entity_synonyms(js)
         entity_synonyms = transform_entity_synonyms(entity_synonyms)
         all_entities = self._list_all_entities(js)
-        print(js)
         for intent in js.get("intents"):
-            examples = intent.get("examples", None)
-            intent = intent.get("intent", None)
-            if examples is None:
+            examples = intent.get("examples")
+            intent = intent.get("intent")
+            if not examples:
                 continue
-            else:
-                for text in examples:
-                    utterance = text.get("text", None)
-                    example_with_entities = self._unpack_entity_examples(
-                        js=js,
-                        text=utterance,
-                        intent=intent,
-                        training_examples=training_examples,
-                        all_entities=all_entities,
-                    )
-                    if utterance in example_with_entities:
-                        continue
-                    else:
-                        self._add_training_examples_without_entities(
-                            training_examples, intent, utterance
-                        )
+            for text in examples:
+                utterance = text.get("text")
+                example_with_entities = self._unpack_entity_examples(
+                    js=js,
+                    text=utterance,
+                    intent=intent,
+                    training_examples=training_examples,
+                    all_entities=all_entities,
+                )
+                if utterance in example_with_entities:
+                    continue
+
+                self._add_training_examples_without_entities(
+                    training_examples, intent, utterance
+                )
 
         return TrainingData(training_examples, entity_synonyms)
 
@@ -52,10 +51,7 @@ class WatsonReader(JsonTrainingDataReader):
     def _add_training_examples_without_entities(
         training_examples: List, intent: str, text: str
     ) -> None:
-        data = {}
-        data[INTENT] = intent
-        data[TEXT] = text
-        training_examples.append(Message(data=data))
+        training_examples.append(Message(data={INTENT: intent, TEXT: text}))
 
     def _unpack_entity_examples(
         self,
@@ -75,48 +71,47 @@ class WatsonReader(JsonTrainingDataReader):
                     for a_dict in all_entities
                     if entity_name in a_dict
                 ]
+                if values_of_entity is None:
+                    continue
                 for val in values_of_entity[0]:
                     entities = []
-                    unpack_text = text.replace(
-                        entity_name, val.get("value", None)
-                    ).replace("@", "")
-                    start_index = unpack_text.index(val.get("value", None))
-                    end_index = start_index + len(val.get("value", None))
+                    unpack_text = text.replace(entity_name, val.get("value")).replace(
+                        "@", ""
+                    )
+                    start_index = unpack_text.index(val.get("value"))
+                    end_index = start_index + len(val.get("value"))
                     entities.append(
                         {
                             "entity": entity_name,
                             "start": start_index,
                             "end": end_index,
-                            "value": val.get("value", None),
+                            "value": val.get("value"),
                         }
                     )
-                    data = {}
-                    data[INTENT] = intent
-                    data[TEXT] = unpack_text
-                    data[ENTITIES] = entities
-                    training_examples.append(Message(data=data))
+                    training_examples.append(
+                        Message(
+                            data={INTENT: intent, TEXT: unpack_text, ENTITIES: entities}
+                        )
+                    )
 
         return examples_with_entities
 
     @staticmethod
     def _list_all_entities(js: Dict[Text, Any]) -> List:
         all_entities = []
-        entities = js.get("entities", None)
-        if entities is not None:
+        entities = js.get("entities")
+        if not entities:
             for entity in entities:
-                all_entities.append({entity["entity"]: entity["values"]})
+                all_entities.append({entity.get("entity"): entity.get("values")})
         return all_entities
 
     def _entity_synonyms(self, js: Dict[Text, Any]) -> List:
         entity_synonyms = []
-        entities = js.get("entities", None)
-        if entities is not None:
+        entities = js.get("entities")
+        if not entities:
             for entity in entities:
-                for val in entity["values"]:
+                for val in entity.get("values"):
                     entity_synonyms.append(
-                        {
-                            "value": val.get("value", None),
-                            "synonyms": val.get("synonyms", None),
-                        }
+                        {"value": val.get("value"), "synonyms": val.get("synonyms"),}
                     )
         return entity_synonyms
