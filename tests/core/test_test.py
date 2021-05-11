10f2980b0c10e3d53c12571f7b35a6efeeaa0589
b-quachtran <b.quachtran@rasa.com>
2021-05-11 14:55:43 -0700

    Added unit test

--- tests/core/test_test.py
+++ tests/core/test_test.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from typing import Text
+
+import pytest
 
 import rasa.core.test
 from _pytest.capture import CaptureFixture
@@ -17,3 +20,16 @@ async def test_testing_warns_if_action_unknown(
     assert "Test story" in output
     assert "contains the bot utterance" in output
     assert "which is not part of the training data / domain" in output
+
+
+async def test_testing_does_not_warn_if_intent_in_domain(
+    default_agent: Agent, stories_path: Text,
+):
+    with pytest.warns(UserWarning) as record:
+        await rasa.core.test.test(Path(stories_path), default_agent)
+
+    assert all("Found intent" not in r.message.args[0] for r in record)
+    assert all(
+        "in stories which is not part of the domain" not in r.message.args[0]
+        for r in record
+    )
