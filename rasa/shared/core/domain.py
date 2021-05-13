ff3b5f2f9ce569832217e1ea51bda96259519fc3
Anca Lita <27920906+ancalita@users.noreply.github.com>
2021-05-13 13:09:44 +0100

    handle empty form slot mappings and add tests

--- rasa/shared/core/domain.py
+++ rasa/shared/core/domain.py
@@ -1860,7 +1860,11 @@ class Domain:
         Returns:
             The slot mapping or an empty dictionary in case no mapping was found.
         """
-        return self.forms.get(form_name, {})[REQUIRED_SLOTS_KEY]
+        form = self.forms.get(form_name)
+        if form:
+            return form.get(REQUIRED_SLOTS_KEY)
+
+        return {}
 
 
 class SlotMapping(Enum):
