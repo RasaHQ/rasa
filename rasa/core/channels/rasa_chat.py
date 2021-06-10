02e6a2140cbea4cbced817718ae4475e66bf4d1a
melindaloubser1 <melinda.loubser@gmail.com>
2021-06-10 20:26:42 +0200

    experiment with adding metadata on rasaa connector

--- rasa/core/channels/rasa_chat.py
+++ rasa/core/channels/rasa_chat.py
@@ -1,17 +1,28 @@
 import json
-from typing import Text, Optional, Dict, Any
+from typing import Text, Optional, Dict, Any, Callable, Awaitable
 
 import aiohttp
+import inspect
+import asyncio
 import logging
 from sanic.exceptions import abort
+from sanic.request import Request
+from sanic.response import HTTPResponse
+from sanic import Blueprint, response
 import jwt
 import jwt.exceptions
+from asyncio import Queue, CancelledError
 
 import rasa.core.channels.channel
 from rasa.core.channels.channel import InputChannel
 from rasa.core.channels.rest import RestInput
 from rasa.core.constants import DEFAULT_REQUEST_TIMEOUT
-from sanic.request import Request
+
+from rasa.core.channels.channel import (
+    InputChannel,
+    CollectingOutputChannel,
+    UserMessage,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -115,6 +126,20 @@ class RasaChatInput(RestInput):
 
         return jwt_payload[JWT_USERNAME_KEY]
 
+    async def _extract_rasax_username(self, req: Request) -> Optional[Text]:
+        """Fetch username from the Rasa X Admin API."""
+        jwt_payload = None
+        if req.headers.get("Authorization"):
+            jwt_payload = await self._decode_bearer_token(req.headers["Authorization"])
+
+        if not jwt_payload:
+            jwt_payload = await self._decode_bearer_token(req.args.get("token"))
+
+        if not jwt_payload:
+            abort(401)
+
+        return jwt_payload[JWT_USERNAME_KEY]
+
     @staticmethod
     def _has_user_permission_to_send_messages_to_conversation(
         jwt_payload: Dict, message: Dict
@@ -123,3 +148,63 @@ class RasaChatInput(RestInput):
         return INTERACTIVE_LEARNING_PERMISSION in user_scopes or message[
             CONVERSATION_ID_KEY
         ] == jwt_payload.get(JWT_USERNAME_KEY)
+
+    def blueprint(
+        self, on_new_message: Callable[[UserMessage], Awaitable[None]]
+    ) -> Blueprint:
+        custom_webhook = Blueprint(
+            "custom_webhook_{}".format(type(self).__name__),
+            inspect.getmodule(self).__name__,
+        )
+
+        # noinspection PyUnusedLocal
+        @custom_webhook.route("/", methods=["GET"])
+        async def health(request: Request) -> HTTPResponse:
+            return response.json({"status": "ok"})
+
+        @custom_webhook.route("/webhook", methods=["POST"])
+        async def receive(request: Request) -> HTTPResponse:
+            sender_id = await self._extract_sender(request)
+            rasa_x_username = await self._extract_rasax_username(request)
+            text = self._extract_message(request)
+            should_use_stream = rasa.utils.endpoints.bool_arg(
+                request, "stream", default=False
+            )
+            input_channel = self._extract_input_channel(request)
+            metadata = self.get_metadata(request) or {}
+            metadata["rasa_x_username"] = rasa_x_username 
+
+            logger.debug(f"METADATA: {metadata}")
+
+            if should_use_stream:
+                return response.stream(
+                    self.stream_response(
+                        on_new_message, text, sender_id, input_channel, metadata
+                    ),
+                    content_type="text/event-stream",
+                )
+            else:
+                collector = CollectingOutputChannel()
+                # noinspection PyBroadException
+                try:
+                    await on_new_message(
+                        UserMessage(
+                            text,
+                            collector,
+                            sender_id,
+                            input_channel=input_channel,
+                            metadata=metadata,
+                        )
+                    )
+                except CancelledError:
+                    logger.error(
+                        f"Message handling timed out for " f"user message '{text}'."
+                    )
+                except Exception:
+                    logger.exception(
+                        f"An exception occured while handling "
+                        f"user message '{text}'."
+                    )
+                return response.json(collector.messages)
+
+        return custom_webhook
