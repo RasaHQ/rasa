0091c4c1462eebbc86049d93904ecda450965905
GitHub CI <builds@github-ci.com>
2021-06-18 18:29:42 +0000

    AUTO sphinxcontrib-versioning 20210618 cea8f530f66
    CI: true
    LANG: C.UTF-8
    USER: runner

---
+++ 1.10.26/_static/ace/src-min-noconflict/snippets/redshift.js
@@ -0,0 +1,8 @@
+;                (function() {
+                    ace.require(["ace/snippets/redshift"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
