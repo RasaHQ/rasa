6d6050c772f6dde8fc328957ce070812d8037daa
Daksh <d.varshneya@rasa.com>
2021-07-02 10:50:24 +0200

    reduce epochs in memory leak test.

--- tests/test_memory_leak.py
+++ tests/test_memory_leak.py
@@ -152,11 +152,11 @@ class TestCoreLeakManyEpochs(MemoryLeakTest):
 
     @property
     def epochs(self) -> int:
-        return 400
+        return 100
 
     @property
     def max_memory_threshold_mb(self) -> float:
-        return 1600
+        return 2000
 
     def function_to_profile(self) -> None:
         import rasa.model_training
