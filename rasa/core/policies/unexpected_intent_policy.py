d6e9d808b75eea23fb87482acd24322877e0288d
Daksh <d.varshneya@rasa.com>
2021-07-05 10:27:13 +0200

    remove commented code

--- rasa/core/policies/unexpected_intent_policy.py
+++ rasa/core/policies/unexpected_intent_policy.py
@@ -4,11 +4,6 @@ import tensorflow as tf
 from pathlib import Path
 from typing import Any, List, Optional, Text, Dict, Type, Union, TYPE_CHECKING
 
-# try:
-#     from typing import TypedDict
-# except ImportError:
-#     from typing_extensions import TypedDict
-
 from rasa.shared.core.domain import Domain
 from rasa.shared.core.trackers import DialogueStateTracker
 from rasa.shared.core.constants import SLOTS, ACTIVE_LOOP, ACTION_UNLIKELY_INTENT_NAME
