922d8489c29f04cd8d77ecfcdbbe233255151469
Vincent <vincentwarmerdam@gmail.com>
2021-07-12 15:28:19 +0200

    black

--- tests/nlu/test_spacy_utils.py
+++ tests/nlu/test_spacy_utils.py
@@ -5,9 +5,16 @@ from rasa.nlu.config import RasaNLUModelConfig
 from rasa.nlu.utils.spacy_utils import SpacyNLP
 
 
-@pytest.mark.parametrize('model_name,msg', [
-    ("dinosaurhead", "Please confirm that dinosaurhead is an available spaCy model"), 
-    (None, "Missing model configuration for `SpacyNLP` in `config.yml`")])
+@pytest.mark.parametrize(
+    "model_name,msg",
+    [
+        (
+            "dinosaurhead",
+            "Please confirm that dinosaurhead is an available spaCy model",
+        ),
+        (None, "Missing model configuration for `SpacyNLP` in `config.yml`"),
+    ],
+)
 def test_model_raises_error_not_exist(model_name, msg):
     """It should throw a direct error when a bad model is passed."""
     with pytest.raises(InvalidModelError) as err:
