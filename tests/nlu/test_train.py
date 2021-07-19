ade8060dbe36193cc56ce73f5ad84c2a980c1af5
kedz <kedzie@cs.columbia.edu>
2021-07-19 14:47:48 -0400

    Removed tests which had HFTransformersNLP a component. Removed HFTransformersNLP from NLU component registry.

--- tests/nlu/test_train.py
+++ tests/nlu/test_train.py
@@ -12,7 +12,6 @@ from typing import Any, Dict, List, Tuple, Text, Union
 COMPONENTS_TEST_PARAMS = {
     "DIETClassifier": {EPOCHS: 1},
     "ResponseSelector": {EPOCHS: 1},
-    "HFTransformersNLP": {"model_name": "bert", "model_weights": "bert-base-uncased"},
     "LanguageModelFeaturizer": {
         "model_name": "bert",
         "model_weights": "bert-base-uncased",
@@ -74,7 +73,6 @@ def pipelines_for_tests() -> List[Tuple[Text, List[Dict[Text, Any]]]]:
             "en",
             as_pipeline(
                 "WhitespaceTokenizer",
-                "HFTransformersNLP",
                 "LanguageModelFeaturizer",
                 "DIETClassifier",
             ),
