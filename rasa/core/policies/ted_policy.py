01013199a622de6e8a7a80e1b129ee149c7f49a2
Daksh Varshneya <d.varshneya@rasa.com>
2021-07-22 11:55:44 +0200

    Fix epoch override for TEDPolicy (#9182)
    * bump rasa-sdk dep and min compat version
    
    * fix bug add tests
    
    * add changelog
    
    * change test cases
    
    * remove test cases

--- rasa/core/policies/ted_policy.py
+++ rasa/core/policies/ted_policy.py
@@ -1004,7 +1004,9 @@ class TEDPolicy(Policy):
         model_utilities = cls._load_model_utilities(model_path)
 
         model_utilities["meta"] = cls._update_loaded_params(model_utilities["meta"])
-        model_utilities["meta"][EPOCHS] = epoch_override
+
+        if should_finetune:
+            model_utilities["meta"][EPOCHS] = epoch_override
 
         (
             model_data_example,
