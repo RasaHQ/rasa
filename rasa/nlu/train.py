6d2c9ab57c7579e4eaefda47b72467eae926f023
aeshky <aciel.eshky@gmail.com>
2021-07-23 21:14:26 +0100

    I removed the entity roles and groups experimental feature warning message from the code, and tested it using 'rasa train' on a minimal example.

--- rasa/nlu/train.py
+++ rasa/nlu/train.py
@@ -108,10 +108,6 @@ async def train(
         training_data = load_data(data, nlu_config.language)
 
     training_data.print_stats()
-    if training_data.entity_roles_groups_used():
-        rasa.shared.utils.common.mark_as_experimental_feature(
-            "Entity Roles and Groups feature"
-        )
 
     interpreter = trainer.train(training_data, **kwargs)
 
