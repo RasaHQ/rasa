c4b283b42386d3c67c01383791cdf97b016427f5
ka-bu <kathrin.bujna@gmail.com>
2021-08-25 09:51:25 +0200

    change (test name, variable names, more explicit reminder)

--- tests/test_graph_components/providers/test_domain_without_responses_provider.py
+++ tests/test_graph_components/providers/test_domain_without_responses_provider.py
@@ -17,7 +17,7 @@ from rasa.shared.core.domain import KEY_E2E_ACTIONS, KEY_RESPONSES, Domain
         ("data/test_domains/mixed_retrieval_intents.yml"),
     ],
 )
-def test_copy_without_responses(
+def test_recreate_without_responses(
     default_model_storage: ModelStorage,
     default_execution_context: ExecutionContext,
     domain_yml: Text,
@@ -30,31 +30,45 @@ def test_copy_without_responses(
         default_execution_context,
     )
 
-    domain = Domain.from_file(path=domain_yml,)
-    domain_copy = component.recreate_without_responses(domain=domain,)
+    original_domain = Domain.from_file(path=domain_yml)
+    original_dict = original_domain.as_dict()
 
-    given_as_dict = domain.as_dict()
-    copy_as_dict = domain_copy.as_dict()
+    modified_domain = component.recreate_without_responses(domain=original_domain)
+    modified_dict = modified_domain.as_dict()
 
     # all configurations not impacted by responses stay intact
-    assert sorted(given_as_dict.keys()) == sorted(copy_as_dict.keys())
-    for key in given_as_dict.keys():
+    assert sorted(original_dict.keys()) == sorted(modified_dict.keys())
+    for key in original_dict.keys():
         if key not in [KEY_RESPONSES, KEY_E2E_ACTIONS]:
-            assert given_as_dict[key] == copy_as_dict[key]
+            assert original_dict[key] == modified_dict[key]
 
     # Note: The given domains do contain responses and some actions for which no
     # responses are defined...
-    assert domain.responses, "choose a different test config"
-    assert domain.user_actions, "choose a different test config"
-    assert set(domain.user_actions).difference(
-        domain.responses
-    ), "choose a different test config"
+    reminder = (
+        f"This test needs to be adapted. The domain yamls have been changed so that "
+        f"they no longer contain some actions and some responses - which do not "
+        f"coincide (i.e. not all responses are listed as actions). Hence "
+        f"{domain_yml} is no longer a good test case here. Please Remove the yaml from "
+        f"the parameterization or replace it with an different yaml."
+    )
+    assert original_domain.responses, reminder
+    assert original_domain.user_actions, reminder
+    assert set(original_domain.user_actions).difference(
+        original_domain.responses
+    ), reminder
 
     # Assert that the recreated copy does not contain any response information
-    assert not domain_copy.responses
-    assert all(action in domain._custom_actions for action in domain_copy.user_actions)
-    assert set(domain_copy.action_names_or_texts) < set(domain.action_names_or_texts)
+    assert not modified_domain.responses
     assert all(
-        response not in domain_copy.action_names_or_texts
-        for response in set(domain.responses).difference(domain._custom_actions)
+        action in original_domain._custom_actions
+        for action in modified_domain.user_actions
+    )
+    assert set(modified_domain.action_names_or_texts) < set(
+        original_domain.action_names_or_texts
+    )
+    assert all(
+        response not in modified_domain.action_names_or_texts
+        for response in set(original_domain.responses).difference(
+            original_domain._custom_actions
+        )
     )
