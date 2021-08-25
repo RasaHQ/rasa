76d7e7d56fae0885c2109122694ce1a086d9602e
aeshky <aciel.eshky@gmail.com>
2021-08-25 14:15:15 +0100

    NLU training data provider implementation and test

---
+++ rasa/graph_components/providers/nlu_training_data_provider.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from typing import Dict, Text, Any, Optional
+
+from rasa.engine.graph import GraphComponent, ExecutionContext
+from rasa.engine.storage.resource import Resource
+from rasa.engine.storage.storage import ModelStorage
+from rasa.shared.importers.importer import TrainingDataImporter
+from rasa.shared.nlu.training_data.training_data import TrainingData
+
+
+class NLUTrainingDataProvider(GraphComponent):
+    """Provides NLU training data during training and inference time."""
+
+    def __init__(
+        self,
+        model_storage: ModelStorage,
+        resource: Resource,
+        training_data: Optional[TrainingData] = None,
+    ) -> None:
+        """Creates NLU training data provider."""
+        self._model_storage = model_storage
+        self._resource = resource
+        self._training_data = training_data
+
+    @classmethod
+    def create(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> NLUTrainingDataProvider:
+        """Creates component (see parent class for full docstring)."""
+        return cls(model_storage, resource)
+
+    @classmethod
+    def load(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+        **kwargs: Any,
+    ) -> NLUTrainingDataProvider:
+        """Creates provider using a persisted version of itself."""
+        with model_storage.read_from(resource) as resource_directory:
+            training_data = TrainingData.from_path(resource_directory)
+        return cls(model_storage, resource, training_data)
+
+    def _persist(self, training_data: TrainingData) -> None:
+        """Persists NLU training data to model storage."""
+        with self._model_storage.write_to(self._resource) as resource_directory:
+            training_data.persist(str(resource_directory), "nlu.yml")
+
+    def provide_train(
+        self,
+        importer: TrainingDataImporter,
+        language: Optional[Text],
+        persist: Optional[bool],
+    ) -> TrainingData:
+        """Provides nlu training data during training."""
+        training_data = importer.get_nlu_data(language=language)
+        if persist:
+            self._persist(training_data)
+        return training_data
+
+    def provide_inference(self) -> Optional[TrainingData]:
+        """Provides the nlu training data during inference."""
+        return self._training_data
---
+++ tests/test_graph_components/providers/test_nlu_training_data_provider.py
@@ -0,0 +1,44 @@
+from typing import Text
+
+from rasa.engine.graph import ExecutionContext
+from rasa.engine.storage.resource import Resource
+from rasa.engine.storage.storage import ModelStorage
+from rasa.graph_components.providers.nlu_training_data_provider import NLUTrainingDataProvider
+from rasa.shared.nlu.training_data.training_data import TrainingData
+from rasa.shared.importers.importer import TrainingDataImporter
+
+
+def test_nlu_training_data_provider_provides_and_persists_data(
+    default_model_storage: ModelStorage,
+    default_execution_context: ExecutionContext,
+    config_path: Text,
+    training_data_path: Text,
+    training_data: TrainingData,
+):
+    resource = Resource("xy")
+    provider_1 = NLUTrainingDataProvider.create(
+        NLUTrainingDataProvider.get_default_config(),
+        default_model_storage,
+        resource,
+        default_execution_context,
+    )
+    assert isinstance(provider_1, NLUTrainingDataProvider)
+
+    importer = TrainingDataImporter.load_from_config(config_path, training_data_path)
+    training_data_from_provider = provider_1.provide_train(importer)
+
+    assert isinstance(training_data, TrainingData)
+    assert training_data.fingerprint() == training_data_from_provider.fingerprint()
+
+    with default_model_storage.read_from(resource) as d:
+        match = list(d.glob("**/nlu.yml"))
+        assert len(match) == 1
+        assert match[0].is_file()
+
+    provider_2 = NLUTrainingDataProvider.load(
+        {}, default_model_storage, resource, default_execution_context
+    )
+    inference_training_data = provider_2.provide_inference()
+
+    assert isinstance(inference_training_data, TrainingData)
+    assert training_data.fingerprint() == inference_training_data.fingerprint()
