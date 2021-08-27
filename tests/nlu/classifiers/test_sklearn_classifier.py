8c2c1ac1d1cbeba0010a541b02897d3cc5eba5bf
Anca Lita <27920906+ancalita@users.noreply.github.com>
2021-08-27 11:59:26 +0100

    change featurizer in test

--- tests/nlu/classifiers/test_sklearn_classifier.py
+++ tests/nlu/classifiers/test_sklearn_classifier.py
@@ -39,7 +39,7 @@ def test_persist_and_load(
 ):
     pipeline = [
         {"name": "WhitespaceTokenizer"},
-        {"name": "CountVectorsFeaturizer"},
+        {"name": "ConveRTFeaturizer"},
     ]
     loaded_pipeline = [
         registry.get_component_class(component.pop("name"))(component)
