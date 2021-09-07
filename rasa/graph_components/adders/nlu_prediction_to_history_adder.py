0c7b3cacb20bb37c3de5d7c05b0906d86af4f1cc
Matthew Summers <m.summers@rasa.com>
2021-09-07 15:14:16 +0100

    Update component with PR feedback

--- rasa/graph_components/adders/nlu_prediction_to_history_adder.py
+++ rasa/graph_components/adders/nlu_prediction_to_history_adder.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
+import logging
 from rasa.shared.core.events import UserUttered
-from typing import Dict, Text, Any, Optional, List
+from typing import Dict, Text, Any, List
 
 from rasa.core.channels.channel import UserMessage
 
@@ -11,6 +12,8 @@ from rasa.shared.nlu.constants import ENTITIES, INTENT, TEXT
 from rasa.shared.nlu.training_data.message import Message
 from rasa.shared.core.trackers import DialogueStateTracker
 from rasa.shared.core.domain import Domain
+
+logger = logging.getLogger(__name__)
 
 
 class NLUPredictionToHistoryAdder(GraphComponent):
@@ -27,17 +30,26 @@ class NLUPredictionToHistoryAdder(GraphComponent):
         """Creates component (see parent class for full docstring)."""
         return cls()
 
-    # Should this be predict since it's run during inference?
     def add(
+        self,
         predictions: List["Message"],
-        dialogue_tracker: "DialogueStateTracker",
+        tracker: "DialogueStateTracker",
         domain: Domain,
         original_message: UserMessage,
     ) -> "DialogueStateTracker":
+        """Adds NLU predictions to the tracker.
+
+        Args:
+            predictions: A list of NLU predictions wrapped as Messages
+            tracker: The tracker the predictions should be attached to
+            domain: The domain of the model.
+            original_message: An original message from the user with
+                extra metadata to annotate the predictions (e.g. channel)
+
+        Returns:
+            The original tracker updated with events created from the predictions
+        """
         for message in predictions:
-            # Need to find out where to get time from
-            # Message contains time but it is an int, we want a float
-            # What is domain for?
             user_utterance = UserUttered(
                 message.data.get(TEXT),
                 message.data.get(INTENT),
@@ -48,6 +60,11 @@ class NLUPredictionToHistoryAdder(GraphComponent):
                 message.data.get("message_id"),
                 message.data.get("metadata"),
             )
-            user_utterance.apply_to(dialogue_tracker)
+            tracker.update(user_utterance, domain)
 
-        return dialogue_tracker
+        logger.debug(
+            f"Logged {len(predictions)} UserUtterance(s) - \
+                tracker now has {len(tracker.events)} events."
+        )
+
+        return tracker
