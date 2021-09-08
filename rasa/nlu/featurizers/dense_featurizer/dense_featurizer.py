b6089b28228bdecd5ee924e4779ee7b137b5aebe
ka-bu <kathrin.bujna@gmail.com>
2021-09-08 09:44:51 +0200

    minor (replace any by len check)

--- rasa/nlu/featurizers/dense_featurizer/dense_featurizer.py
+++ rasa/nlu/featurizers/dense_featurizer/dense_featurizer.py
@@ -41,7 +41,7 @@ class DenseFeaturizer2(Featurizer2[np.ndarray]):
             dense_sequence_features = dense_sequence_features[is_non_zero_vector]
 
             # if features are all zero, then we must continue with zeros
-            if not any(is_non_zero_vector):
+            if not len(dense_sequence_features):
                 dense_sequence_features = np.zeros([1, shape[-1]])
 
         if pooling_operation == MEAN_POOLING:
