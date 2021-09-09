fc27727128b108c871394e375e219a7e27fabd35
Tobias Wochinger <t.wochinger@rasa.com>
2021-09-09 10:56:07 +0200

    migrate `MitieTokenizer`

--- rasa/nlu/tokenizers/mitie_tokenizer.py
+++ rasa/nlu/tokenizers/mitie_tokenizer.py
@@ -1,6 +1,10 @@
-from typing import List, Text
+from __future__ import annotations
+from typing import List, Text, Dict, Any
 
-from rasa.nlu.tokenizers.tokenizer import Token, Tokenizer
+from rasa.engine.graph import ExecutionContext
+from rasa.engine.storage.resource import Resource
+from rasa.engine.storage.storage import ModelStorage
+from rasa.nlu.tokenizers.tokenizer import Token, TokenizerGraphComponent
 from rasa.shared.nlu.training_data.message import Message
 
 from rasa.shared.utils.io import DEFAULT_ENCODING
@@ -10,22 +14,39 @@ from rasa.nlu.tokenizers._mitie_tokenizer import MitieTokenizer
 MitieTokenizer = MitieTokenizer
 
 
-class MitieTokenizerGraphComponent(Tokenizer):
+class MitieTokenizerGraphComponent(TokenizerGraphComponent):
+    """Tokenizes messages using the `mitie` library.."""
 
-    defaults = {
-        # Flag to check whether to split intents
-        "intent_tokenization_flag": False,
-        # Symbol on which intent should be split
-        "intent_split_symbol": "_",
-        # Regular expression to detect tokens
-        "token_pattern": None,
-    }
+    @staticmethod
+    def get_default_config() -> Dict[Text, Any]:
+        """Returns default config (see parent class for full docstring)."""
+        return {
+            # Flag to check whether to split intents
+            "intent_tokenization_flag": False,
+            # Symbol on which intent should be split
+            "intent_split_symbol": "_",
+            # Regular expression to detect tokens
+            "token_pattern": None,
+        }
 
-    @classmethod
-    def required_packages(cls) -> List[Text]:
+    @staticmethod
+    def required_packages() -> List[Text]:
+        """Any extra python dependencies required for this component to run."""
         return ["mitie"]
 
+    @classmethod
+    def create(
+        cls,
+        config: Dict[Text, Any],
+        model_storage: ModelStorage,
+        resource: Resource,
+        execution_context: ExecutionContext,
+    ) -> MitieTokenizerGraphComponent:
+        """Creates a new component (see parent class for full docstring)."""
+        return cls(config)
+
     def tokenize(self, message: Message, attribute: Text) -> List[Token]:
+        """Tokenizes the text of the provided attribute of the incoming message."""
         import mitie
 
         text = message.get(attribute)
