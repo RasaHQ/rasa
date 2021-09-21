7965e7c8a411f1c63539b28bd9cfaa6aa736a509
alwx <alwxndr@gmail.com>
2021-09-21 09:10:59 +0200

    Code style fix for tracker_store.py

--- rasa/core/tracker_store.py
+++ rasa/core/tracker_store.py
@@ -417,9 +417,7 @@ class DynamoTrackerStore(TrackerStore):
         except self.client.exceptions.ResourceNotFoundException:
             table = dynamo.create_table(
                 TableName=self.table_name,
-                KeySchema=[
-                    {"AttributeName": "sender_id", "KeyType": "HASH"},
-                ],
+                KeySchema=[{"AttributeName": "sender_id", "KeyType": "HASH"},],
                 AttributeDefinitions=[
                     {"AttributeName": "sender_id", "AttributeType": "S"},
                 ],
@@ -445,9 +443,7 @@ class DynamoTrackerStore(TrackerStore):
         """Serializes the tracker, returns object with decimal types."""
         d = tracker.as_dialogue().as_dict()
         d.update(
-            {
-                "sender_id": tracker.sender_id,
-            }
+            {"sender_id": tracker.sender_id,}
         )
         # DynamoDB cannot store `float`s, so we'll convert them to `Decimal`s
         return core_utils.replace_floats_with_decimals(d)
