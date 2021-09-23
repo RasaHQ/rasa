64a2c71d0628a17d21853f3389263254821e7f37
alwx <alwxndr@gmail.com>
2021-09-23 10:55:56 +0200

    Lowering the number of epochs, changelog entry update

--- tests/test_default_project.py
+++ tests/test_default_project.py
@@ -3,11 +3,13 @@ from typing import Any, Dict, Text
 from warnings import WarningMessage
 from _pytest.pytester import Testdir
 import pytest
+import copy
 
 from rasa.__main__ import create_argument_parser
 import rasa.cli.data
 import rasa.cli.scaffold
 import rasa.cli.train
+import rasa.cli.shell
 import rasa.shared.utils.io
 
 
@@ -26,11 +28,20 @@ def test_default_project_has_no_warnings(
 ):
     parser = create_argument_parser()
     rasa.cli.scaffold.create_initial_project(".")
-    rasa.shared.utils.io.write_yaml(default_config, "config.yml")
+
+    config = copy.deepcopy(default_config)
+    for model_part, items in config.items():
+        for item in items:
+            if "epochs" in item:
+                item["epochs"] = 1
+                item["evaluate_every_number_of_epochs"] = -1
+
+    rasa.shared.utils.io.write_yaml(config, "config.yml")
 
     with pytest.warns(None) as warning_recorder:
         rasa.cli.data.validate_files(parser.parse_args(["data", "validate"]))
         rasa.cli.train.run_training(parser.parse_args(["train"]))
+        rasa.cli.shell.shell(parser.parse_args(["shell"]))
 
     assert not [
         w for w in warning_recorder._list if not _warning_should_be_filtered_out(w)
