4da72a8bb1552e4b343390b043f79a5cd79e9399
tczekajlo <t.czekajlo@rasa.com>
2021-10-01 14:08:05 +0200

    Reorganize the deploying your assistant section

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -38,7 +38,12 @@ module.exports = {
             'tuning-your-model',
             'testing-your-assistant',
             'setting-up-ci-cd',
-            'how-to-deploy',
+            {
+              type: 'category',
+              label: 'Deploying Your Assistant',
+              collapsed: true,
+              items: ['deploy/introduction', 'deploy/deploy-rasa', 'deploy/deploy-action-server', 'deploy/deploy-rasa-x'],
+            },
           ],
         },
         "glossary",
