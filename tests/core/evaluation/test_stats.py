c56d53cdb36d7f7c3852530e51359ea53793e001
aeshky <aciel.eshky@gmail.com>
2021-10-06 14:58:32 +0100

    Simplified the tracker IDs in the test file. Created fixtures for the expected results to avoid duplicating code.

--- tests/core/evaluation/test_stats.py
+++ tests/core/evaluation/test_stats.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 import math
 import numpy as np
@@ -10,110 +11,9 @@ from rasa.core.evaluation.stats import (
 )
 
 
-def test_compute_summary_stats():
-    """Tests compute summary stats."""
-    a = [20, 30, 32]
-    stats_a = compute_summary_stats(a)
-    assert stats_a["n"] == 3
-    assert math.isclose(stats_a["mean"], 27.33, abs_tol=0.01)
-    assert stats_a["median"] == 30
-    assert stats_a["min"] == 20
-    assert stats_a["max"] == 32
-
-    b = [15]
-    stats_b = compute_summary_stats(b)
-    assert stats_b["n"] == 1
-    assert stats_b["mean"] == 15
-    assert stats_b["median"] == 15
-    assert stats_b["min"] == 15
-    assert stats_b["max"] == 15
-
-
-def test_compute_summary_stats_empty():
-    """Tests compute summary stats on an empty array."""
-    stats = compute_summary_stats([])
-    assert stats == {
-        "n": 0,
-        "mean": np.nan,
-        "median": np.nan,
-        "min": np.nan,
-        "max": np.nan,
-    }
-
-
-def test_compute_single_tracker_stats(extracted_markers_json):
-    """Tests compute summary stats for a single tracker."""
-    expected_output = {
-        "no_restart": {"n": 1, "mean": 20.0, "median": 20.0, "min": 20, "max": 20},
-        "all_required_data_gathered": {
-            "n": 1,
-            "mean": 8.0,
-            "median": 8.0,
-            "min": 8,
-            "max": 8,
-        },
-        "carbon_offset_calculated": {
-            "n": 2,
-            "mean": 10.0,
-            "median": 10.0,
-            "min": 8,
-            "max": 12,
-        },
-        "chitchat_and_FAQ": {"n": 3, "mean": 8.0, "median": 9.0, "min": 2, "max": 13},
-        "dialog_expected_order": {
-            "n": 1,
-            "mean": 5.0,
-            "median": 5.0,
-            "min": 5,
-            "max": 5,
-        },
-    }
-    extracted_markers = load_extracted_markers_json_file(extracted_markers_json)
-    stats = compute_single_tracker_stats(extracted_markers[0])
-    assert stats == expected_output
-
-
-def test_compute_single_tracker_stats_some_empty(extracted_markers_json):
-    """Tests compute summary stats for a single tracker with empty values."""
-    expected_output = {
-        "no_restart": {"n": 1, "mean": 13.0, "median": 13.0, "min": 13, "max": 13},
-        "all_required_data_gathered": {
-            "n": 1,
-            "mean": 8.0,
-            "median": 8.0,
-            "min": 8,
-            "max": 8,
-        },
-        "carbon_offset_calculated": {
-            "n": 2,
-            "mean": 10.0,
-            "median": 10.0,
-            "min": 8,
-            "max": 12,
-        },
-        "chitchat_and_FAQ": {
-            "n": 0,
-            "mean": np.nan,
-            "median": np.nan,
-            "min": np.nan,
-            "max": np.nan,
-        },
-        "dialog_expected_order": {
-            "n": 1,
-            "mean": 7.0,
-            "median": 7.0,
-            "min": 7,
-            "max": 7,
-        },
-    }
-    extracted_markers = load_extracted_markers_json_file(extracted_markers_json)
-    stats = compute_single_tracker_stats(extracted_markers[1])
-    assert stats == expected_output
-
-
-def test_compute_multi_tracker_stats(extracted_markers_json):
-    """Tests compute summary stats on multiple trackers."""
-    expected_overall_stats = {
+@pytest.fixture
+def overall_stats() -> dict:
+    return {
         "num_trackers": 3,
         "no_restart": {"n": 2, "mean": 16.5, "median": 16.5, "min": 13, "max": 20},
         "all_required_data_gathered": {
@@ -139,8 +39,12 @@ def test_compute_multi_tracker_stats(extracted_markers_json):
             "max": 19,
         },
     }
-    expected_per_tracker_stats = {
-        "8b32209b-2015-4c00-97e2-2757836cf8e9": {
+
+
+@pytest.fixture
+def multi_tracker_stats() -> dict:
+    return {
+        "dialogue_1": {
             "no_restart": {"n": 1, "mean": 20.0, "median": 20.0, "min": 20, "max": 20},
             "all_required_data_gathered": {
                 "n": 1,
@@ -171,7 +75,7 @@ def test_compute_multi_tracker_stats(extracted_markers_json):
                 "max": 5,
             },
         },
-        "8b32209b-2015-4c00-97e2-275546548e9": {
+        "dialogue_2": {
             "no_restart": {"n": 1, "mean": 13.0, "median": 13.0, "min": 13, "max": 13},
             "all_required_data_gathered": {
                 "n": 1,
@@ -202,7 +106,7 @@ def test_compute_multi_tracker_stats(extracted_markers_json):
                 "max": 7,
             },
         },
-        "8b32209b-2015-4c00-97e2-2756588e9": {
+        "dialogue_3": {
             "no_restart": {
                 "n": 0,
                 "mean": np.nan,
@@ -240,10 +144,65 @@ def test_compute_multi_tracker_stats(extracted_markers_json):
             },
         },
     }
+
+
+def test_compute_summary_stats():
+    """Tests compute summary stats."""
+    a = [20, 30, 32]
+    stats_a = compute_summary_stats(a)
+    assert stats_a["n"] == 3
+    assert math.isclose(stats_a["mean"], 27.33, abs_tol=0.01)
+    assert stats_a["median"] == 30
+    assert stats_a["min"] == 20
+    assert stats_a["max"] == 32
+
+    b = [15]
+    stats_b = compute_summary_stats(b)
+    assert stats_b["n"] == 1
+    assert stats_b["mean"] == 15
+    assert stats_b["median"] == 15
+    assert stats_b["min"] == 15
+    assert stats_b["max"] == 15
+
+
+def test_compute_summary_stats_empty():
+    """Tests compute summary stats on an empty array."""
+    stats = compute_summary_stats([])
+    assert stats == {
+        "n": 0,
+        "mean": np.nan,
+        "median": np.nan,
+        "min": np.nan,
+        "max": np.nan,
+    }
+
+
+def test_compute_single_tracker_stats(extracted_markers_json, multi_tracker_stats):
+    """Tests compute summary stats for a single tracker."""
+    expected_output = multi_tracker_stats["dialogue_1"]
+    extracted_markers = load_extracted_markers_json_file(extracted_markers_json)
+    stats = compute_single_tracker_stats(extracted_markers[0])
+    assert stats == expected_output
+
+
+def test_compute_single_tracker_stats_some_empty(
+    extracted_markers_json, multi_tracker_stats
+):
+    """Tests compute summary stats for a single tracker with empty values."""
+    expected_output = multi_tracker_stats["dialogue_2"]
+    extracted_markers = load_extracted_markers_json_file(extracted_markers_json)
+    stats = compute_single_tracker_stats(extracted_markers[1])
+    assert stats == expected_output
+
+
+def test_compute_multi_tracker_stats(
+    extracted_markers_json, overall_stats, multi_tracker_stats
+):
+    """Tests compute summary stats on multiple trackers."""
     extracted_markers = load_extracted_markers_json_file(extracted_markers_json)
     stats, per_tracker_stats = compute_multi_tracker_stats(extracted_markers)
-    assert stats == expected_overall_stats
-    assert per_tracker_stats == expected_per_tracker_stats
+    assert stats == overall_stats
+    assert per_tracker_stats == multi_tracker_stats
 
 
 def test_write_stats(extracted_markers_json, marker_stats_output_json):
