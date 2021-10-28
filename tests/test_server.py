d19dd652a077a949bea026a1f2463c85697f87ac
alwx <alwxndr@gmail.com>
2021-10-28 15:58:43 +0200

    Partial `test_server.py` update

--- tests/test_server.py
+++ tests/test_server.py
@@ -63,12 +63,7 @@ from rasa.shared.core.events import (
     SessionStarted,
 )
 from rasa.shared.core.trackers import DialogueStateTracker
-from rasa.shared.nlu.constants import (
-    INTENT_NAME_KEY,
-    ENTITY_ATTRIBUTE_TYPE,
-    ENTITY_ATTRIBUTE_VALUE,
-    PREDICTED_CONFIDENCE_KEY,
-)
+from rasa.shared.nlu.constants import INTENT_NAME_KEY
 from rasa.model_training import TrainingResult
 from rasa.utils.endpoints import EndpointConfig
 from tests.conftest import AsyncMock, with_model_id, with_model_ids
@@ -178,7 +173,7 @@ async def test_status(rasa_app: SanicASGITestClient, trained_rasa_model: Text):
     _, response = await rasa_app.get("/status")
     model_file = response.json["model_file"]
     assert response.status == HTTPStatus.OK
-    assert "model_id" in response.json
+    assert "fingerprint" in response.json
     assert model_file == Path(trained_rasa_model).name
 
 
@@ -188,7 +183,7 @@ async def test_status_nlu_only(
     _, response = await rasa_app_nlu.get("/status")
     model_file = response.json["model_file"]
     assert response.status == HTTPStatus.OK
-    assert "model_id" in response.json
+    assert "fingerprint" in response.json
     assert "model_file" in response.json
     assert model_file == Path(trained_nlu_model).name
 
@@ -576,7 +571,7 @@ async def test_train_with_yaml(
     rasa_app: SanicASGITestClient, tmp_path_factory: TempPathFactory,
 ):
     training_data = """
-version: "3.0"
+version: "2.0"
 
 stories:
 - story: My story
@@ -789,44 +784,6 @@ async def test_evaluate_stories_end_to_end(
         "confidence",
         "policy",
     }
-
-
-async def test_add_message(rasa_app: SanicASGITestClient,):
-
-    conversation_id = "test_add_message_test_id"
-
-    _, response = await rasa_app.get(f"/conversations/{conversation_id}/tracker")
-    previous_num_events = len(response.json["events"])
-
-    unique_text = f"test_add_message_text_{time.time()}"
-    unique_slot_value = f"test_add_message_entity_{time.time()}"
-    data = {
-        "text": unique_text,
-        "sender": "user",  # must be "user"
-        "parse_data": {
-            "text": unique_text,  # this is what is used for "latest_message"
-            "intent": {PREDICTED_CONFIDENCE_KEY: 0.57, INTENT_NAME_KEY: "greet"},
-            "entities": [
-                {
-                    ENTITY_ATTRIBUTE_TYPE: "name",
-                    ENTITY_ATTRIBUTE_VALUE: unique_slot_value,
-                }
-            ],
-        },
-    }
-    _, response = await rasa_app.post(
-        f"/conversations/{conversation_id}/messages",
-        headers={"Content-Type": rasa.server.JSON_CONTENT_TYPE},
-        json=data,
-    )
-    assert response.json["latest_message"]["text"] == unique_text
-
-    _, response = await rasa_app.get(f"/conversations/{conversation_id}/tracker")
-    updated_events = response.json["events"]
-    assert len(updated_events) == previous_num_events + 2
-    assert updated_events[-2]["text"] == unique_text
-    assert updated_events[-1]["event"] == "slot"
-    assert updated_events[-1]["value"] == unique_slot_value
 
 
 async def test_evaluate_intent(rasa_app: SanicASGITestClient, nlu_data_path: Text):
@@ -1552,9 +1509,9 @@ async def test_load_model(rasa_app: SanicASGITestClient, trained_core_model: Tex
     _, response = await rasa_app.get("/status")
 
     assert response.status == HTTPStatus.OK
-    assert "model_id" in response.json
+    assert "fingerprint" in response.json
 
-    old_model_id = response.json["model_id"]
+    old_fingerprint = response.json["fingerprint"]
 
     data = {"model_file": trained_core_model}
     _, response = await rasa_app.put("/model", json=data)
@@ -1564,9 +1521,9 @@ async def test_load_model(rasa_app: SanicASGITestClient, trained_core_model: Tex
     _, response = await rasa_app.get("/status")
 
     assert response.status == HTTPStatus.OK
-    assert "model_id" in response.json
+    assert "fingerprint" in response.json
 
-    assert old_model_id != response.json["model_id"]
+    assert old_fingerprint != response.json["fingerprint"]
 
 
 async def test_load_model_from_model_server(
@@ -1575,9 +1532,9 @@ async def test_load_model_from_model_server(
     _, response = await rasa_app.get("/status")
 
     assert response.status == HTTPStatus.OK
-    assert "model_id" in response.json
+    assert "fingerprint" in response.json
 
-    old_model_id = response.json["model_id"]
+    old_fingerprint = response.json["fingerprint"]
 
     endpoint = EndpointConfig("https://example.com/model/trained_core_model")
     with open(trained_core_model, "rb") as f:
@@ -1602,9 +1559,9 @@ async def test_load_model_from_model_server(
             _, response = await rasa_app.get("/status")
 
             assert response.status == HTTPStatus.OK
-            assert "model_id" in response.json
+            assert "fingerprint" in response.json
 
-            assert old_model_id != response.json["model_id"]
+            assert old_fingerprint != response.json["fingerprint"]
 
 
 async def test_load_model_invalid_request_body(
@@ -1809,7 +1766,7 @@ def test_app_when_app_has_no_input_channels():
             ],
             None,
             True,
-            """version: "3.0"
+            """version: "2.0"
 stories:
 - story: some-conversation-ID
   steps:
@@ -1832,7 +1789,7 @@ stories:
             ],
             None,
             True,
-            """version: "3.0"
+            """version: "2.0"
 stories:
 - story: some-conversation-ID, story 1
   steps:
@@ -1862,7 +1819,7 @@ stories:
             ],
             None,
             False,
-            """version: "3.0"
+            """version: "2.0"
 stories:
 - story: some-conversation-ID
   steps:
@@ -1886,7 +1843,7 @@ stories:
             ],
             None,
             None,
-            """version: "3.0"
+            """version: "2.0"
 stories:
 - story: some-conversation-ID
   steps:
@@ -1909,7 +1866,7 @@ stories:
             ],
             4,
             True,
-            """version: "3.0"
+            """version: "2.0"
 stories:
 - story: some-conversation-ID
   steps:
@@ -1919,7 +1876,7 @@ stories:
   - action: utter_greet""",
         ),
         # empty conversation
-        ([], None, True, 'version: "3.0"'),
+        ([], None, True, 'version: "2.0"'),
         # Conversation with slot
         (
             [
@@ -1931,7 +1888,7 @@ stories:
             ],
             None,
             True,
-            """version: "3.0"
+            """version: "2.0"
 stories:
 - story: some-conversation-ID
   steps:
@@ -2035,7 +1992,7 @@ async def test_get_story_does_not_update_conversation_session(
     # expected story is returned
     assert (
         response.content.decode().strip()
-        == """version: "3.0"
+        == """version: "2.0"
 stories:
 - story: some-conversation-ID
   steps:
