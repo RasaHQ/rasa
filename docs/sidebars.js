34766942eb30f122dbdaa9a585e4b542af3f3546
Tobias Wochinger <t.wochinger@rasa.com>
2021-11-03 17:18:47 +0100

    clearly differentiate between graph components, NLU components and policies

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -62,7 +62,7 @@ module.exports = {
             'model-configuration',
             'components',
             'policies',
-            'custom-components',
+            'custom-graph-components',
             'training-data-importers',
             'language-support',
           ],
