c4e5f5096532a7406c90658f4ca86dae57d65362
Tobias Wochinger <t.wochinger@rasa.com>
2021-11-05 12:09:41 +0100

    fix mypy issues

--- rasa/core/channels/slack.py
+++ rasa/core/channels/slack.py
@@ -356,7 +356,7 @@ class SlackInput(InputChannel):
             )
 
             return response.text(
-                None, status=HTTPStatus.CREATED, headers={"X-Slack-No-Retry": 1}
+                None, status=HTTPStatus.CREATED, headers={"X-Slack-No-Retry": "1"}
             )
 
         if metadata is not None:
