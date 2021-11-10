412896abfc4ce4d1363ccfddf7fc9df5d4b25586
Srinivas Upadhya <srinivas.upadhya@gmail.com>
2021-11-10 16:37:53 +0530

    make get_conversation_id private

--- rasa/core/channels/slack.py
+++ rasa/core/channels/slack.py
@@ -514,7 +514,7 @@ class SlackInput(InputChannel):
                 metadata = self.get_metadata(request)
                 channel_id = metadata.get("out_channel")
                 thread_id = metadata.get("thread_id")
-                conversation_id = self.get_conversation_id(
+                conversation_id = self._get_conversation_id(
                     sender_id, channel_id, thread_id
                 )
 
@@ -554,7 +554,7 @@ class SlackInput(InputChannel):
                         metadata = self.get_metadata(request)
                         channel_id = metadata.get("out_channel")
                         thread_id = metadata.get("thread_id")
-                        conversation_id = self.get_conversation_id(
+                        conversation_id = self._get_conversation_id(
                             sender_id, channel_id, thread_id
                         )
                         return await self.process_message(
@@ -573,7 +573,7 @@ class SlackInput(InputChannel):
 
         return slack_webhook
 
-    def get_conversation_id(self, sender_id, channel_id, thread_id):
+    def _get_conversation_id(self, sender_id, channel_id, thread_id):
         conversation_id = sender_id
         if self.conversation_granularity == "channel" and channel_id is not None:
             conversation_id = sender_id + "_" + channel_id
