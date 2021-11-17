92bccaca4e25add82a775b85251af06e1ab45e89
Aciel Eshky <aciel.eshky@gmail.com>
2021-11-17 11:01:25 +0000

    Markers cli changes (#10234)
    * adapted the markers cli to move the strategy up one level.
    
    * adapted the CLI tests.
    
    * changed some types from text to path.
    
    * clarified which "config" we mean.
    
    * changed sampling strategy name in the tracker loader and used constants in the tracker loader tests.
    
    * changed docstrings which had old strategy names.

--- rasa/core/evaluation/marker_tracker_loader.py
+++ rasa/core/evaluation/marker_tracker_loader.py
@@ -7,7 +7,7 @@ import rasa.shared.utils.io
 
 STRATEGY_ALL = "all"
 STRATEGY_FIRST_N = "first_n"
-STRATEGY_SAMPLE = "sample"
+STRATEGY_SAMPLE_N = "sample_n"
 
 
 def strategy_all(keys: List[Text], count: int) -> Iterable[Text]:
@@ -20,7 +20,7 @@ def strategy_first_n(keys: List[Text], count: int) -> Iterable[Text]:
     return keys[:count]
 
 
-def strategy_sample(keys: List[Text], count: int) -> Iterable[Text]:
+def strategy_sample_n(keys: List[Text], count: int) -> Iterable[Text]:
     """Samples N unique keys from the set of keys."""
     return random.sample(keys, k=count)
 
@@ -31,7 +31,7 @@ class MarkerTrackerLoader:
     _STRATEGY_MAP = {
         "all": strategy_all,
         "first_n": strategy_first_n,
-        "sample": strategy_sample,
+        "sample_n": strategy_sample_n,
     }
 
     def __init__(
@@ -46,22 +46,22 @@ class MarkerTrackerLoader:
         Args:
             tracker_store: The underlying tracker store to access.
             strategy: The strategy to use for selecting trackers,
-                      can be 'all', 'sample', or 'first_n'.
+                      can be 'all', 'sample_n', or 'first_n'.
             count: Number of trackers to return, can only be None if strategy is 'all'.
             seed: Optional seed to set up random number generator,
-                  only useful if strategy is 'sample'.
+                  only useful if strategy is 'sample_n'.
         """
         self.tracker_store = tracker_store
 
         if strategy not in MarkerTrackerLoader._STRATEGY_MAP:
             raise RasaException(
                 f"Invalid strategy for loading markers - '{strategy}' was given, \
-                options 'all', 'sample', or 'first_n' exist."
+                options 'all', 'sample_n', or 'first_n' exist."
             )
 
         self.strategy = MarkerTrackerLoader._STRATEGY_MAP[strategy]
 
-        if strategy != "all":
+        if strategy != STRATEGY_ALL:
             if not count:
                 raise RasaException(
                     f"Desired tracker count must be given for strategy '{strategy}'."
@@ -73,14 +73,14 @@ class MarkerTrackerLoader:
 
         self.count = count
 
-        if count and strategy == "all":
+        if count and strategy == STRATEGY_ALL:
             rasa.shared.utils.io.raise_warning(
                 "Parameter 'count' is ignored by strategy 'all'."
             )
             self.count = None
 
         if seed:
-            if strategy == "sample":
+            if strategy == STRATEGY_SAMPLE_N:
                 random.seed(seed)
             else:
                 rasa.shared.utils.io.raise_warning(
