a1ca616cc1d9ab9f94da361e7471a4660108e3bb
Markus Hinsche <m.hinsche@gmail.com>
2021-11-24 18:18:07 +0100

    Improve bash code

--- .github/scripts/start_dd_agent.sh
+++ .github/scripts/start_dd_agent.sh
@@ -13,12 +13,14 @@ DATADOG_YAML_PATH=/etc/datadog-agent/datadog.yaml
 sudo chmod 666 $DATADOG_YAML_PATH
 
 # Associate metrics with tags and env
-echo "env: rasa-regression-tests" >> $DATADOG_YAML_PATH
-echo "tags:" >> $DATADOG_YAML_PATH
-echo "- service:rasa" >> $DATADOG_YAML_PATH
-echo "- markusci:true" >> $DATADOG_YAML_PATH
-echo "- dataset:$dataset" >> $DATADOG_YAML_PATH
-echo "- config:$config" >> $DATADOG_YAML_PATH
+{
+    echo "env: rasa-regression-tests"
+    echo "tags:"
+    echo "- service:rasa"
+    echo "- markusci:true"
+    echo "- dataset:$dataset"
+    echo "- config:$config"
+} >> $DATADOG_YAML_PATH
 
 # Enable system_core integration
 sudo mv /etc/datadog-agent/conf.d/system_core.d/conf.yaml.example /etc/datadog-agent/conf.d/system_core.d/conf.yaml
