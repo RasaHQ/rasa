749d8fe78f730c3d99b8c3eecd62a8f6cc0a9faf
Markus Hinsche <m.hinsche@gmail.com>
2021-12-02 16:06:15 +0100

    Create report.json

--- .github/scripts/mr_publish_results.py
+++ .github/scripts/mr_publish_results.py
@@ -21,6 +21,11 @@ EXTERNAL_DATASET_REPOSITORY_BRANCH = None
 CONFIG_REPOSITORY = "training-data"
 CONFIG_REPOSITORY_BRANCH = os.environ["DATASET_REPOSITORY_BRANCH"]
 
+SUMMARY_FILE = os.environ["SUMMARY_FILE"]
+CONFIG = os.environ["CONFIG"]
+DATASET = os.environ["DATASET_NAME"]
+TYPE = os.environ["TYPE"]
+
 analytics.write_key = os.environ["SEGMENT_TOKEN"]
 
 task_mapping = {
@@ -182,6 +187,40 @@ def push_results(file_name, file):
     send_to_segment(result)
 
 
+def generate_json(file, task, data):
+    global IS_EXTERNAL
+    global DATASET_REPOSITORY_BRANCH
+
+    if not DATASET in data:
+        data = {DATASET: {CONFIG: {}}, **data}
+    elif not CONFIG in data[DATASET]:
+        data[DATASET] = {CONFIG: {}, **data[DATASET]}
+
+    if str(IS_EXTERNAL).lower() in ("yes", "true", "t", "1"):
+        IS_EXTERNAL = True
+        DATASET_REPOSITORY_BRANCH = os.environ["EXTERNAL_DATASET_REPOSITORY_BRANCH"]
+    else:
+        IS_EXTERNAL = False
+
+    data[DATASET][CONFIG] = {
+        "external_dataset_repository": IS_EXTERNAL,
+        "dataset_repository_branch": DATASET_REPOSITORY_BRANCH,
+        "config_repository": CONFIG_REPOSITORY,
+        "config_repository_branch": CONFIG_REPOSITORY_BRANCH,
+        "dataset_commit": os.environ["DATASET_COMMIT"],
+        "accelerator_type": os.environ["ACCELERATOR_TYPE"],
+        "test_run_time": os.environ["TEST_RUN_TIME"],
+        "train_run_time": os.environ["TRAIN_RUN_TIME"],
+        "total_run_time": os.environ["TOTAL_RUN_TIME"],
+        "type": TYPE,
+        **data[DATASET][CONFIG],
+    }
+
+    data[DATASET][CONFIG][task] = {**read_results(file)}
+
+    return data
+
+
 if __name__ == "__main__":
     send_to_datadog(None)
     for dirpath, dirnames, files in os.walk(os.environ["RESULT_DIR"]):
@@ -189,3 +228,20 @@ if __name__ == "__main__":
             if any(f.endswith(valid_name) for valid_name in task_mapping.keys()):
                 push_results(f, os.path.join(dirpath, f))
     analytics.flush()
+
+    data = {}
+    if os.path.exists(SUMMARY_FILE):
+        with open(SUMMARY_FILE) as json_file:
+            data = json.load(json_file)
+
+    for dirpath, dirnames, files in os.walk(os.environ["RESULT_DIR"]):
+        for f in files:
+            print('input file for report.json:', os.path.join(dirpath, f))  # Debug
+            if f not in task_mapping.keys():
+                continue
+
+            data = generate_json(os.path.join(dirpath, f), task_mapping[f], data)
+
+    with open(SUMMARY_FILE, "w") as f:
+        print("data:", data)  # Debug
+        json.dump(data, f, sort_keys=True, indent=2)
