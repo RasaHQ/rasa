7867d5b7d20def7968ec3ce91f1664cfdfdf1ecd
ka-bu <kathrin.bujna@gmail.com>
2021-12-10 15:27:05 +0100

    reduce tolerance

--- tests/nlu/featurizers/test_lm_featurizer.py
+++ tests/nlu/featurizers/test_lm_featurizer.py
@@ -332,12 +332,12 @@ class TestShapeValuesTrainAndProcess:
             assert np.allclose(
                 computed_sequence_vec[: len(expected_sequence_vec[index]), 0],
                 expected_sequence_vec[index],
-                atol=1e-3,
+                atol=1e-2,
             )
 
             # Look at the first value of first five dimensions
             assert np.allclose(
-                computed_sentence_vec[0][:5], expected_cls_vec[index], atol=1e-3
+                computed_sentence_vec[0][:5], expected_cls_vec[index], atol=1e-2
             )
 
             (intent_sequence_vec, intent_sentence_vec) = messages[
