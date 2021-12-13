57d5d3a422e6fc639cfbed19237390de88820d51
raoul.vonmetzen@telekom.de <raoul.vonmetzen@telekom.de>
2021-12-13 16:42:29 +0100

    add EntityHierarchyExtrator to pipelines_to_tests function.

--- tests/nlu/test_train.py
+++ tests/nlu/test_train.py
@@ -58,6 +58,7 @@ def pipelines_for_tests() -> List[Tuple[Text, List[Dict[Text, Any]]]]:
                 "DIETClassifier",
                 "ResponseSelector",
                 "EntitySynonymMapper",
+                "EntityHierarchyExtractor"
             ),
         ),
         (
