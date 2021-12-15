e5853da3d5ed1200a51ae2c070ae618d6bc65097
Markus Hinsche <m.hinsche@gmail.com>
2021-12-15 17:16:01 +0100

    Make black, make flake8 happy

--- tests/gha/test_mr_generate_summary.py
+++ tests/gha/test_mr_generate_summary.py
@@ -2,55 +2,40 @@ from pathlib import Path
 import sys
 
 REPO_DIR = Path(__file__).parents[2]
-sys.path.append(str(REPO_DIR / '.github/scripts'))
-from mr_generate_summary import combine_result
+sys.path.append(str(REPO_DIR / ".github/scripts"))
+from mr_generate_summary import combine_result  # noqa: E402
 
 
 RESULT1 = {
-  "financial-demo": {
-    "BERT + DIET(bow) + ResponseSelector(bow)": {
-      "Entity Prediction": {
-        "macro avg": {
-          "f1-score": 0.7333333333333333,
+    "financial-demo": {
+        "BERT + DIET(bow) + ResponseSelector(bow)": {
+            "Entity Prediction": {"macro avg": {"f1-score": 0.7333333333333333,}},
+            "test_run_time": "47s",
         }
-      },
-      "test_run_time": "47s"
     }
-  }
 }
+
 
 def test_same_ds_different_config():
     result2 = {
-      "financial-demo": {
-        "Sparse + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-              "f1-score": 0.88,
+        "financial-demo": {
+            "Sparse + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.88,}},
+                "test_run_time": "47s",
             }
-          },
-          "test_run_time": "47s"
         }
-      }
     }
     expected_combined = {
-      "financial-demo": {
-        "BERT + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-            "f1-score": 0.7333333333333333,
-            }
-          },
-          "test_run_time": "47s"
-        },
-        "Sparse + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-              "f1-score": 0.88,
-            }
-          },
-          "test_run_time": "47s"
+        "financial-demo": {
+            "BERT + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.7333333333333333,}},
+                "test_run_time": "47s",
+            },
+            "Sparse + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.88,}},
+                "test_run_time": "47s",
+            },
         }
-      }
     }
 
     actual_combined = combine_result(RESULT1, result2)
@@ -60,41 +45,28 @@ def test_same_ds_different_config():
     assert actual_combined == expected_combined
 
 
-
 def test_different_ds_same_config():
     result2 = {
-      "Carbon Bot": {
-        "Sparse + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-              "f1-score": 0.88,
+        "Carbon Bot": {
+            "Sparse + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.88,}},
+                "test_run_time": "47s",
             }
-          },
-          "test_run_time": "47s"
         }
-      }
     }
     expected_combined = {
-      "financial-demo": {
-        "BERT + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-            "f1-score": 0.7333333333333333,
-            }
-          },
-          "test_run_time": "47s"
+        "financial-demo": {
+            "BERT + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.7333333333333333,}},
+                "test_run_time": "47s",
+            },
         },
-      },
-      "Carbon Bot": {
-        "Sparse + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-              "f1-score": 0.88,
+        "Carbon Bot": {
+            "Sparse + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.88,}},
+                "test_run_time": "47s",
             }
-          },
-          "test_run_time": "47s"
-        }
-      }
+        },
     }
 
     actual_combined = combine_result(RESULT1, result2)
@@ -107,16 +79,12 @@ def test_different_ds_same_config():
 def test_start_empty():
     result2 = {}
     expected_combined = {
-      "financial-demo": {
-        "BERT + DIET(bow) + ResponseSelector(bow)": {
-          "Entity Prediction": {
-            "macro avg": {
-            "f1-score": 0.7333333333333333,
-            }
-          },
-          "test_run_time": "47s"
-        },
-      }
+        "financial-demo": {
+            "BERT + DIET(bow) + ResponseSelector(bow)": {
+                "Entity Prediction": {"macro avg": {"f1-score": 0.7333333333333333,}},
+                "test_run_time": "47s",
+            },
+        }
     }
 
     actual_combined = combine_result(RESULT1, result2)
--- tests/gha/test_mr_publish_results.py
+++ tests/gha/test_mr_publish_results.py
@@ -2,66 +2,67 @@ from pathlib import Path
 import sys
 
 REPO_DIR = Path(__file__).parents[2]
-sys.path.append(str(REPO_DIR / '.github/scripts'))
-from mr_publish_results import (
-    prepare_ml_metric, prepare_ml_metrics, transform_to_seconds)
+sys.path.append(str(REPO_DIR / ".github/scripts"))
+from mr_publish_results import (  # noqa: E402
+    prepare_ml_metric,
+    prepare_ml_metrics,
+    transform_to_seconds,
+)
 
 
 def test_transform_to_seconds():
-    assert 87. == transform_to_seconds('1m27s')
-    assert 87.3 == transform_to_seconds('1m27.3s')
-    assert 27. == transform_to_seconds('27s')
-    assert 3627. == transform_to_seconds('1h27s')
-    assert 3687. == transform_to_seconds('1h1m27s')
+    assert 87.0 == transform_to_seconds("1m27s")
+    assert 87.3 == transform_to_seconds("1m27.3s")
+    assert 27.0 == transform_to_seconds("27s")
+    assert 3627.0 == transform_to_seconds("1h27s")
+    assert 3687.0 == transform_to_seconds("1h1m27s")
 
 
 def test_prepare_ml_model_perf_metrics():
-    results = [{
-        'macro avg': {
-            'precision': 0.8,
-            'recall': 0.8,
-            'f1-score': 0.8,
-            'support': 14
+    results = [
+        {
+            "macro avg": {
+                "precision": 0.8,
+                "recall": 0.8,
+                "f1-score": 0.8,
+                "support": 14,
+            },
+            "micro avg": {
+                "precision": 1.0,
+                "recall": 0.7857142857142857,
+                "f1-score": 0.88,
+                "support": 14,
+            },
+            "file_name": "DIETClassifier_report.json",
+            "task": "Entity Prediction",
         },
-        'micro avg': {
-            'precision': 1.0,
-            'recall': 0.7857142857142857,
-            'f1-score': 0.88,
-            'support': 14
+        {
+            "accuracy": 1.0,
+            "weighted avg": {
+                "precision": 1.0,
+                "recall": 1.0,
+                "f1-score": 1.0,
+                "support": 28,
+            },
+            "macro avg": {
+                "precision": 1.0,
+                "recall": 1.0,
+                "f1-score": 1.0,
+                "support": 28,
+            },
+            "file_name": "intent_report.json",
+            "task": "Intent Classification",
         },
-        'file_name': 'DIETClassifier_report.json',
-        'task': 'Entity Prediction'
-    }, {
-        'accuracy': 1.0,
-        'weighted avg': {
-            'precision': 1.0,
-            'recall': 1.0,
-            'f1-score': 1.0,
-            'support': 28
-        },
-        'macro avg': {
-            'precision': 1.0,
-            'recall': 1.0,
-            'f1-score': 1.0,
-            'support': 28
-        },
-        'file_name': 'intent_report.json',
-        'task': 'Intent Classification'
-    }]
+    ]
     metrics_ml = prepare_ml_metrics(results)
     assert len(metrics_ml) == 17
 
 
 def test_prepare_ml_model_perf_metric():
     result = {
-        'accuracy': 1.0,
-        'weighted avg': {
-            'precision': 1,
-            'recall': 1.0,
-            'f1-score': 1,
-            'support': 28
-        },
-        'task': 'Intent Classification'
+        "accuracy": 1.0,
+        "weighted avg": {"precision": 1, "recall": 1.0, "f1-score": 1, "support": 28},
+        "task": "Intent Classification",
     }
     metrics_ml = prepare_ml_metric(result)
     assert len(metrics_ml) == 5
@@ -69,8 +70,8 @@ def test_prepare_ml_model_perf_metric():
     for _, v in metrics_ml.items():
         assert isinstance(v, float)
 
-    key, value = 'Intent Classification.accuracy', 1.0
+    key, value = "Intent Classification.accuracy", 1.0
     assert key in metrics_ml and value == metrics_ml[key]
 
-    key, value = 'Intent Classification.weighted avg.f1-score', 1.0
+    key, value = "Intent Classification.weighted avg.f1-score", 1.0
     assert key in metrics_ml and value == metrics_ml[key]
