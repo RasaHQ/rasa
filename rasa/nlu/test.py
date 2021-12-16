a01eb797a65c6f8063e2fc3c6b0868d24099d484
GitHub CI <builds@github-ci.com>
2021-12-16 17:29:10 +0000

    AUTO docusaurus 20211216

--- rasa/nlu/test.py
+++ rasa/nlu/test.py
@@ -1461,12 +1461,14 @@ def generate_folds(
                 training_examples=train,
                 entity_synonyms=training_data.entity_synonyms,
                 regex_features=training_data.regex_features,
+                lookup_tables=training_data.lookup_tables,
                 responses=training_data.responses,
             ),
             TrainingData(
                 training_examples=test,
                 entity_synonyms=training_data.entity_synonyms,
                 regex_features=training_data.regex_features,
+                lookup_tables=training_data.lookup_tables,
                 responses=training_data.responses,
             ),
         )
