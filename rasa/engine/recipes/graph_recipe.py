1b25fa3fea941cc45fe24657ae066028c665c9cf
Tayfun Sen <t.sen@rasa.com>
2022-01-14 17:31:31 +0000

    Warn if CLI parameters for graph recipe

--- rasa/engine/recipes/graph_recipe.py
+++ rasa/engine/recipes/graph_recipe.py
@@ -1,9 +1,14 @@
+import logging
+
 from rasa.engine.recipes.recipe import Recipe
 from rasa.engine.graph import GraphModelConfiguration
 from rasa.shared.data import TrainingType
 from rasa.shared.utils.common import mark_as_experimental_feature
 
 from typing import Dict, Text, Any
+
+
+logger = logging.getLogger(__name__)
 
 
 class GraphV1Recipe(Recipe):
@@ -20,3 +25,9 @@ class GraphV1Recipe(Recipe):
     ) -> GraphModelConfiguration:
         """Converts the default config to graphs (see interface for full docstring)."""
         mark_as_experimental_feature("graph recipe")
+        if cli_parameters:
+            logger.warning(
+                "Unlike the Default Recipe, Graph Recipe does not utilize CLI "
+                "parameters and it will be ignored. Add configuration to the "
+                "recipe itself if you want them to be used."
+            )
