797db3daf21091e7af81abd811a77eff8685a29b
danielenricocahall <danielenricocahall@gmail.com>
2022-01-17 15:33:24 -0500

    await tracker store call in twilio voice

--- rasa/core/channels/twilio_voice.py
+++ rasa/core/channels/twilio_voice.py
@@ -249,7 +249,7 @@ class TwilioVoiceInput(InputChannel):
             # If the user doesn't respond resend the last message.
             else:
                 # Get last user utterance from tracker.
-                tracker = request.app.agent.tracker_store.retrieve(sender_id)
+                tracker = await request.app.agent.tracker_store.retrieve(sender_id)
                 last_response = None
                 if tracker:
                     last_response = next(
