a53c1e9a5dd0e0a6056eab7327606b6d32266709
carlad <carlad@users.noreply.github.com>
2022-01-18 12:09:00 +0100

    fix flake8 check

--- rasa/server.py
+++ rasa/server.py
@@ -79,8 +79,7 @@ if TYPE_CHECKING:
         response.HTTPResponse, Coroutine[Any, Any, response.HTTPResponse]
     ]
     SanicView = Callable[
-        [Arg(Request, "request"), VarArg(), KwArg()],
-        SanicResponse,  # noqa: F821
+        [Arg(Request, "request"), VarArg(), KwArg()], SanicResponse  # noqa: F821
     ]
 
 
