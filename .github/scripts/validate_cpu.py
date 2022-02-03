91a440a1d7cedb966df16d5dd30ed71c5d804e7e
Markus Hinsche <m.hinsche@gmail.com>
2022-02-03 14:02:15 +0100

    Use non-experiemental TF API

--- .github/scripts/validate_cpu.py
+++ .github/scripts/validate_cpu.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 
 def check_gpu_not_available():
-    num_gpus = len(tf.config.experimental.list_physical_devices("GPU"))
+    num_gpus = len(tf.config.list_physical_devices("GPU"))
     print(f"Num GPUs Available: {num_gpus}")
     if num_gpus > 0:
         sys.exit(1)
