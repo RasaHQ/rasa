3d21a8bfb9e895cec92cfdc683667b2bddf8838f
m-vdb <m.verger@rasa.com>
2022-02-22 17:39:15 +0100

    fix ListSlot.value setter (super() issue)

--- rasa/shared/core/slots.py
+++ rasa/shared/core/slots.py
@@ -282,12 +282,8 @@ class ListSlot(Slot):
             # we couldn't convert the value to a list - using default value
             return [0.0]
 
-    @property
-    def value(self) -> Any:
-        """Gets the slot's value."""
-        return super().value
-
-    @value.setter
+    # FIXME: https://github.com/python/mypy/issues/8085
+    @Slot.value.setter  # type: ignore[attr-defined]
     def value(self, value: Any) -> None:
         """Sets the slot's value."""
         if value and not isinstance(value, list):
@@ -295,7 +291,8 @@ class ListSlot(Slot):
             value = [value]
 
         # Call property setter of superclass
-        super().value.fset(self, value)
+        # FIXME: https://github.com/python/mypy/issues/8085
+        super(ListSlot, self.__class__).value.fset(self, value)  # type: ignore[attr-defined] # noqa: E501
 
 
 class CategoricalSlot(Slot):
