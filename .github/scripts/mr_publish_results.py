f4006b165a27418cbdf0606be9c9dbc165f88eba
Markus Hinsche <m.hinsche@gmail.com>
2022-02-23 14:05:26 +0100

    Fix other DATASET -> DATASET_NAME occurrance

--- .github/scripts/mr_publish_results.py
+++ .github/scripts/mr_publish_results.py
@@ -252,7 +252,7 @@ def send_all_to_datadog() -> None:
 
 def generate_json(file: Text, task: Text, data: dict) -> dict:
     config = os.environ["CONFIG"]
-    dataset = os.environ["DATASET"]
+    dataset = os.environ["DATASET_NAME"]
 
     if dataset not in data:
         data = {dataset: {config: []}, **data}
