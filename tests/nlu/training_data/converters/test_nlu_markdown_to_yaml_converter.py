97c9e7774373494f1384bef4a2053da0de8feec0
Tobias Wochinger <t.wochinger@rasa.com>
2022-03-15 16:38:41 +0100

    fix: fix JSON to YAML conversion (#10991) (#10996)

--- tests/nlu/training_data/converters/test_nlu_markdown_to_yaml_converter.py
+++ tests/nlu/training_data/converters/test_nlu_markdown_to_yaml_converter.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from typing import Text
 
 import pytest
+from rasa.shared.nlu.training_data.formats.rasa_yaml import RasaYAMLReader
 
 from rasa.nlu.training_data.converters.nlu_markdown_to_yaml_converter import (
-    NLUMarkdownToYamlConverter,
+    NLUToYamlConverter,
 )
 from rasa.shared.constants import LATEST_TRAINING_DATA_FORMAT_VERSION
 
@@ -16,10 +17,11 @@ from rasa.shared.constants import LATEST_TRAINING_DATA_FORMAT_VERSION
         ("data/test_md/default_retrieval_intents.md", True),
         ("data/test_stories/stories.md", False),
         ("data/test_yaml_stories/rules_without_stories.yml", False),
+        ("data/examples/rasa/demo-rasa.json", True),
     ],
 )
 def test_converter_filters_correct_files(training_data_file: Text, should_filter: bool):
-    assert should_filter == NLUMarkdownToYamlConverter.filter(Path(training_data_file))
+    assert should_filter == NLUToYamlConverter.filter(Path(training_data_file))
 
 
 async def test_nlu_intents_are_converted(tmp_path: Path):
@@ -39,7 +41,7 @@ async def test_nlu_intents_are_converted(tmp_path: Path):
     training_data_file.write_text(simple_nlu_md)
 
     with pytest.warns(None) as warnings:
-        await NLUMarkdownToYamlConverter().convert_and_write(
+        await NLUToYamlConverter().convert_and_write(
             training_data_file, converted_data_folder
         )
     assert not warnings
@@ -83,7 +85,7 @@ async def test_nlu_lookup_tables_are_converted(tmp_path: Path):
     training_data_file.write_text(simple_nlu_md)
 
     with pytest.warns(None) as warnings:
-        await NLUMarkdownToYamlConverter().convert_and_write(
+        await NLUToYamlConverter().convert_and_write(
             training_data_file, converted_data_folder
         )
     assert not warnings
@@ -101,3 +103,26 @@ async def test_nlu_lookup_tables_are_converted(tmp_path: Path):
         "    - nlu\n"
         "    - x\n"
     )
+
+
+async def test_convert_json_to_yaml(tmp_path: Path):
+    test_data_dir = Path("data", "examples", "rasa")
+
+    with pytest.warns(None) as warnings:
+        await NLUToYamlConverter().convert_and_write(
+            Path("data/examples/rasa/demo-rasa.json"), tmp_path
+        )
+    assert not warnings
+
+    assert len(os.listdir(tmp_path)) == 1
+
+    converted_file = tmp_path / "demo-rasa_converted.yml"
+
+    actual_data = RasaYAMLReader().read(converted_file)
+    expected = RasaYAMLReader().read(test_data_dir / "demo-rasa.yml")
+
+    assert len(actual_data.training_examples) == len(expected.training_examples)
+    assert len(actual_data.entity_synonyms) == len(expected.entity_synonyms)
+    assert len(actual_data.regex_features) == len(expected.regex_features)
+    assert len(actual_data.lookup_tables) == len(expected.lookup_tables)
+    assert actual_data.entities == expected.entities
