d0ff629eeb61085e29e9647e2dc686413c0a8df8
Samuel Nogueira <samuelnbacelar@hotmail.com>
2022-03-24 09:46:55 -0300

    updating path
    Co-authored-by: guilhesme23 <marquesguilherme3@gmail.com>
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>

--- rasa/lint/linter.py
+++ rasa/lint/linter.py
@@ -1,9 +1,7 @@
-import argparse
-import os
-from rasa.shared.utils.cli import print_error
+from yamllint.cli import run
 
 from yamllint.cli import run
 def lint_file(path: str) -> None:
     #conf = YamlLintConfig('extends: default')
     #gen = linter.run(f, conf)
-    os.system(f'yamllint {path}')
+    run({path})
