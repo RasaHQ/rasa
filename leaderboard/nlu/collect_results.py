2203b57bc30217cf70c218569d861ef13fa23d2a
Kathrin Bujna <k.bujna@rasa.com>
2022-04-19 15:36:00 +0000

    add entity result collection

--- leaderboard/nlu/collect_results.py
+++ leaderboard/nlu/collect_results.py
@@ -9,7 +9,6 @@ import pandas as pd
 
 REPORT_FOLDER = "report"
 TRAIN_TIME_FILENAME = "training_metadata__times.csv"
-INTENT_REPORT_FILENAME = "intent_report.json"
 HYDRA_FOLDER = ".hydra"
 CONFIG_YAML = "config.yaml"
 
@@ -39,7 +38,7 @@ def collection_to_df(row: Dict[str, Dict[str, Any]]):
 
 
 def collect_run(
-    run_dir: str, timings: bool = True, intents: bool = True
+    run_dir: str, timings: bool = True, reports: bool = True
 ) -> Dict[str, Dict[str, Any]]:
     """Collects results for the given run in a nested dict.
 
@@ -49,7 +48,7 @@ def collect_run(
             leveraging hydra multirun from commandline which result
             in result folders with different pattern, use the
             `collect_{timings|intent_report|...}` functions instead)
-        intents: set to True to collect intent reports
+        reports: set to True to collect reports
         timings: set to True to collect train times
     Returns:
         a nested dict with exactly two levels
@@ -60,7 +59,7 @@ def collect_run(
 
     for flag, collector in [
         (True, collect_config),
-        (intents, collect_intent_report),
+        (reports, collect_reports),
         (timings, collect_timings),
     ]:
         if flag:
@@ -71,6 +70,13 @@ def collect_run(
 
 
 def collect_config(run_dir: str) -> Dict[str, Dict[str, Any]]:
+    """Load hyperparameter configuration into nexted dict with 2 levels.
+
+    Args:
+        run_dir: experiment result directory
+    Returns:
+        hyperparameters formatted as nested dictionary with 2 levels
+    """
     row = {}
     config_yaml = os.path.join(run_dir, HYDRA_FOLDER, CONFIG_YAML)
     if os.path.exists(config_yaml):
@@ -83,6 +89,13 @@ def collect_config(run_dir: str) -> Dict[str, Dict[str, Any]]:
 
 
 def collect_timings(run_dir: str) -> Dict[str, Dict[str, Any]]:
+    """Load content of a timings file to a nested dict with 2 levels.
+
+    Args:
+        run_dir: experiment result directory
+    Returns:
+        report results formatted as nested dictionary with 2 levels
+    """
     row = {}
     train_time_file = os.path.join(run_dir, REPORT_FOLDER, TRAIN_TIME_FILENAME)
     if os.path.exists(train_time_file):
@@ -97,32 +110,60 @@ def collect_timings(run_dir: str) -> Dict[str, Dict[str, Any]]:
     return row
 
 
-def collect_intent_report(run_dir: str) -> Dict[str, Dict[str, Any]]:
+def collect_reports(run_dir: str) -> Dict[str, Dict[str, Any]]:
+    """Load content of all report files to a nested dict with 2 levels.
+
+    Args:
+        run_dir: experiment result directory
+    Returns:
+        report results formatted as nested dictionary with 2 levels
+    """
     row = {}
 
-    def format_intent_report_key(key: str) -> str:
-        if "avg" in key:
+    report_files = glob.glob(os.path.join(run_dir, REPORT_FOLDER, "*_report.json"))
+
+    def format_report_key(key: str, tag: str) -> str:
+        if key.endswith(" avg"):
             return key
         else:
-            return f"{TAG_INTENT}_{key}"
+            return f"{tag}_{key}"
 
-    intent_report_file = os.path.join(run_dir, REPORT_FOLDER, INTENT_REPORT_FILENAME)
-    if os.path.exists(intent_report_file):
-        with open(intent_report_file, "r") as f:
-            intent_report = json.load(f)
+    for report_file in report_files:
+
+        prefix = os.path.basename(report_file).replace("_report.json", "")
+        if prefix == "intent":
+            tag = "report"
+        else:
+            tag = prefix
+
+        with open(report_file, "r") as f:
+            report = json.load(f)
 
         row.update(
             {
-                format_intent_report_key(key): value
-                for key, value in intent_report.items()
+                format_report_key(key=key, tag=tag): value
+                for key, value in report.items()
                 if isinstance(value, dict)
             }
         )
         row.update(
             {
-                format_intent_report_key(key): {"-": value}
-                for key, value in intent_report.items()
+                format_report_key(key=key, tag=tag): {"-": value}
+                for key, value in report.items()
                 if not isinstance(value, dict)
+            }
+        )
+        row.update(
+            {
+                format_report_key(key="labels", tag=tag): {
+                    "-": len(
+                        [
+                            key
+                            for key in report.keys()
+                            if not key.endswith(" avg") and key != "accuracy"
+                        ]
+                    )
+                }
             }
         )
 
@@ -130,13 +171,13 @@ def collect_intent_report(run_dir: str) -> Dict[str, Dict[str, Any]]:
 
 
 def results2df(
-    experiment_dir: str, timings: bool = True, intents: bool = True
+    experiment_dir: str, timings: bool = True, reports: bool = True
 ) -> pd.DataFrame:
     """Collect result for all runs in a nested dict (exactly two levels)."""
     script, data_name = Path(experiment_dir).name.split("__")
     results = []
     for run_dir in glob.glob(os.path.join(experiment_dir, "*")):
-        row = collect_run(run_dir, timings=timings, intents=intents)
+        row = collect_run(run_dir, timings=timings, reports=reports)
         row.setdefault("exp", {}).update({"script": script, "data": data_name})
         df_row = collection_to_df(row)
         results.append(df_row)
--- leaderboard/workspace/runs/run_collect_results.py
+++ leaderboard/workspace/runs/run_collect_results.py
@@ -1,12 +1,12 @@
 import glob
 import os
-from leaderboard.nlu import evaluation
+from leaderboard.nlu import collect_results
 
 results_path = "/home/jupyter/results"
 
 if __name__ == "__main__":
 
     for experiment in glob.glob(os.path.join(results_path, "*")):
-        df_full = evaluation.results2df(os.path.join(results_path, experiment))
+        df_full = collect_results.results2df(os.path.join(results_path, experiment))
         filename = os.path.basename(f"./{experiment}.csv")
         df_full.to_csv(filename)
