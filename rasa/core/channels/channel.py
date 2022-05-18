2ffb45296cef2b1d0555e05de5bf70520d8c54cb
Anton Gavrilov <anton.gavrilov@raiffeisen.ru>
2022-05-18 15:14:34 +0300

    Enable splitting of multiline messages with buttons

--- rasa/core/channels/channel.py
+++ rasa/core/channels/channel.py
@@ -393,9 +393,17 @@ class CollectingOutputChannel(OutputChannel):
         buttons: List[Dict[Text, Any]],
         **kwargs: Any,
     ) -> None:
-        await self._persist_message(
-            self._message(recipient_id, text=text, buttons=buttons)
-        )
+        message_parts: List[Text] = text.strip().split("\n\n")
+        last_message_id: int = len(message_parts)
+
+        for i, message_part in enumerate(message_parts, start=1):
+            await self._persist_message(
+                self._message(
+                    recipient_id,
+                    text=message_part,
+                    buttons=buttons if i == last_message_id else None,
+                )
+            )
 
     async def send_custom_json(
         self, recipient_id: Text, json_message: Dict[Text, Any], **kwargs: Any
