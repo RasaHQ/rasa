14deb3784f9ae15affe4b1811256a0b854f3bae6
Sanchari <s.ghosh@rasa.com>
2022-07-04 09:21:40 +0200

    New pytester fixture added as per review feedback

--- tests/integration_tests/core/test_cli_response.py
+++ tests/integration_tests/core/test_cli_response.py
@@ -1,43 +1,74 @@
+from pathlib import Path
+import sys
+import pytest
+from typing import Callable
+import shutil
 import subprocess
-
-VALIDATE_COMMAND = (
-    "rasa data validate --data data/test/test_integration "
-    "-d data/test/test_integration/domain.yml"
-)
-
+from _pytest.pytester import Pytester
+from _pytest.pytester import RunResult
+from _pytest.fixtures import FixtureRequest
 
 # NOTE this will be extended to test cli logs at process run to validate log level
-def capture(command):
-    proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    out = proc.communicate()[0].decode("utf-8")
-    return out, proc.returncode
+@pytest.fixture
+def run(pytester: Pytester) -> Callable[..., RunResult]:
+    def do_run(*args):
+        args = [shutil.which("rasa")] + list(args)
+        return pytester.run(*args)
 
+    return do_run
 
-def test_rasa_validate_debug_no_errors():
+def test_rasa_validate_debug_no_errors(run: Callable[..., RunResult], tmp_path: Path, request: FixtureRequest):
     # Test captures the subprocess output for the command run
     # validates that the data in 'data/test/test_integration' throws no cli errors
     # in 'debug' mode
-    command = [VALIDATE_COMMAND, "--debug"]
-    out, exitcode = capture(command)
-    assert exitcode == 0
-    assert out == ""
+    test_data_dir = Path(request.config.rootdir, "data", "test", "test_integration")
+    source_file = (test_data_dir).absolute()
+    domain_file = (test_data_dir / "domain.yml").absolute()
+    result = run(
+            "data",
+            "validate",
+            "--data",
+            str(source_file),
+            "-d",
+            str(domain_file),
+            "--debug",
+        )
+    assert result.ret == 0
 
 
-def test_rasa_validate_verbose_no_errors():
+def test_rasa_validate_verbose_no_errors(run: Callable[..., RunResult], tmp_path: Path, request: FixtureRequest):
     # Test captures the subprocess output for the command run
     # and validates that the data in 'data/test/test_integration' throws no cli errors
     # in 'verbose' mode
-    command = [VALIDATE_COMMAND, "--verbose"]
-    out, exitcode = capture(command)
-    assert exitcode == 0
-    assert out == ""
+    test_data_dir = Path(request.config.rootdir, "data", "test", "test_integration")
+    source_file = (test_data_dir).absolute()
+    domain_file = (test_data_dir / "domain.yml").absolute()
+    result = run(
+             "data",
+             "validate",
+             "--data",
+             str(source_file),
+             "-d",
+             str(domain_file),
+             "--verbose",
+            )
+    assert result.ret == 0
 
 
-def test_rasa_validate_quiet_no_errors():
+def test_rasa_validate_quiet_no_errors(run: Callable[..., RunResult], tmp_path: Path, request: FixtureRequest):
     # Test captures the subprocess output for the command run
     # and validates that the data in 'data/test/test_integration' throws no cli errors
     # in 'quiet' mode
-    command = [VALIDATE_COMMAND, "--quiet"]
-    out, exitcode = capture(command)
-    assert exitcode == 0
-    assert out == ""
+    test_data_dir = Path(request.config.rootdir, "data", "test", "test_integration")
+    source_file = (test_data_dir).absolute()
+    domain_file = (test_data_dir / "domain.yml").absolute()
+    result = run(
+             "data",
+             "validate",
+             "--data",
+             str(source_file),
+             "-d",
+             str(domain_file),
+             "--quiet",
+             )
+    assert result.ret == 0
