3da7b5e8d73ed95eff58e538924026bc973c0623
郑与天 <zhengyutian@cohl.com>
2022-07-07 18:08:16 +0800

    Delete docs directory

--- docs/themes/theme-custom/theme/Prototyper/index.jsx
+++
@@ -1,6 +0,0 @@
-import DownloadButton from './download-button';
-import Prototyper from './prototyper';
-import TrainButton from './train-button';
-
-export default Prototyper;
-export { DownloadButton, TrainButton };
