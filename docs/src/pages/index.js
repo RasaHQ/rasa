609f49ebcdcec780491f07de40551c51f383ab81
znat <nathan.zylbersztejn@gmail.com>
2022-09-06 13:23:52 +0200

    docs: remove docusaurus default home page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -32,9 +31,7 @@ export default function Home() {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
