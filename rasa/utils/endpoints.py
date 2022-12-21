d0f769fd663b27045448f646c9dd60b2314ed665
Radovan Zivkovic <r.zivkovic@rasa.com>
2022-12-21 22:09:42 +0100

    Update docstring format for request in endpoints.py

--- rasa/utils/endpoints.py
+++ rasa/utils/endpoints.py
@@ -137,8 +137,8 @@ class EndpointConfig:
         """Send a HTTP request to the endpoint. Return json response, if available.
 
         All additional arguments will get passed through
-        to aiohttp's `session.request`."""
-
+        to aiohttp's `session.request`.
+        """
         # create the appropriate headers
         headers = {}
         if content_type:
