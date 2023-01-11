6f5e59a80a9e2ae2e99db3fa160279829df3be28
Radovan Zivkovic <r.zivkovic@rasa.com>
2023-01-11 15:13:09 +0100

    Do not consider alpha releases as latest release when building docs

---
+++ tests/scripts/test_evaluate_release_tag.py
@@ -0,0 +1,60 @@
+from scripts.evaluate_release_tag import filter_non_alpha_releases, should_build_docs
+import pytest
+from pep440_version_utils import Version
+from typing import List
+from unittest.mock import patch
+
+
+@pytest.mark.parametrize("releases, expected",
+    [
+        (
+            [Version("1.1.0"), Version("2.2.0")],
+            [Version("1.1.0"), Version("2.2.0")],
+        ),
+        (
+            [Version("1.1.0"), Version("2.2.0"), Version("1.1.1a")],
+            [Version("1.1.0"), Version("2.2.0")],
+        ),
+(
+            [Version("1.1.0"), Version("2.2.0"), Version("1.1.1a1")],
+            [Version("1.1.0"), Version("2.2.0")],
+        )
+    ],
+)
+def test_filter_non_alpha_releases(releases: List[Version], expected: List[Version]):
+    result = filter_non_alpha_releases(releases)
+    assert result == expected
+
+
+@pytest.mark.parametrize("releases, tag, expected",
+     [
+         (
+            [Version("1.1.0"), Version("2.2.0")], Version("2.3.0"), True,
+         ),
+         (
+            [Version("1.1.0"), Version("2.2.0"), Version("2.3.0a1")], Version("2.2.1"), True,
+         ),
+         (
+            [Version("1.1.0"), Version("2.2.0"), Version("2.3.0")], Version("1.2.0"), True,
+         ),
+         (
+            [Version("1.1.0"), Version("1.2.0a1"), Version("2.3.0")], Version("1.1.2"), True,
+         ),
+         (
+            [Version("1.1.0"), Version("2.2.0"), Version("2.3.0")], Version("2.2.1"), False,
+         ),
+         (
+            [Version("1.1.0"), Version("2.2.0"), Version("2.3.0")], Version("2.2.1a1"), False,
+         ),
+     ],
+)
+@patch("scripts.evaluate_release_tag.get_existing_tag_versions")
+def test_should_build_docs(
+    mock_get_existing_tag_versions,
+    releases: List[Version],
+    tag: Version,
+    expected: bool,
+) -> None:
+    mock_get_existing_tag_versions.return_value = releases
+    result = should_build_docs(tag)
+    assert result == expected
