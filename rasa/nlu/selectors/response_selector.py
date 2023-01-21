b650e0b1460da7e69d0ea96fbc8e57f24bcc685d
Vlad Burlik <vladbph@gmail.com>
2023-01-20 21:33:50 -0800

    Added balance_once parameter to response_selector.py

--- rasa/nlu/selectors/response_selector.py
+++ rasa/nlu/selectors/response_selector.py
@@ -37,6 +37,7 @@ from rasa.utils.tensorflow.constants import (
     NUM_HEADS,
     BATCH_SIZES,
     BATCH_STRATEGY,
+    BALANCE_ONCE,
     EPOCHS,
     RANDOM_SEED,
     LEARNING_RATE,
@@ -170,6 +171,8 @@ class ResponseSelector(DIETClassifier):
             # Strategy used when creating batches.
             # Can be either 'sequence' or 'balanced'.
             BATCH_STRATEGY: BALANCED,
+            # Balance only once instead of each epoch
+            BALANCE_ONCE: False,
             # Number of epochs to train
             EPOCHS: 300,
             # Set random seed to any 'int' to get reproducible results
