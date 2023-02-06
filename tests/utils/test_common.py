920c3ea97482139136f7f9d1123c3c317954d944
Radovan Zivkovic <r.zivkovic@rasa.com>
2023-02-06 15:48:38 +0100

    Test for RasaException in test_get_bool_env_variable_with_invalid_value

--- tests/utils/test_common.py
+++ tests/utils/test_common.py
@@ -12,6 +12,7 @@ from pytest import LogCaptureFixture, MonkeyPatch
 
 from rasa.core.agent import Agent
 from rasa.nlu.classifiers.diet_classifier import DIETClassifier
+from rasa.shared.exceptions import RasaException
 import rasa.utils.common
 from rasa.utils.common import (
     RepeatedLogFilter,
@@ -357,5 +358,5 @@ def test_get_bool_env_variable_with_invalid_value(
 ):
     monkeypatch.setenv(env_name, env_value)
 
-    with pytest.raises(ValueError):
+    with pytest.raises(RasaException):
         get_bool_env_variable(env_name, default_value)
