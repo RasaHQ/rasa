b6095b7e7ac43de53a9a16c67a2d0c549341b7fe
John Trammell <j.trammell@rasa.com>
2023-02-27 12:59:52 -0600

    add docstrings to shared CLI functions

--- rasa/shared/utils/cli.py
+++ rasa/shared/utils/cli.py
@@ -5,6 +5,12 @@ import rasa.shared.utils.io
 
 
 def print_color(*args: Any, color: Text) -> None:
+    """Print the given arguments to STDOUT in the specified color.
+
+    Args:
+        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+        color (:obj:`Text`): A textual representation of the color.
+    """
     output = rasa.shared.utils.io.wrap_with_color(*args, color=color)
     try:
         # colorama is used to fix a regression where colors can not be printed on
@@ -18,23 +24,48 @@ def print_color(*args: Any, color: Text) -> None:
 
 
 def print_success(*args: Any) -> None:
+    """Print the given arguments to STDOUT in green, indicating success.
+
+    Args:
+        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+    """
     print_color(*args, color=rasa.shared.utils.io.bcolors.OKGREEN)
 
 
 def print_info(*args: Any) -> None:
+    """Print the given arguments to STDOUT in blue, indicating an informational
+    message.
+
+    Args:
+        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+    """
     print_color(*args, color=rasa.shared.utils.io.bcolors.OKBLUE)
 
 
 def print_warning(*args: Any) -> None:
+    """Print the given arguments to STDOUT in a color indicating a warning.
+
+    Args:
+        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+    """
     print_color(*args, color=rasa.shared.utils.io.bcolors.WARNING)
 
 
 def print_error(*args: Any) -> None:
+    """Print the given arguments to STDOUT in a color indicating an error.
+
+    Args:
+        args (:obj:`list` of :obj:`Any`): A list of objects to be printed.
+    """
     print_color(*args, color=rasa.shared.utils.io.bcolors.FAIL)
 
 
 def print_error_and_exit(message: Text, exit_code: int = 1) -> NoReturn:
-    """Print error message and exit the application."""
+    """Print an error message and exit the application.
 
+    Args:
+        message (:obj:`Text`): The error message to be printed.
+        exit_code (int): The program exit code, defaults to 1.
+    """
     print_error(message)
     sys.exit(exit_code)
