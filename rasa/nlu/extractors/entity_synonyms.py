382d201cc1d70d1d424ddfac32bd20d7b6a5cd53
vcidst <hello@shailendra.me>
2023-03-28 18:21:19 +0200

    Revert "port changes for ATO-616"
    This reverts commit 9d937d3932143750a66ea0a0bdd78cd3b033857d.

--- rasa/nlu/extractors/entity_synonyms.py
+++ rasa/nlu/extractors/entity_synonyms.py
@@ -93,6 +93,7 @@ class EntitySynonymMapper(GraphComponent, EntityExtractorMixin):
         return messages
 
     def _persist(self) -> None:
+
         if self.synonyms:
             with self._model_storage.write_to(self._resource) as storage:
                 entity_synonyms_file = storage / EntitySynonymMapper.SYNONYM_FILENAME
@@ -142,37 +143,25 @@ class EntitySynonymMapper(GraphComponent, EntityExtractorMixin):
                 entity["value"] = self.synonyms[entity_value.lower()]
                 self.add_processor_name(entity)
 
-    def _add_entities_if_synonyms(self, entity: Text, synonym: Optional[Text]) -> None:
-        """Adds entities to the synonym lookup table.
+    def _add_entities_if_synonyms(
+        self, entity_a: Text, entity_b: Optional[Text]
+    ) -> None:
+        if entity_b is not None:
+            original = str(entity_a)
+            replacement = str(entity_b)
 
-        Lowercase is used as keys to make the lookup case-insensitive.
-        """
-        if synonym is not None:
-            entity = str(entity)
-            synonym = str(synonym)
-            if entity != synonym:
-                entity_lowercase = entity.lower()
-                if (
-                    entity_lowercase in self.synonyms
-                    and self.synonyms[entity_lowercase] != synonym
-                ):
+            if original != replacement:
+                original = original.lower()
+                if original in self.synonyms and self.synonyms[original] != replacement:
                     rasa.shared.utils.io.raise_warning(
                         f"Found conflicting synonym definitions "
-                        f"for {repr(entity_lowercase)}. Overwriting target "
-                        f"{repr(self.synonyms[entity_lowercase])} with "
-                        f"{repr(synonym)}. "
+                        f"for {repr(original)}. Overwriting target "
+                        f"{repr(self.synonyms[original])} with "
+                        f"{repr(replacement)}. "
                         f"Check your training data and remove "
                         f"conflicting synonym definitions to "
                         f"prevent this from happening.",
                         docs=DOCS_URL_TRAINING_DATA + "#synonyms",
                     )
 
-                self.synonyms[entity_lowercase] = synonym
-
-            # add a self-reference to handle entities extracted in alternate cases
-            # i.e. for a synonym Austria,
-            # entities extracted as AUSTRIA, austria, ausTRIA, etc
-            # should also have the value of `Austria`
-            synonym_lowercase = synonym.lower()
-            if synonym_lowercase not in self.synonyms:
-                self.synonyms[synonym_lowercase] = synonym
+                self.synonyms[original] = replacement
