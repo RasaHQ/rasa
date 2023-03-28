c1e1cbe00bb0fe17d6acd83d738d9eadee22569f
Anca Lita <27920906+ancalita@users.noreply.github.com>
2023-03-28 11:45:15 +0100

    add integration test

--- tests/integration_tests/core/brokers/test_pika.py
+++ tests/integration_tests/core/brokers/test_pika.py
@@ -1,3 +1,7 @@
+import logging
+
+from pytest import LogCaptureFixture
+
 from rasa.core.brokers.pika import PikaEventBroker
 from .conftest import (
     RABBITMQ_HOST,
@@ -21,3 +25,34 @@ async def test_pika_event_broker_connect():
         assert broker.is_ready()
     finally:
         await broker.close()
+
+
+async def test_pika_event_broker_publish_after_restart(
+    caplog: LogCaptureFixture,
+):
+    broker = PikaEventBroker(
+        host=RABBITMQ_HOST,
+        username=RABBITMQ_USER,
+        password=RABBITMQ_PASSWORD,
+        port=RABBITMQ_PORT,
+        queues=[RABBITMQ_DEFAULT_QUEUE],
+    )
+    await broker.connect()
+    assert broker.is_ready()
+    broker.publish({"event": "test"})
+
+    with caplog.at_level(logging.DEBUG):
+        await broker.close()
+        assert "Closing RabbitMQ connection." in caplog.text
+
+    caplog.clear()
+
+    # reconnect with the same broker
+    with caplog.at_level(logging.WARNING):
+        await broker.connect()
+        assert broker.is_ready()
+
+        broker.publish({"event": "test_after_restart"})
+        assert caplog.text == ""
+
+    await broker.close()
