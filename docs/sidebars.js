a383184f12daae91d6b7ed64ed665acc0ac01b2f
m-vdb <m.verger@rasa.com>
2023-03-30 15:36:41 +0200

    rename Rasa pro Architecture overview page

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -15,7 +15,7 @@ module.exports = {
                     collapsed: true,
                     type: 'category',
                     items: [
-                        'installation/rasa-pro/architecture-overview',
+                        'installation/rasa-pro/rasa-pro-artifacts',
                         'installation/rasa-pro/installation',
                     ],
                 },
