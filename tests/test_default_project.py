9c2600cf1fe94df6631bc4ff59b81cf623117cc0
Uros Milovanovic <u.milovanovic@rasa.com>
2023-04-24 18:30:15 +0200

    Fix default proj test

--- tests/test_default_project.py
+++ tests/test_default_project.py
@@ -9,6 +9,7 @@ from pytest import Testdir
 
 from rasa.__main__ import create_argument_parser
 import rasa.cli.data
+import rasa.cli.utils
 import rasa.cli.scaffold
 import rasa.cli.train
 import rasa.cli.shell
@@ -36,7 +37,12 @@ def test_default_project_has_no_warnings(
     rasa.shared.utils.io.write_yaml(config, "config.yml")
 
     with pytest.warns() as warning_recorder:
-        rasa.cli.data.validate_files(parser.parse_args(["data", "validate"]))
+        arg_namespace = parser.parse_args(["data", "validate"])
+        rasa.cli.utils.validate_files(
+            arg_namespace.fail_on_warnings,
+            arg_namespace.max_history,
+            rasa.cli.data._build_training_data_importer(arg_namespace),
+        )
         rasa.cli.train.run_training(parser.parse_args(["train"]))
 
     # pytest.warns would override any warning filters that we could set
