1af8e69aa6c73dddd3be9fb9ac4db320d170a795
Tawakalt <olaniyitaiwo95@yahoo.com>
2023-05-05 16:27:07 +0200

    add test to reproduce

--- tests/integration_tests/core/brokers/test_kafka.py
+++ tests/integration_tests/core/brokers/test_kafka.py
@@ -19,3 +19,25 @@ def test_kafka_event_broker_valid():
     finally:
         broker.producer.flush()
         broker._close()
+
+
+def test_kafka_event_broker_raises_buffer_error():
+    broker = KafkaEventBroker(
+        url="localhost",
+        topic="rasa",
+        sasl_username="admin",
+        sasl_password="password",
+        partition_by_sender=True,
+    )
+
+    event_count = 100000
+    for i in range(event_count):
+        try:
+            broker.publish(
+                {"sender_id": "valid_test", "event": "user", "text": "hello world!"},
+                retries=5,
+            )
+            assert broker.producer.poll() == 1
+        finally:
+            broker.producer.flush()
+            broker._close()
