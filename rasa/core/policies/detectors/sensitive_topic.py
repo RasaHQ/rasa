3dc8e1551e6129a65b0b4e9f906dde632fa90f8a
Tom Bocklisch <tom@rasa.com>
2023-06-14 09:17:46 +0200

    cleanups and use jinja2 instead of fstring (unification)

--- rasa/core/policies/detectors/sensitive_topic.py
+++ rasa/core/policies/detectors/sensitive_topic.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
+import importlib
 from typing import Text, Dict, Any, Iterable
 import os
 import openai
 import logging
+from jinja2 import Template
 
 logger = logging.getLogger(__name__)
 
@@ -10,9 +12,16 @@ logger = logging.getLogger(__name__)
 CONFIG_KEY_MODEL_NAME = "sensitive_model_name"
 CONFIG_KEY_ACTION = "sensitive_action"
 CONFIG_KEY_USE_STUB = "sensitive_use_stub"
+CONFIG_KEY_PROMPT_TEMPLATE = "prompt"
+
+DEFAULT_PROMPT_TEMPLATE = importlib.resources.read_text(
+    "rasa.core.policies.detectors", "prompt_sensitive_topic.jinja2"
+)
 
 
 class SensitiveTopicDetectorBase(ABC):
+    """Base class for sensitive topic detectors."""
+
     DEFAULT_ACTION = "flow_sensitive-topic"
 
     def __init__(self, config: Dict[Text, Any]):
@@ -21,18 +30,30 @@ class SensitiveTopicDetectorBase(ABC):
     @classmethod
     def get_default_config(cls) -> Dict[Text, Any]:
         return {
+            # action to be executed if the sensitive topic is detected
             CONFIG_KEY_ACTION: cls.DEFAULT_ACTION,
         }
 
     @abstractmethod
     def check(self, user_msg: Text) -> bool:
+        """Check if the user message contains sensitive topic.
+
+        Args:
+            user_msg: user message to check
+
+        Returns:
+            True if the message contains sensitive topic, False otherwise"""
         ...
 
     def action(self) -> Text:
+        """Return action to be executed if the sensitive topic is detected."""
         return self._action
 
 
 class SensitiveTopicDetector(SensitiveTopicDetectorBase):
+    """Sensitive topic detector based on OpenAI ChatGPT model."""
+
+    # TODO: move to shared configuration of components based on LLM
     DEFAULT_MODEL_NAME = "text-davinci-003"
     DEFAULT_USE_STUB = False
 
@@ -44,9 +65,14 @@ class SensitiveTopicDetector(SensitiveTopicDetectorBase):
 
         # TODO: move the key in more appropriate config (global DM2 config?)
         key = os.getenv("OPENAI_API_KEY")
+
         self._model_name = config.get(CONFIG_KEY_MODEL_NAME, self.DEFAULT_MODEL_NAME)
         self._action = config.get(CONFIG_KEY_ACTION, self.DEFAULT_ACTION)
         self._use_stub = config.get(CONFIG_KEY_USE_STUB, self.DEFAULT_USE_STUB)
+        self._prompt_template = config.get(
+            CONFIG_KEY_PROMPT_TEMPLATE, DEFAULT_PROMPT_TEMPLATE
+        )
+
         if not self._use_stub:
             if key is None:
                 logger.warning(
@@ -59,11 +85,16 @@ class SensitiveTopicDetector(SensitiveTopicDetectorBase):
 
     @classmethod
     def get_default_config(cls) -> Dict[Text, Any]:
-        return {
-            CONFIG_KEY_MODEL_NAME: cls.DEFAULT_MODEL_NAME,
-            CONFIG_KEY_ACTION: cls.DEFAULT_ACTION,
-            CONFIG_KEY_USE_STUB: cls.DEFAULT_USE_STUB,
-        }
+        default_config = super().get_default_config()
+        default_config.update(
+            {
+                CONFIG_KEY_MODEL_NAME: cls.DEFAULT_MODEL_NAME,
+                CONFIG_KEY_USE_STUB: cls.DEFAULT_USE_STUB,
+                CONFIG_KEY_PROMPT_TEMPLATE: DEFAULT_PROMPT_TEMPLATE,
+            }
+        )
+
+        return default_config
 
     def check(self, user_msg: Text) -> bool:
         if self._use_stub:
@@ -76,32 +107,28 @@ class SensitiveTopicDetector(SensitiveTopicDetectorBase):
             )
             resp_text = resp.choices[0].text
             result = self._parse_response(resp_text)
-            logger.warning("Response: %s -> %s", resp_text.strip(), result)
+            logger.info("Response: %s -> %s", resp_text.strip(), result)
         except openai.error.RateLimitError:
             logger.warning("RateLimitError from openai, fall back to stub")
             result = self._stub.check(user_msg)
         return result
 
-    def action(self) -> Text:
-        return self._action
-
-    @staticmethod
-    def _make_prompt(user_msg: Text) -> Text:
-        return f"""Below is the message from the user to the specialized
-financial chatbot. Can you detect the sensitive topic, not related to
-the scope of the bot? Reply "YES" or "NO" and nothing else.
-
-{user_msg}
-"""
+    def _make_prompt(self, user_message: Text) -> Text:
+        """Make prompt for OpenAI ChatGPT model."""
+        return Template(self._prompt_template).render(user_message=user_message)
 
     @staticmethod
     def _parse_response(text: Text) -> bool:
+        """Parse response from OpenAI ChatGPT model.
+
+        Expected responses are "YES" and "NO" (case-insensitive)."""
         return "YES" in text.upper()
 
 
 class SensitiveTopicDetectorStub(SensitiveTopicDetectorBase):
-    """Stub class for testing and debugging. Instead of using
-    ChatGPT, uses fixed substrings for detection.
+    """Stub class for testing and debugging.
+
+    Instead of using ChatGPT, uses fixed substrings for detection.
     """
 
     DEFAULT_POSITIVE = ("voices in my head", "health problems")
