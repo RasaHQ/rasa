c5996faaf4e2ab2aae8a4987d77cecc53bbabd5b
m-vdb <m.verger@rasa.com>
2023-06-15 11:21:10 +0200

    remove docs from sidebar + add links

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -28,20 +28,6 @@ module.exports = {
             items: [
                 'migrate-from',
                 'command-line-interface',
-                
-                {
-                    type: 'category',
-                    label: 'Using LLMs',
-                    collapsed: true,
-                    items: [
-                        'llms/large-language-models',
-                        'llms/llm-setup',
-                        'llms/llm-intent',
-                        'llms/llm-intentless',
-                        'llms/llm-nlg',
-                        'llms/llm-custom',
-                    ],
-                },
                 {
                     type: 'category',
                     label: 'Best Practices',
