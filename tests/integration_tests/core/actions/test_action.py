cc5c606d0f0917d76b27146aff06ab0f1617694c
Anca Lita <27920906+ancalita@users.noreply.github.com>
2023-07-21 12:07:00 +0100

    Extract CRV filtering and enable usage in CallbackNLG (#12575)
    * implement changes
    
    * fix order of args
    
    * update tests
    
    * add changelog entry
    
    * update docstrings and comments, unify if checks to remove indentation
    
    * amend ID to accept null value instead of empty string, edit docs
    
    * fix code quality fail for changelog name
    
    * update schema, improve debug logging

--- tests/integration_tests/core/actions/test_action.py
+++ tests/integration_tests/core/actions/test_action.py
@@ -38,13 +38,100 @@ async def test_action_bot_response_callback_nlg(
         output_channel, callback_nlg, default_tracker, domain_with_response_ids
     )
 
-    body = nlg_request_format(
-        "utter_one_id", default_tracker, output_channel.name(), response_ids=["1"]
+    expected_body = nlg_request_format(
+        "utter_one_id",
+        default_tracker,
+        output_channel.name(),
+        response_id="1",
     )
 
     mock_nlg_endpoint.request.assert_called_once_with(
-        method="post", json=body, timeout=DEFAULT_REQUEST_TIMEOUT
+        method="post", json=expected_body, timeout=DEFAULT_REQUEST_TIMEOUT
     )
 
     assert len(events) == 1
     assert events[0].metadata == {"utter_action": "utter_one_id"}
+
+
+async def test_action_bot_response_callback_with_multiple_response_id(
+    domain_with_response_ids: Domain,
+    default_tracker: DialogueStateTracker,
+    mock_nlg_endpoint: MagicMock,
+) -> None:
+    callback_nlg = CallbackNaturalLanguageGenerator(mock_nlg_endpoint)
+
+    output_channel = CollectingOutputChannel()
+
+    events = await ActionBotResponse("utter_multiple_ids").run(
+        output_channel, callback_nlg, default_tracker, domain_with_response_ids
+    )
+
+    expected_body = nlg_request_format(
+        "utter_multiple_ids",
+        default_tracker,
+        output_channel.name(),
+        response_id="2",
+    )
+
+    mock_nlg_endpoint.request.assert_called_once_with(
+        method="post", json=expected_body, timeout=DEFAULT_REQUEST_TIMEOUT
+    )
+
+    assert len(events) == 1
+    assert events[0].metadata == {"utter_action": "utter_multiple_ids"}
+
+
+async def test_action_bot_response_with_empty_response_id_set(
+    domain_with_response_ids: Domain,
+    default_tracker: DialogueStateTracker,
+    mock_nlg_endpoint: MagicMock,
+) -> None:
+    callback_nlg = CallbackNaturalLanguageGenerator(mock_nlg_endpoint)
+
+    output_channel = CollectingOutputChannel()
+
+    events = await ActionBotResponse("utter_no_id").run(
+        output_channel, callback_nlg, default_tracker, domain_with_response_ids
+    )
+
+    expected_body = nlg_request_format(
+        "utter_no_id",
+        default_tracker,
+        output_channel.name(),
+        response_id=None,
+    )
+
+    mock_nlg_endpoint.request.assert_called_once_with(
+        method="post", json=expected_body, timeout=DEFAULT_REQUEST_TIMEOUT
+    )
+
+    assert len(events) == 1
+    assert events[0].metadata == {"utter_action": "utter_no_id"}
+
+
+async def test_action_bot_response_with_non_existing_id_mapping(
+    domain_with_response_ids: Domain,
+    default_tracker: DialogueStateTracker,
+    mock_nlg_endpoint: MagicMock,
+) -> None:
+    callback_nlg = CallbackNaturalLanguageGenerator(mock_nlg_endpoint)
+
+    output_channel = CollectingOutputChannel()
+
+    events = await ActionBotResponse("utter_non_existing").run(
+        output_channel, callback_nlg, default_tracker, domain_with_response_ids
+    )
+
+    expected_body = nlg_request_format(
+        "utter_non_existing",
+        default_tracker,
+        output_channel.name(),
+        response_id=None,
+    )
+
+    mock_nlg_endpoint.request.assert_called_once_with(
+        method="post", json=expected_body, timeout=DEFAULT_REQUEST_TIMEOUT
+    )
+
+    assert len(events) == 1
+    assert events[0].metadata == {"utter_action": "utter_non_existing"}
