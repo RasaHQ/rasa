ba620a16db776e2d9e5b7bae99720132479d07f5
Alan Nichol <alan@rasa.com>
2023-09-08 13:14:25 +0200

    actions.py file in root of tutorial project

---
+++ rasa/cli/tutorial_project/actions.py
@@ -0,0 +1,19 @@
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionSufficientBalance(Action):
+    def name(self) -> Text:
+        return "action_sufficient_balance"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        balance = 1000 # hard-coded for tutorial purposes
+        # a real api call would look something like
+        # result = requests.get("https://example.com/api/balance")
+        # balance = result.json()["balance"]
+        transfer_amount = tracker.get_slot("amount")
+        has_sufficient_funds = transfer_amount < balance
+        return [SlotSet("has_sufficient_funds", has_sufficient_funds)]
