8ffa9ea2308a8aee2ea5b1ffe1b42e4b3368a4df
m-vdb <m.verger@rasa.com>
2023-09-20 09:36:01 +0200

    content iteration on banner + url

--- docs/themes/theme-custom/theme/RasaCALMBanner/index.jsx
+++ docs/themes/theme-custom/theme/RasaCALMBanner/index.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 function RasaCALMBanner() {
   return (
@@ -7,17 +8,18 @@ function RasaCALMBanner() {
         <div class="mdx-box admonition-heading">
           <h5>
             <span class="admonition-icon">
-              {/* SVG taken from https://primer.style/design/foundations/icons/rocket-24 */}
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                <path d="M20.322.75h1.176a1.75 1.75 0 0 1 1.75 1.749v1.177a10.75 10.75 0 0 1-2.925 7.374l-1.228 1.304a23.699 23.699 0 0 1-1.596 1.542v5.038c0 .615-.323 1.184-.85 1.5l-4.514 2.709a.75.75 0 0 1-1.12-.488l-.963-4.572a1.305 1.305 0 0 1-.14-.129L8.04 15.96l-1.994-1.873a1.305 1.305 0 0 1-.129-.14l-4.571-.963a.75.75 0 0 1-.49-1.12l2.71-4.514c.316-.527.885-.85 1.5-.85h5.037a23.668 23.668 0 0 1 1.542-1.594l1.304-1.23A10.753 10.753 0 0 1 20.321.75Zm-6.344 4.018v-.001l-1.304 1.23a22.275 22.275 0 0 0-3.255 3.851l-2.193 3.29 1.859 1.744a.545.545 0 0 1 .034.034l1.743 1.858 3.288-2.192a22.263 22.263 0 0 0 3.854-3.257l1.228-1.303a9.251 9.251 0 0 0 2.517-6.346V2.5a.25.25 0 0 0-.25-.25h-1.177a9.252 9.252 0 0 0-6.344 2.518ZM6.5 21c-1.209 1.209-3.901 1.445-4.743 1.49a.236.236 0 0 1-.18-.067.236.236 0 0 1-.067-.18c.045-.842.281-3.534 1.49-4.743.9-.9 2.6-.9 3.5 0 .9.9.9 2.6 0 3.5Zm-.592-8.588L8.17 9.017c.23-.346.47-.685.717-1.017H5.066a.25.25 0 0 0-.214.121l-2.167 3.612ZM16 15.112c-.333.248-.672.487-1.018.718l-3.393 2.262.678 3.223 3.612-2.167a.25.25 0 0 0 .121-.214ZM17.5 8a1.5 1.5 0 1 1-3.001-.001A1.5 1.5 0 0 1 17.5 8Z"></path>
+              <svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16">
+                <path fill-rule="evenodd" d="M6.5 0C3.48 0 1 2.19 1 5c0 .92.55 2.25 1 3 1.34 2.25 1.78 2.78 2 4v1h5v-1c.22-1.22.66-1.75 2-4 .45-.75 1-2.08 1-3 0-2.81-2.48-5-5.5-5zm3.64 7.48c-.25.44-.47.8-.67 1.11-.86 1.41-1.25 2.06-1.45 3.23-.02.05-.02.11-.02.17H5c0-.06 0-.13-.02-.17-.2-1.17-.59-1.83-1.45-3.23-.2-.31-.42-.67-.67-1.11C2.44 6.78 2 5.65 2 5c0-2.2 2.02-4 4.5-4 1.22 0 2.36.42 3.22 1.19C10.55 2.94 11 3.94 11 5c0 .66-.44 1.78-.86 2.48zM4 14h5c-.23 1.14-1.3 2-2.5 2s-2.27-.86-2.5-2z">
+                </path>
               </svg>
-            </span>Conversational AI with Language Models
+            </span>NLU-based assistants
           </h5>
         </div>
       <div class="mdx-box admonition-content">
         <p>
-          This page refers to building NLU-based assistants. Want to supercharge your assistant?
-          Check out <a href="#">Conversation AI with Language Models (CALM)</a>.
+          This page refers to building NLU-based assistants.
+          If you are working with <a href={useBaseUrl("/conversational-ai-with-rasa")}>Conversation AI with Language Models (CALM)</a>,
+          this content may not apply to you.
         </p>
       </div>
     </div>
