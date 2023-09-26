92a0e241798b64a63c00dad0011c08898e67eb7e
Uros Milovanovic <u.milovanovic@rasa.com>
2023-09-26 17:14:29 +0200

    init flows_yaml_schema

--- rasa/shared/utils/validation.py
+++ rasa/shared/utils/validation.py
@@ -289,3 +289,17 @@ def validate_training_data_format_version(
         docs=DOCS_URL_TRAINING_DATA,
     )
     return False
+
+def validate_yaml_with_schema(json_data: Dict[Text, Any], schema: Dict[Text, Any]) -> None:
+    from jsonschema import validate
+    from jsonschema import ValidationError
+
+    try:
+        validate(json_data, schema)
+    except ValidationError as e:
+        e.message += (
+            f". Failed to validate data, make sure your data "
+            f"is valid. For more information about the format visit "
+            f"{DOCS_URL_TRAINING_DATA}."
+        )
+        raise SchemaValidationError.create_from(e) from e
