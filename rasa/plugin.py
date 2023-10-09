850b30e6ae94f1654705bd6a27027f9f9f8dcbb5
Anca Lita <27920906+ancalita@users.noreply.github.com>
2023-10-09 12:39:06 +0100

    fix numpy import error

--- rasa/plugin.py
+++ rasa/plugin.py
@@ -6,7 +6,6 @@ from typing import Any, Dict, List, Optional, TYPE_CHECKING, Text, Tuple, Union
 import pluggy
 
 from rasa.cli import SubParsersAction
-from rasa.engine.storage.storage import ModelMetadata
 from rasa.shared.core.trackers import DialogueStateTracker
 from rasa.shared.nlu.training_data.message import Message
 
@@ -14,6 +13,7 @@ if TYPE_CHECKING:
     from rasa.core.brokers.broker import EventBroker
     from rasa.core.tracker_store import TrackerStore
     from rasa.engine.graph import SchemaNode
+    from rasa.engine.storage.storage import ModelMetadata
     from rasa.shared.core.domain import Domain
     from rasa.utils.endpoints import EndpointConfig
 
@@ -95,7 +95,7 @@ def init_telemetry(endpoints_file: Optional[Text]) -> None:
 
 @hookspec  # type: ignore[misc]
 def mock_tracker_for_evaluation(
-    example: Message, model_metadata: Optional[ModelMetadata]
+    example: Message, model_metadata: Optional["ModelMetadata"]
 ) -> Optional[DialogueStateTracker]:
     """Generate a mocked tracker for NLU evaluation."""
 
