63dd66b70561c103c4ddb4e507548aa8a56b26e6
Anca Lita <27920906+ancalita@users.noreply.github.com>
2023-10-09 11:41:05 +0100

    add debug log

--- rasa/utils/endpoints.py
+++ rasa/utils/endpoints.py
@@ -149,6 +149,7 @@ class EndpointConfig:
             headers.update(kwargs["headers"])
             del kwargs["headers"]
 
+        logger.debug(f"OSS request headers: {headers}")
         url = concat_url(self.url, subpath)
 
         sslcontext = None
