15aa90fce489ea68e62c17d44726df528c49cc71
Thomas Werkmeister <thomas@werkmeister.me>
2023-10-26 13:10:06 +0200

    ruff check

--- rasa/shared/core/flows/steps/__init__.py
+++ rasa/shared/core/flows/steps/__init__.py
@@ -1,6 +1,7 @@
 from .action import ActionFlowStep
 from .branch import BranchFlowStep
 from .collect import CollectInformationFlowStep
+from .continuation import ContinueFlowStep
 from .end import EndFlowStep
 from .generate_response import GenerateResponseFlowStep
 from .internal import InternalFlowStep
@@ -8,3 +9,18 @@ from .link import LinkFlowStep
 from .set_slots import SetSlotsFlowStep
 from .start import StartFlowStep
 from .user_message import UserMessageStep
+
+# to make ruff happy and use the imported names
+all_steps = [
+    ActionFlowStep,
+    BranchFlowStep,
+    CollectInformationFlowStep,
+    ContinueFlowStep,
+    EndFlowStep,
+    GenerateResponseFlowStep,
+    InternalFlowStep,
+    LinkFlowStep,
+    SetSlotsFlowStep,
+    StartFlowStep,
+    UserMessageStep,
+]
