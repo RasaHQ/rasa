047ae7514d1df3f2e576cca9ca4bd0f3923f99a3
Tanja Bunk <tabergma@gmail.com>
2023-11-17 14:30:28 +0100

    add str representations for logging

--- rasa/shared/core/trackers.py
+++ rasa/shared/core/trackers.py
@@ -892,6 +892,14 @@ class DialogueStateTracker:
     def __ne__(self, other: Any) -> bool:
         return not self.__eq__(other)
 
+    def __repr__(self) -> Text:
+        """Returns event as string for debugging."""
+        return f"DialogueStateTracker(sender_id: {self.sender_id})"
+
+    def __str__(self) -> Text:
+        """Returns event as human-readable string."""
+        return f"{self.__class__.__name__}({self.sender_id})"
+
     def trigger_followup_action(self, action: Text) -> None:
         """Triggers another action following the execution of the current."""
         self.followup_action = action
