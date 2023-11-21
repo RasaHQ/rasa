998489df2c745602f4959a0a1b28fad410c727cf
OgnjenFrancuski <ognjenfrancuski@gmail.com>
2023-11-21 23:41:50 +0100

    Add magic-filter pkg_resources deprecation warning in EXPECTED_WARNINGs

--- rasa/utils/common.py
+++ rasa/utils/common.py
@@ -55,6 +55,12 @@ EXPECTED_WARNINGS: List[Tuple[Type[Warning], str]] = [
         np.VisibleDeprecationWarning,
         "Creating an ndarray from ragged nested sequences.*",
     ),
+    # This warning is triggered by magic_filter when running
+    # rasa init
+    (
+        DeprecationWarning,
+        "pkg_resources is deprecated as an API."
+    ),
     # This warning is triggered by sanic-cors 2.0.0.
     # The warning can be removed after the packages are updated:
     # sanic-cors: ^2.1.0
