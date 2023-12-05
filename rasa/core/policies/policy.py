ae494bb81a91b73633c8c4ae9e1819ac5af52c9d
vcidst <hello@shailendra.me>
2023-12-05 12:24:45 +0100

    add docstring for params

--- rasa/core/policies/policy.py
+++ rasa/core/policies/policy.py
@@ -117,15 +117,21 @@ class Policy(GraphComponent):
         only_after_user_message: bool = True,
         empty_stack_behavior: bool = True,
     ) -> bool:
-        """Check whether the policy is allowed to act."""
+        """Check whether the policy is allowed to act.
+        
+        Args:
+            tracker: The tracker.
+            only_after_user_message: Whether to only allow actions after a user message.
+            empty_stack_behavior: Whether to allow actions if the stack is empty.
+
+        Returns:
+            `True` if the policy is allowed to act, `False` otherwise.
+        """
         if top_frame := tracker.stack.top():
-            # if the stack is not empty
             return self.does_support_stack_frame(top_frame)
         elif only_after_user_message and len(tracker.events) > 0:
-            # if the stack is empty and the tracker has events
             return not tracker.has_action_after_latest_user_message()
         else:
-            # if the stack is empty and the tracker has no events
             return empty_stack_behavior
 
     def __init__(
