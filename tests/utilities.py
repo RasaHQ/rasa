7fb90231580e2de503d3ddccfaf804f2e7b1d294
OgnjenFrancuski <ognjenfrancuski@gmail.com>
2023-12-19 13:46:58 +0100

    Add docstring to filter_logs function and make it more readable

--- tests/utilities.py
+++ tests/utilities.py
@@ -31,14 +31,43 @@ def filter_logs(
     event: Optional[Text] = None,
     log_level: Optional[Text] = None,
     log_message_parts: Optional[List[Text]] = None,
+    log_contains_all_message_parts: bool = True,
 ) -> List[Dict]:
-    return [
-        log
-        for log in caplog
-        if (event is None or log["event"] == event)
-        and (log_level is None or log["log_level"] == log_level)
-        and (
-            log_message_parts is None
-            or all(part in log["event_info"] for part in log_message_parts)
-        )
-    ]
+    """Filters structlog logs based on specified criteria:
+
+    Args:
+        caplog:
+            List of structlog logs
+        event:
+            The specific event type in log.
+        log_level:
+            Log level
+        log_message_parts:
+            Parts of the user-facing message, found within the `event_info` key of a
+            log.
+        log_contains_all_message_parts:
+            Flag determining the filtering logic for message parts. If True, a log
+            entry must contain all specified parts to be included. If False, the
+            presence of any specified part suffices for inclusion.
+
+    Returns:
+        A list of logs that matches the filtering criteria.
+    """
+    def contains_message_parts(log: Dict) -> bool:
+        contains_parts = [part in log["event_info"] for part in log_message_parts]
+        if log_contains_all_message_parts:
+            return all(contains_parts)
+        return any(contains_parts)
+
+    filtered_logs = []
+
+    for log in caplog:
+
+        matches_event = event is None or log["event"] == event
+        matches_log_level = log_level is None or log["log_level"] == log_level
+        matches_message_parts = log_message_parts is None or contains_message_parts(log)
+
+        if matches_event and matches_log_level and matches_message_parts:
+            filtered_logs.append(log)
+
+    return filtered_logs
