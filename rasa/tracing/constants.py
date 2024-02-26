2efe7a3e5bab717f1822a77dbb5d5eb9ff813b9f
Anca Lita <27920906+ancalita@users.noreply.github.com>
2024-02-26 13:00:23 +0000

    Implement `MetricInstrumentProvider` (#277)
    * implement instrument provider, add tests
    
    * address PR review
    
    * add changelog entry

---
+++ rasa/tracing/constants.py
@@ -0,0 +1,28 @@
+# metrics constants
+LLM_COMMAND_GENERATOR_CPU_USAGE_INSTRUMENT_NAME = "llm_command_generator_cpu_usage"
+LLM_COMMAND_GENERATOR_MEMORY_USAGE_INSTRUMENT_NAME = (
+    "llm_command_generator_memory_usage"
+)
+LLM_COMMAND_GENERATOR_PROMPT_TOKEN_USAGE_INSTRUMENT_NAME = (
+    "llm_command_generator_prompt_token_usage"
+)
+LLM_COMMAND_GENERATOR_LLM_RESPONSE_DURATION_INSTRUMENT_NAME = (
+    "llm_command_generator_llm_response_duration"
+)
+
+ENTERPRISE_SEARCH_POLICY_LLM_RESPONSE_DURATION_INSTRUMENT_NAME = (
+    "enterprise_search_policy_llm_response_duration"
+)
+INTENTLESS_POLICY_LLM_RESPONSE_DURATION_INSTRUMENT_NAME = (
+    "intentless_policy_llm_response_duration"
+)
+CONTEXTUAL_RESPONSE_REPHRASER_LLM_RESPONSE_DURATION_INSTRUMENT_NAME = (
+    "contextual_nlg_llm_response_duration"
+)
+
+ACTION_SERVER_RASA_CLIENT_REQUEST_DURATION_INSTRUMENT_NAME = (
+    "action_server_rasa_client_request_duration"
+)
+ACTION_SERVER_RASA_CLIENT_REQUEST_BODY_SIZE_INSTRUMENT_NAME = (
+    "action_server_rasa_client_request_body_size"
+)
--- tests/tracing/test_config.py
+++ tests/tracing/test_config.py
@@ -1,12 +1,32 @@
 import socket
 import threading
+from typing import Generator, Any
 
 import grpc
 import pytest
+import opentelemetry.metrics
+from opentelemetry.metrics import Histogram, Instrument
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics._internal.export import (
+    PeriodicExportingMetricReader,
+    ConsoleMetricExporter,
+)
+
+from rasa.tracing.constants import (
+    LLM_COMMAND_GENERATOR_CPU_USAGE_INSTRUMENT_NAME,
+    LLM_COMMAND_GENERATOR_MEMORY_USAGE_INSTRUMENT_NAME,
+    LLM_COMMAND_GENERATOR_PROMPT_TOKEN_USAGE_INSTRUMENT_NAME,
+    LLM_COMMAND_GENERATOR_LLM_RESPONSE_DURATION_INSTRUMENT_NAME,
+    ENTERPRISE_SEARCH_POLICY_LLM_RESPONSE_DURATION_INSTRUMENT_NAME,
+    INTENTLESS_POLICY_LLM_RESPONSE_DURATION_INSTRUMENT_NAME,
+    CONTEXTUAL_RESPONSE_REPHRASER_LLM_RESPONSE_DURATION_INSTRUMENT_NAME,
+    ACTION_SERVER_RASA_CLIENT_REQUEST_DURATION_INSTRUMENT_NAME,
+    ACTION_SERVER_RASA_CLIENT_REQUEST_BODY_SIZE_INSTRUMENT_NAME,
+)
 from rasa.utils.endpoints import EndpointConfig
 
 from rasa.tracing import config
-from rasa.tracing.config import JaegerTracerConfigurer
+from rasa.tracing.config import JaegerTracerConfigurer, MetricInstrumentProvider
 from tests.conftest import wait
 from tests.tracing import conftest
 from tests.tracing.conftest import (
@@ -124,3 +144,69 @@ def test_get_tracer_provider_jaeger(udp_server: socket.socket) -> None:
 
     assert len(batch.spans) == 1
     assert batch.spans[0].operationName == "jaeger_test_span"
+
+
+def set_up_meter_provider() -> Generator[MeterProvider, None, None]:
+    meter_provider = MeterProvider(
+        metric_readers=[PeriodicExportingMetricReader(exporter=ConsoleMetricExporter())]
+    )
+    opentelemetry.metrics.set_meter_provider(meter_provider)
+    yield meter_provider
+    meter_provider.shutdown()
+
+
+def test_metric_instrument_provider_is_singleton() -> None:
+    instrument_provider_1 = MetricInstrumentProvider()
+    instrument_provider_2 = MetricInstrumentProvider()
+
+    assert instrument_provider_1 is instrument_provider_2
+    assert instrument_provider_1.instruments is instrument_provider_2.instruments
+
+
+def test_metric_instrument_provider_register_instruments() -> None:
+    set_up_meter_provider()
+
+    instrument_provider = MetricInstrumentProvider()
+    assert instrument_provider.instruments == {}
+
+    instrument_provider.register_instruments()
+    assert len(instrument_provider.instruments) > 0
+    assert all(
+        [
+            isinstance(instrument, Instrument)
+            for instrument in instrument_provider.instruments.values()
+        ]
+    )
+
+
+@pytest.mark.parametrize(
+    "instrument_name, expected_instrument_type",
+    [
+        (LLM_COMMAND_GENERATOR_CPU_USAGE_INSTRUMENT_NAME, Histogram),
+        (LLM_COMMAND_GENERATOR_MEMORY_USAGE_INSTRUMENT_NAME, Histogram),
+        (LLM_COMMAND_GENERATOR_PROMPT_TOKEN_USAGE_INSTRUMENT_NAME, Histogram),
+        (LLM_COMMAND_GENERATOR_LLM_RESPONSE_DURATION_INSTRUMENT_NAME, Histogram),
+        (ENTERPRISE_SEARCH_POLICY_LLM_RESPONSE_DURATION_INSTRUMENT_NAME, Histogram),
+        (INTENTLESS_POLICY_LLM_RESPONSE_DURATION_INSTRUMENT_NAME, Histogram),
+        (
+            CONTEXTUAL_RESPONSE_REPHRASER_LLM_RESPONSE_DURATION_INSTRUMENT_NAME,
+            Histogram,
+        ),
+        (ACTION_SERVER_RASA_CLIENT_REQUEST_DURATION_INSTRUMENT_NAME, Histogram),
+        (ACTION_SERVER_RASA_CLIENT_REQUEST_BODY_SIZE_INSTRUMENT_NAME, Histogram),
+    ],
+)
+def test_metric_instrument_provider_get_instrument(
+    instrument_name: str, expected_instrument_type: Any
+) -> None:
+    # arrange
+    set_up_meter_provider()
+    instrument_provider = MetricInstrumentProvider()
+    instrument_provider.register_instruments()
+
+    # act
+    instrument = instrument_provider.get_instrument(instrument_name)
+
+    # assert
+    assert instrument is not None
+    assert isinstance(instrument, expected_instrument_type)
