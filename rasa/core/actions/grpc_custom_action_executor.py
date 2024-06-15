b77ddfd6834649a2a662759c1014f8b59af75a1b
Radovan Zivkovic <r.zivkovic@rasa.com>
2024-06-16 00:21:37 +0200

    Add code docs for gRPC executor

--- rasa/core/actions/grpc_custom_action_executor.py
+++ rasa/core/actions/grpc_custom_action_executor.py
@@ -1,4 +1,3 @@
-import logging
 from os.path import abspath
 from typing import Dict, Text, Any, Optional, TYPE_CHECKING
 
@@ -30,6 +29,12 @@ class GRPCCustomActionExecutor(CustomActionExecutor):
     """
 
     def __init__(self, action_name: str, action_endpoint: EndpointConfig) -> None:
+        """Initializes the gRPC custom action executor.
+
+        Args:
+            action_name: Name of the custom action.
+            action_endpoint: Endpoint configuration of the custom action.
+        """
         self.action_name = action_name
         self.action_endpoint = action_endpoint
         self.request_writer = CustomActionRequestWriter(action_name, action_endpoint)
@@ -37,16 +42,33 @@ class GRPCCustomActionExecutor(CustomActionExecutor):
     async def run(
         self, tracker: "DialogueStateTracker", domain: Optional["Domain"] = None
     ) -> Dict[Text, Any]:
-        """Execute the custom action using a gRPC request."""
+        """Execute the custom action using a gRPC request.
+
+        Args:
+            tracker: Tracker for the current conversation.
+            domain: Domain of the assistant.
+
+        Returns:
+            Response from the action server.
+        """
 
         json_body = self.request_writer.create(tracker=tracker, domain=domain)
 
-        return self._perform_one_request(json_body)
+        return self._request(json_body)
 
-    def _perform_one_request(
+    def _request(
         self,
         json_body: Dict[Text, Any],
     ) -> Dict[Text, Any]:
+        """Perform a single gRPC request to the action server.
+
+        Args:
+            json_body: JSON body of the request.
+
+        Returns:
+            Response from the action server.
+        """
+
         client = self._create_grpc_client(
             self.action_endpoint.url, self.action_endpoint.cafile
         )
@@ -60,32 +82,45 @@ class GRPCCustomActionExecutor(CustomActionExecutor):
         except grpc.RpcError as e:
             status_code = e.code()
             details = e.details()
-            if status_code == grpc.StatusCode.NOT_FOUND:
-                not_found_error = ResourceNotFound.model_validate_json(details)
-                if not_found_error:
-                    if not_found_error.resource_type == ResourceNotFoundType.DOMAIN:
-                        structlogger.error(
-                            "rasa.core.actions.grpc_custom_action_executor.domain_not_found",
-                            event_info=(
-                                f"Failed to execute custom action '{self.action_endpoint}'. "
-                                f"Could not find domain. {not_found_error.message}"
-                            ),
-                        )
-                        raise DomainNotFound()
-                    elif not_found_error.resource_type == ResourceNotFoundType.ACTION:
-                        structlogger.error(
-                            "rasa.core.actions.grpc_custom_action_executor.action_not_found",
-                            event_info=(
-                                f"Failed to execute custom action '{self.action_name}'. "
-                                f"Could not find action. {not_found_error.message}"
-                            ),
-                        )
+            if status_code is not grpc.StatusCode.NOT_FOUND:
+                raise ClientResponseError(status=status_code, message=details, text="")
+
+            resource_not_found_error = ResourceNotFound.model_validate_json(details)
+            if not resource_not_found_error:
+                raise ClientResponseError(status=status_code, message=details, text="")
+
+            if resource_not_found_error.resource_type == ResourceNotFoundType.DOMAIN:
+                structlogger.error(
+                    "rasa.core.actions.grpc_custom_action_executor.domain_not_found",
+                    event_info=(
+                        f"Failed to execute custom action '{self.action_endpoint}'. "
+                        f"Could not find domain. {resource_not_found_error.message}"
+                    ),
+                )
+                raise DomainNotFound()
+            elif resource_not_found_error.resource_type == ResourceNotFoundType.ACTION:
+                structlogger.error(
+                    "rasa.core.actions.grpc_custom_action_executor.action_not_found",
+                    event_info=(
+                        f"Failed to execute custom action '{self.action_name}'. "
+                        f"Could not find action. {resource_not_found_error.message}"
+                    ),
+                )
             raise ClientResponseError(status=status_code, message=details, text="")
 
     @staticmethod
     def _create_grpc_client(
         url: Text, cert_file: Optional[Text] = None
     ) -> action_webhook_pb2_grpc.ActionServerWebhookStub:
+        """Create a gRPC client for the action server.
+
+        Args:
+            url: URL of the action server.
+            cert_file: Path to the certificate file for TLS encryption.
+
+        Returns:
+            gRPC client for the action server.
+        """
         channel = GRPCCustomActionExecutor._create_channel(url, cert_file)
         return action_webhook_pb2_grpc.ActionServerWebhookStub(channel)
 
@@ -93,6 +128,15 @@ class GRPCCustomActionExecutor(CustomActionExecutor):
     def _create_channel(
         url: Optional[Text], cert_ca_file: Optional[Text] = None
     ) -> grpc.Channel:
+        """Create a gRPC channel for the action server.
+
+        Args:
+            url: URL of the action server.
+            cert_ca_file: Path to the certificate file for TLS encryption.
+
+        Returns:
+            gRPC channel for the action server.
+        """
         if cert_ca_file:
             try:
                 credentials = grpc.ssl_channel_credentials(
