0730531ec0499c786901fa8df17be8a659fe233c
Radovan Zivkovic <r.zivkovic@rasa.com>
2024-06-18 17:56:45 +0200

    Parametrise integration test for custom actions

--- tests/integration_tests/core/actions/custom_actions/test_custom_actions.py
+++ tests/integration_tests/core/actions/custom_actions/test_custom_actions.py
@@ -1,3 +1,5 @@
+import pytest
+
 from tests.integration_tests.conftest import send_message_to_rasa_server
 
 HTTP_RASA_SERVER = "http://localhost:5010"
@@ -6,42 +8,13 @@ GRPC_RASA_SERVER = "http://localhost:5012"
 GRPC_SSL_RASA_SERVER = "http://localhost:5013"
 
 
-def test_grpc_custom_action_invocation() -> None:
+@pytest.mark.parametrize(
+    "server_location",
+    [HTTP_RASA_SERVER, HTTPS_RASA_SERVER, GRPC_RASA_SERVER, GRPC_SSL_RASA_SERVER],
+)
+def test_custom_action_invocation(server_location: str) -> None:
     sender_id, response_messages = send_message_to_rasa_server(
-        server_location=GRPC_RASA_SERVER, message="cu"
-    )
-
-    assert len(response_messages) == 1
-    response_message = response_messages[0]
-    assert response_message["recipient_id"] == sender_id
-    assert response_message["text"] == "Goodbye!"
-
-
-def test_grpc_ssl_custom_action_invocation() -> None:
-    sender_id, response_messages = send_message_to_rasa_server(
-        server_location=GRPC_SSL_RASA_SERVER, message="cu"
-    )
-
-    assert len(response_messages) == 1
-    response_message = response_messages[0]
-    assert response_message["recipient_id"] == sender_id
-    assert response_message["text"] == "Goodbye!"
-
-
-def test_http_custom_action_invocation() -> None:
-    sender_id, response_messages = send_message_to_rasa_server(
-        server_location=HTTP_RASA_SERVER, message="cu"
-    )
-
-    assert len(response_messages) == 1
-    response_message = response_messages[0]
-    assert response_message["recipient_id"] == sender_id
-    assert response_message["text"] == "Goodbye!"
-
-
-def test_https_custom_action_invocation() -> None:
-    sender_id, response_messages = send_message_to_rasa_server(
-        server_location=HTTPS_RASA_SERVER, message="cu"
+        server_location=server_location, message="cu"
     )
 
     assert len(response_messages) == 1
