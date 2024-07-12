f844521e853da53975b9807d0ee4488c469c941c
Tanja <tabergma@gmail.com>
2024-07-12 10:25:45 +0200

    [ENG-1125] Add commands and prompt to message object in case of fine-tuning in SingleStepLLMCommandGenerator (#838)
    * add commands and prompt to message object
    
    * introduce global preparing_fine_tuning_data to only update message if needed
    
    * Add tests
    
    * fix formatting and types

--- rasa/llm_fine_tuning/annotation_module.py
+++ rasa/llm_fine_tuning/annotation_module.py
@@ -1,13 +1,29 @@
-from typing import List
+from contextlib import contextmanager
+from typing import List, Generator
 
 from rasa.e2e_test.e2e_test_case import TestSuite
 from rasa.e2e_test.e2e_test_runner import E2ETestRunner
 from rasa.llm_fine_tuning.conversations import Conversation
 
+preparing_fine_tuning_data = False
+
+
+@contextmanager
+def set_preparing_fine_tuning_data() -> Generator:
+    global preparing_fine_tuning_data
+    preparing_fine_tuning_data = True
+    try:
+        yield
+    finally:
+        preparing_fine_tuning_data = False
+
 
 def annotate_e2e_tests(
     e2e_test_runner: E2ETestRunner, test_suite: TestSuite, output_dir: str
 ) -> List[Conversation]:
+    with set_preparing_fine_tuning_data():
+        # e2e_test_runner.run_tests()
+        pass
     # TODO placeholder
 
     return []
