732aa640627709c913ca7ccb2fb07af8e6d67b53
OgnjenFrancuski <ognjenfrancuski@gmail.com>
2024-07-24 17:57:06 +0200

    Fix instrumentation not accounting for kwargs that are passed to `NLUCommandAdapter.predict_commands` (#904)
    * Fix instrumentation nott accounting for kwargs that are passed to NLUCommandAdapter.predict_commands
    
    * Add changelog
    
    * Update NLUCommandAdapter instrumentation unittest to check for domain being passed as kwarg.

--- tests/tracing/instrumentation/test_nlu_command_adapter_instrumentation.py
+++ tests/tracing/instrumentation/test_nlu_command_adapter_instrumentation.py
@@ -1,12 +1,14 @@
 from typing import Sequence
 from unittest.mock import Mock
 
+import pytest
 from opentelemetry.sdk.trace import ReadableSpan, TracerProvider
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 
 from rasa.engine.graph import ExecutionContext
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
+from rasa.shared.core.domain import Domain
 from rasa.shared.core.events import UserUttered
 from rasa.shared.core.flows import FlowsList
 from rasa.shared.core.trackers import DialogueStateTracker
@@ -15,7 +17,15 @@ from rasa.tracing.instrumentation import instrumentation
 from tests.tracing.instrumentation.conftest import MockNLUCommandAdapter
 
 
+@pytest.mark.parametrize(
+    "predict_commands_kwargs",
+    [
+        {},
+        {"domain": Mock(spec_set=Domain)},
+    ],
+)
 async def test_tracing_nlu_command_adapter_predict_commands(
+    predict_commands_kwargs: dict,
     tracer_provider: TracerProvider,
     span_exporter: InMemorySpanExporter,
     default_model_storage: ModelStorage,
@@ -44,6 +54,7 @@ async def test_tracing_nlu_command_adapter_predict_commands(
         Message(data={"intent": {"name": "health_advice"}}),
         FlowsList(underlying_flows=[]),
         tracker,
+        **predict_commands_kwargs,
     )
 
     captured_spans: Sequence[ReadableSpan] = span_exporter.get_finished_spans()  # type: ignore
