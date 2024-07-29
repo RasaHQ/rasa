2a2b9a52becdb19e1be5554d301bd3b93102936e
Jamie MacDonald <j.macdonald@rasa.com>
2024-07-29 15:59:51 +0100

    Ignore low severity or false positive tests

--- tests/cli/conftest.py
+++ tests/cli/conftest.py
@@ -64,6 +64,7 @@ def trained_simple_project(tmp_path_factory: TempPathFactory) -> Text:
 
     os.environ["LOG_LEVEL"] = "DEBUG"
 
+    # deepcode ignore CommandInjection/test: Very low risk - an exploit would first need to compromise the host running the tests and maliciously edit the environment variables.
     check_call([shutil.which(RASA_EXE), "train"], cwd=path)
 
     return str(path)
