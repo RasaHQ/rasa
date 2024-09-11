477e8db377ff2177c446ecc05fba72b51167ebf0
Maksim Moiseikin <m.moiseikin@gmail.com>
2024-09-11 10:36:50 +0200

    Throw deprecation warning when REQUESTS_CA_BUNDLE env var is used [ENG-1312] (#1177)
    * Throw deprecation warning when REQUESTS_CA_BUNDLE env var is used [ENG-1312]
    
    * Add changelog entry
    
    * Fix lint issue
    
    * Add tests and handle a special case when both variables are set

--- tests/shared/providers/test_ssl_verification_utils.py
+++ tests/shared/providers/test_ssl_verification_utils.py
@@ -18,6 +18,7 @@ import structlog
 from rasa.shared.providers._ssl_verification_utils import (
     ensure_ssl_certificates_for_litellm_non_openai_based_clients,
     ensure_ssl_certificates_for_litellm_openai_based_clients,
+    _get_ssl_verify,
 )
 from pytest import MonkeyPatch
 
@@ -179,3 +180,67 @@ def test_ensure_ssl_certificates_for_litellm_openai_based_clients_sets_cert(
     else:
         assert litellm.aclient_session is None
         assert litellm.client_session is None
+
+
+@pytest.mark.parametrize(
+    "env_var," "certificate_path",
+    [
+        (REQUESTS_CA_BUNDLE_ENV_VAR, "path/to/certificate.pem"),
+    ],
+)
+def test_ensure_deprecation_warning_is_thrown(
+    env_var: Optional[str],
+    certificate_path: Optional[str],
+    monkeypatch: MonkeyPatch,
+    reset_litellm_sessions,
+):
+    # Given
+    monkeypatch.delenv(env_var, raising=False)
+    if certificate_path is not None:
+        monkeypatch.setenv(env_var, str(certificate_path))
+
+    with pytest.warns(FutureWarning) as record:
+        assert _get_ssl_verify() == "path/to/certificate.pem"
+
+    assert len(record) == 1
+    assert (
+        record[0].message.args[0] == "Support of the REQUESTS_CA_BUNDLE "
+        "environment variable is deprecated and will be removed in Rasa Pro 4.0.0. "
+        "Please set the RASA_CA_BUNDLE environment variable instead."
+    )
+    assert isinstance(record[0].message, FutureWarning)
+
+
+@pytest.mark.parametrize(
+    "env_vars_and_paths",
+    [
+        {
+            REQUESTS_CA_BUNDLE_ENV_VAR: "path/to/certificate1.pem",
+            RASA_CA_BUNDLE_ENV_VAR: "path/to/certificate2.pem",
+        },
+    ],
+)
+def test_ensure_correct_env_var_is_used(
+    env_vars_and_paths: dict,
+    monkeypatch: MonkeyPatch,
+    reset_litellm_sessions,
+):
+    # Given
+    for env_var, certificate_path in env_vars_and_paths.items():
+        monkeypatch.delenv(env_var, raising=False)
+        if certificate_path is not None:
+            monkeypatch.setenv(env_var, str(certificate_path))
+
+    with pytest.warns(FutureWarning) as record:
+        assert _get_ssl_verify() == "path/to/certificate2.pem"
+
+    assert len(record) == 1
+    assert (
+        record[0].message.args[0]
+        == "Both REQUESTS_CA_BUNDLE and RASA_CA_BUNDLE environment variables are set. "
+        "RASA_CA_BUNDLE will be used as the SSL verification path.\n"
+        "Support of the REQUESTS_CA_BUNDLE environment variable is deprecated and "
+        "will be removed in Rasa Pro 4.0.0. Please set the RASA_CA_BUNDLE "
+        "environment variable instead."
+    )
+    assert isinstance(record[0].message, FutureWarning)
