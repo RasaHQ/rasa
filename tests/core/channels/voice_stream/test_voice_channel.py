a0422521e8b4824a4a6adc4d3579af465f70d30b
SOUVIK GHOSH <souvikg10@users.noreply.github.com>
2024-11-20 14:10:01 +0100

    feat: add byo speech services impl (#1524)
    * feat: add byo speech services impl
    
    * fix: lint issues
    
    * refactor the custom module import
    
    * change error type in test
    
    * format and lint voice_channel.py
    
    * add changelog for 1524
    
    * add beta feature warning
    
    * add a warning for beta features
    
    * add tests for custom TTS
    
    * reformat test_voice_channel.py
    
    ---------
    
    Co-authored-by: Shailendra Paliwal <hello@shailendra.me>

--- tests/core/channels/voice_stream/test_voice_channel.py
+++ tests/core/channels/voice_stream/test_voice_channel.py
@@ -1,7 +1,11 @@
 import pytest
-
 from rasa.core.channels.voice_stream.tts.azure import AzureTTS
-from rasa.core.channels.voice_stream.voice_channel import tts_engine_from_config
+from rasa.core.channels.voice_stream.voice_channel import (
+    tts_engine_from_config,
+    asr_engine_from_config,
+)
+from data.test_voice_channel.custom_asr_engine import CustomASREngine
+from data.test_voice_channel.custom_tts_engine import CustomTTSEngine
 
 
 def test_azure_tts_engine_from_config():
@@ -16,5 +20,36 @@ def test_azure_tts_engine_from_config():
 
 def test_tts_engine_from_config_fails_for_not_implemented_engine():
     config = {"name": "XY_non_existent"}
-    with pytest.raises(NotImplementedError):
+    with pytest.raises(ImportError):
         tts_engine_from_config(config)
+
+
+def test_custom_asr_service() -> None:
+    # Given a custom ASR engine
+    config = {
+        "name": "data.test_voice_channel.custom_asr_engine.CustomASREngine",
+        "endpoint": "http://localhost:8000",
+        "language": "en",
+    }
+
+    # When the ASR engine is created from the config
+    asr_engine = asr_engine_from_config(config)
+
+    # Then the ASR engine should be an instance of the custom ASR engine
+    assert isinstance(asr_engine, CustomASREngine)
+
+
+def test_custom_tts_service() -> None:
+    # Given a custom TTS engine
+    config = {
+        "name": "data.test_voice_channel.custom_tts_engine.CustomTTSEngine",
+        "server_url": "http://localhost:8000",
+        "language": "hi",
+    }
+
+    # When the TTS engine is created from the config
+    tts_engine = tts_engine_from_config(config)
+
+    # Then the ASR engine should be an instance of the custom ASR engine
+    assert isinstance(tts_engine, CustomTTSEngine)
+    assert tts_engine.config.language == "hi"
