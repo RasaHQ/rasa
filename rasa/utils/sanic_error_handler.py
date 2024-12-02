a2b8b71160ceda7572cbf2d6a6355443a5ad2271
Tom Bocklisch <tom@rasa.com>
2024-12-02 12:15:58 +0100

    supress sanic websocket error on closing the socket. this is a sanic bug, fixed in later sanic versions

---
+++ rasa/utils/sanic_error_handler.py
@@ -0,0 +1,32 @@
+from sanic import Sanic
+from sanic.handlers import ErrorHandler
+from sanic.request import Request
+from sanic.exceptions import ServerError
+
+
+# TODO: remove custom handler when upgrading to sanic >= 24
+#       the underlying issue https://github.com/sanic-org/sanic/issues/2572
+#       has been fixed in sanic 24
+class IgnoreWSServerErrorHandler(ErrorHandler):
+    @staticmethod
+    def log(request: Request, exception: Exception) -> None:
+        try:
+            if (
+                request.url.startswith("ws")
+                and isinstance(exception, ServerError)
+                and exception.args
+                and (
+                    exception.args[0]
+                    == "Invalid response type None (need HTTPResponse)"
+                )
+            ):
+                # in case we are in a websocket connection, we don't want to log the
+                # the error, as this is a bug in sanic
+                return
+        except Exception:
+            pass
+        ErrorHandler.log(request, exception)  # type: ignore
+
+
+def register_custom_sanic_error_handler(app: Sanic) -> None:
+    app.error_handler = IgnoreWSServerErrorHandler()
