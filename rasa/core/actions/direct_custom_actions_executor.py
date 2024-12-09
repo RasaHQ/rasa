c3f88fadaca88576df0c4f8a9c5bf779f70c3aec
aleksandarmijat <a.mijatovic@rasa.com>
2024-12-09 13:37:18 +0100

    Direct Custom Action Execution Hot Reloading

--- rasa/core/actions/direct_custom_actions_executor.py
+++ rasa/core/actions/direct_custom_actions_executor.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from importlib.util import find_spec
 from typing import (
     Any,
@@ -21,6 +22,8 @@ structlogger = structlog.get_logger(__name__)
 
 
 class DirectCustomActionExecutor(CustomActionExecutor):
+    _actions_module_registered: bool = False
+
     def __init__(self, action_name: str, action_endpoint: EndpointConfig):
         """Initializes the direct custom action executor.
 
@@ -30,9 +33,34 @@ class DirectCustomActionExecutor(CustomActionExecutor):
         """
         self.action_name = action_name
         self.action_endpoint = action_endpoint
-        self.action_executor = ActionExecutor()
+        self.action_executor = self._create_action_executor()
+        self.register_actions_from_a_module()
+        self.action_executor.reload()
+
+    @staticmethod
+    @lru_cache(maxsize=1)
+    def _create_action_executor():
+        """Creates and returns a cached ActionExecutor instance.
+
+        Returns:
+            ActionExecutor: The cached ActionExecutor instance.
+        """
+        return ActionExecutor()
 
     def register_actions_from_a_module(self) -> None:
+        """Registers actions from the specified module if not already registered.
+
+        This method checks if the actions module has already been registered to prevent
+        duplicate registrations. If not registered, it attempts to register the actions
+        module specified in the action endpoint configuration. If the module does not
+        exist, it raises a RasaException.
+
+        Raises:
+            RasaException: If the actions module specified does not exist.
+        """
+        if self.__class__._actions_module_registered:
+            return
+
         module_name = self.action_endpoint.actions_module
         if not find_spec(module_name):
             raise RasaException(
@@ -42,6 +70,7 @@ class DirectCustomActionExecutor(CustomActionExecutor):
             )
 
         self.action_executor.register_package(module_name)
+        self.__class__._actions_module_registered = True
 
     async def run(
         self,
@@ -63,7 +92,6 @@ class DirectCustomActionExecutor(CustomActionExecutor):
             "action.direct_custom_action_executor.run",
             action_name=self.action_name,
         )
-        self.register_actions_from_a_module()
 
         tracker_state = tracker.current_state(EventVerbosity.ALL)
         action_call = {
--- tests/core/actions/test_direct_custom_action_executor.py
+++ tests/core/actions/test_direct_custom_action_executor.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from unittest.mock import MagicMock
 
 import pytest
@@ -178,3 +179,59 @@ async def test_executor_runs_action_invalid_actions_module(
         "Please check the logs of your action server for more information."
     )
     assert message in caplog.messages
+
+
+def test_action_executor_is_being_cached(mock_endpoint: EndpointConfig):
+    executor_1 = DirectCustomActionExecutor(
+        action_name=DUMMY_ACTION_NAME, action_endpoint=mock_endpoint
+    )
+    executor_2 = DirectCustomActionExecutor(
+        action_name=DUMMY_ACTION_NAME, action_endpoint=mock_endpoint
+    )
+    assert executor_1.action_executor == executor_2.action_executor
+
+
+@pytest.mark.asyncio
+async def test_custom_actions_hot_reloading():
+    def create_action_code(value: str) -> str:
+        return f"""from typing import Any, Dict
+from rasa_sdk.interfaces import Action
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class CustomAction(Action):
+    def name(self) -> str:
+        return "custom_action"
+
+    async def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[str, Any]
+    ) -> Any:
+        return [{{"event": "slot", "name": "test_slot", "value": "{value}"}}]
+"""
+
+    # Create a custom action file with initial value
+    action_module = Path(DUMMY_ACTIONS_MODULE_PATH.replace(".", "/"))
+    action_file = action_module / "custom_action.py"
+    initial_value = "initial_value"
+    action_file.write_text(create_action_code(initial_value))
+
+    # Create an endpoint and executor with the initial custom action
+    endpoint = EndpointConfig(actions_module=DUMMY_ACTIONS_MODULE_PATH)
+    executor = DirectCustomActionExecutor("custom_action", endpoint)
+
+    # Run the custom action with the initial value
+    tracker = DialogueStateTracker("default", [])
+    domain = Domain.empty()
+    result_initial = await executor.run(tracker, domain)
+    assert result_initial["events"][0]["value"] == initial_value
+
+    # Modify the custom action file with a new value
+    modified_value = "modified_value"
+    action_file.write_text(create_action_code(modified_value))
+
+    # Run the custom action with the modified value
+    executor = DirectCustomActionExecutor("custom_action", endpoint)
+    result_modified = await executor.run(tracker, domain)
+    assert result_modified["events"][0]["value"] == modified_value
+    action_file.unlink()
