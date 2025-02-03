69e49e057a09cf8d58c981dddd4e890ffd9cfa51
OLANIYI TAWAKALT <olaniyitaiwo95@yahoo.com>
2025-02-03 10:55:00 +0100

    [ATO-3227] Fix Security Vulnerabilities in Inspector Packages (#1913)
    * upgrade cross-spawn
    
    * upgrade mermaid and dompurify
    
    * upgrade vite
    
    * upgrade braces
    
    * upgrade ws
    
    * upgrade axios
    
    * upgrade rollup
    
    * run yarn build

---
+++ rasa/core/channels/inspector/dist/assets/channel-e265ea59.js
@@ -0,0 +1,1 @@
+import{aI as o,aJ as r}from"./index-7794b245.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
---
+++ rasa/core/channels/inspector/dist/assets/clone-21f8a43d.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph-be6ef5d8.js";var e=4;function a(o){return r(o,e)}export{a as c};
