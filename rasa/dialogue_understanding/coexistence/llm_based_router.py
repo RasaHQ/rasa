e88ad6d2d9d36a25cf7a14bc0c5f4397c1bba476
Tanja <tabergma@gmail.com>
2025-06-25 08:20:26 +0200

    [ENG-2005] Add logic bias back to the LLMBasedRouter (#2713)
    * add logic bias back to the llm based router
    
    * update script get_logic_bias_token_ids

--- rasa/dialogue_understanding/coexistence/llm_based_router.py
+++ rasa/dialogue_understanding/coexistence/llm_based_router.py
@@ -21,6 +21,7 @@ from rasa.engine.recipes.default_recipe import DefaultV1Recipe
 from rasa.engine.storage.resource import Resource
 from rasa.engine.storage.storage import ModelStorage
 from rasa.shared.constants import (
+    LOGIT_BIAS_CONFIG_KEY,
     MAX_COMPLETION_TOKENS_CONFIG_KEY,
     MODEL_CONFIG_KEY,
     OPENAI_PROVIDER,
@@ -57,12 +58,22 @@ DEFAULT_COMMAND_PROMPT_TEMPLATE = importlib.resources.read_text(
 )
 LLM_BASED_ROUTER_CONFIG_FILE_NAME = "config.json"
 
+# Token ids for gpt-4o corresponding to space + capitalized Letter
+A_TO_C_TOKEN_IDS_CHATGPT = [
+    355,  # " A"
+    418,  # " B"
+    363,  # " C"
+]
+
 DEFAULT_LLM_CONFIG = {
     PROVIDER_CONFIG_KEY: OPENAI_PROVIDER,
     MODEL_CONFIG_KEY: DEFAULT_OPENAI_CHAT_MODEL_NAME,
     TIMEOUT_CONFIG_KEY: 7,
     TEMPERATURE_CONFIG_KEY: 0.0,
     MAX_COMPLETION_TOKENS_CONFIG_KEY: 1,
+    LOGIT_BIAS_CONFIG_KEY: {
+        str(token_id): 100 for token_id in A_TO_C_TOKEN_IDS_CHATGPT
+    },
 }
 
 structlogger = structlog.get_logger()
---
+++ scripts/get_logic_bias_token_ids.py
@@ -0,0 +1,22 @@
+import tiktoken
+
+# Print all available encodings
+print(tiktoken.list_encoding_names())
+# Print the encoding for a specific model
+model_name = "gpt-4o"
+print(tiktoken.encoding_for_model(model_name))
+
+# Choose the tokenizer for your model
+# gpt-3.5-turbo and gpt-4 uses "cl100k_base"
+# gpt-4o uses "o200k_base"
+tokenizer = tiktoken.get_encoding("o200k_base")
+
+# The tokens you want to check
+tokens = [" A", " B", " C"]
+
+# Encode tokens and get their IDs
+token_ids = [tokenizer.encode(token) for token in tokens]
+
+# Print the results
+for token, token_id in zip(tokens, token_ids):
+    print(f"Token: {token}, Token ID: {token_id}")
